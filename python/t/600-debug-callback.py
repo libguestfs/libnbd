aa4b9e4046d26b19d1b3fbfe4c5332552e660f8c
Richard W.M. Jones <rjones@redhat.com>
2019-05-28 15:36:06 +0100

    python: Add test of set_debug_callback.

---
+++ python/t/600-debug-callback.py
@@ -0,0 +1,34 @@
+# libnbd Python bindings
+# Copyright (C) 2010-2019 Red Hat Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+import nbd
+
+h = nbd.NBD ()
+
+messages = []
+def f(id, context, msg):
+    global messages
+
+    assert id == 42
+    messages.append (msg)
+
+h.set_debug_callback (42, f)
+
+h.connect_command (["nbdkit", "-s", "--exit-with-parent", "null"])
+h.shutdown ()
+
+print ("%r" % messages)
