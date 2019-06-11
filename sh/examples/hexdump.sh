83dcd0dfae40ef3c5fc2fc3fd8d7bb7497aba203
Richard W.M. Jones <rjones@redhat.com>
2019-06-11 09:48:05 +0100

    sh: Add examples section to the manual.

---
+++ sh/examples/hexdump.sh
@@ -0,0 +1,8 @@
+nbdsh -c - <<'EOF'
+from subprocess import *
+
+h.connect_uri ("nbd://localhost")
+bootsect = h.pread (512, 0)
+p = Popen ("hexdump -C", shell=True, stdin=PIPE)
+p.stdin.write (bootsect)
+EOF
