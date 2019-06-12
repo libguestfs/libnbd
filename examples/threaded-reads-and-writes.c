libguestfs/libnbd

commit 1c5f2163656c107f09965c31372fdcf528c6f3f2

examples/threaded-reads-and-writes.c: Malloc buffer
pthreads often have a limited stack size; attempting to stack-allocate
a 1M buffer (or even larger if BUFFER_SIZE is tweaked) can cause odd
problems, compared to just allocating on the heap.

examples/threaded-reads-and-writes.c
@@ -186,13 +186,19 @@ start_thread (void *arg)
   struct nbd_handle *nbd;
   struct pollfd fds[1];
   struct thread_status *status = arg;
-  char buf[BUFFER_SIZE];
+  char *buf;
   size_t i, j;
   uint64_t offset, handle;
   uint64_t handles[MAX_IN_FLIGHT];
   size_t in_flight;        /* counts number of requests in flight */
   int dir, r, cmd;
   bool want_to_send;
+
+  buf = malloc (BUFFER_SIZE);
+  if (buf == NULL) {
+    perror ("malloc");
+    exit (EXIT_FAILURE);
+  }
 
   nbd = nbd_create ();
   if (nbd == NULL) {
@@ -310,10 +316,12 @@ start_thread (void *arg)
 
   printf ("thread %zu: finished OK\n", status->i);
 
+  free (buf);
   status->status = 0;
   pthread_exit (status);
 
  error:
+  free (buf);
   fprintf (stderr, "thread %zu: failed\n", status->i);
   status->status = -1;
   pthread_exit (status);

commit 68563270af44aa1f65cf07ca318cdf66bee2c3f4

examples: Fix threaded-reads-and-writes sizing
When I changed from a stack-allocated to heap-allocated buffer, I
forgot to check for instances of 'sizeof buf' which now became 8
instead of the intended BUFFER_SIZE.  Scale down the number of cycles
to reflect that much more traffic is now going over the wire
(pre-patch, I was running 1000000 cycles of 8-byte traffic in 41s,
post-patch it is 10000 cycles of 1M traffic in 23s).

Fixes: 1c5f2163

examples/threaded-reads-and-writes.c
@@ -50,7 +50,7 @@ static int64_t exportsize;
 #define BUFFER_SIZE (1024*1024)
 
 /* Number of commands we issue (per thread). */
-#define NR_CYCLES 1000000
+#define NR_CYCLES 10000
 
 struct thread_status {
   size_t i;                     /* Thread index, 0 .. NR_MULTI_CONN-1 */
@@ -220,7 +220,7 @@ start_thread (void *arg)
     }
   }
 
-  for (i = 0; i < sizeof buf; ++i)
+  for (i = 0; i < BUFFER_SIZE; ++i)
     buf[i] = rand ();
 
   /* Issue commands. */
@@ -239,12 +239,12 @@ start_thread (void *arg)
      * but that would be Very Bad in a real application.
      */
     while (i > 0 && in_flight < MAX_IN_FLIGHT) {
-      offset = rand () % (exportsize - sizeof buf);
+      offset = rand () % (exportsize - BUFFER_SIZE);
       cmd = rand () & 1;
       if (cmd == 0)
-        handle = nbd_aio_pwrite (nbd, buf, sizeof buf, offset, 0);
+        handle = nbd_aio_pwrite (nbd, buf, BUFFER_SIZE, offset, 0);
       else
-        handle = nbd_aio_pread (nbd, buf, sizeof buf, offset, 0);
+        handle = nbd_aio_pread (nbd, buf, BUFFER_SIZE, offset, 0);
       if (handle == -1) {
         fprintf (stderr, "%s\n", nbd_get_error ());
         goto error;