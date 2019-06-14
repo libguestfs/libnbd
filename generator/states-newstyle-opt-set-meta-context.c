libguestfs/libnbd

commit d0ec1ee8e14814233eb22f29074f4410c29ad16a

api: Allow arbitrary metadata contexts to be negotiated in handshake.
Change the handshake so that it doesn't always request
"base:allocation".  Instead you can now call nbd_request_meta_context
to request the metadata context(s) that you want.  If you don't call
that function at all then it still negotiates structured replies but
no meta contexts.

generator/states-newstyle-opt-set-meta-context.c
@@ -18,29 +18,33 @@
 
 /* State machine for negotiating NBD_OPT_SET_META_CONTEXT. */
 
-const char base_allocation[] = "base:allocation";
-
 /* STATE MACHINE */ {
  NEWSTYLE.OPT_SET_META_CONTEXT.START:
-  /* If the server doesn't support SRs then we must skip this state. */
-  if (!conn->structured_replies) {
+  size_t i, nr_queries;
+  uint32_t len;
+
+  /* If the server doesn't support SRs then we must skip this group.
+   * Also we skip the group if the client didn't request any metadata
+   * contexts.
+   */
+  if (!conn->structured_replies ||
+      h->request_meta_contexts == NULL ||
+      nbd_internal_string_list_length (h->request_meta_contexts) == 0) {
     SET_NEXT_STATE (%FINISH);
     return 0;
   }
 
-  /* Reset this because we don't know if it is enabled until the
-   * request here is successful.
-   */
-  conn->has_base_allocation = false;
+  assert (conn->meta_contexts == NULL);
+
+  /* Calculate the length of the option request data. */
+  len = 4 /* exportname len */ + strlen (h->export_name) + 4 /* nr queries */;
+  nr_queries = nbd_internal_string_list_length (h->request_meta_contexts);
+  for (i = 0; i < nr_queries; ++i)
+    len += 4 /* length of query */ + strlen (h->request_meta_contexts[i]);
 
   conn->sbuf.option.version = htobe64 (NBD_NEW_VERSION);
   conn->sbuf.option.option = htobe32 (NBD_OPT_SET_META_CONTEXT);
-  conn->sbuf.option.optlen =
-    htobe32 (4 /* exportname len */ +
-             strlen (h->export_name) +
-             4 /* nr queries */ +
-             4 /* length of base:allocation string */ +
-             strlen (base_allocation));
+  conn->sbuf.option.optlen = htobe32 (len);
   conn->wbuf = &conn->sbuf;
   conn->wlen = sizeof (conn->sbuf.option);
   SET_NEXT_STATE (%SEND);
@@ -71,7 +75,8 @@ const char base_allocation[] = "base:allocation";
   switch (send_from_wbuf (conn)) {
   case -1: SET_NEXT_STATE (%.DEAD); return -1;
   case 0:
-    conn->sbuf.nrqueries = htobe32 (1);
+    conn->sbuf.nrqueries =
+      htobe32 (nbd_internal_string_list_length (h->request_meta_contexts));
     conn->wbuf = &conn->sbuf;
     conn->wlen = sizeof conn->sbuf.nrqueries;
     SET_NEXT_STATE (%SEND_NRQUERIES);
@@ -82,27 +87,43 @@ const char base_allocation[] = "base:allocation";
   switch (send_from_wbuf (conn)) {
   case -1: SET_NEXT_STATE (%.DEAD); return -1;
   case 0:
-    conn->sbuf.len = htobe32 (strlen (base_allocation));
-    conn->wbuf = &conn->sbuf.len;
-    conn->wlen = sizeof conn->sbuf.len;
-    SET_NEXT_STATE (%SEND_BASE_ALLOCATION_QUERYLEN);
+    conn->querynum = 0;
+    SET_NEXT_STATE (%PREPARE_NEXT_QUERY);
   }
   return 0;
 
- NEWSTYLE.OPT_SET_META_CONTEXT.SEND_BASE_ALLOCATION_QUERYLEN:
+ NEWSTYLE.OPT_SET_META_CONTEXT.PREPARE_NEXT_QUERY:
+  const char *query = h->request_meta_contexts[conn->querynum];
+
+  if (query == NULL) { /* end of list of requested meta contexts */
+    SET_NEXT_STATE (%PREPARE_FOR_REPLY);
+    return 0;
+  }
+
+  conn->sbuf.len = htobe32 (strlen (query));
+  conn->wbuf = &conn->sbuf.len;
+  conn->wlen = sizeof conn->sbuf.len;
+  SET_NEXT_STATE (%SEND_QUERYLEN);
+  return 0;
+
+ NEWSTYLE.OPT_SET_META_CONTEXT.SEND_QUERYLEN:
+  const char *query = h->request_meta_contexts[conn->querynum];
+
   switch (send_from_wbuf (conn)) {
   case -1: SET_NEXT_STATE (%.DEAD); return -1;
   case 0:
-    conn->wbuf = base_allocation;
-    conn->wlen = strlen (base_allocation);
-    SET_NEXT_STATE (%SEND_BASE_ALLOCATION_QUERY);
+    conn->wbuf = query;
+    conn->wlen = strlen (query);
+    SET_NEXT_STATE (%SEND_QUERY);
   }
   return 0;
 
- NEWSTYLE.OPT_SET_META_CONTEXT.SEND_BASE_ALLOCATION_QUERY:
+ NEWSTYLE.OPT_SET_META_CONTEXT.SEND_QUERY:
   switch (send_from_wbuf (conn)) {
   case -1: SET_NEXT_STATE (%.DEAD); return -1;
-  case 0:  SET_NEXT_STATE (%PREPARE_FOR_REPLY);
+  case 0:
+    conn->querynum++;
+    SET_NEXT_STATE (%PREPARE_NEXT_QUERY);
   }
   return 0;
 
@@ -151,7 +172,8 @@ const char base_allocation[] = "base:allocation";
       break;
     default:
       /* Anything else is an error, ignore it */
-      debug (conn->h, "base:allocation is not supported by this server");
+      debug (conn->h, "handshake: unexpected error from "
+             "NBD_OPT_SET_META_CONTEXT (%" PRIu32 ")", reply);
       conn->rbuf = NULL;
       conn->rlen = len;
       SET_NEXT_STATE (%RECV_SKIP_PAYLOAD);
@@ -160,19 +182,35 @@ const char base_allocation[] = "base:allocation";
   return 0;
 
  NEWSTYLE.OPT_SET_META_CONTEXT.RECV_REPLY_PAYLOAD:
+  struct meta_context *meta_context;
+  uint32_t len;
+
   switch (recv_into_rbuf (conn)) {
   case -1: SET_NEXT_STATE (%.DEAD); return -1;
   case 0:
     if (conn->rbuf != NULL) {
-      /* String payload is not NUL-terminated. */
-      if (memcmp (base_allocation, conn->sbuf.or.payload.context.str,
-                  strlen (base_allocation)) == 0) {
-        conn->has_base_allocation = true;
-        conn->base_allocation =
-          be32toh (conn->sbuf.or.payload.context.context.context_id);
-        debug (h, "negotiated %s with context ID %" PRIu32,
-               base_allocation, conn->base_allocation);
+      len = be32toh (conn->sbuf.or.option_reply.replylen);
+
+      meta_context = malloc (sizeof *meta_context);
+      if (meta_context == NULL) {
+        set_error (errno, "malloc");
+        SET_NEXT_STATE (%.DEAD);
+        return -1;
       }
+      /* String payload is not NUL-terminated. */
+      meta_context->name = strndup (conn->sbuf.or.payload.context.str, len);
+      if (meta_context->name == NULL) {
+        set_error (errno, "strdup");
+        SET_NEXT_STATE (%.DEAD);
+        free (meta_context);
+        return -1;
+      }
+      meta_context->context_id =
+        be32toh (conn->sbuf.or.payload.context.context.context_id);
+      debug (h, "negotiated %s with context ID %" PRIu32,
+             meta_context->name, meta_context->context_id);
+      meta_context->next = conn->meta_contexts;
+      conn->meta_contexts = meta_context;
     }
     SET_NEXT_STATE (%PREPARE_FOR_REPLY);
   }

generator/generator
@@ -463,15 +463,22 @@ and newstyle_opt_set_meta_context_state_machine = [
 
   State {
     default_state with
-    name = "SEND_BASE_ALLOCATION_QUERYLEN";
-    comment = "Send newstyle NBD_OPT_SET_META_CONTEXT base:allocation query length";
+    name = "PREPARE_NEXT_QUERY";
+    comment = "Prepare to send newstyle NBD_OPT_SET_META_CONTEXT query";
+    external_events = [];
+  };
+
+  State {
+    default_state with
+    name = "SEND_QUERYLEN";
+    comment = "Send newstyle NBD_OPT_SET_META_CONTEXT query length";
     external_events = [ NotifyWrite, "" ];
   };
 
   State {
     default_state with
-    name = "SEND_BASE_ALLOCATION_QUERY";
-    comment = "Send newstyle NBD_OPT_SET_META_CONTEXT base:allocation";
+    name = "SEND_QUERY";
+    comment = "Send newstyle NBD_OPT_SET_META_CONTEXT query";
     external_events = [ NotifyWrite, "" ];
   };
 
@@ -996,6 +1003,26 @@ authentication cannot be used to connect to the server.";
 Get the current TLS PSK filename.  See C<nbd_set_tls_psk_file>.";
   };
 *)
+
+  "request_meta_context", {
+    default_call with
+    args = [ String "name" ]; ret = RErr;
+    shortdesc = "request metadata context from the server";
+    longdesc = "\
+This is used to enable a metadata context on the server.  This
+can be used to allow you to read extra information about
+data blocks, such as whether they are sparse (holes) or
+allocated, and dirty bitmaps.  You must call this before you
+connect to the server because metadata context negotiation
+happens during the initial handshake.
+
+The single parameter is the name of the metadata context,
+for example C<\"base:allocation\">.
+
+Other metadata contexts are server-specific, but include
+C<\"qemu:dirty-bitmap:...\"> for qemu-nbd
+(see qemu-nbd I<-B> option).  See also C<nbd_block_status>.";
+  };
 
   "connect_unix", {
     default_call with

lib/handle.c
@@ -64,6 +64,13 @@ free_cmd_list (struct command_in_flight *list)
 static void
 close_conn (struct nbd_connection *conn)
 {
+  struct meta_context *m, *m_next;
+
+  for (m = conn->meta_contexts; m != NULL; m = m_next) {
+    m_next = m->next;
+    free (m->name);
+    free (m);
+  }
   free_cmd_list (conn->cmds_to_issue);
   free_cmd_list (conn->cmds_in_flight);
   free_cmd_list (conn->cmds_done);
@@ -147,6 +154,7 @@ nbd_close (struct nbd_handle *h)
   free (h->tls_certificates);
   free (h->tls_username);
   free (h->tls_psk_file);
+  nbd_internal_free_string_list (h->request_meta_contexts);
   pthread_mutex_destroy (&h->lock);
   free (h);
 }
@@ -284,4 +292,32 @@ nbd_unlocked_get_export_name (struct nbd_handle *h)
   }
 
   return copy;
+}
+
+int
+nbd_unlocked_request_meta_context (struct nbd_handle *h, const char *name)
+{
+  char *copy;
+  size_t len;
+  char **list;
+
+  copy = strdup (name);
+  if (!copy) {
+    set_error (errno, "strdup");
+    return -1;
+  }
+  len = h->request_meta_contexts == NULL ? 0
+    : nbd_internal_string_list_length (h->request_meta_contexts);
+  list = realloc (h->request_meta_contexts,
+                  sizeof (char *) * (len+2 /* + new entry + NULL */));
+  if (list == NULL) {
+    free (copy);
+    set_error (errno, "realloc");
+    return -1;
+  }
+  h->request_meta_contexts = list;
+  list[len] = copy;
+  list[len+1] = NULL;
+
+  return 0;
 }

lib/internal.h
@@ -37,7 +37,9 @@
  */
 #define MAX_REQUEST_SIZE (64 * 1024 * 1024)
 
+struct meta_context;
 struct socket;
+struct command_in_flight;
 
 struct nbd_handle {
   /* Lock protecting concurrent access to either this handle or the
@@ -61,6 +63,9 @@ struct nbd_handle {
   bool tls_verify_peer;         /* Verify the peer certificate. */
   char *tls_username;           /* Username, NULL = use current username */
   char *tls_psk_file;           /* PSK filename, NULL = no PSK */
+
+  /* Desired metadata contexts. */
+  char **request_meta_contexts;
 
   /* Export size and per-export flags, received during handshake.  NB:
    * These are *both* *only* valid if eflags != 0.  This is because
@@ -93,8 +98,8 @@ struct nbd_connection {
   enum state state;             /* State machine. */
 
   bool structured_replies;      /* If we negotiated NBD_OPT_STRUCTURED_REPLY */
-  bool has_base_allocation;     /* If we negotiated base:allocation. */
-  uint32_t base_allocation;     /* Context ID of base:allocation. */
+  /* Linked list of negotiated metadata contexts. */
+  struct meta_context *meta_contexts;
 
   /* The socket or a wrapper if using GnuTLS. */
   struct socket *sock;
@@ -123,7 +128,7 @@ struct nbd_connection {
         struct nbd_fixed_new_option_reply_info_export export;
         struct {
           struct nbd_fixed_new_option_reply_meta_context context;
-          char str[32];
+          char str[64];
         }  __attribute__((packed)) context;
       } payload;
     }  __attribute__((packed)) or;
@@ -161,6 +166,9 @@ struct nbd_connection {
   struct addrinfo hints;
   struct addrinfo *result, *rp;
 
+  /* When sending metadata contexts, this is used. */
+  size_t querynum;
+
   /* Global flags from the server. */
   uint16_t gflags;
 
@@ -171,6 +179,12 @@ struct nbd_connection {
    * acknowledge them.
    */
   struct command_in_flight *cmds_to_issue, *cmds_in_flight, *cmds_done;
+};
+
+struct meta_context {
+  struct meta_context *next;    /* Linked list. */
+  char *name;                   /* Name of meta context. */
+  uint32_t context_id;          /* Context ID negotiated with the server. */
 };
 
 struct socket_ops {

commit d4a7372a5959b49204e2f51412e81e97aff9e5b0

set-meta-context: Properly truncate server's context name
The 'len' variable is the overall length of the NBD_REP_META_CONTEXT
payload, which includes the 4-byte context_id field. We were using
strndup() on the undoctored length but on the name field starting at
offset 4, which meant we could end up dup'ing up to 4 bytes of garbage
after the values we read from the server. So far, we appear to have
gotten lucky that those four bytes have tended to start with NUL; I
did not audit if there was a way for a malicious server to
purposefully leave non-zero bytes in the tail, but it is probably
possible by replying with a long context name followed by a short
one. On the other hand, the worst that happens is that the garbage
name doesn't match the user's nbd_add_meta_context() request, so we
behave as if the server does not support the context we wanted after
all.

Fixes: d0ec1ee8

generator/states-newstyle-opt-set-meta-context.c
@@ -202,16 +202,17 @@
         SET_NEXT_STATE (%.DEAD);
         return -1;
       }
+      meta_context->context_id =
+        be32toh (h->sbuf.or.payload.context.context.context_id);
       /* String payload is not NUL-terminated. */
-      meta_context->name = strndup (h->sbuf.or.payload.context.str, len);
+      meta_context->name = strndup (h->sbuf.or.payload.context.str,
+                                    len - sizeof meta_context->context_id);
       if (meta_context->name == NULL) {
         set_error (errno, "strdup");
         SET_NEXT_STATE (%.DEAD);
         free (meta_context);
         return -1;
       }
-      meta_context->context_id =
-        be32toh (h->sbuf.or.payload.context.context.context_id);
       debug (h, "negotiated %s with context ID %" PRIu32,
              meta_context->name, meta_context->context_id);
       meta_context->next = h->meta_contexts;