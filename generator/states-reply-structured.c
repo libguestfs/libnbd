libguestfs/libnbd

commit 6f238add6598c6416dd27f901a73e25aed017519

states: Validate error message size
If the server passes us a malformed error reply type with a message
length longer than the overall structured reply, we would blindly obey
that size and get out of sync with the server (perhaps even hanging on
a read for data that will never come).  Broken since its introduction
in commit 28952eda.

Fix it by parsing the tail of an error separate from the message,
which also lets us add other sanity checking, and makes it easier if a
future patch wants to capture instead of ignore the server's message.

generator/states-reply-structured.c
@@ -149,21 +149,69 @@
   }
 
  REPLY.STRUCTURED_REPLY.RECV_ERROR:
+  uint32_t length, msglen;
+
   switch (recv_into_rbuf (h)) {
   case -1: SET_NEXT_STATE (%.DEAD); return -1;
   case 0:
+    length = be32toh (h->sbuf.sr.structured_reply.length);
+    msglen = be16toh (h->sbuf.sr.payload.error.len);
+    if (msglen > length - sizeof h->sbuf.sr.payload.error) {
+      SET_NEXT_STATE (%.DEAD);
+      set_error (0, "error message length too large");
+      return -1;
+    }
+
     /* We skip the human readable error for now. XXX */
     h->rbuf = NULL;
-    h->rlen = be16toh (h->sbuf.sr.payload.error.len);
+    h->rlen = msglen;
     SET_NEXT_STATE (%RECV_ERROR_MESSAGE);
   }
   return 0;
 
  REPLY.STRUCTURED_REPLY.RECV_ERROR_MESSAGE:
+  uint32_t length, msglen;
+  uint16_t type;
+
+  switch (recv_into_rbuf (h)) {
+  case -1: SET_NEXT_STATE (%.DEAD); return -1;
+  case 0:
+    length = be32toh (h->sbuf.sr.structured_reply.length);
+    msglen = be16toh (h->sbuf.sr.payload.error.len);
+    type = be16toh (h->sbuf.sr.structured_reply.type);
+
+    length -= sizeof h->sbuf.sr.payload.error - msglen;
+
+    /* Special case two specific errors; ignore the tail for all others */
+    h->rbuf = NULL;
+    h->rlen = length;
+    switch (type) {
+    case NBD_REPLY_TYPE_ERROR:
+      if (length != 0) {
+        SET_NEXT_STATE (%.DEAD);
+        set_error (0, "error payload length too large");
+        return -1;
+      }
+      break;
+    case NBD_REPLY_TYPE_ERROR_OFFSET:
+      if (length != sizeof h->offset) {
+        SET_NEXT_STATE (%.DEAD);
+        set_error (0, "invalid error payload length");
+        return -1;
+      }
+      h->rbuf = &h->offset;
+      break;
+    }
+    SET_NEXT_STATE (%RECV_ERROR_TAIL);
+  }
+  return 0;
+
+ REPLY.STRUCTURED_REPLY.RECV_ERROR_TAIL:
   struct command_in_flight *cmd;
   uint16_t flags;
   uint64_t handle;
   uint32_t error;
+  uint64_t offset;
 
   switch (recv_into_rbuf (h)) {
   case -1: SET_NEXT_STATE (%.DEAD); return -1;
@@ -182,6 +230,20 @@
     /* Preserve first error encountered */
     if (cmd->error == 0)
       cmd->error = nbd_internal_errno_of_nbd_error (error);
+
+    /* Sanity check that any error offset is in range */
+    if (error == NBD_REPLY_TYPE_ERROR_OFFSET) {
+      offset = be64toh (h->offset);
+      if (offset < cmd->offset || offset >= cmd->offset + cmd->count) {
+        SET_NEXT_STATE (%.DEAD);
+        set_error (0, "offset of error reply is out of bounds, "
+                   "offset=%" PRIu64 ", cmd->offset=%" PRIu64 ", "
+                   "cmd->count=%" PRIu32 ", "
+                   "this is likely to be a bug in the server",
+                   offset, cmd->offset, cmd->count);
+        return -1;
+      }
+    }
 
     if (flags & NBD_REPLY_FLAG_DONE)
       SET_NEXT_STATE (%^FINISH_COMMAND);

generator/generator
@@ -773,6 +773,13 @@ and structured_reply_state_machine = [
 
   State {
     default_state with
+    name = "RECV_ERROR_TAIL";
+    comment = "Receive a structured reply error tail";
+    external_events = [ NotifyRead, "" ];
+  };
+
+  State {
+    default_state with
     name = "RECV_OFFSET_DATA";
     comment = "Receive a structured reply offset-data header";
     external_events = [ NotifyRead, "" ];

lib/internal.h
@@ -174,6 +174,9 @@ struct nbd_handle {
   /* When sending metadata contexts, this is used. */
   size_t querynum;
 
+  /* When receiving structured replies, this is used. */
+  uint64_t offset;
+
   /* When receiving block status, this is used. */
   uint32_t *bs_entries;
 

commit 15d0f03d51638b53c60d3e46c8deff7853bedc49

states: Fix ERROR_OFFSET sanity check
Due to comparison against the wrong field, the sanity check for the
offset tail field in NBD_REPLY_TYPE_ERROR_OFFSET could never fire,
unless a server sent a non-standard NBD_E* code that coincides with
the reply type.  While at it, sink the setting of cmd->error after the
sanity checking. (The bug evaded my notice at the time since I'm not
aware of any NBD server that sends this type of error by default; but
I hit it while intentionally making a temporary hack to qemu-nbd to
provoke it - more on that hack in a later patch adding support for
nbd_pread_callback).

Fixes: 6f238add6

generator/states-reply-structured.c
@@ -229,6 +229,7 @@
   uint64_t handle;
   uint32_t error;
   uint64_t offset;
+  uint16_t type;
 
   switch (recv_into_rbuf (h)) {
   case -1: SET_NEXT_STATE (%.DEAD); return -1;
@@ -236,6 +237,7 @@
     flags = be16toh (h->sbuf.sr.structured_reply.flags);
     handle = be64toh (h->sbuf.sr.structured_reply.handle);
     error = be32toh (h->sbuf.sr.payload.error.error.error);
+    type = be16toh (h->sbuf.sr.structured_reply.type);
 
     /* Find the command amongst the commands in flight. */
     for (cmd = h->cmds_in_flight; cmd != NULL; cmd = cmd->next) {
@@ -244,12 +246,8 @@
     }
     assert (cmd); /* guaranteed by CHECK */
 
-    /* Preserve first error encountered */
-    if (cmd->error == 0)
-      cmd->error = nbd_internal_errno_of_nbd_error (error);
-
     /* Sanity check that any error offset is in range */
-    if (error == NBD_REPLY_TYPE_ERROR_OFFSET) {
+    if (type == NBD_REPLY_TYPE_ERROR_OFFSET) {
       offset = be64toh (h->sbuf.sr.payload.error.offset);
       if (offset < cmd->offset || offset >= cmd->offset + cmd->count) {
         SET_NEXT_STATE (%.DEAD);
@@ -261,6 +259,10 @@
         return -1;
       }
     }
+
+    /* Preserve first error encountered */
+    if (cmd->error == 0)
+      cmd->error = nbd_internal_errno_of_nbd_error (error);
 
     if (flags & NBD_REPLY_FLAG_DONE)
       SET_NEXT_STATE (%^FINISH_COMMAND);