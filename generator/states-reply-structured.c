libguestfs/libnbd

commit e4bd477f6ac7cdd7d99137bbec136407d22089d8

states: Log structured error messages, if any
Similar to how option_reply_meta_context had storage for the server's
name reply, add storage for the maximum-length string error message
that the server may optionally use for any structured error.

This increases the size of struct nbd_handle from about 700 bytes to
instead being just under 5k; that's not too bad of a cost, and future
patches will reuse same storage for logging any error messages during
handshake.  Besides, struct nbd_handle is already opaque and
heap-allocated during nbd_create().

While at it, rearrange the 'offset' field added in commit 6f238add to
instead be alongside the rest of the .sr storage.

generator/states-reply-structured.c
@@ -69,13 +69,13 @@
   }
 
   if (NBD_REPLY_TYPE_IS_ERR (type)) {
-    if (length < sizeof h->sbuf.sr.payload.error) {
+    if (length < sizeof h->sbuf.sr.payload.error.error) {
       SET_NEXT_STATE (%.DEAD);
       set_error (0, "too short length in structured reply error");
       return -1;
     }
-    h->rbuf = &h->sbuf.sr.payload.error;
-    h->rlen = sizeof h->sbuf.sr.payload.error;
+    h->rbuf = &h->sbuf.sr.payload.error.error;
+    h->rlen = sizeof h->sbuf.sr.payload.error.error;
     SET_NEXT_STATE (%RECV_ERROR);
     return 0;
   }
@@ -155,15 +155,15 @@
   case -1: SET_NEXT_STATE (%.DEAD); return -1;
   case 0:
     length = be32toh (h->sbuf.sr.structured_reply.length);
-    msglen = be16toh (h->sbuf.sr.payload.error.len);
-    if (msglen > length - sizeof h->sbuf.sr.payload.error) {
+    msglen = be16toh (h->sbuf.sr.payload.error.error.len);
+    if (msglen > length - sizeof h->sbuf.sr.payload.error.error ||
+        msglen > sizeof h->sbuf.sr.payload.error.msg) {
       SET_NEXT_STATE (%.DEAD);
       set_error (0, "error message length too large");
       return -1;
     }
 
-    /* We skip the human readable error for now. XXX */
-    h->rbuf = NULL;
+    h->rbuf = h->sbuf.sr.payload.error.msg;
     h->rlen = msglen;
     SET_NEXT_STATE (%RECV_ERROR_MESSAGE);
   }
@@ -177,10 +177,14 @@
   case -1: SET_NEXT_STATE (%.DEAD); return -1;
   case 0:
     length = be32toh (h->sbuf.sr.structured_reply.length);
-    msglen = be16toh (h->sbuf.sr.payload.error.len);
+    msglen = be16toh (h->sbuf.sr.payload.error.error.len);
     type = be16toh (h->sbuf.sr.structured_reply.type);
 
-    length -= sizeof h->sbuf.sr.payload.error - msglen;
+    length -= sizeof h->sbuf.sr.payload.error.error - msglen;
+
+    if (msglen)
+      debug (h, "structured error server message: %.*s", (int) msglen,
+             h->sbuf.sr.payload.error.msg);
 
     /* Special case two specific errors; ignore the tail for all others */
     h->rbuf = NULL;
@@ -194,12 +198,12 @@
       }
       break;
     case NBD_REPLY_TYPE_ERROR_OFFSET:
-      if (length != sizeof h->offset) {
+      if (length != sizeof h->sbuf.sr.payload.error.offset) {
         SET_NEXT_STATE (%.DEAD);
         set_error (0, "invalid error payload length");
         return -1;
       }
-      h->rbuf = &h->offset;
+      h->rbuf = &h->sbuf.sr.payload.error.offset;
       break;
     }
     SET_NEXT_STATE (%RECV_ERROR_TAIL);
@@ -218,7 +222,7 @@
   case 0:
     flags = be16toh (h->sbuf.sr.structured_reply.flags);
     handle = be64toh (h->sbuf.sr.structured_reply.handle);
-    error = be32toh (h->sbuf.sr.payload.error.error);
+    error = be32toh (h->sbuf.sr.payload.error.error.error);
 
     /* Find the command amongst the commands in flight. */
     for (cmd = h->cmds_in_flight; cmd != NULL; cmd = cmd->next) {
@@ -233,7 +237,7 @@
 
     /* Sanity check that any error offset is in range */
     if (error == NBD_REPLY_TYPE_ERROR_OFFSET) {
-      offset = be64toh (h->offset);
+      offset = be64toh (h->sbuf.sr.payload.error.offset);
       if (offset < cmd->offset || offset >= cmd->offset + cmd->count) {
         SET_NEXT_STATE (%.DEAD);
         set_error (0, "offset of error reply is out of bounds, "

lib/internal.h
@@ -136,7 +136,11 @@ struct nbd_handle {
       union {
         struct nbd_structured_reply_offset_data offset_data;
         struct nbd_structured_reply_offset_hole offset_hole;
-        struct nbd_structured_reply_error error;
+        struct {
+          struct nbd_structured_reply_error error;
+          char msg[NBD_MAX_STRING]; /* Common to all error types */
+          uint64_t offset; /* Only used for NBD_REPLY_TYPE_ERROR_OFFSET */
+        } __attribute__((packed)) error;
       } payload;
     }  __attribute__((packed)) sr;
     uint32_t cflags;
@@ -173,9 +177,6 @@ struct nbd_handle {
 
   /* When sending metadata contexts, this is used. */
   size_t querynum;
-
-  /* When receiving structured replies, this is used. */
-  uint64_t offset;
 
   /* When receiving block status, this is used. */
   uint32_t *bs_entries;

commit 1aa33e0959158f1da455ce6d97137b203ff0fc8b

states: Fix math error
When computing the size of NBD_REPLY_TYPE_ERROR_OFFSET, I went the
wrong direction, which only matters for a non-zero server message
length. (It evaded my notice at the time since I'm not aware of any
NBD server that sends this type of error by default; but I hit it
while intentionally making a temporary hack to qemu-nbd to provoke it
- more on that hack in a later patch adding support for
nbd_pread_callback).

Fixes: e4bd477f6a

generator/states-reply-structured.c
@@ -193,7 +193,7 @@
     msglen = be16toh (h->sbuf.sr.payload.error.error.len);
     type = be16toh (h->sbuf.sr.structured_reply.type);
 
-    length -= sizeof h->sbuf.sr.payload.error.error - msglen;
+    length -= sizeof h->sbuf.sr.payload.error.error + msglen;
 
     if (msglen)
       debug (h, "structured error server message: %.*s", (int) msglen,

lib/nbd-protocol.h
@@ -202,7 +202,7 @@ struct nbd_structured_reply_offset_hole {
 struct nbd_structured_reply_error {
   uint32_t error;               /* NBD_E* error number */
   uint16_t len;                 /* Length of human readable error. */
-  /* Followed by human readable error string. */
+  /* Followed by human readable error string, and possibly more structure. */
 } __attribute__((packed));
 
 #define NBD_REQUEST_MAGIC           0x25609513