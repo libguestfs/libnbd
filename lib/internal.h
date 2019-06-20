libguestfs/libnbd

commit adb11a32f3b28265435e86e90c323aab28c8657f

api: Implement nbd_get_error, nbd_get_errno.
These store the last error/errno in thread-local storage, and thus the
last error/errno can be retrieved immediately using nbd_get_error ()
or nbd_get_errno () with no parameters (assuming called from the same
thread immediately after the failed nbd_* call).

lib/internal.h
@@ -19,6 +19,7 @@
 #ifndef LIBNBD_INTERNAL_H
 #define LIBNBD_INTERNAL_H
 
+#include <string.h>
 #include <netdb.h>
 #include <sys/types.h>
 
@@ -152,9 +153,23 @@ struct command_in_flight {
 #define debug(fs, ...) fprintf (stderr, fs "\n", ##__VA_ARGS__)
 
 /* errors.c */
-/* XXX */
-#include <string.h>
-#define set_error(errno, fs, ...) do { fprintf (stderr, "libnbd: " fs, ##__VA_ARGS__); if (errno) fprintf (stderr, ": %s", strerror (errno)); fprintf (stderr, "\n"); } while (0)
+extern void nbd_internal_reset_error (const char *context);
+extern const char *nbd_internal_get_error_context (void);
+extern void nbd_internal_set_last_error (int errnum, char *error);
+#define set_error(errnum, fs, ...)                                      \
+  do {                                                                  \
+    const char *_context =                                              \
+      nbd_internal_get_error_context () ? : "unknown";                  \
+    char *_errp;                                                        \
+    int _r;                                                             \
+    if ((errnum) == 0)                                                  \
+      _r = asprintf (&_errp, "%s: " fs, _context, ##__VA_ARGS__);       \
+    else                                                                \
+      _r = asprintf (&_errp, "%s: " fs ": %s",                          \
+                     _context, ##__VA_ARGS__, strerror ((errnum)));     \
+    if (_r >= 0)                                                        \
+      nbd_internal_set_last_error ((errnum), _errp);                    \
+  } while (0)
 
 /* protocol.c */
 extern int nbd_internal_errno_of_nbd_error (uint32_t error);

.gitignore
@@ -54,6 +54,7 @@ Makefile.in
 /tests/can-zero-flag
 /tests/connect-tcp
 /tests/connect-unix
+/tests/errors
 /tests/get-size
 /tests/is-rotational-flag
 /tests/is-not-rotational-flag

docs/libnbd.pod
@@ -84,11 +84,11 @@ Create a handle and connect to the server:
  
  nbd = nbd_create ();
  if (!nbd) {
-   perror ("nbd_create");
+   fprintf (stderr, "%s\n", nbd_get_error ());
    exit (EXIT_FAILURE);
  }
  if (nbd_connect_tcp (nbd, "server.example.com", "nbd") == -1) {
-   fprintf (stderr, "connect: %s\n", nbd_get_error (nbd));
+   fprintf (stderr, "%s\n", nbd_get_error ());
    exit (EXIT_FAILURE);
  }
 
@@ -97,7 +97,7 @@ Read the first sector (512 bytes) from the NBD export:
  char buf[512];
  
  if (nbd_pread (nbd, buf, sizeof buf, 0) == -1) {
-   fprintf (stderr, "read: %s\n", nbd_get_error (nbd));
+   fprintf (stderr, "%s\n", nbd_get_error ());
    exit (EXIT_FAILURE);
  }
 
@@ -131,7 +131,7 @@ object:
  
  nbd = nbd_create ();
  if (nbd == NULL) {
-   perror ("nbd_create");
+   fprintf (stderr, "%s\n", nbd_get_error ());
    exit (EXIT_FAILURE);
  }
  conn = nbd_get_connection (nbd, 0);
@@ -147,12 +147,12 @@ until the connection becomes ready:
  /* some code to set up addr,
     then ... */
  if (nbd_aio_connect (conn, &addr, len) == -1) {
-   fprintf (stderr, "connect: %s\n", nbd_get_error (nbd));
+   fprintf (stderr, "%s\n", nbd_get_error ());
    exit (EXIT_FAILURE);
  }
  while (!nbd_aio_is_ready (conn)) {
    if (nbd_poll (h, -1) == -1) {
-     fprintf (stderr, "poll: %s\n", nbd_get_error (nbd));
+     fprintf (stderr, "%s\n", nbd_get_error ());
      exit (EXIT_FAILURE);
    }
  }

examples/simple-fetch-first-sector.c
@@ -30,21 +30,21 @@ main (int argc, char *argv[])
 
   nbd = nbd_create ();
   if (nbd == NULL) {
-    perror ("nbd_create");
+    fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);
   }
   if (nbd_connect_unix (nbd, argv[1]) == -1) {
-    /* XXX PRINT ERROR */
+    fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);
   }
 
   if (nbd_pread (nbd, buf, sizeof buf, 0) == -1) {
-    /* XXX PRINT ERROR */
+    fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);
   }
 
   if (nbd_shutdown (nbd) == -1) {
-    /* XXX PRINT ERROR */
+    fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);
   }
 

examples/simple-reads-and-writes.c
@@ -35,17 +35,17 @@ main (int argc, char *argv[])
 
   nbd = nbd_create ();
   if (nbd == NULL) {
-    perror ("nbd_create");
+    fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);
   }
   if (nbd_connect_unix (nbd, argv[1]) == -1) {
-    /* XXX PRINT ERROR */
+    fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);
   }
 
   exportsize = nbd_get_size (nbd);
   if (exportsize == -1) {
-    /* XXX PRINT ERROR */
+    fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);
   }
   assert (exportsize >= sizeof buf);
@@ -63,7 +63,7 @@ main (int argc, char *argv[])
     offset = rand () % (exportsize - sizeof buf);
 
     if (nbd_pwrite (nbd, buf, sizeof buf, offset, 0) == -1) {
-      /* XXX PRINT ERROR */
+      fprintf (stderr, "%s\n", nbd_get_error ());
       exit (EXIT_FAILURE);
     }
   }
@@ -72,19 +72,19 @@ main (int argc, char *argv[])
   for (i = 0; i < 1000; ++i) {
     offset = rand () % (exportsize - sizeof buf);
     if (nbd_pread (nbd, buf, sizeof buf, offset) == -1) {
-      /* XXX PRINT ERROR */
+      fprintf (stderr, "%s\n", nbd_get_error ());
       exit (EXIT_FAILURE);
     }
 
     offset = rand () % (exportsize - sizeof buf);
     if (nbd_pwrite (nbd, buf, sizeof buf, offset, 0) == -1) {
-      /* XXX PRINT ERROR */
+      fprintf (stderr, "%s\n", nbd_get_error ());
       exit (EXIT_FAILURE);
     }
   }
 
   if (nbd_shutdown (nbd) == -1) {
-    /* XXX PRINT ERROR */
+    fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);
   }
 

examples/threaded-reads-and-writes.c
@@ -80,23 +80,23 @@ main (int argc, char *argv[])
 
   nbd = nbd_create ();
   if (nbd == NULL) {
-    perror ("nbd_create");
+    fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);
   }
   if (nbd_set_multi_conn (nbd, NR_MULTI_CONN) == -1) {
-    /* XXX PRINT ERROR */
+    fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);
   }
 
   /* Connect all connections synchronously as this is simpler. */
   if (nbd_connect_unix (nbd, argv[1]) == -1) {
-    /* XXX PRINT ERROR */
+    fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);
   }
 
   exportsize = nbd_get_size (nbd);
   if (exportsize == -1) {
-    /* XXX PRINT ERROR */
+    fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);
   }
 
@@ -147,7 +147,7 @@ main (int argc, char *argv[])
   }
 
   if (nbd_shutdown (nbd) == -1) {
-    /* XXX PRINT ERROR */
+    fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);
   }
 
@@ -233,7 +233,7 @@ start_thread (void *arg)
       else
         handle = nbd_aio_pread (conn, buf, sizeof buf, offset);
       if (handle == -1) {
-        /* XXX PRINT ERROR */
+        fprintf (stderr, "%s\n", nbd_get_error ());
         goto error;
       }
       handles[in_flight] = handle;
@@ -247,7 +247,7 @@ start_thread (void *arg)
     for (j = 0; j < in_flight; ++j) {
       r = nbd_aio_command_completed (conn, handles[j]);
       if (r == -1) {
-        /* XXX PRINT ERROR */
+        fprintf (stderr, "%s\n", nbd_get_error ());
         goto error;
       }
       if (r) {

generator/generator
@@ -1236,6 +1236,8 @@ let generate_lib_libnbd_syms () =
   pr "    nbd_create;\n";
   pr "    nbd_close;\n";
   pr "    nbd_get_connection;\n";
+  pr "    nbd_get_errno;\n";
+  pr "    nbd_get_error;\n";
   pr "    nbd_connection_close;\n";
   List.iter (fun (name, _) -> pr "    nbd_%s;\n" name) handle_calls;
   List.iter (fun (name, _) -> pr "    nbd_%s;\n" name) connection_calls;
@@ -1302,6 +1304,8 @@ let generate_include_libnbd_h () =
   pr "\n";
   pr "extern struct nbd_handle *nbd_create (void);\n";
   pr "extern void nbd_close (struct nbd_handle *h);\n";
+  pr "extern const char *nbd_get_error (void);\n";
+  pr "extern int nbd_get_errno (void);\n";
   pr "\n";
   List.iter (
     fun (name, { args; ret }) -> print_extern name "handle" args ret
@@ -1373,6 +1377,7 @@ let generate_lib_api_c () =
     );
     pr "\n";
     pr "  pthread_mutex_lock (&%s);\n" lock;
+    pr "  nbd_internal_reset_error (\"nbd_%s\");\n" name;
     pr "  ret = nbd_unlocked_%s (%s" name hname;
     let argnames = List.flatten (List.map name_of_arg args) in
     List.iter (pr ", %s") argnames;

lib/Makefile.am
@@ -35,6 +35,7 @@ libnbd_la_SOURCES = \
 	create.c \
 	connect.c \
 	disconnect.c \
+	errors.c \
 	flags.c \
 	internal.h \
 	nbd-protocol.h \

commit 0645a2f2d6c83adcd5b2170012671fcb8851438d

internal: Don't lose errno during set_error()
A successful call to asprintf() leaves errno in an unspecified state;
if our macro is called with 'errno' as its errnum parameter, our later
call to nbd_internal_set_last_error() that meant to use the original
value of errno at the start of the macro may instead be setting things
to whatever garbage asprintf left behind.

Fixes: adb11a32

lib/internal.h
@@ -275,17 +275,18 @@ extern const char *nbd_internal_get_error_context (void);
 extern void nbd_internal_set_last_error (int errnum, char *error);
 #define set_error(errnum, fs, ...)                                      \
   do {                                                                  \
+    int _e = (errnum);                                                  \
     const char *_context =                                              \
       nbd_internal_get_error_context () ? : "unknown";                  \
     char *_errp;                                                        \
     int _r;                                                             \
-    if ((errnum) == 0)                                                  \
+    if (_e == 0)                                                        \
       _r = asprintf (&_errp, "%s: " fs, _context, ##__VA_ARGS__);       \
     else                                                                \
       _r = asprintf (&_errp, "%s: " fs ": %s",                          \
-                     _context, ##__VA_ARGS__, strerror ((errnum)));     \
+                     _context, ##__VA_ARGS__, strerror (_e));           \
     if (_r >= 0)                                                        \
-      nbd_internal_set_last_error ((errnum), _errp);                    \
+      nbd_internal_set_last_error (_e, _errp);                          \
   } while (0)
 
 /* flags.c */