libguestfs/libnbd

commit 1d73554a80be8366a214916d52a674b122bf136b

lib: Return errors from failed commands.

lib/aio.c
@@ -60,6 +60,8 @@ nbd_unlocked_aio_command_completed (struct nbd_connection *conn,
                                     int64_t handle)
 {
   struct command_in_flight *prev_cmd, *cmd;
+  uint16_t type;
+  uint32_t error;
 
   /* Find the command amongst the completed commands. */
   for (cmd = conn->cmds_done, prev_cmd = NULL;
@@ -71,6 +73,9 @@ nbd_unlocked_aio_command_completed (struct nbd_connection *conn,
   if (!cmd)
     return 0;
 
+  type = cmd->type;
+  error = cmd->error;
+
   /* Retire it from the list and free it. */
   if (prev_cmd != NULL)
     prev_cmd->next = cmd->next;
@@ -78,5 +83,14 @@ nbd_unlocked_aio_command_completed (struct nbd_connection *conn,
     conn->cmds_done = cmd->next;
 
   free (cmd);
-  return 1;
+
+  /* If the command was successful, return true. */
+  if (error == 0)
+    return 1;
+
+  /* The command failed, set an error indication and return an error. */
+  error = nbd_internal_errno_of_nbd_error (error);
+  set_error (error, "%s: command failed: %s",
+             nbd_internal_name_of_nbd_cmd (type), strerror (error));
+  return -1;
 }

lib/Makefile.am
@@ -37,6 +37,7 @@ libnbd_la_SOURCES = \
 	internal.h \
 	nbd-protocol.h \
 	poll.c \
+	protocol.c \
 	rw.c \
 	states.c \
 	states.h \

lib/internal.h
@@ -125,6 +125,10 @@ struct command_in_flight {
 #include <string.h>
 #define set_error(errno, fs, ...) do { fprintf (stderr, "libnbd: " fs, ##__VA_ARGS__); if (errno) fprintf (stderr, ": %s", strerror (errno)); fprintf (stderr, "\n"); } while (0)
 
+/* protocol.c */
+extern int nbd_internal_errno_of_nbd_error (uint32_t error);
+extern const char *nbd_internal_name_of_nbd_cmd (uint16_t type);
+
 /* states.c */
 extern int nbd_internal_run (struct nbd_handle *h, struct nbd_connection *conn,
                              enum external_event ev);

lib/protocol.c
@@ -1,0 +1,59 @@
+/* NBD client library in userspace
+ * Copyright (C) 2013-2019 Red Hat Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <errno.h>
+
+#include "internal.h"
+
+int
+nbd_internal_errno_of_nbd_error (uint32_t error)
+{
+  switch (error) {
+  case NBD_SUCCESS: return 0;
+  case NBD_EPERM: return EPERM;
+  case NBD_EIO: return EIO;
+  case NBD_ENOMEM: return ENOMEM;
+  case NBD_EINVAL: return EINVAL;
+  case NBD_ENOSPC: return ENOSPC;
+  case NBD_ESHUTDOWN: return ESHUTDOWN;
+  default: return EINVAL;
+  }
+}
+
+/* XXX In nbdkit this function is generated from the nbd-protocol.h
+ * header file.  We should unify this across the two programs.
+ */
+const char *
+nbd_internal_name_of_nbd_cmd (uint16_t type)
+{
+  switch (type) {
+  case NBD_CMD_READ: return "read";
+  case NBD_CMD_WRITE: return "write";
+  case NBD_CMD_DISC: return "disconnect";
+  case NBD_CMD_FLUSH: return "flush";
+  case NBD_CMD_TRIM: return "trim";
+  case NBD_CMD_WRITE_ZEROES: return "write-zeroes";
+  case NBD_CMD_BLOCK_STATUS: return "block-status";
+  default: return "UNKNOWN!";
+  }
+}

lib/rw.c
@@ -34,17 +34,18 @@ nbd_unlocked_pread (struct nbd_handle *h, void *buf,
 {
   struct nbd_connection *conn = h->conns[h->unique % h->multi_conn];
   int64_t ch;
+  int r;
 
   ch = nbd_unlocked_aio_pread (conn, buf, count, offset);
   if (ch == -1)
     return -1;
 
-  while (!nbd_unlocked_aio_command_completed (conn, ch)) {
+  while ((r = nbd_unlocked_aio_command_completed (conn, ch)) == 0) {
     if (nbd_unlocked_poll (h, -1) == -1)
       return -1;
   }
 
-  return 0;
+  return r == -1 ? -1 : 0;
 }
 
 /* Issue a write command on any connection and wait for the reply.
@@ -56,17 +57,18 @@ nbd_unlocked_pwrite (struct nbd_handle *h, const void *buf,
 {
   struct nbd_connection *conn = h->conns[h->unique % h->multi_conn];
   int64_t ch;
+  int r;
 
   ch = nbd_unlocked_aio_pwrite (conn, buf, count, offset);
   if (ch == -1)
     return -1;
 
-  while (!nbd_unlocked_aio_command_completed (conn, ch)) {
+  while ((r = nbd_unlocked_aio_command_completed (conn, ch)) == 0) {
     if (nbd_poll (h, -1) == -1)
       return -1;
   }
 
-  return 0;
+  return r == -1 ? -1 : 0;
 }
 
 int64_t

commit 64c30d1f2b0c30183f91a8f966fd5625680339ba

aio: Don't duplicate strerror
Calling set_error(value,...) already appends strerror(value) to the
resulting string; no need to repeat it ourself.

Ultimately, we should fix our code to not use strerror(), as it is not
thread-safe. This patch does not do that (because using strerror_r()
is a portability nightmare and strerror_l() is not yet widespread);
but the fewer places where we use strerror(), the more confined that
future patch will be.

Fixes: 1d73554a

lib/aio.c
@@ -92,8 +92,8 @@ nbd_unlocked_aio_command_completed (struct nbd_handle *h,
     return 1;
 
   /* The command failed, set an error indication and return an error. */
-  set_error (error, "%s: command failed: %s",
-             nbd_internal_name_of_nbd_cmd (type), strerror (error));
+  set_error (error, "%s: command failed",
+             nbd_internal_name_of_nbd_cmd (type));
   return -1;
 }
 