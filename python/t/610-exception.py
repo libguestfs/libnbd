259d46cb9252fc4402fecae447d2d6e321fc2118
Richard W.M. Jones <rjones@redhat.com>
2019-06-28 10:01:00 +0100

    python: Raise a custom exception containing error string and errno.
    Previously errors caused a RuntimeException to be raised.  This commit
    defines a custom exception (nbd.Error) which has two parameters, the
    required error string, and the optional errno (which may be 0 if
    unavailable).
    
    For example:
    
    $ ./run nbdsh -c 'h.pread(0, 0)'
    Traceback (most recent call last):
      File "/usr/lib64/python3.7/runpy.py", line 193, in _run_module_as_main
        "__main__", mod_spec)
      File "/usr/lib64/python3.7/runpy.py", line 85, in _run_code
        exec(code, run_globals)
      File "/home/rjones/d/libnbd/python/nbd.py", line 1163, in <module>
        nbdsh.shell()
      File "/home/rjones/d/libnbd/python/nbdsh.py", line 62, in shell
        exec (c)
      File "<string>", line 1, in <module>
      File "/home/rjones/d/libnbd/python/nbd.py", line 483, in pread
        return libnbdmod.pread (self._o, count, offset, flags)
    nbd.Error: nbd_pread: invalid state: START: the handle must be connected and finished handshaking with the server: Transport endpoint is not connected (ENOTCONN)

---
+++ python/t/610-exception.py
@@ -0,0 +1,32 @@
+# libnbd Python bindings
+# Copyright (C) 2010-2019 Red Hat Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+import nbd
+
+h = nbd.NBD ()
+
+try:
+    # This will always throw an exception because the handle is not
+    # connected.
+    h.pread (0, 0)
+except nbd.Error as ex:
+    print ("string = %s" % ex.string)
+    print ("errno = %s" % ex.errno)
+    exit (0)
+
+# If we reach here then we didn't catch the exception above.
+exit (1)
