e1efb1fd2efbadfa0a2b53662f2757c728e119ab
Richard W.M. Jones <rjones@redhat.com>
2019-08-03 11:00:40 +0100

    Fix compilation with C++.
    We lacked the appropriate extern "C" section in the header, so
    compilation would fail if you were using C++.
    
    This also adds a simple regression test.

---
+++ tests/compile-cxx.cpp
@@ -0,0 +1,52 @@
+/* NBD client library in userspace
+ * Copyright (C) 2013-2019 Red Hat Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/* Test compilation with C++. */
+
+#ifndef __cplusplus
+#error "this test should be compiled with a C++ compiler"
+#endif
+
+#include <config.h>
+
+#include <iostream>
+#include <cstdlib>
+
+#include <libnbd.h>
+
+using namespace std;
+
+int
+main ()
+{
+  struct nbd_handle *nbd;
+
+  nbd = nbd_create ();
+  if (nbd == NULL) {
+    cerr << nbd_get_error () << endl;
+    exit (EXIT_FAILURE);
+  }
+
+  cout << nbd_get_package_name (nbd)
+       << " "
+       << nbd_get_version (nbd)
+       << endl;
+
+  nbd_close (nbd);
+  exit (EXIT_SUCCESS);
+}
