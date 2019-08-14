libguestfs/libnbd

commit 65165ca73d06153fe7fd95bdd02e6a1b7ec656ab

docs: Replace libnbd-api(3) with man pages for individual APIs.
These are generated with the exception of nbd_create(3) which is hand
written; and nbd_close(3), nbd_get_error(3) and nbd_get_errno(3) which
are links to nbd_create(3).

Note that unusually two generated files are checked into git,
docs/.gitignore and docs/Makefile.inc.  That is to make it easier to
autoreconf straight from a fresh git clone.

docs/Makefile.am
@@ -18,30 +18,43 @@
 include $(top_srcdir)/subdir-rules.mk
 
 generator_built = \
-	libnbd-api.pod \
+	.gitignore \
+	Makefile.inc \
 	$(NULL)
 
 EXTRA_DIST = \
 	$(generator_built) \
 	libnbd.pod \
+	nbd_create.pod \
+	nbd_close.3 \
+	nbd_get_error.3 \
+	nbd_get_errno.3 \
 	$(NULL)
 
 if HAVE_POD
 
 man_MANS = \
 	libnbd.3 \
-	libnbd-api.3 \
+	nbd_create.3 \
+	nbd_close.3 \
+	nbd_get_error.3 \
+	nbd_get_errno.3 \
 	$(NULL)
-CLEANFILES += $(man_MANS)
+CLEANFILES += \
+	libnbd.3 \
+	nbd_create.3 \
+	$(NULL)
 
 libnbd.3: libnbd.pod
 	$(PODWRAPPER) --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-libnbd-api.3: libnbd-api.pod
+nbd_create.3: nbd_create.pod
 	$(PODWRAPPER) --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
 endif HAVE_POD
+
+include $(srcdir)/Makefile.inc

commit 1a48cc87c9acf0fef39ac387f3edd6ae572416b4

docs: Rebuild podwrapper.pl automatically
Any rule that relies on $(PODWRAPPER) should in turn depend on
podwrapper.pl being up-to-date.

Without this fix, an incremental build where podwrapper.pl was
generated prior to our split to per-function pages would fail
with:

perl /home/eblake/libnbd-tmp/podwrapper.pl --allow-long-lines --section=3 --man nbd_set_debug.3 \
    --html ../html/nbd_set_debug.3.html \
    nbd_set_debug.pod
Unknown option: allow-long-lines
Usage:
...

because nothing in the subdirectory forced the refresh in the top
directory to pick up the newer podwrapper.pl.

Fixes: 65165ca7

docs/Makefile.am
@@ -45,12 +45,12 @@ CLEANFILES += \
 	nbd_create.3 \
 	$(NULL)
 
-libnbd.3: libnbd.pod
+libnbd.3: libnbd.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_create.3: nbd_create.pod
+nbd_create.3: nbd_create.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<

docs/Makefile.inc
@@ -255,377 +255,377 @@ man_MANS += \
 	nbd_supports_uri.3 \
 	$(NULL)
 
-nbd_set_debug.3: nbd_set_debug.pod
+nbd_set_debug.3: nbd_set_debug.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_get_debug.3: nbd_get_debug.pod
+nbd_get_debug.3: nbd_get_debug.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_set_debug_callback.3: nbd_set_debug_callback.pod
+nbd_set_debug_callback.3: nbd_set_debug_callback.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_clear_debug_callback.3: nbd_clear_debug_callback.pod
+nbd_clear_debug_callback.3: nbd_clear_debug_callback.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_set_handle_name.3: nbd_set_handle_name.pod
+nbd_set_handle_name.3: nbd_set_handle_name.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_get_handle_name.3: nbd_get_handle_name.pod
+nbd_get_handle_name.3: nbd_get_handle_name.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_set_export_name.3: nbd_set_export_name.pod
+nbd_set_export_name.3: nbd_set_export_name.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_get_export_name.3: nbd_get_export_name.pod
+nbd_get_export_name.3: nbd_get_export_name.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_set_tls.3: nbd_set_tls.pod
+nbd_set_tls.3: nbd_set_tls.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_get_tls.3: nbd_get_tls.pod
+nbd_get_tls.3: nbd_get_tls.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_set_tls_certificates.3: nbd_set_tls_certificates.pod
+nbd_set_tls_certificates.3: nbd_set_tls_certificates.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_set_tls_verify_peer.3: nbd_set_tls_verify_peer.pod
+nbd_set_tls_verify_peer.3: nbd_set_tls_verify_peer.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_get_tls_verify_peer.3: nbd_get_tls_verify_peer.pod
+nbd_get_tls_verify_peer.3: nbd_get_tls_verify_peer.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_set_tls_username.3: nbd_set_tls_username.pod
+nbd_set_tls_username.3: nbd_set_tls_username.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_get_tls_username.3: nbd_get_tls_username.pod
+nbd_get_tls_username.3: nbd_get_tls_username.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_set_tls_psk_file.3: nbd_set_tls_psk_file.pod
+nbd_set_tls_psk_file.3: nbd_set_tls_psk_file.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_add_meta_context.3: nbd_add_meta_context.pod
+nbd_add_meta_context.3: nbd_add_meta_context.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_connect_uri.3: nbd_connect_uri.pod
+nbd_connect_uri.3: nbd_connect_uri.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_connect_unix.3: nbd_connect_unix.pod
+nbd_connect_unix.3: nbd_connect_unix.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_connect_tcp.3: nbd_connect_tcp.pod
+nbd_connect_tcp.3: nbd_connect_tcp.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_connect_command.3: nbd_connect_command.pod
+nbd_connect_command.3: nbd_connect_command.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_is_read_only.3: nbd_is_read_only.pod
+nbd_is_read_only.3: nbd_is_read_only.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_can_flush.3: nbd_can_flush.pod
+nbd_can_flush.3: nbd_can_flush.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_can_fua.3: nbd_can_fua.pod
+nbd_can_fua.3: nbd_can_fua.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_is_rotational.3: nbd_is_rotational.pod
+nbd_is_rotational.3: nbd_is_rotational.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_can_trim.3: nbd_can_trim.pod
+nbd_can_trim.3: nbd_can_trim.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_can_zero.3: nbd_can_zero.pod
+nbd_can_zero.3: nbd_can_zero.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_can_df.3: nbd_can_df.pod
+nbd_can_df.3: nbd_can_df.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_can_multi_conn.3: nbd_can_multi_conn.pod
+nbd_can_multi_conn.3: nbd_can_multi_conn.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_can_cache.3: nbd_can_cache.pod
+nbd_can_cache.3: nbd_can_cache.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_can_meta_context.3: nbd_can_meta_context.pod
+nbd_can_meta_context.3: nbd_can_meta_context.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_get_size.3: nbd_get_size.pod
+nbd_get_size.3: nbd_get_size.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_pread.3: nbd_pread.pod
+nbd_pread.3: nbd_pread.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_pread_structured.3: nbd_pread_structured.pod
+nbd_pread_structured.3: nbd_pread_structured.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_pwrite.3: nbd_pwrite.pod
+nbd_pwrite.3: nbd_pwrite.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_shutdown.3: nbd_shutdown.pod
+nbd_shutdown.3: nbd_shutdown.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_flush.3: nbd_flush.pod
+nbd_flush.3: nbd_flush.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_trim.3: nbd_trim.pod
+nbd_trim.3: nbd_trim.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_cache.3: nbd_cache.pod
+nbd_cache.3: nbd_cache.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_zero.3: nbd_zero.pod
+nbd_zero.3: nbd_zero.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_block_status.3: nbd_block_status.pod
+nbd_block_status.3: nbd_block_status.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_poll.3: nbd_poll.pod
+nbd_poll.3: nbd_poll.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_connect.3: nbd_aio_connect.pod
+nbd_aio_connect.3: nbd_aio_connect.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_connect_uri.3: nbd_aio_connect_uri.pod
+nbd_aio_connect_uri.3: nbd_aio_connect_uri.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_connect_unix.3: nbd_aio_connect_unix.pod
+nbd_aio_connect_unix.3: nbd_aio_connect_unix.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_connect_tcp.3: nbd_aio_connect_tcp.pod
+nbd_aio_connect_tcp.3: nbd_aio_connect_tcp.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_connect_command.3: nbd_aio_connect_command.pod
+nbd_aio_connect_command.3: nbd_aio_connect_command.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_pread.3: nbd_aio_pread.pod
+nbd_aio_pread.3: nbd_aio_pread.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_pread_structured.3: nbd_aio_pread_structured.pod
+nbd_aio_pread_structured.3: nbd_aio_pread_structured.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_pwrite.3: nbd_aio_pwrite.pod
+nbd_aio_pwrite.3: nbd_aio_pwrite.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_disconnect.3: nbd_aio_disconnect.pod
+nbd_aio_disconnect.3: nbd_aio_disconnect.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_flush.3: nbd_aio_flush.pod
+nbd_aio_flush.3: nbd_aio_flush.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_trim.3: nbd_aio_trim.pod
+nbd_aio_trim.3: nbd_aio_trim.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_cache.3: nbd_aio_cache.pod
+nbd_aio_cache.3: nbd_aio_cache.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_zero.3: nbd_aio_zero.pod
+nbd_aio_zero.3: nbd_aio_zero.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_block_status.3: nbd_aio_block_status.pod
+nbd_aio_block_status.3: nbd_aio_block_status.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_get_fd.3: nbd_aio_get_fd.pod
+nbd_aio_get_fd.3: nbd_aio_get_fd.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_get_direction.3: nbd_aio_get_direction.pod
+nbd_aio_get_direction.3: nbd_aio_get_direction.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_notify_read.3: nbd_aio_notify_read.pod
+nbd_aio_notify_read.3: nbd_aio_notify_read.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_notify_write.3: nbd_aio_notify_write.pod
+nbd_aio_notify_write.3: nbd_aio_notify_write.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_is_created.3: nbd_aio_is_created.pod
+nbd_aio_is_created.3: nbd_aio_is_created.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_is_connecting.3: nbd_aio_is_connecting.pod
+nbd_aio_is_connecting.3: nbd_aio_is_connecting.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_is_ready.3: nbd_aio_is_ready.pod
+nbd_aio_is_ready.3: nbd_aio_is_ready.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_is_processing.3: nbd_aio_is_processing.pod
+nbd_aio_is_processing.3: nbd_aio_is_processing.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_is_dead.3: nbd_aio_is_dead.pod
+nbd_aio_is_dead.3: nbd_aio_is_dead.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_is_closed.3: nbd_aio_is_closed.pod
+nbd_aio_is_closed.3: nbd_aio_is_closed.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_command_completed.3: nbd_aio_command_completed.pod
+nbd_aio_command_completed.3: nbd_aio_command_completed.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_peek_command_completed.3: nbd_aio_peek_command_completed.pod
+nbd_aio_peek_command_completed.3: nbd_aio_peek_command_completed.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_aio_in_flight.3: nbd_aio_in_flight.pod
+nbd_aio_in_flight.3: nbd_aio_in_flight.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_connection_state.3: nbd_connection_state.pod
+nbd_connection_state.3: nbd_connection_state.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_get_package_name.3: nbd_get_package_name.pod
+nbd_get_package_name.3: nbd_get_package_name.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_get_version.3: nbd_get_version.pod
+nbd_get_version.3: nbd_get_version.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_kill_command.3: nbd_kill_command.pod
+nbd_kill_command.3: nbd_kill_command.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_supports_tls.3: nbd_supports_tls.pod
+nbd_supports_tls.3: nbd_supports_tls.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<
 
-nbd_supports_uri.3: nbd_supports_uri.pod
+nbd_supports_uri.3: nbd_supports_uri.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<

generator/generator
@@ -3778,7 +3778,7 @@ let generate_docs_Makefile_inc () =
 
   List.iter (
     fun (name, _) ->
-      pr "nbd_%s.3: nbd_%s.pod\n" name name;
+      pr "nbd_%s.3: nbd_%s.pod $(top_builddir)/podwrapper.pl\n" name name;
       pr "\t$(PODWRAPPER) --allow-long-lines --section=3 --man $@ \\\n";
       pr "\t    --html $(top_builddir)/html/$@.html \\\n";
       pr "\t    $<\n";

ocaml/Makefile.am
@@ -35,7 +35,7 @@ if HAVE_POD
 man_MANS = libnbd-ocaml.3
 CLEANFILES += $(man_MANS)
 
-libnbd-ocaml.3: libnbd-ocaml.pod
+libnbd-ocaml.3: libnbd-ocaml.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --section=3 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    $<

sh/Makefile.am
@@ -32,7 +32,7 @@ if HAVE_POD
 man_MANS = nbdsh.1
 CLEANFILES += $(man_MANS)
 
-nbdsh.1: nbdsh.pod
+nbdsh.1: nbdsh.pod $(top_builddir)/podwrapper.pl
 	$(PODWRAPPER) --section=1 --man $@ \
 	    --html $(top_builddir)/html/$@.html \
 	    --verbatim $(srcdir)/examples/hexdump.sh:__EXAMPLES_HEXDUMP__ \

subdir-rules.mk
@@ -28,3 +28,6 @@ if HAVE_OCAMLOPT
 %.cmx: $(srcdir)/%.ml
 	$(OCAMLFIND) ocamlopt $(OCAMLFLAGS) $(OCAMLPACKAGES) -c $< -o $@
 endif
+
+$(top_builddir)/podwrapper.pl: $(top_srcdir)/podwrapper.pl.in
+	$(MAKE) -C $(top_builddir) podwrapper.pl