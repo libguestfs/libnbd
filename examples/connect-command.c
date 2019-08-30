libguestfs/libnbd

commit a288c3df8fd105e107876038b6b6d7d976df2a14

docs: Include examples in generated man pages.
Allow examples to be included in generated man pages:

  "connect_unix", {
    default_call with
    ...
    example = Some "examples/fetch-first-sector.c";
  };

The examples come out of the examples/ subdirectory, and one advantage
of this is that they are compiled during the build process so we know
that they at least build correctly.

Example coverage is fairly poor at the moment but that can be improved
later.

examples/connect-command.c
@@ -1,0 +1,81 @@
+/* This example shows how to run an NBD server
+ * (nbdkit) as a subprocess of libnbd.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <libnbd.h>
+
+int
+main (int argc, char *argv[])
+{
+  struct nbd_handle *nbd;
+  char wbuf[512], rbuf[512];
+  size_t i;
+
+  /* Create the libnbd handle. */
+  nbd = nbd_create ();
+  if (nbd == NULL) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  /* Run nbdkit as a subprocess. */
+  char *args[] = {
+    "nbdkit",
+
+    /* You must use ‘-s’ (which tells nbdkit to serve
+     * a single connection on stdin/stdout).
+     */
+    "-s",
+
+    /* It is recommended to use ‘--exit-with-parent’
+     * to ensure nbdkit is always cleaned up even
+     * if the main program crashes.
+     */
+    "--exit-with-parent",
+
+    /* Use this to enable nbdkit debugging. */
+    "-v",
+
+    /* The nbdkit plugin name - this is a RAM disk. */
+    "memory", "1M",
+
+    /* Use NULL to terminate the arg list. */
+    NULL
+  };
+  if (nbd_connect_command (nbd, args) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  /* Write some random data to the first sector. */
+  for (i = 0; i < sizeof wbuf; ++i)
+    wbuf[i] = i % 13;
+  if (nbd_pwrite (nbd, wbuf, sizeof wbuf, 0, 0) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  /* Read the first sector back. */
+  if (nbd_pread (nbd, rbuf, sizeof rbuf, 0, 0) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  /* Close the libnbd handle. */
+  nbd_close (nbd);
+
+  /* What was read must be exactly the same as what
+   * was written.
+   */
+  if (memcmp (rbuf, wbuf, sizeof rbuf) != 0) {
+    fprintf (stderr, "FAILED: "
+             "read data did not match written data\n");
+    exit (EXIT_FAILURE);
+  }
+
+  exit (EXIT_SUCCESS);
+}

.gitignore
@@ -45,11 +45,14 @@ Makefile.in
 !/docs/nbd_create.pod
 !/docs/nbd_get_err??.3
 /examples/batched-read-write
+/examples/connect-command
+/examples/fetch-first-sector
+/examples/get-size
 /examples/glib-main-loop
-/examples/threaded-reads-and-writes
-/examples/simple-fetch-first-sector
-/examples/simple-reads-and-writes
+/examples/reads-and-writes
+/examples/server-flags
 /examples/strict-structured-reads
+/examples/threaded-reads-and-writes
 /generator/generator-cache.v1
 /generator/stamp-generator
 /html/*.?.html

examples/Makefile.am
@@ -21,10 +21,13 @@ EXTRA_DIST = LICENSE-FOR-EXAMPLES
 
 noinst_PROGRAMS = \
 	batched-read-write \
-	simple-fetch-first-sector \
-	simple-reads-and-writes \
-	threaded-reads-and-writes \
+	connect-command \
+	fetch-first-sector \
+	get-size \
+	reads-and-writes \
+	server-flags \
 	strict-structured-reads \
+	threaded-reads-and-writes \
 	$(NULL)
 
 if HAVE_GLIB
@@ -32,29 +35,68 @@ noinst_PROGRAMS += \
 	glib-main-loop
 endif
 
-simple_fetch_first_sector_SOURCES = \
-	simple-fetch-first-sector.c \
+connect_command_SOURCES = \
+	connect-command.c \
 	$(NULL)
-simple_fetch_first_sector_CPPFLAGS = \
+connect_command_CPPFLAGS = \
 	-I$(top_srcdir)/include \
 	$(NULL)
-simple_fetch_first_sector_CFLAGS = \
+connect_command_CFLAGS = \
 	$(WARNINGS_CFLAGS) \
 	$(NULL)
-simple_fetch_first_sector_LDADD = \
+connect_command_LDADD = \
 	$(top_builddir)/lib/libnbd.la \
 	$(NULL)
 
-simple_reads_and_writes_SOURCES = \
-	simple-reads-and-writes.c \
+fetch_first_sector_SOURCES = \
+	fetch-first-sector.c \
 	$(NULL)
-simple_reads_and_writes_CPPFLAGS = \
+fetch_first_sector_CPPFLAGS = \
 	-I$(top_srcdir)/include \
 	$(NULL)
-simple_reads_and_writes_CFLAGS = \
+fetch_first_sector_CFLAGS = \
 	$(WARNINGS_CFLAGS) \
 	$(NULL)
-simple_reads_and_writes_LDADD = \
+fetch_first_sector_LDADD = \
+	$(top_builddir)/lib/libnbd.la \
+	$(NULL)
+
+get_size_SOURCES = \
+	get-size.c \
+	$(NULL)
+get_size_CPPFLAGS = \
+	-I$(top_srcdir)/include \
+	$(NULL)
+get_size_CFLAGS = \
+	$(WARNINGS_CFLAGS) \
+	$(NULL)
+get_size_LDADD = \
+	$(top_builddir)/lib/libnbd.la \
+	$(NULL)
+
+reads_and_writes_SOURCES = \
+	reads-and-writes.c \
+	$(NULL)
+reads_and_writes_CPPFLAGS = \
+	-I$(top_srcdir)/include \
+	$(NULL)
+reads_and_writes_CFLAGS = \
+	$(WARNINGS_CFLAGS) \
+	$(NULL)
+reads_and_writes_LDADD = \
+	$(top_builddir)/lib/libnbd.la \
+	$(NULL)
+
+server_flags_SOURCES = \
+	server-flags.c \
+	$(NULL)
+server_flags_CPPFLAGS = \
+	-I$(top_srcdir)/include \
+	$(NULL)
+server_flags_CFLAGS = \
+	$(WARNINGS_CFLAGS) \
+	$(NULL)
+server_flags_LDADD = \
 	$(top_builddir)/lib/libnbd.la \
 	$(NULL)
 

examples/fetch-first-sector.c
@@ -1,0 +1,66 @@
+/* This example shows how to connect to an NBD server
+ * and fetch and print the first sector (usually the
+ * boot sector or partition table or filesystem
+ * superblock).
+ *
+ * You can test it with nbdkit like this:
+ *
+ * nbdkit -U - floppy . \
+ *   --run './fetch-first-sector $unixsocket'
+ *
+ * The nbdkit floppy plugin creates an MBR disk so the
+ * first sector is the partition table.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <libnbd.h>
+
+int
+main (int argc, char *argv[])
+{
+  struct nbd_handle *nbd;
+  char buf[512];
+  FILE *pp;
+
+  if (argc != 2) {
+    fprintf (stderr, "%s socket\n", argv[0]);
+    exit (EXIT_FAILURE);
+  }
+
+  /* Create the libnbd handle. */
+  nbd = nbd_create ();
+  if (nbd == NULL) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  /* Connect to the NBD server over a
+   * Unix domain socket.
+   */
+  if (nbd_connect_unix (nbd, argv[1]) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  /* Read the first sector synchronously. */
+  if (nbd_pread (nbd, buf, sizeof buf, 0, 0) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  /* Close the libnbd handle. */
+  nbd_close (nbd);
+
+  /* Print the first sector. */
+  pp = popen ("hexdump -C", "w");
+  if (pp == NULL) {
+    perror ("popen: hexdump");
+    exit (EXIT_FAILURE);
+  }
+  fwrite (buf, sizeof buf, 1, pp);
+  pclose (pp);
+
+  exit (EXIT_SUCCESS);
+}

examples/get-size.c
@@ -1,0 +1,56 @@
+/* This example shows how to connect to an NBD
+ * server and read the size of the disk.
+ *
+ * You can test it with nbdkit like this:
+ *
+ * nbdkit -U - memory 1M \
+ *   --run './get-size $unixsocket'
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+#include <libnbd.h>
+
+int
+main (int argc, char *argv[])
+{
+  struct nbd_handle *nbd;
+  int64_t size;
+
+  if (argc != 2) {
+    fprintf (stderr, "%s socket\n", argv[0]);
+    exit (EXIT_FAILURE);
+  }
+
+  /* Create the libnbd handle. */
+  nbd = nbd_create ();
+  if (nbd == NULL) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  /* Connect to the NBD server over a
+   * Unix domain socket.
+   */
+  if (nbd_connect_unix (nbd, argv[1]) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  /* Read the size in bytes and print it. */
+  size = nbd_get_size (nbd);
+  if (size == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  printf ("%s: size = %" PRIi64 " bytes\n",
+          argv[1], size);
+
+  /* Close the libnbd handle. */
+  nbd_close (nbd);
+
+  exit (EXIT_SUCCESS);
+}

examples/reads-and-writes.c
@@ -1,9 +1,12 @@
-/* Example usage with nbdkit:
+/* This example shows how to do synchronous reads
+ * and writes randomly over the first megabyte of an
+ * NBD server.  Note this will destroy any existing
+ * content on the NBD server.
  *
- * nbdkit -U - memory 1M --run './simple-reads-and-writes $unixsocket'
+ * To test it with nbdkit and a RAM disk:
  *
- * This will read and write randomly over the first megabyte of the
- * plugin using synchronous I/O.
+ * nbdkit -U - memory 1M \
+ *     --run './simple-reads-and-writes $unixsocket'
  */
 
 #include <stdio.h>
@@ -30,16 +33,24 @@ main (int argc, char *argv[])
     exit (EXIT_FAILURE);
   }
 
+  /* Create the libnbd handle. */
   nbd = nbd_create ();
   if (nbd == NULL) {
     fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);
   }
+
+  /* Connect to the NBD server over a
+   * Unix domain socket.
+   */
   if (nbd_connect_unix (nbd, argv[1]) == -1) {
     fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);
   }
 
+  /* Get the size of the disk and check
+   * it's large enough.
+   */
   exportsize = nbd_get_size (nbd);
   if (exportsize == -1) {
     fprintf (stderr, "%s\n", nbd_get_error ());
@@ -47,8 +58,11 @@ main (int argc, char *argv[])
   }
   assert (exportsize >= sizeof buf);
 
+  /* Check that the server is writable. */
   if (nbd_is_read_only (nbd) == 1) {
-    fprintf (stderr, "%s: error: this NBD export is read-only\n", argv[0]);
+    fprintf (stderr, "%s: "
+             "error: this NBD export is read-only\n",
+             argv[0]);
     exit (EXIT_FAILURE);
   }
 
@@ -59,7 +73,8 @@ main (int argc, char *argv[])
   for (i = 0; i < 1000; ++i) {
     offset = rand () % (exportsize - sizeof buf);
 
-    if (nbd_pwrite (nbd, buf, sizeof buf, offset, 0) == -1) {
+    if (nbd_pwrite (nbd, buf, sizeof buf,
+                    offset, 0) == -1) {
       fprintf (stderr, "%s\n", nbd_get_error ());
       exit (EXIT_FAILURE);
     }
@@ -68,18 +83,21 @@ main (int argc, char *argv[])
   /* 1000 reads and writes. */
   for (i = 0; i < 1000; ++i) {
     offset = rand () % (exportsize - sizeof buf);
-    if (nbd_pread (nbd, buf, sizeof buf, offset, 0) == -1) {
+    if (nbd_pread (nbd, buf, sizeof buf,
+                   offset, 0) == -1) {
       fprintf (stderr, "%s\n", nbd_get_error ());
       exit (EXIT_FAILURE);
     }
 
     offset = rand () % (exportsize - sizeof buf);
-    if (nbd_pwrite (nbd, buf, sizeof buf, offset, 0) == -1) {
+    if (nbd_pwrite (nbd, buf, sizeof buf,
+                    offset, 0) == -1) {
       fprintf (stderr, "%s\n", nbd_get_error ());
       exit (EXIT_FAILURE);
     }
   }
 
+  /* Sends a graceful shutdown to the server. */
   if (nbd_shutdown (nbd, 0) == -1) {
     fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);

examples/server-flags.c
@@ -1,0 +1,65 @@
+/* This example shows how to connect to an NBD
+ * server and print the export flags.
+ *
+ * You can test it with nbdkit like this:
+ *
+ * nbdkit -U - memory 1M \
+ *   --run './server-flags $unixsocket'
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <libnbd.h>
+
+int
+main (int argc, char *argv[])
+{
+  struct nbd_handle *nbd;
+  int flag;
+
+  if (argc != 2) {
+    fprintf (stderr, "%s socket\n", argv[0]);
+    exit (EXIT_FAILURE);
+  }
+
+  /* Create the libnbd handle. */
+  nbd = nbd_create ();
+  if (nbd == NULL) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  /* Connect to the NBD server over a
+   * Unix domain socket.
+   */
+  if (nbd_connect_unix (nbd, argv[1]) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  /* Read and print the flags. */
+#define PRINT_FLAG(flag_fn)                     \
+  flag = flag_fn (nbd);                         \
+  if (flag == -1) {                             \
+    fprintf (stderr, "%s\n", nbd_get_error ()); \
+    exit (EXIT_FAILURE);                        \
+  }                                             \
+  printf (#flag_fn " = %s\n",                   \
+          flag ? "true" : "false");
+
+  PRINT_FLAG (nbd_can_cache);
+  PRINT_FLAG (nbd_can_df);
+  PRINT_FLAG (nbd_can_flush);
+  PRINT_FLAG (nbd_can_fua);
+  PRINT_FLAG (nbd_can_multi_conn);
+  PRINT_FLAG (nbd_can_trim);
+  PRINT_FLAG (nbd_can_zero);
+  PRINT_FLAG (nbd_is_read_only);
+  PRINT_FLAG (nbd_is_rotational);
+
+  /* Close the libnbd handle. */
+  nbd_close (nbd);
+
+  exit (EXIT_SUCCESS);
+}

examples/simple-fetch-first-sector.c
@@ -1,58 +1,0 @@
-/* Example usage with nbdkit:
- *
- * nbdkit -U - pattern 1M --run './simple-fetch-first-sector $unixsocket sector'
- *
- * This will read the first sector (512 bytes) from the NBD server and
- * write it to the local file 'sector'.  nbdkit-pattern-plugin
- * contains a simple regular pattern which you can use to check the
- * data read is correct.
- */
-
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <libnbd.h>
-
-int
-main (int argc, char *argv[])
-{
-  struct nbd_handle *nbd;
-  char buf[512];
-  FILE *fp;
-
-  if (argc != 3) {
-    fprintf (stderr, "%s socket output\n", argv[0]);
-    exit (EXIT_FAILURE);
-  }
-
-  nbd = nbd_create ();
-  if (nbd == NULL) {
-    fprintf (stderr, "%s\n", nbd_get_error ());
-    exit (EXIT_FAILURE);
-  }
-  if (nbd_connect_unix (nbd, argv[1]) == -1) {
-    fprintf (stderr, "%s\n", nbd_get_error ());
-    exit (EXIT_FAILURE);
-  }
-
-  if (nbd_pread (nbd, buf, sizeof buf, 0, 0) == -1) {
-    fprintf (stderr, "%s\n", nbd_get_error ());
-    exit (EXIT_FAILURE);
-  }
-
-  if (nbd_shutdown (nbd, 0) == -1) {
-    fprintf (stderr, "%s\n", nbd_get_error ());
-    exit (EXIT_FAILURE);
-  }
-
-  fp = fopen (argv[2], "w");
-  if (fp == NULL) {
-    perror (argv[2]);
-    exit (EXIT_FAILURE);
-  }
-  fwrite (buf, sizeof buf, 1, fp);
-  fclose (fp);
-
-  nbd_close (nbd);
-  exit (EXIT_SUCCESS);
-}

commit fe01c809171d0778ab979ec448a6a69a47de1552

examples: Fix for nbdkit 1.12
Fedora 30 still ships an nbdkit that doesn't have the magic size=
parameter for the memory plugin.

Fixes: a288c3df

examples/connect-command.c
@@ -41,7 +41,7 @@ main (int argc, char *argv[])
     "-v",
 
     /* The nbdkit plugin name - this is a RAM disk. */
-    "memory", "1M",
+    "memory", "size=1M",
 
     /* Use NULL to terminate the arg list. */
     NULL