libguestfs/libnbd

commit 33f6d55891f2c00a5deac190954ea6a93a3e6894

generator: Split the generator script into separate files and compile it.
The generator was over 6000 lines of code.  Split it into separate
files to make it more manageable.  This also forces the interfaces
between parts of the generator to be properly documented (ie. the
*.mli files).

Instead of interpreting it as a big script, we compile it using the
OCaml bytecode compiler (ocamlc).  The option of continuing to
interpret the script and using #use for textual inclusion was
attempted but in the end did not work well
(https://sympa.inria.fr/sympa/arc/caml-list/2020-02/msg00051.html)

We now require the bytecode compiler instead of the OCaml interpreter.
The bytecode compiler is available on all platforms, which is better
than requiring the native OCaml compiler which is not available
everywhere.  In practical terms this means we require ocamlc (instead
of ocaml) to build from git.  In Fedora ocaml and ocamlc are packaged
together.  In Debian they are packaged separately (ocaml-interp vs
ocaml-nox).

This is refactoring.  The generated output does not change.

generator/API.mli
@@ -1,0 +1,134 @@
+(* nbd client library in userspace: the API
+ * Copyright (C) 2013-2020 Red Hat Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *)
+
+type call = {
+  args : arg list;         (** parameters (except handle) *)
+  optargs : optarg list;   (** optional parameters (not optional in C) *)
+  ret : ret;               (** return value *)
+  shortdesc : string;      (** short description *)
+  longdesc : string;       (** long description *)
+  example : string option; (** path to example code (under top_srcdir) *)
+  see_also : link list;    (** "SEE ALSO" section in man page *)
+  (** List of permitted states for making this call.  [[]] = Any state. *)
+  permitted_states : permitted_state list;
+  (** Most functions must take a lock.  The only known exceptions are:
+      - functions which return a constant (eg. [nbd_supports_uri])
+      - functions which {b only} read from the atomic
+        [get_public_state] and do nothing else with the handle. *)
+  is_locked : bool;
+  (** Most functions can call set_error.  For functions which are
+      {b guaranteed} never to do that we can save a bit of time by
+      setting this to false. *)
+  may_set_error : bool;
+  (** The first stable version that the symbol appeared in, for
+      example (1, 2) if the symbol was added in development cycle
+      1.1.x and thus the first stable version was 1.2.  This is
+      filled in by the generator, add new calls to the first_version
+      table instead. *)
+  mutable first_version : int * int;
+}
+and arg =
+| Bool of string           (** bool *)
+| BytesIn of string * string (** byte array + size passed in to the function *)
+| BytesOut of string * string(** byte array + size specified by caller,
+                                 written by the function *)
+| BytesPersistIn of string * string (** same as above, but buffer persists *)
+| BytesPersistOut of string * string
+| Closure of closure       (** function pointer + void *opaque *)
+| Enum of string * enum    (** enum/union type, int in C *)
+| Fd of string             (** file descriptor *)
+| Flags of string * flags  (** flags, uint32_t in C *)
+| Int of string            (** small int *)
+| Int64 of string          (** 64 bit signed int *)
+| Path of string           (** filename or path *)
+| SockAddrAndLen of string * string (** struct sockaddr * + socklen_t *)
+| String of string         (** string, cannot be NULL *)
+| StringList of string     (** argv-style NULL-terminated array of strings *)
+| UInt of string           (** small unsigned int *)
+| UInt32 of string         (** 32 bit unsigned int *)
+| UInt64 of string         (** 64 bit unsigned int *)
+and optarg =
+| OClosure of closure      (** optional closure *)
+| OFlags of string * flags (** optional flags, uint32_t in C *)
+and ret =
+| RBool                    (** return a boolean, or error *)
+| RStaticString            (** return a static string (must be located in
+                               .text section), NULL for error *)
+| RErr                     (** return 0 = ok, -1 = error *)
+| RFd                      (** return a file descriptor, or error *)
+| RInt                     (** return a small int, -1 = error *)
+| RInt64                   (** 64 bit int, -1 = error *)
+| RCookie                  (** 64 bit command cookie (>= 1), -1 = error *)
+| RString                  (** return a newly allocated string,
+                               caller frees, NULL for error *)
+| RUInt                    (** return a bitmask, no error possible *)
+and closure = {
+  cbname : string;         (** name of callback function *)
+  cbargs : cbarg list;     (** all closures return int for now *)
+}
+and cbarg =
+| CBArrayAndLen of arg * string (** array + number of entries *)
+| CBBytesIn of string * string (** like BytesIn *)
+| CBInt of string          (** like Int *)
+| CBInt64 of string        (** like Int64 *)
+| CBMutable of arg         (** mutable argument, eg. int* *)
+| CBString of string       (** like String *)
+| CBUInt of string         (** like UInt *)
+| CBUInt64 of string       (** like UInt64 *)
+and enum = {
+  enum_prefix : string;    (** prefix of each enum variant *)
+  enums : (string * int) list (** enum names and their values in C *)
+}
+and flags = {
+  flag_prefix : string;    (** prefix of each flag name *)
+  flags : (string * int) list (** flag names and their values in C *)
+}
+and permitted_state =
+| Created                  (** can be called in the START state *)
+| Connecting               (** can be called when connecting/handshaking *)
+| Connected                (** when connected and READY or processing, but
+                               not including CLOSED or DEAD *)
+| Closed | Dead            (** can be called when the handle is
+                               CLOSED or DEAD *)
+and link =
+| Link of string           (** link to L<nbd_PAGE(3)> *)
+| SectionLink of string    (** link to L<libnbd(3)/SECTION> *)
+| MainPageLink             (** link to L<libnbd(3)> *)
+| ExternalLink of string * int (** link to L<EXTERNAL(SECTION)> *)
+| URLLink of string        (** link to L<URL> *)
+
+val handle_calls : (string * call) list
+(** List of API calls.  The string is the name of the call. *)
+
+val first_version : (string * (int * int)) list
+(** The first stable version that the symbol appeared in, for
+    example (1, 2) if the symbol was added in development cycle
+    1.1.x and thus the first stable version was 1.2. *)
+
+val all_closures : closure list
+val all_enums : enum list
+val all_flags : flags list
+val constants : (string * int) list
+val metadata_namespaces : (string * (string * (string * int) list) list) list
+
+val extract_links : string -> link list
+(** Extract links from POD text. *)
+
+val pod_of_link : link -> string
+val verify_link : link -> unit
+val sort_uniq_links : link list -> link list

generator/C.mli
@@ -1,0 +1,33 @@
+(* nbd client library in userspace: generate the C API and documentation
+ * Copyright (C) 2013-2020 Red Hat Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *)
+
+val generate_lib_libnbd_syms : unit -> unit
+val generate_include_libnbd_h : unit -> unit
+val generate_lib_unlocked_h : unit -> unit
+val generate_lib_api_c : unit -> unit
+val generate_docs_Makefile_inc : unit -> unit
+val generate_docs_api_links_pod : unit -> unit
+val generate_docs_api_flag_links_pod : unit -> unit
+val generate_docs_nbd_pod : string -> API.call -> unit -> unit
+val print_arg_list : ?wrap:bool -> ?maxcol:int ->
+                     ?handle:bool -> ?types:bool ->
+                     API.arg list -> API.optarg list -> unit
+val print_cbarg_list : ?wrap:bool -> ?maxcol:int -> ?types:bool ->
+                       API.cbarg list -> unit
+val errcode_of_ret : API.ret -> string option
+val type_of_ret : API.ret -> string

generator/OCaml.mli
@@ -1,0 +1,21 @@
+(* nbd client library in userspace: generator
+ * Copyright (C) 2013-2020 Red Hat Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *)
+
+val generate_ocaml_nbd_mli : unit -> unit
+val generate_ocaml_nbd_ml : unit -> unit
+val generate_ocaml_nbd_c : unit -> unit

generator/Python.mli
@@ -1,0 +1,22 @@
+(* nbd client library in userspace: Python bindings
+ * Copyright (C) 2013-2020 Red Hat Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *)
+
+val generate_python_methods_h : unit -> unit
+val generate_python_libnbdmod_c : unit -> unit
+val generate_python_methods_c : unit -> unit
+val generate_python_nbd_py : unit -> unit

commit ad0d23b541ebc41842aca3be1b05b8ddd295f079

generator: Re-add emacs hints
When we split the monolithic executable generator into multiple
.ml files, we accidentally left generator.ml marked executable,
and lost the emacs tuareg-mode hint that makes editing the files
easier.

Fixes: 33f6d558

generator/API.ml
@@ -1,3 +1,4 @@
+(* hey emacs, this is OCaml code: -*- tuareg -*- *)
 (* nbd client library in userspace: the API
  * Copyright (C) 2013-2020 Red Hat Inc.
  *

generator/API.mli
@@ -1,3 +1,4 @@
+(* hey emacs, this is OCaml code: -*- tuareg -*- *)
 (* nbd client library in userspace: the API
  * Copyright (C) 2013-2020 Red Hat Inc.
  *

generator/C.ml
@@ -1,3 +1,4 @@
+(* hey emacs, this is OCaml code: -*- tuareg -*- *)
 (* nbd client library in userspace: generate the C API and documentation
  * Copyright (C) 2013-2020 Red Hat Inc.
  *

generator/C.mli
@@ -1,3 +1,4 @@
+(* hey emacs, this is OCaml code: -*- tuareg -*- *)
 (* nbd client library in userspace: generate the C API and documentation
  * Copyright (C) 2013-2020 Red Hat Inc.
  *

generator/OCaml.ml
@@ -1,3 +1,4 @@
+(* hey emacs, this is OCaml code: -*- tuareg -*- *)
 (* nbd client library in userspace: generator
  * Copyright (C) 2013-2020 Red Hat Inc.
  *

generator/OCaml.mli
@@ -1,3 +1,4 @@
+(* hey emacs, this is OCaml code: -*- tuareg -*- *)
 (* nbd client library in userspace: generator
  * Copyright (C) 2013-2020 Red Hat Inc.
  *

generator/Python.ml
@@ -1,3 +1,4 @@
+(* hey emacs, this is OCaml code: -*- tuareg -*- *)
 (* nbd client library in userspace: Python bindings
  * Copyright (C) 2013-2020 Red Hat Inc.
  *

generator/Python.mli
@@ -1,3 +1,4 @@
+(* hey emacs, this is OCaml code: -*- tuareg -*- *)
 (* nbd client library in userspace: Python bindings
  * Copyright (C) 2013-2020 Red Hat Inc.
  *

generator/generator.ml
@@ -1,3 +1,4 @@
+(* hey emacs, this is OCaml code: -*- tuareg -*- *)
 (* nbd client library in userspace: generator
  * Copyright (C) 2013-2020 Red Hat Inc.
  *

generator/state_machine.ml
@@ -1,3 +1,4 @@
+(* hey emacs, this is OCaml code: -*- tuareg -*- *)
 (* nbd client library in userspace: state machine definition
  * Copyright (C) 2013-2020 Red Hat Inc.
  *

generator/state_machine.mli
@@ -1,3 +1,4 @@
+(* hey emacs, this is OCaml code: -*- tuareg -*- *)
 (* nbd client library in userspace: state machine definition
  * Copyright (C) 2013-2020 Red Hat Inc.
  *

generator/state_machine_generator.ml
@@ -1,3 +1,4 @@
+(* hey emacs, this is OCaml code: -*- tuareg -*- *)
 (* nbd client library in userspace: generate the code for the state machine
  * Copyright (C) 2013-2020 Red Hat Inc.
  *

generator/state_machine_generator.mli
@@ -1,3 +1,4 @@
+(* hey emacs, this is OCaml code: -*- tuareg -*- *)
 (* nbd client library in userspace: generate the code for the state machine
  * Copyright (C) 2013-2020 Red Hat Inc.
  *

generator/utils.ml
@@ -1,3 +1,4 @@
+(* hey emacs, this is OCaml code: -*- tuareg -*- *)
 (* nbd client library in userspace: utilities
  * Copyright (C) 2013-2020 Red Hat Inc.
  *

generator/utils.mli
@@ -1,3 +1,4 @@
+(* hey emacs, this is OCaml code: -*- tuareg -*- *)
 (* nbd client library in userspace: utilities
  * Copyright (C) 2013-2020 Red Hat Inc.
  *