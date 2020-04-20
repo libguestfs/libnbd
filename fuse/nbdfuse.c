libguestfs/libnbd

commit 85405a7426d344b3a512b29288193ee5c52d6582

nbdfuse: New tool to present a network block device in a FUSE filesystem.
This program allows you to turn a network block device source into a
FUSE filesystem containing a virtual file:

  $ nbdkit memory 128M
  $ mkdir mp
  $ nbdfuse mp/ramdisk nbd://localhost &
  $ ls -l mp
  total 0
  -rw-rw-rw-. 1 rjones rjones 134217728 Oct 12 15:09 ramdisk
  $ dd if=/dev/urandom bs=1M count=128 of=mp/ramdisk conv=notrunc,nocreat
  128+0 records in
  128+0 records out
  134217728 bytes (134 MB, 128 MiB) copied, 3.10171 s, 43.3 MB/s
  $ fusermount -u mp

There are still some shortcomings, such as lack of zero and trim
support.  These are documented in the TODO file.

Now for some history:

In libguestfs which is where most of this code derives from we have a
program called ‘guestmount’ which is a FUSE interface to libguestfs:

  http://libguestfs.org/guestmount.1.html

Originally that was a standalone program like nbdfuse, but after some
time we realized that the ability to mount libguestfs under a
directory was generally useful to all guestfs API users and we created
new APIs for it.  guestmount has now become a thin wrapper around
those APIs.

This of course argues that we should do the same thing for libnbd.
But ...

(1) For NBD this is a little less useful than for libguestfs.

(2) We can always do this in future if we need to.

Most importantly:

(3) the libguestfs FUSE API turned out to have a problem - still
unresolved - with handling threads and SELinux and it may not be a
good idea to bake this into the libnbd API until that problem has been
solved.  For more details about (3), read this:

  https://bugzilla.redhat.com/show_bug.cgi?id=1060423#c2

fuse/nbdfuse.c
@@ -1,0 +1,600 @@
+/* NBD client library in userspace
+ * Copyright (C) 2013-2019 Red Hat Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/* FUSE support. */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include <getopt.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define FUSE_USE_VERSION 26
+
+#include <fuse.h>
+#include <fuse_lowlevel.h>
+
+#include <libnbd.h>
+
+#define MAX_REQUEST_SIZE (64 * 1024 * 1024)
+
+static struct nbd_handle *nbd;
+static bool readonly = false;
+static char *mountpoint, *filename;
+static const char *pidfile;
+static char *fuse_options;
+static struct fuse_chan *ch;
+static struct fuse *fuse;
+static struct timespec start_t;
+static uint64_t size;
+
+static int nbdfuse_getattr (const char *path, struct stat *stbuf);
+static int nbdfuse_readdir (const char *path, void *buf,
+                            fuse_fill_dir_t filler,
+                            off_t offset, struct fuse_file_info *fi);
+static int nbdfuse_open (const char *path, struct fuse_file_info *fi);
+static int nbdfuse_read (const char *path, char *buf,
+                         size_t count, off_t offset,
+                         struct fuse_file_info *fi);
+static int nbdfuse_write (const char *path, const char *buf,
+                          size_t count, off_t offset,
+                          struct fuse_file_info *fi);
+static int nbdfuse_fsync (const char *path, int datasync,
+                          struct fuse_file_info *fi);
+static int nbdfuse_release (const char *path, struct fuse_file_info *fi);
+
+static struct fuse_operations fuse_operations = {
+  .getattr           = nbdfuse_getattr,
+  .readdir           = nbdfuse_readdir,
+  .open              = nbdfuse_open,
+  .read              = nbdfuse_read,
+  .write             = nbdfuse_write,
+  .fsync             = nbdfuse_fsync,
+  .release           = nbdfuse_release,
+};
+
+static void __attribute__((noreturn))
+usage (FILE *fp, int exitcode)
+{
+  fprintf (fp,
+"\n"
+"Mount NBD URI as a file:\n"
+"\n"
+"    nbdfuse [-o FUSE-OPTION] [-P PIDFILE] [-r] MOUNTPOINT[/FILENAME] URI\n"
+"\n"
+"Other modes:\n"
+"\n"
+"    nbdfuse MOUNTPOINT[/FILENAME] --command CMD [ARGS ...]\n"
+"    nbdfuse MOUNTPOINT[/FILENAME] --socket-activation CMD [ARGS ...]\n"
+"    nbdfuse MOUNTPOINT[/FILENAME] --fd N\n"
+"    nbdfuse MOUNTPOINT[/FILENAME] --tcp HOST PORT\n"
+"    nbdfuse MOUNTPOINT[/FILENAME] --unix SOCKET\n"
+"\n"
+"Please read the nbdfuse(1) manual page for full usage.\n"
+"\n"
+);
+  exit (exitcode);
+}
+
+static void
+display_version (void)
+{
+  printf ("%s %s\n", PACKAGE_NAME, PACKAGE_VERSION);
+}
+
+static void
+fuse_help (const char *prog)
+{
+  static struct fuse_operations null_operations;
+  const char *tmp_argv[] = { prog, "--help", NULL };
+  fuse_main (2, (char **) tmp_argv, &null_operations, NULL);
+  exit (EXIT_SUCCESS);
+}
+
+static bool
+is_directory (const char *path)
+{
+  struct stat statbuf;
+
+  if (stat (path, &statbuf) == -1)
+    return false;
+  return S_ISDIR (statbuf.st_mode);
+}
+
+int
+main (int argc, char *argv[])
+{
+  enum {
+    MODE_URI,
+    MODE_COMMAND,
+    MODE_FD,
+    MODE_SOCKET_ACTIVATION,
+    MODE_TCP,
+    MODE_UNIX,
+  } mode = MODE_URI;
+  enum {
+    HELP_OPTION = CHAR_MAX + 1,
+    FUSE_HELP_OPTION,
+  };
+  /* Note the "+" means we stop processing as soon as we get to the
+   * first non-option argument (the mountpoint) and then we parse the
+   * rest of the command line without getopt.
+   */
+  const char *short_options = "+o:P:rV";
+  const struct option long_options[] = {
+    { "fuse-help",          no_argument,       NULL, FUSE_HELP_OPTION },
+    { "help",               no_argument,       NULL, HELP_OPTION },
+    { "pidfile",            required_argument, NULL, 'P' },
+    { "pid-file",           required_argument, NULL, 'P' },
+    { "readonly",           no_argument,       NULL, 'r' },
+    { "read-only",          no_argument,       NULL, 'r' },
+    { "version",            no_argument,       NULL, 'V' },
+
+    { NULL }
+  };
+  int c, fd, r;
+  int64_t ssize;
+  const char *s;
+  struct fuse_args fuse_args = FUSE_ARGS_INIT (0, NULL);
+  struct sigaction sa;
+  FILE *fp;
+
+  for (;;) {
+    c = getopt_long (argc, argv, short_options, long_options, NULL);
+    if (c == -1)
+      break;
+
+    switch (c) {
+    case HELP_OPTION:
+      usage (stdout, EXIT_SUCCESS);
+
+    case FUSE_HELP_OPTION:
+      fuse_help (argv[0]);
+      exit (EXIT_SUCCESS);
+
+    case 'o':
+      fuse_opt_add_opt_escaped (&fuse_options, optarg);
+      break;
+
+    case 'P':
+      pidfile = optarg;
+      break;
+
+    case 'r':
+      readonly = true;
+      break;
+
+    case 'V':
+      display_version ();
+      exit (EXIT_SUCCESS);
+
+    default:
+      usage (stderr, EXIT_FAILURE);
+    }
+  }
+
+  /* There must be at least 2 parameters (mountpoint and
+   * URI/--command/etc).
+   */
+  if (argc - optind < 2)
+    usage (stderr, EXIT_FAILURE);
+
+  /* Parse and check the mountpoint.  It might be MOUNTPOINT or
+   * MOUNTPOINT/FILENAME.  In either case MOUNTPOINT must be an
+   * existing directory.
+   */
+  s = argv[optind++];
+  if (is_directory (s)) {
+    mountpoint = strdup (s);
+    filename = strdup ("nbd");
+    if (mountpoint == NULL || filename == NULL) {
+    strdup_error:
+      perror ("strdup");
+      exit (EXIT_FAILURE);
+    }
+  }
+  else {
+    const char *p = strrchr (s, '/');
+
+    if (p == NULL) {
+    mp_error:
+      fprintf (stderr, "%s: %s: "
+               "mountpoint must be \"directory\" or \"directory/filename\"\n",
+               argv[0], s);
+      exit (EXIT_FAILURE);
+    }
+    mountpoint = strndup (s, p-s);
+    if (mountpoint == NULL) goto strdup_error;
+    if (! is_directory (mountpoint)) goto mp_error;
+    if (strlen (p+1) == 0) goto mp_error;
+    filename = strdup (p+1);
+    if (filename == NULL) goto strdup_error;
+  }
+
+  /* The next parameter is either a URI or a mode switch. */
+  if (strcmp (argv[optind], "--command") == 0 ||
+      strcmp (argv[optind], "--cmd") == 0) {
+    mode = MODE_COMMAND;
+    optind++;
+  }
+  else if (strcmp (argv[optind], "--socket-activation") == 0 ||
+           strcmp (argv[optind], "--systemd-socket-activation") == 0) {
+    mode = MODE_SOCKET_ACTIVATION;
+    optind++;
+  }
+  else if (strcmp (argv[optind], "--fd") == 0) {
+    mode = MODE_FD;
+    optind++;
+  }
+  else if (strcmp (argv[optind], "--tcp") == 0) {
+    mode = MODE_TCP;
+    optind++;
+  }
+  else if (strcmp (argv[optind], "--unix") == 0) {
+    mode = MODE_UNIX;
+    optind++;
+  }
+  /* This is undocumented, but allow either URI or --uri URI. */
+  else if (strcmp (argv[optind], "--uri") == 0) {
+    mode = MODE_URI;
+    optind++;
+  }
+  else if (argv[optind][0] == '-') {
+    fprintf (stderr, "%s: unknown mode: %s\n", argv[0], argv[optind]);
+    usage (stderr, EXIT_FAILURE);
+  }
+
+  /* Check there are enough parameters following given the mode. */
+  switch (mode) {
+  case MODE_URI:
+  case MODE_FD:
+  case MODE_UNIX:
+    if (argc - optind != 1)
+      usage (stderr, EXIT_FAILURE);
+    break;
+  case MODE_TCP:
+    if (argc - optind != 2)
+      usage (stderr, EXIT_FAILURE);
+    break;
+  case MODE_COMMAND:
+  case MODE_SOCKET_ACTIVATION:
+    if (argc - optind < 1)
+      usage (stderr, EXIT_FAILURE);
+    break;
+  }
+  /* At this point we know the command line is valid, and so can start
+   * opening FUSE and libnbd.
+   */
+
+  /* Create the libnbd handle. */
+  nbd = nbd_create ();
+  if (nbd == NULL) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  /* Connect to the NBD server synchronously. */
+  switch (mode) {
+  case MODE_URI:
+    if (nbd_connect_uri (nbd, argv[optind]) == -1) {
+      fprintf (stderr, "%s\n", nbd_get_error ());
+      exit (EXIT_FAILURE);
+    }
+    break;
+
+  case MODE_COMMAND:
+    if (nbd_connect_command (nbd, &argv[optind]) == -1) {
+      fprintf (stderr, "%s\n", nbd_get_error ());
+      exit (EXIT_FAILURE);
+    }
+    break;
+
+  case MODE_SOCKET_ACTIVATION:
+    if (nbd_connect_systemd_socket_activation (nbd, &argv[optind]) == -1) {
+      fprintf (stderr, "%s\n", nbd_get_error ());
+      exit (EXIT_FAILURE);
+    }
+    break;
+
+  case MODE_FD:
+    if (sscanf (argv[optind], "%d", &fd) != 1) {
+      fprintf (stderr, "%s: could not parse file descriptor: %s\n\n",
+               argv[0], argv[optind]);
+      exit (EXIT_FAILURE);
+    }
+    if (nbd_connect_socket (nbd, fd) == -1) {
+      fprintf (stderr, "%s\n", nbd_get_error ());
+      exit (EXIT_FAILURE);
+    }
+    break;
+
+  case MODE_TCP:
+    if (nbd_connect_tcp (nbd, argv[optind], argv[optind+1]) == -1) {
+      fprintf (stderr, "%s\n", nbd_get_error ());
+      exit (EXIT_FAILURE);
+    }
+    break;
+
+  case MODE_UNIX:
+    if (nbd_connect_unix (nbd, argv[optind]) == -1) {
+      fprintf (stderr, "%s\n", nbd_get_error ());
+      exit (EXIT_FAILURE);
+    }
+    break;
+  }
+
+  ssize = nbd_get_size (nbd);
+  if (ssize == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  size = (uint64_t) ssize;
+
+  /* This is just used to give an unchanging time when they stat in
+   * the mountpoint.
+   */
+  clock_gettime (CLOCK_REALTIME, &start_t);
+
+  /* Create the FUSE args. */
+  if (fuse_opt_add_arg (&fuse_args, argv[0]) == -1) {
+  fuse_opt_error:
+    perror ("fuse_opt_add_arg");
+    exit (EXIT_FAILURE);
+  }
+
+  if (fuse_options) {
+    if (fuse_opt_add_arg (&fuse_args, "-o") == -1 ||
+        fuse_opt_add_arg (&fuse_args, fuse_options) == -1)
+      goto fuse_opt_error;
+  }
+
+  /* Create the FUSE mountpoint. */
+  ch = fuse_mount (mountpoint, &fuse_args);
+  if (ch == NULL) {
+    fprintf (stderr,
+             "%s: fuse_mount failed: see error messages above", argv[0]);
+    exit (EXIT_FAILURE);
+  }
+
+  /* Set F_CLOEXEC on the channel.  Some versions of libfuse don't do
+   * this.
+   */
+  fd = fuse_chan_fd (ch);
+  if (fd >= 0) {
+    int flags = fcntl (fd, F_GETFD, 0);
+    if (flags >= 0)
+      fcntl (fd, F_SETFD, flags & ~FD_CLOEXEC);
+  }
+
+  /* Create the FUSE handle. */
+  fuse = fuse_new (ch, &fuse_args,
+                   &fuse_operations, sizeof fuse_operations, NULL);
+  if (!fuse) {
+    perror ("fuse_new");
+    exit (EXIT_FAILURE);
+  }
+  fuse_opt_free_args (&fuse_args);
+
+  /* Catch signals since they can leave the mountpoint in a funny
+   * state.  To exit the program callers must use ‘fusermount -u’.  We
+   * also must be careful not to call exit(2) in this program until we
+   * have unmounted the filesystem below.
+   */
+  memset (&sa, 0, sizeof sa);
+  sa.sa_handler = SIG_IGN;
+  sa.sa_flags = SA_RESTART;
+  sigaction (SIGPIPE, &sa, NULL);
+  sigaction (SIGINT, &sa, NULL);
+  sigaction (SIGQUIT, &sa, NULL);
+
+  /* Ready to serve, write pidfile. */
+  if (pidfile) {
+    fp = fopen (pidfile, "w");
+    if (fp) {
+      fprintf (fp, "%ld", (long) getpid ());
+      fclose (fp);
+    }
+  }
+
+  /* Enter the main loop. */
+  r = fuse_loop (fuse);
+  if (r != 0)
+    perror ("fuse_loop");
+
+  /* Close FUSE. */
+  fuse_unmount (mountpoint, ch);
+  fuse_destroy (fuse);
+
+  /* Close NBD handle. */
+  nbd_close (nbd);
+
+  free (mountpoint);
+  free (filename);
+  free (fuse_options);
+
+  exit (r == 0 ? EXIT_SUCCESS : EXIT_FAILURE);
+}
+
+/* Wraps calls to libnbd functions and automatically checks for a
+ * returns errors in the format required by FUSE.  It also prints out
+ * the full error message on stderr, so that we don't lose it.
+ */
+#define CHECK_NBD_ERROR(CALL)                                   \
+  do { if ((CALL) == -1) return check_nbd_error (); } while (0)
+static int
+check_nbd_error (void)
+{
+  int err;
+
+  fprintf (stderr, "%s\n", nbd_get_error ());
+  err = nbd_get_errno ();
+  if (err != 0)
+    return -err;
+  else
+    return -EIO;
+}
+
+static int
+nbdfuse_getattr (const char *path, struct stat *statbuf)
+{
+  const int mode = readonly ? 0444 : 0666;
+
+  memset (statbuf, 0, sizeof (struct stat));
+
+  /* We're probably making some Linux-specific assumptions here, but
+   * this file is not compiled on non-Linux systems.
+   */
+  statbuf->st_atim = start_t;
+  statbuf->st_mtim = start_t;
+  statbuf->st_ctim = start_t;
+  statbuf->st_uid = geteuid ();
+  statbuf->st_gid = getegid ();
+
+  if (strcmp (path, "/") == 0) {
+    /* getattr "/" */
+    statbuf->st_mode = S_IFDIR | (mode & 0111);
+    statbuf->st_nlink = 2;
+  }
+  else if (path[0] == '/' && strcmp (path+1, filename) == 0) {
+    /* getattr "/filename" */
+    statbuf->st_mode = S_IFREG | mode;
+    statbuf->st_nlink = 1;
+    statbuf->st_size = size;
+  }
+  else
+    return -ENOENT;
+
+  return 0;
+}
+
+static int
+nbdfuse_readdir (const char *path, void *buf,
+                 fuse_fill_dir_t filler,
+                 off_t offset, struct fuse_file_info *fi)
+{
+  if (strcmp (path, "/") != 0)
+    return -ENOENT;
+
+  filler (buf, ".", NULL, 0);
+  filler (buf, "..", NULL, 0);
+  filler (buf, filename, NULL, 0);
+
+  return 0;
+}
+
+/* This function checks the O_RDONLY/O_RDWR flags passed to the
+ * open(2) call, so we have to check the open mode is compatible with
+ * the readonly flag.
+ */
+static int
+nbdfuse_open (const char *path, struct fuse_file_info *fi)
+{
+  if (path[0] != '/' || strcmp (path+1, filename) != 0)
+    return -ENOENT;
+
+  if (readonly && (fi->flags & O_ACCMODE) != O_RDONLY)
+    return -EACCES;
+
+  return 0;
+}
+
+static int
+nbdfuse_read (const char *path, char *buf,
+              size_t count, off_t offset,
+              struct fuse_file_info *fi)
+{
+  if (path[0] != '/' || strcmp (path+1, filename) != 0)
+    return -ENOENT;
+
+  if (offset >= size)
+    return 0;
+
+  if (count > MAX_REQUEST_SIZE)
+    count = MAX_REQUEST_SIZE;
+
+  if (offset + count > size)
+    count = size - offset;
+
+  CHECK_NBD_ERROR (nbd_pread (nbd, buf, count, offset, 0));
+
+  return (int) count;
+}
+
+static int
+nbdfuse_write (const char *path, const char *buf,
+               size_t count, off_t offset,
+               struct fuse_file_info *fi)
+{
+  /* Probably shouldn't happen because of nbdfuse_open check. */
+  if (readonly)
+    return -EACCES;
+
+  if (path[0] != '/' || strcmp (path+1, filename) != 0)
+    return -ENOENT;
+
+  if (offset >= size)
+    return 0;
+
+  if (count > MAX_REQUEST_SIZE)
+    count = MAX_REQUEST_SIZE;
+
+  if (offset + count > size)
+    count = size - offset;
+
+  CHECK_NBD_ERROR (nbd_pwrite (nbd, buf, count, offset, 0));
+
+  return (int) count;
+}
+
+static int
+nbdfuse_fsync (const char *path, int datasync, struct fuse_file_info *fi)
+{
+  if (readonly)
+    return 0;
+
+  /* If the server doesn't support flush then the operation is
+   * silently ignored.
+   */
+  if (nbd_can_flush (nbd))
+    CHECK_NBD_ERROR (nbd_flush (nbd, 0));
+
+  return 0;
+}
+
+/* This is called on the last close of a file.  We do a flush here to
+ * be on the safe side, but it's not strictly necessary.
+ */
+static int
+nbdfuse_release (const char *path, struct fuse_file_info *fi)
+{
+  if (readonly)
+    return 0;
+
+  return nbdfuse_fsync (path, 0, fi);
+}

.gitignore
@@ -57,6 +57,8 @@ Makefile.in
 /examples/server-flags
 /examples/strict-structured-reads
 /examples/threaded-reads-and-writes
+/fuse/nbdfuse
+/fuse/nbdfuse.1
 /fuzzing/libnbd-fuzz-wrapper
 /fuzzing/sync_dir/
 /generator/generator-cache.v1

Makefile.am
@@ -39,6 +39,7 @@ SUBDIRS = \
 	tests \
 	python \
 	sh \
+	fuse \
 	ocaml \
 	ocaml/examples \
 	ocaml/tests \
@@ -64,7 +65,7 @@ maintainer-check-extra-dist:
 	@echo PASS: EXTRA_DIST tests
 
 check-valgrind: all
-	@for d in tests ocaml/tests interop; do \
+	@for d in tests fuse ocaml/tests interop; do \
 	    $(MAKE) -C $$d check-valgrind || exit 1; \
 	done
 

README
@@ -82,6 +82,8 @@ Optional:
 
  * Python >= 3.3 to build the Python 3 bindings and NBD shell (nbdsh).
 
+ * FUSE to build the nbdfuse program.
+
 Optional, only needed to run the test suite:
 
  * nbdkit >= 1.12, the nbdkit basic plugins and the nbdkit basic

TODO
@@ -27,6 +27,13 @@ Should we ship a "nbdcp" copying tool?
  - Could upload, download or copy between servers.
  - Duplicates functionality already available in qemu-img convert.
 
+nbdfuse:
+ - If you write beyond the end of the virtual file, it returns EIO.
+ - Implement trim/discard.
+ - Implement write_zeroes.
+ - Implement block_status.
+ - Could be made multithreaded for improved performance.
+
 Suggested API improvements:
   general:
   - synchronous APIs that have a timeout or can be cancelled

configure.ac
@@ -200,6 +200,23 @@ PKG_CHECK_MODULES([GLIB], [glib-2.0], [
 ])
 AM_CONDITIONAL([HAVE_GLIB], [test "x$GLIB_LIBS" != "x"])
 
+dnl FUSE is optional to build the FUSE module.
+AC_ARG_ENABLE([fuse],
+    AS_HELP_STRING([--disable-fuse], [disable FUSE (guestmount) support]),
+    [],
+    [enable_fuse=yes])
+AS_IF([test "x$enable_fuse" != "xno"],[
+    PKG_CHECK_MODULES([FUSE],[fuse],[
+        AC_SUBST([FUSE_CFLAGS])
+        AC_SUBST([FUSE_LIBS])
+        AC_DEFINE([HAVE_FUSE],[1],[Define to 1 if you have FUSE.])
+    ],[
+        enable_fuse=no
+        AC_MSG_WARN([FUSE library and headers are missing, so optional FUSE module won't be built])
+    ])
+])
+AM_CONDITIONAL([HAVE_FUSE],[test "x$enable_fuse" != "xno"])
+
 dnl Check we have enough to run podwrapper.
 AC_CHECK_PROG([PERL],[perl],[perl],[no])
 AS_IF([test "x$PERL" != "xno"],[
@@ -353,6 +370,7 @@ AC_CONFIG_FILES([Makefile
                  common/include/Makefile
                  docs/Makefile
                  examples/Makefile
+                 fuse/Makefile
                  fuzzing/Makefile
                  generator/Makefile
                  include/Makefile
@@ -393,6 +411,8 @@ feature "TLS support ............................ " \
         test "x$HAVE_GNUTLS_TRUE" = "x"
 feature "NBD URI support ........................ " \
         test "x$LIBXML2_LIBS" != "x"
+feature "FUSE support ........................... " \
+        test "x$HAVE_FUSE_TRUE" = "x"
 feature "Manual pages ........................... " \
         test "x$HAVE_POD_TRUE" = "x"
 

docs/libnbd.pod
@@ -840,6 +840,7 @@ L<https://github.com/NetworkBlockDevice/nbd/blob/master/doc/uri.md>.
 
 L<libnbd-security(3)>,
 L<nbdsh(1)>,
+L<nbdfuse(1)>,
 L<qemu(1)>.
 
 =head1 AUTHORS

fuse/Makefile.am
@@ -1,0 +1,60 @@
+# nbd client library in userspace
+# Copyright (C) 2013-2019 Red Hat Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+include $(top_srcdir)/subdir-rules.mk
+
+EXTRA_DIST = \
+	nbdfuse.pod \
+	test-nbdkit.sh \
+	test-qcow2.sh \
+	$(NULL)
+
+TESTS_ENVIRONMENT = LIBNBD_DEBUG=1
+LOG_COMPILER = $(top_builddir)/run
+TESTS =
+
+if HAVE_FUSE
+
+bin_PROGRAMS = nbdfuse
+
+nbdfuse_SOURCES = nbdfuse.c
+nbdfuse_CPPFLAGS = -I$(top_srcdir)/include
+nbdfuse_CFLAGS = $(WARNINGS_CFLAGS) $(FUSE_CFLAGS)
+nbdfuse_LDADD = $(top_builddir)/lib/libnbd.la $(FUSE_LIBS)
+
+if HAVE_POD
+
+man_MANS = \
+	nbdfuse.1 \
+	$(NULL)
+
+nbdfuse.1: nbdfuse.pod $(top_builddir)/podwrapper.pl
+	$(PODWRAPPER) --section=1 --man $@ \
+	    --html $(top_builddir)/html/$@.html \
+	    $<
+
+endif HAVE_POD
+
+TESTS += \
+	test-nbdkit.sh \
+	test-qcow2.sh \
+	$(NULL)
+
+check-valgrind:
+	LIBNBD_VALGRIND=1 $(MAKE) check
+
+endif HAVE_FUSE

commit 6eaf4ad49c54231905a33970e23e9b447ed4eebf

nbdfuse: Fix setting of FD_CLOEXEC
The comment mentions setting cloexec, but the code cleared it instead.
Blame bad copy-and-paste ;)

Fixes: 85405a74

fuse/nbdfuse.c
@@ -1,5 +1,5 @@
 /* NBD client library in userspace
- * Copyright (C) 2013-2019 Red Hat Inc.
+ * Copyright (C) 2013-2020 Red Hat Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -440,14 +440,14 @@ main (int argc, char *argv[])
     exit (EXIT_FAILURE);
   }
 
-  /* Set F_CLOEXEC on the channel.  Some versions of libfuse don't do
+  /* Set FD_CLOEXEC on the channel.  Some versions of libfuse don't do
    * this.
    */
   fd = fuse_chan_fd (ch);
   if (fd >= 0) {
     int flags = fcntl (fd, F_GETFD, 0);
     if (flags >= 0)
-      fcntl (fd, F_SETFD, flags & ~FD_CLOEXEC);
+      fcntl (fd, F_SETFD, flags | FD_CLOEXEC);
   }
 
   /* Create the FUSE handle. */