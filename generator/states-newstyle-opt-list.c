libguestfs/libnbd

commit c2851cf516c6680688bf580e2d4e9b7ce5a56d33

Add APIs for listing exports from an NBD server.
A major missing feature of this library was the ability to list
exports from an NBD server.  This implements the feature by adding a
new handle mode and additional functions for querying the list of
export names.

generator/states-newstyle-opt-list.c
@@ -1,0 +1,141 @@
+/* nbd client library in userspace: state machine
+ * Copyright (C) 2013-2020 Red Hat Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/* State machine for sending NBD_OPT_LIST to list exports.
+ *
+ * This is skipped unless list exports mode was enabled on the handle.
+ */
+
+STATE_MACHINE {
+ NEWSTYLE.OPT_LIST.START:
+  if (!h->list_exports) {
+    SET_NEXT_STATE (%^OPT_STRUCTURED_REPLY.START);
+    return 0;
+  }
+
+  h->sbuf.option.version = htobe64 (NBD_NEW_VERSION);
+  h->sbuf.option.option = htobe32 (NBD_OPT_LIST);
+  h->sbuf.option.optlen = 0;
+  h->wbuf = &h->sbuf;
+  h->wlen = sizeof (h->sbuf.option);
+  SET_NEXT_STATE (%SEND);
+  return 0;
+
+ NEWSTYLE.OPT_LIST.SEND:
+  switch (send_from_wbuf (h)) {
+  case -1: SET_NEXT_STATE (%.DEAD); return 0;
+  case 0:
+    h->rbuf = &h->sbuf;
+    h->rlen = sizeof (h->sbuf.or.option_reply);
+    SET_NEXT_STATE (%RECV_REPLY);
+  }
+  return 0;
+
+ NEWSTYLE.OPT_LIST.RECV_REPLY:
+  switch (recv_into_rbuf (h)) {
+  case -1: SET_NEXT_STATE (%.DEAD); return 0;
+  case 0:
+    if (prepare_for_reply_payload (h, NBD_OPT_LIST) == -1) {
+      SET_NEXT_STATE (%.DEAD);
+      return 0;
+    }
+    SET_NEXT_STATE (%RECV_REPLY_PAYLOAD);
+  }
+  return 0;
+
+ NEWSTYLE.OPT_LIST.RECV_REPLY_PAYLOAD:
+  switch (recv_into_rbuf (h)) {
+  case -1: SET_NEXT_STATE (%.DEAD); return 0;
+  case 0:  SET_NEXT_STATE (%CHECK_REPLY);
+  }
+  return 0;
+
+ NEWSTYLE.OPT_LIST.CHECK_REPLY:
+  const size_t maxpayload = sizeof h->sbuf.or.payload.server;
+  uint32_t reply;
+  uint32_t len;
+  uint32_t elen;
+  char *name;
+  char **new_exports;
+
+  reply = be32toh (h->sbuf.or.option_reply.reply);
+  len = be32toh (h->sbuf.or.option_reply.replylen);
+  switch (reply) {
+  case NBD_REP_SERVER:
+    /* Got one export. */
+    if (len > maxpayload)
+      debug (h, "skipping too large export name reply");
+    else {
+      elen = be32toh (h->sbuf.or.payload.server.server.export_name_len);
+      if (elen > len - 4) {
+        set_error (0, "invalid export length");
+        SET_NEXT_STATE (%.DEAD);
+        return 0;
+      }
+      /* Copy the export name to the handle list. */
+      name = strndup (h->sbuf.or.payload.server.str, elen);
+      if (name == NULL) {
+        set_error (errno, "strdup");
+        SET_NEXT_STATE (%.DEAD);
+        return 0;
+      }
+      new_exports = realloc (h->exports, sizeof (char *) * (h->nr_exports+1));
+      if (new_exports == NULL) {
+        set_error (errno, "strdup");
+        SET_NEXT_STATE (%.DEAD);
+        free (name);
+        return 0;
+      }
+      h->exports = new_exports;
+      h->exports[h->nr_exports++] = name;
+    }
+
+    /* Just limit this so we don't receive unlimited amounts
+     * of data from the server.  Note each export name can be
+     * up to 4K.
+     */
+    if (h->nr_exports > 10000) {
+      set_error (0, "too many export names sent by the server");
+      SET_NEXT_STATE (%.DEAD);
+      return 0;
+    }
+
+    /* Wait for more replies. */
+    h->rbuf = &h->sbuf;
+    h->rlen = sizeof (h->sbuf.or.option_reply);
+    SET_NEXT_STATE (%RECV_REPLY);
+    return 0;
+
+  case NBD_REP_ACK:
+    /* Finished receiving the list. */
+    SET_NEXT_STATE (%^OPT_STRUCTURED_REPLY.START);
+    return 0;
+
+  default:
+    if (handle_reply_error (h) == -1) {
+      SET_NEXT_STATE (%.DEAD);
+      return 0;
+    }
+    set_error (ENOTSUP, "unexpected response, possibly the server does not "
+               "support listing exports");
+    SET_NEXT_STATE (%.DEAD);
+    return 0;
+  }
+  return 0;
+
+} /* END STATE MACHINE */

lib/internal.h
@@ -93,6 +93,11 @@ struct nbd_handle {
   int uri_allow_tls;
   bool uri_allow_local_file;
 
+  /* List exports mode. */
+  bool list_exports;
+  size_t nr_exports;
+  char **exports;
+
   /* Global flags from the server. */
   uint16_t gflags;
 
@@ -158,6 +163,10 @@ struct nbd_handle {
     struct {
       struct nbd_fixed_new_option_reply option_reply;
       union {
+        struct {
+          struct nbd_fixed_new_option_reply_server server;
+          char str[NBD_MAX_STRING];
+        } __attribute__((packed)) server;
         struct nbd_fixed_new_option_reply_info_export export;
         struct {
           struct nbd_fixed_new_option_reply_meta_context context;

.gitignore
@@ -59,6 +59,7 @@ Makefile.in
 /examples/fetch-first-sector
 /examples/get-size
 /examples/glib-main-loop
+/examples/list-exports
 /examples/open-qcow2
 /examples/reads-and-writes
 /examples/server-flags
@@ -93,6 +94,8 @@ Makefile.in
 /interop/interop-qemu-nbd
 /interop/interop-qemu-nbd-tls-certs
 /interop/interop-qemu-nbd-tls-psk
+/interop/list-exports-nbd-server
+/interop/list-exports-qemu-nbd
 /interop/socket-activation-nbdkit
 /interop/socket-activation-qemu-nbd
 /interop/structured-read

examples/Makefile.am
@@ -26,6 +26,7 @@ noinst_PROGRAMS = \
 	encryption \
 	fetch-first-sector \
 	get-size \
+	list-exports \
 	open-qcow2 \
 	reads-and-writes \
 	server-flags \
@@ -100,6 +101,19 @@ get_size_CFLAGS = \
 	$(WARNINGS_CFLAGS) \
 	$(NULL)
 get_size_LDADD = \
+	$(top_builddir)/lib/libnbd.la \
+	$(NULL)
+
+list_exports_SOURCES = \
+	list-exports.c \
+	$(NULL)
+list_exports_CPPFLAGS = \
+	-I$(top_srcdir)/include \
+	$(NULL)
+list_exports_CFLAGS = \
+	$(WARNINGS_CFLAGS) \
+	$(NULL)
+list_exports_LDADD = \
 	$(top_builddir)/lib/libnbd.la \
 	$(NULL)
 

examples/list-exports.c
@@ -1,0 +1,108 @@
+/* This example shows how to list NBD exports.
+ *
+ * To test this with qemu-nbd:
+ *   $ qemu-nbd -x "hello" -t -k /tmp/sock disk.img
+ *   $ ./run examples/list-exports /tmp/sock
+ *   [0] hello
+ *   Which export to connect to? 0
+ *   Connecting to hello ...
+ *   /tmp/sock: hello: size = 2048 bytes
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <errno.h>
+
+#include <libnbd.h>
+
+int
+main (int argc, char *argv[])
+{
+  struct nbd_handle *nbd, *nbd2;
+  int r, i;
+  char *name;
+  int64_t size;
+
+  if (argc != 2) {
+    fprintf (stderr, "%s socket\n", argv[0]);
+    exit (EXIT_FAILURE);
+  }
+
+  /* Create the libnbd handle for querying exports. */
+  nbd = nbd_create ();
+  if (nbd == NULL) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  /* Set the list exports mode in the handle. */
+  nbd_set_list_exports (nbd, true);
+
+  /* Connect to the NBD server over a
+   * Unix domain socket.  A side effect of
+   * connecting is to list the exports.
+   * This operation can fail normally, so
+   * we need to check the return value and
+   * error code.
+   */
+  r = nbd_connect_unix (nbd, argv[1]);
+  if (r == -1 && nbd_get_errno () == ENOTSUP) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  if (nbd_get_nr_list_exports (nbd) == 0) {
+    fprintf (stderr, "Server does not support "
+             "listing exports.\n");
+    exit (EXIT_FAILURE);
+  }
+
+  /* Display the list of exports. */
+  for (i = 0;
+       i < nbd_get_nr_list_exports (nbd);
+       i++) {
+    name = nbd_get_list_export_name (nbd, i);
+    printf ("[%d] %s\n", i, name);
+    free (name);
+  }
+  printf ("Which export to connect to? ");
+  if (scanf ("%d", &i) != 1) exit (EXIT_FAILURE);
+  name = nbd_get_list_export_name (nbd, i);
+  printf ("Connecting to %s ...\n", name);
+  nbd_close (nbd);
+
+  /* Connect again to the chosen export. */
+  nbd2 = nbd_create ();
+  if (nbd2 == NULL) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  if (nbd_set_export_name (nbd2, name) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  if (nbd_connect_unix (nbd2, argv[1]) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  /* Read the size in bytes and print it. */
+  size = nbd_get_size (nbd2);
+  if (size == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  printf ("%s: %s: size = %" PRIi64 " bytes\n",
+          argv[1], name, size);
+
+  /* Close the libnbd handle. */
+  nbd_close (nbd2);
+
+  free (name);
+
+  exit (EXIT_SUCCESS);
+}

generator/API.ml
@@ -588,6 +588,81 @@ the time of compilation.";
                 Link "aio_is_created"; Link "aio_is_ready"];
   };
 
+  "set_list_exports", {
+    default_call with
+    args = [Bool "list"]; ret = RErr;
+    permitted_states = [ Created ];
+    shortdesc = "set whether to list server exports";
+    longdesc = "\
+Set this flag to true in order to list NBD exports provided
+by the server.
+
+In this mode, during connection we query the server for the list
+of NBD exports and collect them in the handle.  You can query
+the list of exports provided by the server by calling
+C<nbd_get_nr_exports> and C<nbd_get_export_name>.  After choosing
+the export you want, you should close this handle, create a new
+NBD handle (C<nbd_create>), set the export name (C<nbd_set_export_name>),
+and connect on the new handle.
+
+Some servers do not support listing exports at all.  In
+that case the connect call will fail with errno C<ENOTSUP>
+and C<nbd_get_nr_exports> will return 0.
+
+Some servers do not respond with all the exports they
+support, either because of an incomplete implementation of
+this feature, or because they only list exports relevant
+to non-TLS or TLS when a corresponding non-TLS or TLS
+connection is opened.
+
+For L<qemu-nbd(8)> when using the I<-x> option you may find
+that the original connect call fails with
+C<server has no export named ''> and errno C<ENOENT>.
+However you can still proceed to call C<nbd_get_nr_exports> etc.
+
+For L<nbd-server(1)> you will need to allow clients to make
+list requests by adding C<allowlist=true> to the C<[generic]>
+section of F</etc/nbd-server/config>.";
+    example = Some "examples/list-exports.c";
+    see_also = [Link "get_list_exports";
+                Link "get_nr_list_exports"; Link "get_list_export_name"];
+  };
+
+  "get_list_exports", {
+    default_call with
+    args = []; ret = RBool;
+    may_set_error = false;
+    shortdesc = "return whether list exports mode was enabled";
+    longdesc = "\
+Return true if list exports mode was enabled on this handle.";
+    see_also = [Link "set_list_exports"];
+  };
+
+  "get_nr_list_exports", {
+    default_call with
+    args = []; ret = RInt;
+    permitted_states = [ Closed; Dead ];
+    shortdesc = "return the number of exports returned by the server";
+    longdesc = "\
+If list exports mode was enabled on the handle and you connected
+to the server, this returns the number of exports returned by the
+server.  This may be 0 or incomplete for reasons given in
+C<nbd_set_list_exports>.";
+    see_also = [Link "set_list_exports"];
+  };
+
+  "get_list_export_name", {
+    default_call with
+    args = [ Int "i" ]; ret = RString;
+    permitted_states = [ Closed; Dead ];
+    shortdesc = "return the i'th export name";
+    longdesc = "\
+If list exports mode was enabled on the handle and you connected
+to the server, this can be used to return the i'th export name
+from the list returned by the server.";
+    see_also = [Link "set_list_exports"];
+  };
+
   "add_meta_context", {
     default_call with
     args = [ String "name" ]; ret = RErr;
@@ -2196,6 +2271,12 @@ let first_version = [
   "set_uri_allow_transports", (1, 2);
   "set_uri_allow_tls", (1, 2);
   "set_uri_allow_local_file", (1, 2);
+
+  (* Added in 1.3.x development cycle, will be stable and supported in 1.4. *)
+  "set_list_exports", (1, 4);
+  "get_list_exports", (1, 4);
+  "get_nr_list_exports", (1, 4);
+  "get_list_export_name", (1, 4);
 
   (* These calls are proposed for a future version of libnbd, but
    * have not been added to any released version so far.

generator/Makefile.am
@@ -30,6 +30,7 @@ states_code = \
 	states-issue-command.c \
 	states-magic.c \
 	states-newstyle-opt-export-name.c \
+	states-newstyle-opt-list.c \
 	states-newstyle-opt-go.c \
 	states-newstyle-opt-set-meta-context.c \
 	states-newstyle-opt-starttls.c \

generator/state_machine.ml
@@ -273,6 +273,7 @@ and newstyle_state_machine = [
    * state needs to run and skip to the next state in the list if not.
    *)
   Group ("OPT_STARTTLS", newstyle_opt_starttls_state_machine);
+  Group ("OPT_LIST", newstyle_opt_list_state_machine);
   Group ("OPT_STRUCTURED_REPLY", newstyle_opt_structured_reply_state_machine);
   Group ("OPT_SET_META_CONTEXT", newstyle_opt_set_meta_context_state_machine);
   Group ("OPT_GO", newstyle_opt_go_state_machine);
@@ -338,6 +339,44 @@ and newstyle_opt_starttls_state_machine = [
     name = "TLS_HANDSHAKE_WRITE";
     comment = "TLS handshake (writing)";
     external_events = [ NotifyWrite, "" ];
+  };
+]
+
+(* Fixed newstyle NBD_OPT_LIST option. *)
+and newstyle_opt_list_state_machine = [
+  State {
+    default_state with
+    name = "START";
+    comment = "Start listing exports if in list mode.";
+    external_events = [];
+  };
+
+  State {
+    default_state with
+    name = "SEND";
+    comment = "Send newstyle NBD_OPT_LIST to begin listing exports";
+    external_events = [ NotifyWrite, "" ];
+  };
+
+  State {
+    default_state with
+    name = "RECV_REPLY";
+    comment = "Receive NBD_REP_SERVER reply";
+    external_events = [ NotifyRead, "" ];
+  };
+
+  State {
+    default_state with
+    name = "RECV_REPLY_PAYLOAD";
+    comment = "Receive NBD_REP_SERVER reply payload";
+    external_events = [ NotifyRead, "" ];
+  };
+
+  State {
+    default_state with
+    name = "CHECK_REPLY";
+    comment = "Check NBD_REP_SERVER reply";
+    external_events = [];
   };
 ]
 

commit 3f7663e7f9bd77fff6a74538e92145c1bd9231c9

info: Allow for servers with max name and description
Although the code is currently only storing a name, we need to allow
for a server that sends a max-length name and a max-length description
in a single NBD_REP_SERVER.  Fortunately, we were not causing a buffer
overflow, but merely ignoring valid data from the server.

Reproducer:
$ long=$(printf %04096d 1)
$ qemu-nbd -D $long -x $long file
$ ./run nbdinfo -L

Fixes: c2851cf516

generator/states-newstyle-opt-list.c
@@ -82,7 +82,7 @@ STATE_MACHINE {
       debug (h, "skipping too large export name reply");
     else {
       elen = be32toh (h->sbuf.or.payload.server.server.export_name_len);
-      if (elen > len - 4) {
+      if (elen > len - 4 || elen > NBD_MAX_STRING) {
         set_error (0, "invalid export length");
         SET_NEXT_STATE (%.DEAD);
         return 0;

lib/internal.h
@@ -170,7 +170,7 @@ struct nbd_handle {
       union {
         struct {
           struct nbd_fixed_new_option_reply_server server;
-          char str[NBD_MAX_STRING];
+          char str[NBD_MAX_STRING * 2]; /* name and description */
         } __attribute__((packed)) server;
         struct nbd_fixed_new_option_reply_info_export export;
         struct nbd_fixed_new_option_reply_info_block_size block_size;