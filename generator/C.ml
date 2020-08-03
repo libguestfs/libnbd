libguestfs/libnbd

commit 0b7bdf62c87f39dc4551e01057a023bc50e7deb4

generator: Improve trace messages.
Improve function trace output so that:
 - Long strings are truncated.
 - Strings with non-printable characters are escaped.
 - Buffers are partially printed.
 - Lists of strings are printed.

lib/utils.c
@@ -167,3 +167,109 @@ nbd_internal_fork_safe_perror (const char *s)
    */
   errno = err;
 }
+
+/* nbd_internal_printable_* functions are used by the API code to
+ * print debug messages when we trace calls in and out of libnbd.  The
+ * calls should attempt to convert the parameter into something
+ * printable.
+ *
+ * They cannot fail, but it's OK if they return NULL.
+ *
+ * Caller frees the result.
+ */
+
+char *
+nbd_internal_printable_buffer (const void *buf, size_t count)
+{
+  char *s = NULL;
+  size_t len = 0, truncated;
+  FILE *fp;
+
+  fp = open_memstream (&s, &len);
+  if (fp == NULL)
+    return NULL;
+
+  /* If the buffer is very long, truncate it to 1 sector. */
+  if (count > 512) {
+    truncated = count - 512;
+    count = 512;
+  }
+  else
+    truncated = 0;
+
+  fprintf (fp, "\n");
+  nbd_internal_hexdump (buf, count, fp);
+
+  if (truncated)
+    fprintf (fp, "[... %zu more bytes truncated ...]\n", truncated);
+  fclose (fp);
+
+  return s;
+}
+
+static void
+printable_string (const char *str, FILE *fp)
+{
+  size_t i, n, truncated;
+
+  n = strlen (str);
+  if (n > 512) {
+    truncated = n - 512;
+    n = 512;
+  }
+  else
+    truncated = 0;
+
+  fprintf (fp, "\"");
+  for (i = 0; i < n; ++i) {
+    if (isprint (str[i]))
+      fputc (str[i], fp);
+    else
+      fprintf (fp, "\\x%02x", str[i]);
+  }
+
+  if (truncated)
+    fprintf (fp, "[... %zu more bytes truncated ...]", truncated);
+  fprintf (fp, "\"");
+}
+
+char *
+nbd_internal_printable_string (const char *str)
+{
+  char *s = NULL;
+  size_t len = 0;
+  FILE *fp;
+
+  fp = open_memstream (&s, &len);
+  if (fp == NULL)
+    return NULL;
+
+  printable_string (str, fp);
+  fclose (fp);
+
+  return s;
+}
+
+char *
+nbd_internal_printable_string_list (char **list)
+{
+  char *s = NULL;
+  size_t len = 0, i;
+  FILE *fp;
+
+  fp = open_memstream (&s, &len);
+  if (fp == NULL)
+    return NULL;
+
+  fprintf (fp, "[");
+  for (i = 0; list[i] != NULL; ++i) {
+    if (i > 0)
+      fprintf (fp, ", ");
+    printable_string (list[i], fp);
+  }
+  fprintf (fp, "]");
+  fclose (fp);
+
+  return s;
+
+}

TODO
@@ -22,12 +22,6 @@ Performance: Chart it over various buffer sizes and threads, as that
 
 Examine other fuzzers: https://gitlab.com/akihe/radamsa
 
-Improve function trace output so that:
- - Long strings are truncated.
- - Strings with non-printable characters are escaped.
- - Buffers are partially printed.
- - Lists of strings are printed.
-
 Should we ship a "nbdcp" copying tool?
  - Could upload, download or copy between servers.
  - Duplicates functionality already available in qemu-img convert.

generator/generator
@@ -4608,14 +4608,33 @@ let generate_lib_api_c () =
 
   (* Print the trace when we enter a call with debugging enabled. *)
   and print_trace_enter args optargs =
-    pr "  debug (h, \"enter:";
+    pr "  if_debug (h) {\n";
+    List.iter (
+      function
+      | BytesIn (n, count)
+      | BytesPersistIn (n, count) ->
+         pr "    char *%s_printable =\n" n;
+         pr "        nbd_internal_printable_buffer (%s, %s);\n" n count
+      | Path n
+      | String n ->
+         pr "    char *%s_printable =\n" n;
+         pr "        nbd_internal_printable_string (%s);\n" n
+      | StringList n ->
+         pr "    char *%s_printable =\n" n;
+         pr "        nbd_internal_printable_string_list (%s);\n" n
+      | BytesOut _ | BytesPersistOut _
+      | Bool _ | Closure _ | Enum _ | Flags _ | Fd _ | Int _
+      | Int64 _ | SockAddrAndLen _ | UInt _ | UInt32 _ | UInt64 _ -> ()
+    ) args;
+    pr "    debug (h, \"enter:";
     List.iter (
       function
       | Bool n -> pr " %s=%%s" n
-      | BytesIn (n, count)
-      | BytesPersistIn (n, count)
       | BytesOut (n, count)
       | BytesPersistOut (n, count) -> pr " %s=<buf> %s=%%zu" n count
+      | BytesIn (n, count)
+      | BytesPersistIn (n, count) ->
+         pr " %s=\\\"%%s\\\" %s=%%zu" n count
       | Closure { cbname } -> pr " %s=<fun>" cbname
       | Enum (n, _) -> pr " %s=%%d" n
       | Flags (n, _) -> pr " %s=0x%%x" n
@@ -4623,8 +4642,8 @@ let generate_lib_api_c () =
       | Int64 n -> pr " %s=%%\" PRIi64 \"" n
       | SockAddrAndLen (n, len) -> pr " %s=<sockaddr> %s=%%d" n len
       | Path n
-      | String n -> pr " %s=\\\"%%s\\\"" n
-      | StringList n -> pr " %s=<list>" n
+      | String n -> pr " %s=%%s" n
+      | StringList n -> pr " %s=%%s" n
       | UInt n -> pr " %s=%%u" n
       | UInt32 n -> pr " %s=%%\" PRIu32 \"" n
       | UInt64 n -> pr " %s=%%\" PRIu64 \"" n
@@ -4638,18 +4657,21 @@ let generate_lib_api_c () =
     List.iter (
       function
       | Bool n -> pr ", %s ? \"true\" : \"false\"" n
-      | BytesIn (_, count)
-      | BytesPersistIn (_, count)
-      | BytesOut (_, count)
-      | BytesPersistOut (_, count) -> pr ", %s" count
+      | BytesOut (n, count)
+      | BytesPersistOut (n, count) -> pr ", %s" count
+      | BytesIn (n, count)
+      | BytesPersistIn (n, count) ->
+         pr ", %s_printable ? %s_printable : \"\", %s" n n count
       | Closure { cbname } -> ()
       | Enum (n, _) -> pr ", %s" n
       | Flags (n, _) -> pr ", %s" n
       | Fd n | Int n -> pr ", %s" n
       | Int64 n -> pr ", %s" n
       | SockAddrAndLen (_, len) -> pr ", (int) %s" len
-      | Path n | String n -> pr ", %s ? %s : \"NULL\"" n n
-      | StringList n -> ()
+      | Path n | String n ->
+         pr ", %s ? %s_printable ? %s_printable : \"\" : \"NULL\"" n n n
+      | StringList n ->
+         pr ", %s_printable ? %s_printable : \"\"" n n
       | UInt n | UInt32 n | UInt64 n -> pr ", %s" n
     ) args;
     List.iter (
@@ -4658,7 +4680,20 @@ let generate_lib_api_c () =
          pr ", CALLBACK_IS_NULL (%s_callback) ? \"<fun>\" : \"NULL\"" cbname
       | OFlags (n, _) -> pr ", %s" n
     ) optargs;
-    pr ");\n"
+    pr ");\n";
+    List.iter (
+      function
+      | BytesIn (n, _)
+      | BytesPersistIn (n, _)
+      | Path n
+      | String n
+      | StringList n ->
+         pr "    free (%s_printable);\n" n
+      | BytesOut _ | BytesPersistOut _
+      | Bool _ | Closure _ | Enum _ | Flags _ | Fd _ | Int _
+      | Int64 _ | SockAddrAndLen _ | UInt _ | UInt32 _ | UInt64 _ -> ()
+    ) args;
+    pr "  }\n"
   (* Print the trace when we leave a call with debugging enabled. *)
   and print_trace_leave ret =
     pr "  if_debug (h) {\n";
@@ -4670,15 +4705,31 @@ let generate_lib_api_c () =
         pr "    if (0)\n";
     );
     pr "      debug (h, \"leave: error=\\\"%%s\\\"\", nbd_get_error ());\n";
-    pr "    else\n";
-    pr "      debug (h, \"leave: ret=";
+    pr "    else {\n";
     (match ret with
-     | RBool | RErr | RFd | RInt -> pr "%%d"
-     | RInt64 | RCookie -> pr "%%\" PRIi64 \""
-     | RStaticString | RString -> pr "\\\"%%s\\\""
-     | RUInt -> pr "%%u"
+     | RStaticString | RString ->
+        pr "      char *ret_printable =\n";
+        pr "          nbd_internal_printable_string (ret);\n"
+     | RBool | RErr | RFd | RInt
+     | RInt64 | RCookie
+     | RUInt -> ()
     );
-    pr "\", ret);\n";
+    pr "      debug (h, \"leave: ret=\" ";
+    (match ret with
+     | RBool | RErr | RFd | RInt -> pr "\"%%d\", ret"
+     | RInt64 | RCookie -> pr "\"%%\" PRIi64 \"\", ret"
+     | RStaticString | RString ->
+        pr "\"\\\"%%s\\\"\", ret_printable ? ret_printable : \"\""
+     | RUInt -> pr "\"%%u\", ret"
+    );
+    pr ");\n";
+    (match ret with
+     | RStaticString | RString -> pr "      free (ret_printable);\n"
+     | RBool | RErr | RFd | RInt
+     | RInt64 | RCookie
+     | RUInt -> ()
+    );
+    pr "    }\n";
     pr "  }\n"
   in
 

lib/internal.h
@@ -409,5 +409,8 @@ extern char **nbd_internal_copy_string_list (char **argv);
 extern void nbd_internal_free_string_list (char **argv);
 extern const char *nbd_internal_fork_safe_itoa (long v, char *buf, size_t len);
 extern void nbd_internal_fork_safe_perror (const char *s);
+extern char *nbd_internal_printable_buffer (const void *buf, size_t count);
+extern char *nbd_internal_printable_string (const char *str);
+extern char *nbd_internal_printable_string_list (char **list);
 
 #endif /* LIBNBD_INTERNAL_H */

commit c3a267927ea63c8ad4c732e0c08f123b4ded5a83

generator: Fix string debugging
Two bugs both introduced at the same time, related to our debug
tracing code:

Although the generator was outputting code to special-case NULL input
even where it was not permitted (such as nbd_set_export_name), that
code occurred after we called nbd_internal_printable_string which
crashed on a strlen(NULL) call.  Once internal printing handles NULL,
the generated code can be simplified.

String return values were showing up in traces as ret = ""xyz"",
because the generator was adding quotes on top of the quotes added by
nbd_internal_printable_string.

Fixes: 0b7bdf62c87

generator/C.ml
@@ -620,9 +620,7 @@ let generate_lib_api_c () =
       | Fd n | Int n -> pr ", %s" n
       | Int64 n -> pr ", %s" n
       | SockAddrAndLen (_, len) -> pr ", (int) %s" len
-      | Path n | String n ->
-         pr ", %s ? %s_printable ? %s_printable : \"\" : \"NULL\"" n n n
-      | StringList n ->
+      | Path n | String n | StringList n ->
          pr ", %s_printable ? %s_printable : \"\"" n n
       | UInt n | UInt32 n | UInt64 n -> pr ", %s" n
     ) args;
@@ -651,7 +649,7 @@ let generate_lib_api_c () =
     pr "  if_debug (h) {\n";
     let errcode = errcode_of_ret ret in
     (match errcode with
-     | Some r -> 
+     | Some r ->
         pr "    if (ret == %s)\n" r;
      | None ->
         pr "    if (0)\n";
@@ -671,7 +669,7 @@ let generate_lib_api_c () =
      | RBool | RErr | RFd | RInt -> pr "\"%%d\", ret"
      | RInt64 | RCookie -> pr "\"%%\" PRIi64 \"\", ret"
      | RStaticString | RString ->
-        pr "\"\\\"%%s\\\"\", ret_printable ? ret_printable : \"\""
+        pr "\"%%s\", ret_printable ? ret_printable : \"\""
      | RUInt -> pr "\"%%u\", ret"
     );
     pr ");\n";

lib/utils.c
@@ -1,5 +1,5 @@
 /* NBD client library in userspace
- * Copyright (C) 2013-2019 Red Hat Inc.
+ * Copyright (C) 2013-2020 Red Hat Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -218,6 +218,11 @@ static void
 printable_string (const char *str, FILE *fp)
 {
   size_t i, n, truncated;
+
+  if (str == NULL) {
+    fprintf (fp, "NULL");
+    return;
+  }
 
   n = strlen (str);
   if (n > 512) {