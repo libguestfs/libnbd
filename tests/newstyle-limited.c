libguestfs/libnbd

commit 8b8ed26c710202a3f98d50df955e4d31bdd13cc6

states: Add NBD_OPT_EXPORT_NAME handling
Used both for servers that are not fixed newstyle, and for servers
that don't understand NBD_OPT_GO.

generator/states-newstyle-opt-export-name.c
@@ -1,0 +1,73 @@
+/* nbd client library in userspace: state machine
+ * Copyright (C) 2013-2019 Red Hat Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/* State machine for ending newstyle handshake with NBD_OPT_EXPORT_NAME. */
+
+/* STATE MACHINE */ {
+ NEWSTYLE.OPT_EXPORT_NAME.START:
+  conn->sbuf.option.version = htobe64 (NBD_NEW_VERSION);
+  conn->sbuf.option.option = htobe32 (NBD_OPT_EXPORT_NAME);
+  conn->sbuf.option.optlen = strlen (h->export_name);
+  conn->wbuf = &conn->sbuf;
+  conn->wlen = sizeof conn->sbuf.option;
+  SET_NEXT_STATE (%SEND);
+  return 0;
+
+ NEWSTYLE.OPT_EXPORT_NAME.SEND:
+  switch (send_from_wbuf (conn)) {
+  case -1: SET_NEXT_STATE (%.DEAD); return -1;
+  case 0:
+    conn->wbuf = h->export_name;
+    conn->wlen = strlen (h->export_name);
+    SET_NEXT_STATE (%SEND_EXPORT);
+  }
+  return 0;
+
+ NEWSTYLE.OPT_EXPORT_NAME.SEND_EXPORT:
+  switch (send_from_wbuf (conn)) {
+  case -1: SET_NEXT_STATE (%.DEAD); return -1;
+  case 0:
+    conn->rbuf = &conn->sbuf;
+    conn->rlen = sizeof conn->sbuf.export_name_reply;
+    if ((conn->gflags & NBD_FLAG_NO_ZEROES) != 0)
+      conn->rlen -= sizeof conn->sbuf.export_name_reply.zeroes;
+    SET_NEXT_STATE (%RECV_REPLY);
+  }
+  return 0;
+
+ NEWSTYLE.OPT_EXPORT_NAME.RECV_REPLY:
+  switch (recv_into_rbuf (conn)) {
+  case -1: SET_NEXT_STATE (%.DEAD); return -1;
+  case 0:  SET_NEXT_STATE (%CHECK_REPLY);
+  }
+  return 0;
+
+ NEWSTYLE.OPT_EXPORT_NAME.CHECK_REPLY:
+  conn->h->exportsize = be64toh (conn->sbuf.export_name_reply.exportsize);
+  conn->h->eflags = be16toh (conn->sbuf.export_name_reply.eflags);
+  debug (conn->h, "exportsize: %" PRIu64 " eflags: 0x%" PRIx16,
+	 conn->h->exportsize, conn->h->eflags);
+  if (conn->h->eflags == 0) {
+    SET_NEXT_STATE (%.DEAD);
+    set_error (EINVAL, "handshake: invalid eflags == 0 from server");
+    return -1;
+  }
+  SET_NEXT_STATE (%.READY);
+  return 0;
+
+} /* END STATE MACHINE */

generator/Makefile.am
@@ -26,6 +26,7 @@ states_code = \
 	states-connect.c \
 	states-issue-command.c \
 	states-magic.c \
+	states-newstyle-opt-export-name.c \
 	states-newstyle-opt-go.c \
 	states-newstyle-opt-set-meta-context.c \
 	states-newstyle-opt-starttls.c \

generator/generator
@@ -330,6 +330,7 @@ and newstyle_state_machine = [
   Group ("OPT_STRUCTURED_REPLY", newstyle_opt_structured_reply_state_machine);
   Group ("OPT_SET_META_CONTEXT", newstyle_opt_set_meta_context_state_machine);
   Group ("OPT_GO", newstyle_opt_go_state_machine);
+  Group ("OPT_EXPORT_NAME", newstyle_opt_export_name_state_machine);
 ]
 
 (* Fixed newstyle NBD_OPT_STARTTLS option. *)
@@ -564,6 +565,44 @@ and newstyle_opt_go_state_machine = [
     default_state with
     name = "CHECK_REPLY";
     comment = "Check newstyle NBD_OPT_GO reply";
+    external_events = [];
+  };
+]
+
+(* Newstyle NBD_OPT_EXPORT_NAME option. *)
+and newstyle_opt_export_name_state_machine = [
+  State {
+    default_state with
+    name = "START";
+    comment = "Try to send newstyle NBD_OPT_EXPORT_NAME to end handshake";
+    external_events = [];
+  };
+
+  State {
+    default_state with
+    name = "SEND";
+    comment = "Send newstyle NBD_OPT_EXPORT_NAME to end handshake";
+    external_events = [ NotifyWrite, "" ];
+  };
+
+  State {
+    default_state with
+    name = "SEND_EXPORT";
+    comment = "Send newstyle NBD_OPT_EXPORT_NAME export name";
+    external_events = [ NotifyWrite, "" ];
+  };
+
+  State {
+    default_state with
+    name = "RECV_REPLY";
+    comment = "Receive newstyle NBD_OPT_EXPORT_NAME reply";
+    external_events = [ NotifyRead, "" ];
+  };
+
+  State {
+    default_state with
+    name = "CHECK_REPLY";
+    comment = "Check newstyle NBD_OPT_EXPORT_NAME reply";
     external_events = [];
   };
 ]

generator/states-newstyle-opt-go.c
@@ -149,10 +149,10 @@
     SET_NEXT_STATE (%RECV_REPLY);
     return 0;
   case NBD_REP_ERR_UNSUP:
-    /* XXX fall back to NBD_OPT_EXPORT_NAME */
-    SET_NEXT_STATE (%.DEAD);
-    set_error (0, "handshake: server does not support NBD_OPT_GO");
-    return -1;
+    debug (conn->h,
+	   "server is confused by NBD_OPT_GO, continuing anyway");
+    SET_NEXT_STATE (%^OPT_EXPORT_NAME.START);
+    return 0;
   default:
     SET_NEXT_STATE (%.DEAD);
     set_error (0, "handshake: unknown reply from NBD_OPT_GO: 0x%" PRIx32,

generator/states-newstyle.c
@@ -36,9 +36,11 @@
   uint32_t cflags;
 
   conn->gflags = be16toh (conn->gflags);
-  if ((conn->gflags & NBD_FLAG_FIXED_NEWSTYLE) == 0) {
+  if ((conn->gflags & NBD_FLAG_FIXED_NEWSTYLE) == 0 &&
+      h->tls == 2) {
     SET_NEXT_STATE (%.DEAD);
-    set_error (0, "handshake: server is not a fixed newstyle NBD server");
+    set_error (ENOTSUP, "handshake: server is not fixed newstyle, "
+	       "but handle TLS setting is require (2)");
     return -1;
   }
 
@@ -54,7 +56,10 @@
   case -1: SET_NEXT_STATE (%.DEAD); return -1;
   case 0:
     /* Start sending options. */
-    SET_NEXT_STATE (%OPT_STARTTLS.START);
+    if ((conn->gflags & NBD_FLAG_FIXED_NEWSTYLE) == 0)
+      SET_NEXT_STATE (%OPT_EXPORT_NAME.START);
+    else
+      SET_NEXT_STATE (%OPT_STARTTLS.START);
   }
   return 0;
 

lib/internal.h
@@ -127,6 +127,7 @@ struct nbd_connection {
         }  __attribute__((packed)) context;
       } payload;
     }  __attribute__((packed)) or;
+    struct nbd_export_name_option_reply export_name_reply;
     struct nbd_request request;
     struct nbd_simple_reply simple_reply;
     struct {

lib/nbd-protocol.h
@@ -74,6 +74,13 @@ struct nbd_new_option {
   /* option data follows */
 } __attribute__((packed));
 
+/* Newstyle handshake OPT_EXPORT_NAME reply message. */
+struct nbd_export_name_option_reply {
+  uint64_t exportsize;          /* size of export */
+  uint16_t eflags;              /* per-export flags */
+  char zeroes[124];             /* optional zeroes */
+} __attribute__((packed));;
+
 /* Fixed newstyle handshake reply message. */
 struct nbd_fixed_new_option_reply {
   uint64_t magic;             /* NBD_REP_MAGIC */

commit 2731f9ad38b69f8d21f63a5a295be1ac3175700c

states: Fix bug in NBD_OPT_EXPORT_NAME with non-empty export
How embarrassing: on little-endian machines, a 0-byte export name
works, but a 5-byte export name is enough to make nbdkit reject the
client out-right, and a 1-byte export name causes deadlock where the
server is stuck waiting for a 16M option that will never arrive, and
the client is waiting for a response from the server.

A quick search of 'grep "sbuf.*= [^h]' did not find any other endian
bugs of a similar nature.

Add testsuite coverage, copying liberally from the existing oldstyle
test; the lack of coverage explains why we failed to detect this for
so long (although admittedly, 'nbdkit --mask-handshake' did not exist
when libnbd first added NBD_OPT_EXPORT_NAME support).

Fixes: 8b8ed26c71

.gitignore
@@ -170,6 +170,7 @@ Makefile.in
 /tests/is-rotational-flag
 /tests/keys.psk
 /tests/meta-base-allocation
+/tests/newstyle-limited
 /tests/oldstyle
 /tests/pki/
 /tests/read-only-flag

generator/states-newstyle-opt-export-name.c
@@ -1,5 +1,5 @@
 /* nbd client library in userspace: state machine
- * Copyright (C) 2013-2019 Red Hat Inc.
+ * Copyright (C) 2013-2020 Red Hat Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -22,7 +22,7 @@ STATE_MACHINE {
  NEWSTYLE.OPT_EXPORT_NAME.START:
   h->sbuf.option.version = htobe64 (NBD_NEW_VERSION);
   h->sbuf.option.option = htobe32 (NBD_OPT_EXPORT_NAME);
-  h->sbuf.option.optlen = strlen (h->export_name);
+  h->sbuf.option.optlen = htobe32 (strlen (h->export_name));
   h->wbuf = &h->sbuf;
   h->wlen = sizeof h->sbuf.option;
   h->wflags = MSG_MORE;

tests/Makefile.am
@@ -1,5 +1,5 @@
 # nbd client library in userspace
-# Copyright (C) 2013-2019 Red Hat Inc.
+# Copyright (C) 2013-2020 Red Hat Inc.
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -162,6 +162,7 @@ check_PROGRAMS += \
 	can-cache-flag \
 	can-not-cache-flag \
 	oldstyle \
+	newstyle-limited \
 	connect-unix \
 	connect-tcp \
 	aio-parallel \
@@ -196,6 +197,7 @@ TESTS += \
 	can-cache-flag \
 	can-not-cache-flag \
 	oldstyle \
+	newstyle-limited \
 	connect-unix \
 	connect-tcp \
 	aio-parallel.sh \
@@ -365,6 +367,11 @@ oldstyle_SOURCES = oldstyle.c
 oldstyle_CPPFLAGS = -I$(top_srcdir)/include
 oldstyle_CFLAGS = $(WARNINGS_CFLAGS)
 oldstyle_LDADD = $(top_builddir)/lib/libnbd.la
+
+newstyle_limited_SOURCES = newstyle-limited.c
+newstyle_limited_CPPFLAGS = -I$(top_srcdir)/include
+newstyle_limited_CFLAGS = $(WARNINGS_CFLAGS)
+newstyle_limited_LDADD = $(top_builddir)/lib/libnbd.la
 
 connect_unix_SOURCES = connect-unix.c
 connect_unix_CPPFLAGS = -I$(top_srcdir)/include

tests/newstyle-limited.c
@@ -1,0 +1,238 @@
+/* NBD client library in userspace
+ * Copyright (C) 2013-2020 Red Hat Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/* Test interoperability with newstyle (not newstyle-fixed) servers. */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <string.h>
+#include <errno.h>
+
+#include <libnbd.h>
+
+#define SIZE 65536
+#define XSTR(s) #s
+#define STR(s) XSTR(s)
+
+static char wbuf[512] = { 1, 2, 3, 4 }, rbuf[512];
+static const char *progname;
+
+static int
+pread_cb (void *data,
+          const void *buf, size_t count, uint64_t offset,
+          unsigned status, int *error)
+{
+  int *calls;
+
+  calls = data;
+  ++*calls;
+
+  if (buf != rbuf || count != sizeof rbuf) {
+    fprintf (stderr, "%s: callback called with wrong buffer\n", progname);
+    exit (EXIT_FAILURE);
+  }
+  if (offset != 2 * sizeof rbuf) {
+    fprintf (stderr, "%s: callback called with wrong offset\n", progname);
+    exit (EXIT_FAILURE);
+  }
+  if (*error != 0) {
+    fprintf (stderr, "%s: callback called with unexpected error\n", progname);
+    exit (EXIT_FAILURE);
+  }
+  if (status != LIBNBD_READ_DATA) {
+    fprintf (stderr, "%s: callback called with wrong status\n", progname);
+    exit (EXIT_FAILURE);
+  }
+
+  if (memcmp (rbuf, wbuf, sizeof rbuf) != 0) {
+    fprintf (stderr, "%s: DATA INTEGRITY ERROR!\n", progname);
+    exit (EXIT_FAILURE);
+  }
+
+  if (*calls > 1) {
+    *error = ECONNREFUSED; /* Something NBD servers can't send */
+    return -1;
+  }
+
+  return 0;
+}
+
+int
+main (int argc, char *argv[])
+{
+  struct nbd_handle *nbd;
+  int64_t r;
+  char *args[] = { "nbdkit", "-s", "--mask-handshake", "0",
+                   "--exit-with-parent", "-v", "eval",
+                   "list_exports=printf a\\nb\\n",
+                   "get_size=echo " STR(SIZE),
+                   "open=if [ \"$3\" != a ]; then exit 1; fi\n"
+                   " truncate --size=" STR(SIZE) " $tmpdir/a",
+                   "pread=dd bs=1 skip=$4 count=$3 if=$tmpdir/a || exit 1",
+                   "pwrite=dd bs=1 conv=notrunc seek=$4 of=$tmpdir/a || exit 1",
+                   "can_write=exit 0",
+                   NULL };
+  int calls = 0;
+  const char *s;
+
+  progname = argv[0];
+
+  /* Quick check that nbdkit is new enough */
+  if (system ("nbdkit eval --dump-plugin | grep -q has_list_exports=1")) {
+    fprintf (stderr, "skipping: nbdkit too old for this test\n");
+    exit (77);
+  }
+
+  /* Initial sanity check that we can't require TLS */
+  nbd = nbd_create ();
+  if (nbd == NULL) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  nbd_set_export_name (nbd, "a");
+  if (nbd_supports_tls (nbd)) {
+    if (nbd_set_tls (nbd, LIBNBD_TLS_REQUIRE) == -1) {
+      fprintf (stderr, "%s\n", nbd_get_error ());
+      exit (EXIT_FAILURE);
+    }
+    if (nbd_connect_command (nbd, args) != -1) {
+      fprintf (stderr, "%s\n", "expected failure");
+      exit (EXIT_FAILURE);
+    }
+  }
+  nbd_close (nbd);
+
+  /* And another try: an incorrect export name kills the connection,
+   * rather than allowing a second try.
+   */
+  nbd = nbd_create ();
+  if (nbd == NULL ||
+      nbd_set_export_name (nbd, "b") == -1 ||
+      nbd_connect_command (nbd, args) == 1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  if (!nbd_aio_is_dead (nbd)) {
+    fprintf (stderr, "unexpected state after failed export\n");
+    exit (EXIT_FAILURE);
+  }
+  nbd_close (nbd);
+
+  /* Now for a working connection.  Protocol should be "newstyle",
+   * with no structured replies and no meta contexts.
+   */
+  nbd = nbd_create ();
+  if (nbd == NULL ||
+      nbd_set_export_name (nbd, "a") == -1 ||
+      nbd_add_meta_context (nbd, LIBNBD_CONTEXT_BASE_ALLOCATION) == -1 ||
+      nbd_connect_command (nbd, args) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  if (nbd_aio_is_ready (nbd) != true) {
+    fprintf (stderr, "unexpected state after connection\n");
+    exit (EXIT_FAILURE);
+  }
+  s = nbd_get_protocol (nbd);
+  if (strcmp (s, "newstyle") != 0) {
+    fprintf (stderr,
+             "incorrect protocol \"%s\", expected \"newstyle\"\n", s);
+    exit (EXIT_FAILURE);
+  }
+  if ((r = nbd_get_structured_replies_negotiated (nbd)) != 0) {
+    fprintf (stderr,
+             "incorrect structured replies %" PRId64 ", expected 0\n", r);
+    exit (EXIT_FAILURE);
+  }
+  if ((r = nbd_can_meta_context (nbd, LIBNBD_CONTEXT_BASE_ALLOCATION)) != 0) {
+    fprintf (stderr,
+             "incorrect meta context %" PRId64 ", expected 0\n", r);
+    exit (EXIT_FAILURE);
+  }
+
+  if ((r = nbd_get_size (nbd)) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  if (r != SIZE) {
+    fprintf (stderr, "%s: test failed: incorrect size, "
+             "actual %" PRIi64 ", expected %d",
+             argv[0], r, SIZE);
+    exit (EXIT_FAILURE);
+  }
+
+  /* Plain I/O */
+  if (nbd_pwrite (nbd, wbuf, sizeof wbuf, 2 * sizeof wbuf, 0) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  if (nbd_pread (nbd, rbuf, sizeof rbuf, 2 * sizeof rbuf, 0) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  if (memcmp (rbuf, wbuf, sizeof rbuf) != 0) {
+    fprintf (stderr, "%s: DATA INTEGRITY ERROR!\n", argv[0]);
+    exit (EXIT_FAILURE);
+  }
+
+  /* Test again for callback operation. */
+  memset (rbuf, 0, sizeof rbuf);
+  if (nbd_pread_structured (nbd, rbuf, sizeof rbuf, 2 * sizeof rbuf,
+                            (nbd_chunk_callback) { .callback = pread_cb, .user_data = &calls },
+                            0) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  if (calls != 1) {
+    fprintf (stderr, "%s: callback called wrong number of times\n", argv[0]);
+    exit (EXIT_FAILURE);
+  }
+  if (memcmp (rbuf, wbuf, sizeof rbuf) != 0) {
+    fprintf (stderr, "%s: DATA INTEGRITY ERROR!\n", argv[0]);
+    exit (EXIT_FAILURE);
+  }
+
+  /* Also test that callback errors are reflected correctly. */
+  if (nbd_pread_structured (nbd, rbuf, sizeof rbuf, 2 * sizeof rbuf,
+                            (nbd_chunk_callback) { .callback = pread_cb, .user_data = &calls },
+                            0) != -1) {
+    fprintf (stderr, "%s: expected failure from callback\n", argv[0]);
+    exit (EXIT_FAILURE);
+  }
+  if (nbd_get_errno () != ECONNREFUSED) {
+    fprintf (stderr, "%s: wrong errno value after failed callback\n", argv[0]);
+    exit (EXIT_FAILURE);
+  }
+
+  if (nbd_shutdown (nbd, 0) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  nbd_close (nbd);
+  exit (EXIT_SUCCESS);
+}