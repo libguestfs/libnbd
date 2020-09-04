libguestfs/libnbd

commit d9346f78fd6d91e26d246523dcc22f4052062f26

Implement NBD_CMD_TRIM and NBD_CMD_WRITE_ZEROES.

lib/rw.c
@@ -136,17 +136,86 @@ nbd_unlocked_flush (struct nbd_handle *h)
   return r == -1 ? -1 : 0;
 }
 
+/* Issue a trim command on any connection and wait for the reply. */
+int
+nbd_unlocked_trim (struct nbd_handle *h,
+                   uint64_t count, uint64_t offset, uint32_t flags)
+{
+  struct nbd_connection *conn;
+  int64_t ch;
+  int r;
+
+  conn = pick_connection (h, "nbd_trim");
+  if (h == NULL)
+    return -1;
+
+  ch = nbd_unlocked_aio_trim (conn, count, offset, flags);
+  if (ch == -1)
+    return -1;
+
+  while ((r = nbd_unlocked_aio_command_completed (conn, ch)) == 0) {
+    if (nbd_unlocked_poll (h, -1) == -1)
+      return -1;
+  }
+
+  return r == -1 ? -1 : 0;
+}
+
+/* Issue a zero command on any connection and wait for the reply. */
+int
+nbd_unlocked_zero (struct nbd_handle *h,
+                   uint64_t count, uint64_t offset, uint32_t flags)
+{
+  struct nbd_connection *conn;
+  int64_t ch;
+  int r;
+
+  conn = pick_connection (h, "nbd_zero");
+  if (h == NULL)
+    return -1;
+
+  ch = nbd_unlocked_aio_zero (conn, count, offset, flags);
+  if (ch == -1)
+    return -1;
+
+  while ((r = nbd_unlocked_aio_command_completed (conn, ch)) == 0) {
+    if (nbd_unlocked_poll (h, -1) == -1)
+      return -1;
+  }
+
+  return r == -1 ? -1 : 0;
+}
+
 static struct command_in_flight *
 command_common (struct nbd_connection *conn,
                 uint16_t flags, uint16_t type,
-                uint64_t offset, uint32_t count, void *data)
+                uint64_t offset, uint64_t count, void *data)
 {
   struct command_in_flight *cmd;
 
-  if (count > MAX_REQUEST_SIZE) {
-    set_error (ERANGE, "request too large: maximum request size is %d",
-               MAX_REQUEST_SIZE);
-    return NULL;
+  switch (type) {
+    /* Commands which send or receive data are limited to MAX_REQUEST_SIZE. */
+  case NBD_CMD_READ:
+  case NBD_CMD_WRITE:
+    if (count > MAX_REQUEST_SIZE) {
+      set_error (ERANGE, "request too large: maximum request size is %d",
+                 MAX_REQUEST_SIZE);
+      return NULL;
+    }
+    break;
+
+    /* Other commands are currently limited by the 32 bit field in the
+     * command structure on the wire, but in future we hope to support
+     * 64 bit values here with a change to the NBD protocol which is
+     * being discussed upstream.
+     */
+  default:
+    if (count > UINT32_MAX) {
+      set_error (ERANGE, "request too large: maximum request size is %" PRIu32,
+                 UINT32_MAX);
+      return NULL;
+    }
+    break;
   }
 
   cmd = malloc (sizeof *cmd);
@@ -226,6 +295,80 @@ nbd_unlocked_aio_flush (struct nbd_connection *conn)
   }
 
   cmd = command_common (conn, 0, NBD_CMD_FLUSH, 0, 0, NULL);
+  if (!cmd)
+    return -1;
+  if (nbd_internal_run (conn->h, conn, cmd_issue) == -1)
+    return -1;
+
+  return cmd->handle;
+}
+
+int64_t
+nbd_unlocked_aio_trim (struct nbd_connection *conn,
+                       uint64_t count, uint64_t offset,
+                       uint32_t flags)
+{
+  struct command_in_flight *cmd;
+
+  if (nbd_unlocked_read_only (conn->h) == 1) {
+    set_error (EINVAL, "server does not support write operations");
+    return -1;
+  }
+
+  if ((flags & ~LIBNBD_CMD_FLAG_FUA) != 0) {
+    set_error (EINVAL, "nbd_aio_trim: invalid flag: %" PRIu32, flags);
+    return -1;
+  }
+
+  if ((flags & LIBNBD_CMD_FLAG_FUA) != 0 &&
+      nbd_unlocked_can_fua (conn->h) != 1) {
+    set_error (EINVAL, "server does not support the FUA flag");
+    return -1;
+  }
+
+  if (count == 0) {             /* NBD protocol forbids this. */
+    set_error (EINVAL, "nbd_aio_trim: count cannot be 0");
+    return -1;
+  }
+
+  cmd = command_common (conn, flags, NBD_CMD_TRIM, offset, count, NULL);
+  if (!cmd)
+    return -1;
+  if (nbd_internal_run (conn->h, conn, cmd_issue) == -1)
+    return -1;
+
+  return cmd->handle;
+}
+
+int64_t
+nbd_unlocked_aio_zero (struct nbd_connection *conn,
+                       uint64_t count, uint64_t offset,
+                       uint32_t flags)
+{
+  struct command_in_flight *cmd;
+
+  if (nbd_unlocked_read_only (conn->h) == 1) {
+    set_error (EINVAL, "server does not support write operations");
+    return -1;
+  }
+
+  if ((flags & ~LIBNBD_CMD_FLAG_FUA) != 0) {
+    set_error (EINVAL, "nbd_aio_zero: invalid flag: %" PRIu32, flags);
+    return -1;
+  }
+
+  if ((flags & LIBNBD_CMD_FLAG_FUA) != 0 &&
+      nbd_unlocked_can_fua (conn->h) != 1) {
+    set_error (EINVAL, "server does not support the FUA flag");
+    return -1;
+  }
+
+  if (count == 0) {             /* NBD protocol forbids this. */
+    set_error (EINVAL, "nbd_aio_trim: count cannot be 0");
+    return -1;
+  }
+
+  cmd = command_common (conn, flags, NBD_CMD_WRITE_ZEROES, offset, count, NULL);
   if (!cmd)
     return -1;
   if (nbd_internal_run (conn->h, conn, cmd_issue) == -1)

generator/generator
@@ -467,6 +467,48 @@ committed to permanent storage on the server.  Note this will
 return an error if C<nbd_can_flush> is false.";
   };
 
+  "trim", {
+    default_call with
+    args = [ UInt64 "count"; UInt64 "offset"; UInt32 "flags" ];
+    ret = RErr;
+    shortdesc = "send trim to the NBD server";
+    longdesc = "\
+Issue a trim command to the NBD server, which if supported
+by the server causes a hole to be punched in the backing
+store starting at C<offset> and ending at C<offset> + C<count> - 1.
+The call returns when the command has been acknowledged by the server,
+or there is an error.
+
+The C<flags> parameter may be C<0> for no flags, or may contain
+C<LIBNBD_CMD_FLAG_FUA> meaning that the server should not
+return until the data has been committed to permanent storage
+(if that is supported - some servers cannot do this).
+
+If multi-conn is enabled, the command is issued on the next
+ready connection, picked in a round-robin manner.";
+  };
+
+  "zero", {
+    default_call with
+    args = [ UInt64 "count"; UInt64 "offset"; UInt32 "flags" ];
+    ret = RErr;
+    shortdesc = "send write zeroes to the NBD server";
+    longdesc = "\
+Issue a write zeroes command to the NBD server, which if supported
+by the server causes a zeroes to be written efficiently
+starting at C<offset> and ending at C<offset> + C<count> - 1.
+The call returns when the command has been acknowledged by the server,
+or there is an error.
+
+The C<flags> parameter may be C<0> for no flags, or may contain
+C<LIBNBD_CMD_FLAG_FUA> meaning that the server should not
+return until the data has been committed to permanent storage
+(if that is supported - some servers cannot do this).
+
+If multi-conn is enabled, the command is issued on the next
+ready connection, picked in a round-robin manner.";
+  };
+
   "poll", {
     default_call with
     args = [ Int "timeout" ];
@@ -544,6 +586,32 @@ C<nbd_aio_is_closed> to become true on the connection.";
 Issue the flush command to the NBD server.  This returns the
 unique 64 bit handle for this command.  To check if the command
 completed, call C<nbd_aio_command_completed>.";
+  };
+
+  "aio_trim", {
+    default_call with
+    args = [ UInt64 "count"; UInt64 "offset"; UInt32 "flags" ];
+    ret = RInt64;
+    shortdesc = "send trim to the NBD server";
+    longdesc = "\
+Issue a trim command to the NBD server.  This returns the
+unique 64 bit handle for this command.  To check if the command
+completed, call C<nbd_aio_command_completed>.  Note that
+you must ensure C<buf> is valid until the command has
+completed.";
+  };
+
+  "aio_zero", {
+    default_call with
+    args = [ UInt64 "count"; UInt64 "offset"; UInt32 "flags" ];
+    ret = RInt64;
+    shortdesc = "send write zeroes to the NBD server";
+    longdesc = "\
+Issue a write zeroes command to the NBD server.  This returns the
+unique 64 bit handle for this command.  To check if the command
+completed, call C<nbd_aio_command_completed>.  Note that
+you must ensure C<buf> is valid until the command has
+completed.";
   };
 
   "aio_get_fd", {

commit bf38b218577bf9e260e7eae25ea8e36a6833dd13

lib: Avoid unadvertised trim, zero
Gating trim and zero on whether the server is read-only is not fully
correct: we can end up sending the server a command it does not
expect if the server supports writes but not trim/zero.

Fixes: d9346f78

lib/rw.c
@@ -362,6 +362,10 @@ nbd_unlocked_aio_trim (struct nbd_handle *h,
 {
   struct command_cb cb = { .completion = completion };
 
+  if (nbd_unlocked_can_trim (h) != 1) {
+    set_error (EINVAL, "server does not support trim operations");
+    return -1;
+  }
   if (nbd_unlocked_is_read_only (h) == 1) {
     set_error (EPERM, "server does not support write operations");
     return -1;
@@ -421,6 +425,10 @@ nbd_unlocked_aio_zero (struct nbd_handle *h,
 {
   struct command_cb cb = { .completion = completion };
 
+  if (nbd_unlocked_can_zero (h) != 1) {
+    set_error (EINVAL, "server does not support zero operations");
+    return -1;
+  }
   if (nbd_unlocked_is_read_only (h) == 1) {
     set_error (EPERM, "server does not support write operations");
     return -1;

tests/errors.c
@@ -240,6 +240,15 @@ main (int argc, char *argv[])
   }
   check (ERANGE, "nbd_aio_pwrite: ");
 
+  /* Use unadvertised command */
+  if (nbd_trim (nbd, 512, 0, 0) != -1) {
+    fprintf (stderr, "%s: test failed: "
+             "unpermitted nbd_trim did not fail\n",
+             argv[0]);
+    exit (EXIT_FAILURE);
+  }
+  check (EINVAL, "nbd_trim: ");
+
   /* Send a read that the nbdkit sh plugin will fail. */
   if (nbd_pread (nbd, buf, 512, 0, 0) != -1) {
     fprintf (stderr, "%s: test failed: "