libguestfs/libnbd

commit 4fab2104ab0c64a26ed1fce8ebe857bceb3e60f2

api: Get rid of running subprocess via system(3).
This is problematic for several reasons:

 * The shell runs first, breaking --exit-with-parent and process inheritence.

 * Quoting of command line arguments.

 * Handling errors.

This involves changing the nbd_connect_command API to make it use an
argv-style array instead of a command, but that is generally an
improvement.

python/utils.c
@@ -1,0 +1,85 @@
+/* NBD client library in userspace
+ * Copyright (C) 2013-2019 Red Hat Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/* Miscellaneous helper functions for Python. */
+
+#include <config.h>
+
+#define PY_SSIZE_T_CLEAN 1
+#include <Python.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include <libnbd.h>
+
+#include "methods.h"
+
+/* These two functions are used when parsing argv parameters. */
+char **
+nbd_internal_py_get_string_list (PyObject *obj)
+{
+  size_t i, len;
+  char **r;
+
+  assert (obj);
+
+  if (!PyList_Check (obj)) {
+    PyErr_SetString (PyExc_TypeError, "expecting a list parameter");
+    return NULL;
+  }
+
+  Py_ssize_t slen = PyList_Size (obj);
+  if (slen == -1) {
+    PyErr_SetString (PyExc_RuntimeError,
+                     "get_string_list: PyList_Size failure");
+    return NULL;
+  }
+  len = (size_t) slen;
+  r = malloc (sizeof (char *) * (len+1));
+  if (r == NULL) {
+    PyErr_NoMemory ();
+    return NULL;
+  }
+
+  for (i = 0; i < len; ++i) {
+    PyObject *bytes = PyUnicode_AsUTF8String (PyList_GetItem (obj, i));
+    r[i] = strdup (PyBytes_AS_STRING (bytes));
+    if (r[i] == NULL) {
+      PyErr_NoMemory ();
+      return NULL;
+    }
+  }
+  r[len] = NULL;
+
+  return r;
+}
+
+void
+nbd_internal_py_free_string_list (char **argv)
+{
+  size_t i;
+
+  if (!argv)
+    return;
+
+  for (i = 0; argv[i] != NULL; ++i)
+    free (argv[i]);
+  free (argv);
+}

lib/handle.c
@@ -67,7 +67,7 @@ close_conn (struct nbd_connection *conn)
   free_cmd_list (conn->cmds_to_issue);
   free_cmd_list (conn->cmds_in_flight);
   free_cmd_list (conn->cmds_done);
-  free (conn->command);
+  nbd_internal_free_string_list (conn->argv);
   free (conn->hostname);
   free (conn->port);
   if (conn->result)

lib/utils.c
@@ -48,3 +48,51 @@ nbd_internal_hexdump (const void *data, size_t len, FILE *fp)
     fprintf (fp, "|\n");
   }
 }
+
+size_t
+nbd_internal_string_list_length (char **argv)
+{
+  size_t ret;
+
+  for (ret = 0; argv[ret] != NULL; ++ret)
+    ;
+  return ret;
+}
+
+char **
+nbd_internal_copy_string_list (char **argv)
+{
+  size_t i, j, n;
+  char **ret;
+
+  n = nbd_internal_string_list_length (argv);
+  ret = calloc (n+1, sizeof (char *));
+  if (!ret)
+    return NULL;
+
+  for (i = 0; i < n; ++i) {
+    ret[i] = strdup (argv[i]);
+    if (ret[i] == NULL) {
+      for (j = 0; j < i; ++j)
+        free (ret[j]);
+      free (ret);
+      return NULL;
+    }
+  }
+  ret[n] = NULL;
+
+  return ret;
+}
+
+void
+nbd_internal_free_string_list (char **argv)
+{
+  size_t i;
+
+  if (!argv)
+    return;
+
+  for (i = 0; argv[i] != NULL; ++i)
+    free (argv[i]);
+  free (argv);
+}

docs/libnbd.pod
@@ -259,7 +259,8 @@ these servers as a subprocess of your main program.  This example
 creates a 1G writable RAM disk for testing which is discarded as soon
 as the libnbd handle is closed:
 
- nbd_connect_command (nbd, "nbdkit -s memory 1G");
+ char *argv[] = { "nbdkit", "-s", "memory", "1G", NULL };
+ nbd_connect_command (nbd, argv);
 
 =head1 EXPORTS AND FLAGS
 

generator/generator
@@ -350,6 +350,7 @@ and arg =
 | Path of string           (* filename or path *)
 | SockAddrAndLen of string * string (* struct sockaddr * + socklen_t *)
 | String of string         (* string *)
+| StringList of string     (* argv-style NULL-terminated array of strings *)
 | UInt of string           (* small unsigned int *)
 | UInt32 of string         (* 32 bit unsigned int *)
 | UInt64 of string         (* 64 bit unsigned int *)
@@ -603,10 +604,10 @@ any one of them is connected.";
 
   "connect_command", {
     default_call with
-    args = [ String "command" ]; ret = RErr;
+    args = [ StringList "argv" ]; ret = RErr;
     shortdesc = "connect to NBD server command";
     longdesc = "\
-Run the C<command> as a subprocess and connect to it over
+Run the command as a subprocess and connect to it over
 stdin/stdout.  This is for use with NBD servers which can
 behave like inetd clients, such as C<nbdkit --single>.";
   };
@@ -864,10 +865,10 @@ the connection.";
 
   "aio_connect_command", {
     default_call with
-    args = [ String "command" ]; ret = RErr;
+    args = [ StringList "argv" ]; ret = RErr;
     shortdesc = "connect to the NBD server";
     longdesc = "\
-Run the C<command> as a subprocess and begin connecting to it over
+Run the command as a subprocess and begin connecting to it over
 stdin/stdout.  You can check if the connection has connected to
 the server and completed the NBD handshake by calling
 C<nbd_aio_is_ready> on the connection.";
@@ -1525,6 +1526,7 @@ let name_of_arg = function
 | Path n -> [n]
 | SockAddrAndLen (n, len) -> [n; len]
 | String n -> [n]
+| StringList n -> [n]
 | UInt n -> [n]
 | UInt32 n -> [n]
 | UInt64 n -> [n]
@@ -1554,6 +1556,7 @@ let rec print_c_arg_list ?handle args =
       | Int64 n -> pr "int64_t %s" n
       | Path n
       | String n -> pr "const char *%s" n
+      | StringList n -> pr "char **%s" n
       | SockAddrAndLen (n, len) ->
          pr "const struct sockaddr *%s, socklen_t %s" n len
       | UInt n -> pr "unsigned %s" n
@@ -1711,6 +1714,9 @@ let generate_python_methods_h () =
   pr "#include <assert.h>\n";
   pr "\n";
   pr "\
+extern char **nbd_internal_py_get_string_list (PyObject *);
+extern void nbd_internal_py_free_string_list (char **);
+
 static inline struct nbd_handle *
 get_handle (PyObject *obj)
 {
@@ -1820,13 +1826,16 @@ let print_python_binding name { args; ret } htype hname =
     | Int64 n ->
        pr "  int64_t %s_i64;\n" n;
        pr "  long long %s; /* really int64_t */\n" n
-    | Path n -> pr "  char *%s;\n" n
+    | Path n -> pr "  char *%s = NULL;\n" n
     | SockAddrAndLen (n, _) ->
        pr "  /* XXX Complicated - Python uses a tuple of different\n";
        pr "   * lengths for the different socket types.\n";
        pr "   */\n";
        pr "  PyObject *%s;\n" n
     | String n -> pr "  const char *%s;\n" n
+    | StringList n ->
+       pr "  PyObject *py_%s;\n" n;
+       pr "  char **%s = NULL;\n" n
     | UInt n -> pr "  unsigned int %s;\n" n
     | UInt32 n ->
        pr "  uint32_t %s_u32;\n" n;
@@ -1850,6 +1859,7 @@ let print_python_binding name { args; ret } htype hname =
     | Path n -> pr " \"O&\""
     | SockAddrAndLen (n, _) -> pr " \"O\""
     | String n -> pr " \"s\""
+    | StringList n -> pr " \"O\""
     | UInt n -> pr " \"I\""
     | UInt32 n -> pr " \"I\""
     | UInt64 n -> pr " \"K\""
@@ -1868,6 +1878,7 @@ let print_python_binding name { args; ret } htype hname =
     | Path n -> pr ", PyUnicode_FSConverter, &%s" n
     | SockAddrAndLen (n, _) -> pr ", &%s" n
     | String n -> pr ", &%s" n
+    | StringList n -> pr ", &py_%s" n
     | UInt n -> pr ", &%s" n
     | UInt32 n -> pr ", &%s" n
     | UInt64 n -> pr ", &%s" n
@@ -1902,6 +1913,9 @@ let print_python_binding name { args; ret } htype hname =
     | SockAddrAndLen _ ->
        pr "  abort (); /* XXX SockAddrAndLen not implemented */\n";
     | String _ -> ()
+    | StringList n ->
+       pr "  %s = nbd_internal_py_get_string_list (py_%s);\n" n n;
+       pr "  if (!%s) { py_ret = NULL; goto out; }\n" n
     | UInt _ -> ()
     | UInt32 n -> pr "  %s_u32 = %s;\n" n n
     | UInt64 n -> pr "  %s_u64 = %s;\n" n n
@@ -1918,6 +1932,7 @@ let print_python_binding name { args; ret } htype hname =
     | Path n -> pr ", %s" n
     | SockAddrAndLen (n, _) -> pr ", /* XXX */ (void *) %s, 0" n
     | String n -> pr ", %s" n
+    | StringList n -> pr ", %s" n
     | UInt n -> pr ", %s" n
     | UInt32 n -> pr ", %s_u32" n
     | UInt64 n -> pr ", %s_u64" n
@@ -1947,6 +1962,7 @@ let print_python_binding name { args; ret } htype hname =
     | Path _
     | SockAddrAndLen _
     | String _
+    | StringList _
     | UInt _
     | UInt32 _
     | UInt64 _ -> ()
@@ -1984,6 +2000,7 @@ let print_python_binding name { args; ret } htype hname =
     | Path n -> pr "  free (%s);\n" n
     | SockAddrAndLen _ -> ()
     | String n -> ()
+    | StringList n -> pr "  nbd_internal_py_free_string_list (%s);\n" n
     | UInt _ -> ()
     | UInt32 _ -> ()
     | UInt64 _ -> ()
@@ -2064,6 +2081,7 @@ class NBD (object):
                      | Path n -> n
                      | SockAddrAndLen (n, _) -> n
                      | String n -> n
+                     | StringList n -> n
                      | UInt n -> n
                      | UInt32 n -> n
                      | UInt64 n -> n
@@ -2107,6 +2125,7 @@ class NBDConnection (object):
                      | Path n -> n
                      | SockAddrAndLen (n, _) -> n
                      | String n -> n
+                     | StringList n -> n
                      | UInt n -> n
                      | UInt32 n -> n
                      | UInt64 n -> n

generator/states.c
@@ -262,11 +262,12 @@ send_from_wbuf (struct nbd_connection *conn)
   return 0;
 
  CONNECT_COMMAND:
-  int sv[2], r;
+  int sv[2];
   pid_t pid;
 
   assert (!conn->sock);
-  assert (conn->command);
+  assert (conn->argv);
+  assert (conn->argv[0]);
   if (socketpair (AF_UNIX, SOCK_STREAM|SOCK_NONBLOCK|SOCK_CLOEXEC, 0,
                   sv) == -1) {
     SET_NEXT_STATE (%DEAD);
@@ -290,16 +291,11 @@ send_from_wbuf (struct nbd_connection *conn)
     dup2 (sv[1], 1);
     close (sv[1]);
 
-    /* Restore SIGPIPE back to SIG_DFL, since shell can't undo SIG_IGN */
+    /* Restore SIGPIPE back to SIG_DFL. */
     signal (SIGPIPE, SIG_DFL);
 
-    r = system (conn->command);
-    if (r == -1) {
-      perror (conn->command);
-      _exit (EXIT_FAILURE);
-    }
-    if (WIFEXITED (r))
-      _exit (WEXITSTATUS (r));
+    execvp (conn->argv[0], conn->argv);
+    perror (conn->argv[0]);
     _exit (EXIT_FAILURE);
   }
 

interop/Makefile.am
@@ -30,7 +30,8 @@ TESTS += interop-nbd-server
 interop_nbd_server_SOURCES = interop.c
 interop_nbd_server_CPPFLAGS = \
 	-I$(top_srcdir)/include \
-	-DSERVER='"$(NBD_SERVER) -d -C /dev/null 0 %s", tmpfile' \
+	-DSERVER=\"$(NBD_SERVER)\" \
+	-DSERVER_PARAMS='"-d", "-C", "/dev/null", "0", tmpfile' \
 	-DEXPORT_NAME='""'
 interop_nbd_server_CFLAGS = $(WARNINGS_CFLAGS)
 interop_nbd_server_LDADD = $(top_builddir)/lib/libnbd.la

interop/interop.c
@@ -42,8 +42,6 @@ main (int argc, char *argv[])
   int port;
   char port_str[16];
   pid_t pid = -1;
-#else
-  char cmd[256];
 #endif
   int64_t actual_size;
   char buf[512];
@@ -121,8 +119,8 @@ main (int argc, char *argv[])
 
 #else /* !SERVE_OVER_TCP */
 
-  snprintf (cmd, sizeof cmd, SERVER);
-  if (nbd_connect_command (nbd, cmd) == -1) {
+  char *args[] = { SERVER, SERVER_PARAMS, NULL };
+  if (nbd_connect_command (nbd, args) == -1) {
     fprintf (stderr, "%s\n", nbd_get_error ());
     goto out;
   }

commit 89af010d75d33f4c9c598ebe1a640d2c7299942f

python: Fix more memory leaks
h.nbd_connect_command was leaking a python object per parameter, and
also leaks memory on failure.  In fact, it was possible to segfault on
something as trivial as:
nbdsh -c 'h.connect_command(["true",1])'

h.nbd_pread was leaking a read buffer on every single synchronous read.

My previous patch to address closure callbacks had a bug in
h.nbd_pread_structured and similar: if the allocation for Closure
fails, we hit 'goto err' prior to initializing the OClosure pointers,
which could lead to freeing garbage.  Similarly, when messing with
non-callable for either the Closure or the OClosure, there was enough
inaccuracy in python reference counting to cause a leak or
double-free.

We were not consistently checking for python function failures (such
as when a wrong type is passed in).

While touching this, drop a pointless cast to char* when calling
PyArg_ParseTuple, and rearrange some of the code for fewer loops over
args/optargs in the generator.

Fixes: 4fab2104ab
Fixes: bf0eee111f
Fixes: 82dac49af0

generator/Python.ml
@@ -271,7 +271,7 @@ let print_python_binding name { args; optargs; ret; may_set_error } =
     | BytesIn (n, _) ->
        pr "  Py_buffer %s;\n" n
     | BytesOut (n, count) ->
-       pr "  char *%s;\n" n;
+       pr "  char *%s = NULL;\n" n;
        pr "  Py_ssize_t %s;\n" count
     | BytesPersistIn (n, _)
     | BytesPersistOut (n, _) ->
@@ -279,11 +279,10 @@ let print_python_binding name { args; optargs; ret; may_set_error } =
           n;
        pr "  struct py_aio_buffer *%s_buf;\n" n
     | Closure { cbname } ->
-       pr "  struct user_data *%s_user_data = alloc_user_data ();\n" cbname;
-       pr "  if (%s_user_data == NULL) goto out;\n" cbname;
+       pr "  struct user_data *%s_user_data = NULL;\n" cbname;
+       pr "  PyObject *py_%s_fn;\n" cbname;
        pr "  nbd_%s_callback %s = { .callback = %s_wrapper,\n"
          cbname cbname cbname;
-       pr "                         .user_data = %s_user_data,\n" cbname;
        pr "                         .free = free_user_data };\n"
     | Enum (n, _) -> pr "  int %s;\n" n
     | Flags (n, _) ->
@@ -316,11 +315,10 @@ let print_python_binding name { args; optargs; ret; may_set_error } =
   List.iter (
     function
     | OClosure { cbname } ->
-       pr "  struct user_data *%s_user_data = alloc_user_data ();\n" cbname;
-       pr "  if (%s_user_data == NULL) goto out;\n" cbname;
+       pr "  struct user_data *%s_user_data = NULL;\n" cbname;
+       pr "  PyObject *py_%s_fn;\n" cbname;
        pr "  nbd_%s_callback %s = { .callback = %s_wrapper,\n"
          cbname cbname cbname;
-       pr "                         .user_data = %s_user_data,\n" cbname;
        pr "                         .free = free_user_data };\n"
     | OFlags (n, _) ->
        pr "  uint32_t %s_u32;\n" n;
@@ -329,7 +327,7 @@ let print_python_binding name { args; optargs; ret; may_set_error } =
   pr "\n";
 
   (* Parse the Python parameters. *)
-  pr "  if (!PyArg_ParseTuple (args, (char *) \"O\"";
+  pr "  if (!PyArg_ParseTuple (args, \"O\"";
   List.iter (
     function
     | Bool n -> pr " \"p\""
@@ -364,7 +362,7 @@ let print_python_binding name { args; optargs; ret; may_set_error } =
     | BytesIn (n, _) | BytesPersistIn (n, _)
     | BytesPersistOut (n, _) -> pr ", &%s" n
     | BytesOut (_, count) -> pr ", &%s" count
-    | Closure { cbname } -> pr ", &%s_user_data->fn" cbname
+    | Closure { cbname } -> pr ", &py_%s_fn" cbname
     | Enum (n, _) -> pr ", &%s" n
     | Flags (n, _) -> pr ", &%s" n
     | Fd n | Int n -> pr ", &%s" n
@@ -379,30 +377,57 @@ let print_python_binding name { args; optargs; ret; may_set_error } =
   ) args;
   List.iter (
     function
-    | OClosure { cbname } -> pr ", &%s_user_data->fn" cbname
+    | OClosure { cbname } -> pr ", &py_%s_fn" cbname
     | OFlags (n, _) -> pr ", &%s" n
   ) optargs;
   pr "))\n";
   pr "    goto out;\n";
 
   pr "  h = get_handle (py_h);\n";
+  pr "  if (!h) goto out;\n";
+  List.iter (
+    function
+    | OClosure { cbname } ->
+       pr "  %s.user_data = %s_user_data = alloc_user_data ();\n" cbname cbname;
+       pr "  if (%s_user_data == NULL) goto out;\n" cbname;
+       pr "  if (py_%s_fn != Py_None) {\n" cbname;
+       pr "    if (!PyCallable_Check (py_%s_fn)) {\n" cbname;
+       pr "      PyErr_SetString (PyExc_TypeError,\n";
+       pr "                       \"callback parameter %s is not callable\");\n" cbname;
+       pr "      goto out;\n";
+       pr "    }\n";
+       pr "    /* Increment refcount since pointer may be saved by libnbd. */\n";
+       pr "    Py_INCREF (py_%s_fn);\n" cbname;
+       pr "    %s_user_data->fn = py_%s_fn;\n" cbname cbname;
+       pr "  }\n";
+       pr "  else\n";
+       pr "    %s.callback = NULL; /* we're not going to call it */\n" cbname
+    | OFlags (n, _) -> pr "  %s_u32 = %s;\n" n n
+  ) optargs;
   List.iter (
     function
     | Bool _ -> ()
     | BytesIn _ -> ()
     | BytesOut (n, count) ->
-       pr "  %s = malloc (%s);\n" n count
+       pr "  %s = malloc (%s);\n" n count;
+       pr "  if (%s == NULL) { PyErr_NoMemory (); goto out; }\n" n
     | BytesPersistIn (n, _) | BytesPersistOut (n, _) ->
-       pr "  %s_buf = nbd_internal_py_get_aio_buffer (%s);\n" n n
+       pr "  %s_buf = nbd_internal_py_get_aio_buffer (%s);\n" n n;
+       pr "  if (!%s_buf) goto out;\n" n;
+       pr "  /* Increment refcount since buffer may be saved by libnbd. */\n";
+       pr "  Py_INCREF (%s);\n" n;
+       pr "  completion_user_data->buf = %s;\n" n
     | Closure { cbname } ->
-       pr "  /* Increment refcount since pointer may be saved by libnbd. */\n";
-       pr "  if (!PyCallable_Check (%s_user_data->fn)) {\n" cbname;
+       pr "  %s.user_data = %s_user_data = alloc_user_data ();\n" cbname cbname;
+       pr "  if (%s_user_data == NULL) goto out;\n" cbname;
+       pr "  if (!PyCallable_Check (py_%s_fn)) {\n" cbname;
        pr "    PyErr_SetString (PyExc_TypeError,\n";
        pr "                     \"callback parameter %s is not callable\");\n" cbname;
-       pr "    %s_user_data->fn = NULL;\n" cbname;
        pr "    goto out;\n";
        pr "  }\n";
-       pr "  Py_INCREF (%s_user_data->fn);\n" cbname
+       pr "  /* Increment refcount since pointer may be saved by libnbd. */\n";
+       pr "  Py_INCREF (py_%s_fn);\n" cbname;
+       pr "  %s_user_data->fn = py_%s_fn;\n" cbname cbname
     | Enum _ -> ()
     | Flags (n, _) -> pr "  %s_u32 = %s;\n" n n
     | Fd _ | Int _ -> ()
@@ -420,37 +445,6 @@ let print_python_binding name { args; optargs; ret; may_set_error } =
     | UInt32 n -> pr "  %s_u32 = %s;\n" n n
     | UInt64 n -> pr "  %s_u64 = %s;\n" n n
   ) args;
-  List.iter (
-    function
-    | OClosure { cbname } ->
-       pr "  if (%s_user_data->fn != Py_None) {\n" cbname;
-       pr "    /* Increment refcount since pointer may be saved by libnbd. */\n";
-       pr "    if (!PyCallable_Check (%s_user_data->fn)) {\n" cbname;
-       pr "      PyErr_SetString (PyExc_TypeError,\n";
-       pr "                       \"callback parameter %s is not callable\");\n" cbname;
-       pr "      %s_user_data->fn = NULL;\n" cbname;
-       pr "      goto out;\n";
-       pr "    }\n";
-       pr "    Py_INCREF (%s_user_data->fn);\n" cbname;
-       pr "  }\n";
-       pr "  else\n";
-       pr "    %s.callback = NULL; /* we're not going to call it */\n" cbname
-    | OFlags (n, _) -> pr "  %s_u32 = %s;\n" n n
-  ) optargs;
-
-  (* If there is a BytesPersistIn/Out parameter then we need to
-   * increment the refcount and save the pointer into
-   * completion_callback.user_data so we can decrement the
-   * refcount on command completion.
-   *)
-  List.iter (
-    function
-    | BytesPersistIn (n, _) | BytesPersistOut (n, _) ->
-       pr "  /* Increment refcount since buffer may be saved by libnbd. */\n";
-       pr "  Py_INCREF (%s);\n" n;
-       pr "  completion_user_data->buf = %s;\n" n;
-    | _ -> ()
-    ) args;
   pr "\n";
 
   (* Call the underlying C function. *)
@@ -547,9 +541,10 @@ let print_python_binding name { args; optargs; ret; may_set_error } =
     function
     | Bool _ -> ()
     | BytesIn (n, _) -> pr "  PyBuffer_Release (&%s);\n" n
-    | BytesPersistIn _ | BytesOut _ | BytesPersistOut _ -> ()
+    | BytesOut (n, _) -> pr "  free (%s);\n" n
+    | BytesPersistIn _ | BytesPersistOut _ -> ()
     | Closure { cbname } ->
-       pr "  if (%s_user_data) free_user_data (%s_user_data);\n" cbname cbname
+       pr "  free_user_data (%s_user_data);\n" cbname
     | Enum _ -> ()
     | Flags _ -> ()
     | Fd _ | Int _ -> ()
@@ -566,7 +561,7 @@ let print_python_binding name { args; optargs; ret; may_set_error } =
   List.iter (
     function
     | OClosure { cbname } ->
-       pr "  if (%s_user_data) free_user_data (%s_user_data);\n" cbname cbname
+       pr "  free_user_data (%s_user_data);\n" cbname
     | OFlags _ -> ()
   ) optargs;
   pr "  return py_ret;\n";
@@ -613,9 +608,11 @@ let generate_python_methods_c () =
   pr "{\n";
   pr "  struct user_data *data = user_data;\n";
   pr "\n";
-  pr "  Py_XDECREF (data->fn);\n";
-  pr "  Py_XDECREF (data->buf);\n";
-  pr "  free (data);\n";
+  pr "  if (data) {\n";
+  pr "    Py_XDECREF (data->fn);\n";
+  pr "    Py_XDECREF (data->buf);\n";
+  pr "    free (data);\n";
+  pr "  }\n";
   pr "}\n";
   pr "\n";
 

python/handle.c
@@ -1,5 +1,5 @@
 /* NBD client library in userspace
- * Copyright (C) 2013-2019 Red Hat Inc.
+ * Copyright (C) 2013-2020 Red Hat Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -91,7 +91,8 @@ free_aio_buffer (PyObject *capsule)
 {
   struct py_aio_buffer *buf = PyCapsule_GetPointer (capsule, aio_buffer_name);
 
-  free (buf->data);
+  if (buf)
+    free (buf->data);
   free (buf);
 }
 

python/utils.c
@@ -60,15 +60,24 @@ nbd_internal_py_get_string_list (PyObject *obj)
 
   for (i = 0; i < len; ++i) {
     PyObject *bytes = PyUnicode_AsUTF8String (PyList_GetItem (obj, i));
+    if (!bytes)
+      goto err;
     r[i] = strdup (PyBytes_AS_STRING (bytes));
+    Py_DECREF (bytes);
     if (r[i] == NULL) {
       PyErr_NoMemory ();
-      return NULL;
+      goto err;
     }
   }
   r[len] = NULL;
 
   return r;
+
+ err:
+  while (i--)
+    free (r[i]);
+  free (r);
+  return NULL;
 }
 
 void