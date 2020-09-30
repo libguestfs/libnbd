3d846fe2123200f4c031aa2953dbe92d2d6cb33b
Eric Blake <eblake@redhat.com>
2020-09-30 15:45:19 -0500

    tests: Adjust opt-list for latest nbdkit
    As previously noted in our tests, nbdkit .list_exports is not a stable
    interface until nbdkit 1.24.  As such, recent nbdkit commit 3c06520edc
    decided that in the name of security, allowing more than one
    NBD_OPT_LIST per connection is too risky for abuse as an amplification
    attack (for a lengthy list, the server sends far more reply traffic
    than the client requested, which can be abused if the client is not
    serious about connecting), and this breaks our tests that repeat it on
    a single connection.
    
    Hence, we have to fix opt-list (and its variants in other languages)
    to use means other than repeated nbd_opt_list() calls on a single
    connection to test the various control paths.
    
    For python, the changes to the test are more concise by use of a
    context manager.  For ocaml and go, I opted for a helper function that
    takes an anonymous function argument.  Hopefully my lack of
    familiarity with language idioms does not show too badly through my
    efforts to avoid pointless repetition of the setup/teardown code.
    
    (cherry picked from commit 6f73b530f7cc5280bb7afd4dc5a6913c7f3573a4)

--- ocaml/tests/test_220_opt_list.ml
+++ ocaml/tests/test_220_opt_list.ml
@@ -33,6 +33,18 @@ let f user_data name desc =
   exports := !exports @ [name];
   0
 
+let conn mode body expect =
+  exports := [];
+  let nbd = NBD.create () in
+  NBD.set_opt_mode nbd true;
+  let mode = sprintf "mode=%d" mode in
+  NBD.connect_command nbd
+                      ["nbdkit"; "-s"; "--exit-with-parent"; "-v";
+                       "sh"; script; mode];
+  body nbd;
+  assert (!exports = expect);
+  NBD.opt_abort nbd
+
 let () =
   (* Require new-enough nbdkit *)
   let cmd = "nbdkit sh --dump-plugin | grep -q has_list_exports=1" in
@@ -40,40 +52,35 @@ let () =
     exit 77
   );
 
-  let nbd = NBD.create () in
-  NBD.set_opt_mode nbd true;
-  NBD.connect_command nbd
-                      ["nbdkit"; "-s"; "--exit-with-parent"; "-v";
-                       "sh"; script];
-
   (* First pass: server fails NBD_OPT_LIST *)
-  exports := [];
-  ( try
+  conn 0 (
+    fun (nbd) ->
+    try
       let _ = NBD.opt_list nbd (f 42) in
-      assert (false)
+      assert false
     with
       NBD.Error (errstr, errno) -> ()
-  );
-  assert (!exports = []);
+    ) [];
 
   (* Second pass: server advertises 'a' and 'b' *)
-  exports := [];
-  let count = NBD.opt_list nbd (f 42) in
-  assert (count = 2);
-  assert (!exports = [ "a"; "b" ]);
+  conn 1 (
+    fun (nbd) ->
+    let count = NBD.opt_list nbd (f 42) in
+    assert (count = 2);
+    ) [ "a"; "b" ];
 
   (* Third pass: server advertises empty list *)
-  exports := [];
-  let count = NBD.opt_list nbd (f 42) in
-  assert (count = 0);
-  assert (!exports = []);
+  conn 2 (
+    fun (nbd) ->
+    let count = NBD.opt_list nbd (f 42) in
+    assert (count = 0);
+    ) [];
 
   (* Final pass: server advertises 'a' *)
-  exports := [];
-  let count = NBD.opt_list nbd (f 42) in
-  assert (count = 1);
-  assert (!exports = [ "a" ]);
-
-  NBD.opt_abort nbd
+  conn 3 (
+    fun (nbd) ->
+    let count = NBD.opt_list nbd (f 42) in
+    assert (count = 1);
+    ) [ "a" ]
 
 let () = Gc.compact ()
