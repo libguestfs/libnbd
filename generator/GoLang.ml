libguestfs/libnbd

commit 5fa60dc7d8ca5af271116546c7eebdafea5138e4

generator: Add RSizeT type, maps to C ssize_t
Counterpart to previous parameter addition, for round-trip use.

generator/GoLang.ml
@@ -96,6 +96,7 @@ let go_ret_type = function
   | RInt -> Some "uint"
   | RInt64 -> Some "uint64"
   | RCookie -> Some "uint64"
+  | RSizeT -> Some "int"
   | RString -> Some "*string"
   (* RUInt returns (type, error) for consistency, but the error is
    * always nil unless h is closed
@@ -107,28 +108,16 @@ let go_ret_type = function
 let go_ret_error = function
   | RErr -> None
   | RBool -> Some "false"
-  | RStaticString -> Some "nil"
-  | RFd -> Some "0"
-  | RInt -> Some "0"
-  | RInt64 -> Some "0"
-  | RCookie -> Some "0"
-  | RString -> Some "nil"
-  | RUInt -> Some "0"
-  | REnum _ -> Some "0"
-  | RFlags _ -> Some "0"
+  | RStaticString | RString -> Some "nil"
+  | RFd | RInt | RInt64 | RCookie | RSizeT | RUInt
+  | REnum _ | RFlags _ -> Some "0"
 
 let go_ret_c_errcode = function
   | RBool -> Some "-1"
   | RStaticString -> Some "nil"
-  | RErr -> Some "-1"
-  | RFd -> Some "-1"
-  | RInt -> Some "-1"
-  | RInt64 -> Some "-1"
-  | RCookie -> Some "-1"
+  | RErr | RFd | RInt | RInt64 | RCookie | RSizeT -> Some "-1"
   | RString -> Some "nil"
-  | RUInt -> None
-  | REnum _ -> None
-  | RFlags _ -> None
+  | RUInt | REnum _ | RFlags _ -> None
 
 (* We need a wrapper around every function (except Close) to
  * handle errors because cgo calls are sequence points and
@@ -392,6 +381,8 @@ let print_binding (name, { args; optargs; ret; shortdesc }) =
       pr "    return uint64 (ret), nil\n"
    | RCookie ->
       pr "    return uint64 (ret), nil\n"
+   | RSizeT ->
+      pr "    return int (ret), nil\n"
    | RString ->
       pr "    r := C.GoString (ret)\n";
       pr "    C.free (unsafe.Pointer (ret))\n";

generator/API.ml
@@ -65,6 +65,7 @@ and ret =
 | RInt
 | RInt64
 | RCookie
+| RSizeT
 | RString
 | RUInt
 | REnum of enum

generator/API.mli
@@ -77,6 +77,7 @@ and ret =
 | RInt                     (** return a small int, -1 = error *)
 | RInt64                   (** 64 bit int, -1 = error *)
 | RCookie                  (** 64 bit command cookie (>= 1), -1 = error *)
+| RSizeT                   (** return a count like ssize_t, -1 = error *)
 | RString                  (** return a newly allocated string,
                                caller frees, NULL for error *)
 | RUInt                    (** return a bitmask, no error possible *)

generator/C.ml
@@ -66,7 +66,7 @@ let generate_lib_libnbd_syms () =
 
 let errcode_of_ret =
   function
-  | RBool | RErr | RFd | RInt | RInt64 | RCookie -> Some "-1"
+  | RBool | RErr | RFd | RInt | RInt64 | RCookie | RSizeT -> Some "-1"
   | RStaticString | RString -> Some "NULL"
   | RUInt | REnum _ | RFlags _ -> None (* errors not possible *)
 
@@ -74,6 +74,7 @@ let type_of_ret =
   function
   | RBool | RErr | RFd | RInt | REnum _ -> "int"
   | RInt64 | RCookie -> "int64_t"
+  | RSizeT -> "ssize_t"
   | RStaticString -> "const char *"
   | RString -> "char *"
   | RUInt -> "unsigned"
@@ -712,13 +713,14 @@ let generate_lib_api_c () =
         pr "      char *ret_printable =\n";
         pr "          nbd_internal_printable_string (ret);\n"
      | RBool | RErr | RFd | RInt
-     | RInt64 | RCookie
+     | RInt64 | RCookie | RSizeT
      | RUInt | REnum _ | RFlags _ -> ()
     );
     pr "      debug (h, \"leave: ret=\" ";
     (match ret with
      | RBool | RErr | RFd | RInt | REnum _ -> pr "\"%%d\", ret"
      | RInt64 | RCookie -> pr "\"%%\" PRIi64 \"\", ret"
+     | RSizeT -> pr "\"%%zd\", ret"
      | RStaticString | RString ->
         pr "\"%%s\", ret_printable ? ret_printable : \"\""
      | RUInt | RFlags _ -> pr "\"%%u\", ret"
@@ -727,7 +729,7 @@ let generate_lib_api_c () =
     (match ret with
      | RStaticString | RString -> pr "      free (ret_printable);\n"
      | RBool | RErr | RFd | RInt
-     | RInt64 | RCookie
+     | RInt64 | RCookie | RSizeT
      | RUInt | REnum _ | RFlags _ -> ()
     );
     pr "    }\n";
@@ -869,6 +871,8 @@ let generate_docs_nbd_pod name { args; optargs; ret;
       pr "This call returns the 64 bit cookie of the command.\n";
       pr "The cookie is E<ge> C<1>.\n";
       pr "Cookies are unique (per libnbd handle, not globally).\n"
+   | RSizeT ->
+      pr "This call returns an integer size E<ge> C<0>.\n";
    | RString ->
       pr "This call returns a string.  The caller must free the\n";
       pr "returned string to avoid a memory leak.\n";

generator/OCaml.ml
@@ -65,6 +65,7 @@ and ocaml_ret_to_string = function
   | RInt -> "int"
   | RInt64 -> "int64"
   | RCookie -> "cookie"
+  | RSizeT -> "int"
   | RString -> "string"
   | RUInt -> "int"
   | REnum { enum_prefix } -> enum_prefix ^ ".t"
@@ -716,7 +717,7 @@ let print_ocaml_binding (name, { args; optargs; ret }) =
   (match ret with
    | RBool -> pr "  rv = Val_bool (r);\n"
    | RErr -> pr "  rv = Val_unit;\n"
-   | RFd | RInt | RUInt -> pr "  rv = Val_int (r);\n"
+   | RFd | RInt | RSizeT | RUInt -> pr "  rv = Val_int (r);\n"
    | REnum { enum_prefix } -> pr "  rv = Val_%s (r);\n" enum_prefix
    | RFlags { flag_prefix } -> pr "  rv = Val_%s (r);\n" flag_prefix
    | RInt64 | RCookie -> pr "  rv = caml_copy_int64 (r);\n"

generator/Python.ml
@@ -531,7 +531,7 @@ let print_python_binding name { args; optargs; ret; may_set_error } =
     | RErr ->
        pr "  py_ret = Py_None;\n";
        pr "  Py_INCREF (py_ret);\n"
-    | RFd | RInt | REnum _ | RFlags _ | RUInt ->
+    | RFd | RInt | REnum _ | RFlags _ | RSizeT | RUInt ->
        pr "  py_ret = PyLong_FromLong (ret);\n"
     | RInt64 | RCookie ->
        pr "  py_ret = PyLong_FromLongLong (ret);\n"

commit 42d432ddbbb3b4a9a983a43a10eedd48a164b4d9

golang: Fix binding for RSizeT
Although ssize_t is signed in C, we are really returning either a size
(unsigned) or an error, which fits more naturally in Go as 'uint, err'.

Fixes: 5fa60dc7d8

generator/GoLang.ml
@@ -96,7 +96,7 @@ let go_ret_type = function
   | RInt -> Some "uint"
   | RInt64 -> Some "uint64"
   | RCookie -> Some "uint64"
-  | RSizeT -> Some "int"
+  | RSizeT -> Some "uint"
   | RString -> Some "*string"
   (* RUInt returns (type, error) for consistency, but the error is
    * always nil unless h is closed
@@ -382,7 +382,7 @@ let print_binding (name, { args; optargs; ret; shortdesc }) =
    | RCookie ->
       pr "    return uint64 (ret), nil\n"
    | RSizeT ->
-      pr "    return int (ret), nil\n"
+      pr "    return uint (ret), nil\n"
    | RString ->
       pr "    r := C.GoString (ret)\n";
       pr "    C.free (unsafe.Pointer (ret))\n";