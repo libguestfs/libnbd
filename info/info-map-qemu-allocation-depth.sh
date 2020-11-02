libguestfs/libnbd

commit 71455c021ff99c3b270502a282aa3d650e7ecc4f

info: Add support for new 'qemu-nbd -A' qemu:allocation-depth
A rather trivial decoding; we may enhance it further if qemu extends
things to give an integer depth alongside its tri-state encoding.

info/nbdinfo.c
@@ -784,6 +784,13 @@ extent_description (const char *metacontext, uint32_t type)
     case 1: return "dirty";
     }
   }
+  else if (strcmp (metacontext, "qemu:allocation-depth") == 0) {
+    switch (type & 3) {
+    case 0: return "unallocated";
+    case 1: return "local";
+    case 2: return "backing";
+    }
+  }
 
   return NULL;   /* Don't know - description field will be omitted. */
 }

commit 123a62177b27de40e8b49966cd66a07d18ca0eb0

info: Adjust to actual 'qemu-nbd -A' semantics
Review on the qemu list has led to an altered definition of what
'qemu:allocation-depth' should report: rather than a tri-state value,
it is an actual depth.  It's time to match what actually got committed
into qemu, which in turn means a slight refactoring to use a malloc'd
string for a description.  And testsuite coverage never hurts.

Fixes: 71455c021

info/Makefile.am
@@ -33,6 +33,7 @@ info_sh_files = \
 	info-map-base-allocation-json.sh \
 	info-map-base-allocation-large.sh \
 	info-map-qemu-dirty-bitmap.sh \
+	info-map-qemu-allocation-depth.sh \
 	info-atomic-output.sh \
 	$(NULL)
 

info/info-map-qemu-allocation-depth.sh
@@ -1,0 +1,90 @@
+#!/usr/bin/env bash
+# nbd client library in userspace
+# Copyright (C) 2020 Red Hat Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+# Attempt to test using nbdinfo --map=qemu:dirty-bitmap.
+# See also interop/dirty-bitmap.{c,sh}
+
+. ../tests/functions.sh
+
+set -e
+set -x
+
+requires qemu-img --version
+requires qemu-io --version
+requires qemu-nbd -A --version
+requires tr --version
+
+base=info-map-qemu-allocation-depth
+f1=$base.1.qcow2
+f2=$base.2.qcow2
+f3=$base.3.qcow2
+f4=$base.4.qcow2
+out=$base.out
+cleanup_fn rm -f $f1 $f2 $f3 $f4 $out
+rm -f $f1 $f2 $f3 $f4 $out
+
+# Create chain of files:
+#    f1: XXXX----
+#    f2: --XXXX--
+#    f3: -XX--XX-
+#    f4: -X---X--
+# depth: 41233120
+qemu-img create -f qcow2 $f1 8M
+qemu-io -f qcow2 -c 'w 0 4M' $f1
+qemu-img create -f qcow2 -b $f1 -F qcow2 $f2
+qemu-io -f qcow2 -c 'w 2M 4M' $f2
+qemu-img create -f qcow2 -b $f2 -F qcow2 $f3
+qemu-io -f qcow2 -c 'w 1M 2M' -c 'w 5M 2M' $f3
+qemu-img create -f qcow2 -b $f3 -F qcow2 $f4
+qemu-io -f qcow2 -c 'w 1M 1M' -c 'w 5M 1M' $f4
+
+# We have to run qemu-nbd and attempt to clean it up afterwards.
+sock=$(mktemp -u /tmp/libnbd-test-info.XXXXXX)
+pid=$base.pid
+cleanup_fn rm -f $sock $pid
+rm -f $sock $pid
+
+qemu-nbd -t --socket=$sock --pid-file=$pid -f qcow2 -A $f4 &
+cleanup_fn kill $!
+
+# Wait for qemu-nbd to start up.
+for i in {1..60}; do
+    if test -f $pid; then
+        break
+    fi
+    sleep 1
+done
+if ! test -f $pid; then
+    echo "$0: qemu-nbd did not start up"
+    exit 1
+fi
+
+$VG nbdinfo --map=qemu:allocation-depth "nbd+unix://?socket=$sock" > $out
+cat $out
+
+if [ "$(tr -s ' ' < $out)" != "\
+ 0 1048576 4 backing depth 4
+ 1048576 1048576 1 local
+ 2097152 1048576 2 backing depth 2
+ 3145728 2097152 3 backing depth 3
+ 5242880 1048576 1 local
+ 6291456 1048576 2 backing depth 2
+ 7340032 1048576 0 unallocated" ]; then
+    echo "$0: unexpected output from nbdinfo --map"
+    exit 1
+fi

info/nbdinfo.c
@@ -767,28 +767,30 @@ get_content (struct nbd_handle *nbd, int64_t size)
 }
 
 /* Callback handling --map. */
-static const char *
+static char *
 extent_description (const char *metacontext, uint32_t type)
 {
+  char *ret;
+
   if (strcmp (metacontext, "base:allocation") == 0) {
     switch (type) {
-    case 0: return "allocated";
-    case 1: return "hole";
-    case 2: return "zero";
-    case 3: return "hole,zero";
+    case 0: return strdup ("allocated");
+    case 1: return strdup ("hole");
+    case 2: return strdup ("zero");
+    case 3: return strdup ("hole,zero");
     }
   }
   else if (strncmp (metacontext, "qemu:dirty-bitmap:", 18) == 0) {
     switch (type) {
-    case 0: return "clean";
-    case 1: return "dirty";
+    case 0: return strdup ("clean");
+    case 1: return strdup ("dirty");
     }
   }
   else if (strcmp (metacontext, "qemu:allocation-depth") == 0) {
-    switch (type & 3) {
-    case 0: return "unallocated";
-    case 1: return "local";
-    case 2: return "backing";
+    switch (type) {
+    case 0: return strdup ("unallocated");
+    case 1: return strdup ("local");
+    default: asprintf (&ret, "backing depth %u", type); return ret;
     }
   }
 
@@ -810,7 +812,7 @@ extent_callback (void *user_data, const char *metacontext,
 
   /* Print the entries received. */
   for (i = 0; i < nr_entries; i += 2) {
-    const char *descr = extent_description (map, entries[i+1]);
+    char *descr = extent_description (map, entries[i+1]);
 
     if (!json_output) {
       fprintf (fp, "%10" PRIu64 "  "
@@ -837,6 +839,7 @@ extent_callback (void *user_data, const char *metacontext,
       comma = true;
     }
 
+    free (descr);
     offset += entries[i];
   }
 