libguestfs/libnbd

commit f153a1b771bac1dddf87f524affa2e081fea905d

copy: Add a new test of copying to a pipe.

copy/copy-nbd-to-hexdump.sh
@@ -1,0 +1,46 @@
+#!/usr/bin/env bash
+# nbd client library in userspace
+# Copyright (C) 2020 Red Hat Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+. ../tests/functions.sh
+
+set -e
+set -x
+
+requires nbdkit --exit-with-parent --version
+requires hexdump --version
+
+file=copy-nbd-to-hexdump.file
+cleanup_fn rm -f $file
+
+$VG nbdcopy -- [ nbdkit --exit-with-parent \
+                        data data=' ( "Hello" )*2000 ' size=8192 \
+               ] - | hexdump -C | tail > $file
+cat $file
+
+# Test the data is as expected.
+test "$(cat $file)" = \
+'00001f70  6c 6f 48 65 6c 6c 6f 48  65 6c 6c 6f 48 65 6c 6c  |loHelloHelloHell|
+00001f80  6f 48 65 6c 6c 6f 48 65  6c 6c 6f 48 65 6c 6c 6f  |oHelloHelloHello|
+00001f90  48 65 6c 6c 6f 48 65 6c  6c 6f 48 65 6c 6c 6f 48  |HelloHelloHelloH|
+00001fa0  65 6c 6c 6f 48 65 6c 6c  6f 48 65 6c 6c 6f 48 65  |elloHelloHelloHe|
+00001fb0  6c 6c 6f 48 65 6c 6c 6f  48 65 6c 6c 6f 48 65 6c  |lloHelloHelloHel|
+00001fc0  6c 6f 48 65 6c 6c 6f 48  65 6c 6c 6f 48 65 6c 6c  |loHelloHelloHell|
+00001fd0  6f 48 65 6c 6c 6f 48 65  6c 6c 6f 48 65 6c 6c 6f  |oHelloHelloHello|
+00001fe0  48 65 6c 6c 6f 48 65 6c  6c 6f 48 65 6c 6c 6f 48  |HelloHelloHelloH|
+00001ff0  65 6c 6c 6f 48 65 6c 6c  6f 48 65 6c 6c 6f 48 65  |elloHelloHelloHe|
+00002000'

copy/Makefile.am
@@ -23,6 +23,7 @@ EXTRA_DIST = \
 	copy-file-to-nbd.sh \
 	copy-nbd-to-block.sh \
 	copy-nbd-to-file.sh \
+	copy-nbd-to-hexdump.sh \
 	copy-nbd-to-nbd.sh \
 	copy-nbd-to-small-block-error.sh \
 	copy-nbd-to-small-nbd-error.sh \
@@ -96,6 +97,7 @@ TESTS += \
 	copy-file-to-file.sh \
 	copy-file-to-nbd.sh \
 	copy-nbd-to-file.sh \
+	copy-nbd-to-hexdump.sh \
 	copy-nbd-to-nbd.sh \
 	copy-nbd-to-small-nbd-error.sh \
 	copy-nbd-to-sparse-file.sh \

commit 6601454980b473d57241cf68f0334ea6177b6bae

copy: Fix requires for test copy-nbd-to-hexdump.sh
Fixes: commit f153a1b771bac1dddf87f524affa2e081fea905d

copy/copy-nbd-to-hexdump.sh
@@ -24,6 +24,10 @@ set -x
 requires nbdkit --exit-with-parent --version
 requires hexdump --version
 
+# This test requires nbdkit >= 1.22.
+minor=$( nbdkit --dump-config | grep ^version_minor | cut -d= -f2 )
+requires test $minor -gt 22
+
 file=copy-nbd-to-hexdump.file
 cleanup_fn rm -f $file
 