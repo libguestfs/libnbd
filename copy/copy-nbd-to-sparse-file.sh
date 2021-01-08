libguestfs/libnbd

commit 28fe8d9d8d1ecb491070d20f22e2f34bb147f19f

copy: Add sparseness detection.

copy/copy-nbd-to-sparse-file.sh
@@ -1,0 +1,49 @@
+#!/usr/bin/env bash
+# nbd client library in userspace
+# Copyright (C) 2020 Red Hat Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+. ../tests/functions.sh
+
+set -e
+set -x
+
+requires cmp --version
+requires dd --version
+requires dd oflag=seek_bytes </dev/null
+requires test -r /dev/urandom
+requires test -r /dev/zero
+
+file=copy-nbd-to-sparse-file.file
+file2=copy-nbd-to-sparse-file.file2
+cleanup_fn rm -f $file $file2
+
+# Start with a random, but partially sparse file, apply nbdkit on top.
+# Copy it and make sure that it doesn't get corrupted.
+
+touch $file
+
+for i in `seq 1 100`; do
+    dd if=/dev/urandom of=$file ibs=512 count=1 \
+       oflag=seek_bytes seek=$((RANDOM * 9973))
+    dd if=/dev/zero of=$file ibs=512 count=1 \
+       oflag=seek_bytes seek=$((RANDOM * 9973))
+done
+
+nbdcopy -S 512 -- [ nbdkit --exit-with-parent file $file ] $file2
+
+ls -ls $file $file2
+cmp $file $file2

TODO
@@ -31,8 +31,6 @@ Examine other fuzzers: https://gitlab.com/akihe/radamsa
 
 nbdcopy:
  - --synchronous mode does not yet support extents.
- - Detect zeroes (optionally) and turn into sparseness
-   (like qemu-img convert -S).
  - Progress bar: allow it to be written to a file descriptor
    and/or written in a machine-consumable format.
  - Minimum/preferred/maximum block size.

common/include/rounding.h
@@ -1,0 +1,57 @@
+/* nbdkit
+ * Copyright (C) 2018 Red Hat Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Red Hat nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY RED HAT AND CONTRIBUTORS ''AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RED HAT OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef NBDKIT_ROUNDING_H
+#define NBDKIT_ROUNDING_H
+
+#include <assert.h>
+
+#include "ispowerof2.h"
+
+/* Round up i to next multiple of n (n must be a power of 2).
+ */
+#define ROUND_UP(i, n) ({                          \
+      assert (is_power_of_2 (n));                  \
+      ((i) + (n) - 1) & -((typeof (i))(n));        \
+})
+
+/* Round down i to next multiple of n (n must be a power of 2).
+ */
+#define ROUND_DOWN(i, n) ({                         \
+      assert (is_power_of_2 (n));                   \
+      (i) & -((typeof (i))(n));                     \
+})
+
+/* Return n / d, rounding the result up to the next integer. */
+#define DIV_ROUND_UP(n, d) (((n) + (d) - 1) / (d))
+
+#endif /* NBDKIT_ROUNDING_H */

copy/Makefile.am
@@ -21,11 +21,13 @@ EXTRA_DIST = \
 	copy-block-to-nbd.sh \
 	copy-file-to-file.sh \
 	copy-file-to-nbd.sh \
+	copy-file-to-sparse-file.sh \
 	copy-nbd-to-block.sh \
 	copy-nbd-to-file.sh \
 	copy-nbd-to-nbd.sh \
 	copy-nbd-to-small-block-error.sh \
 	copy-nbd-to-small-nbd-error.sh \
+	copy-nbd-to-sparse-file.sh \
 	copy-nbd-to-stdout.sh \
 	copy-sparse.sh \
 	copy-sparse-allocated.sh \
@@ -89,9 +91,11 @@ ROOT_TESTS = \
 TESTS += \
 	copy-file-to-file.sh \
 	copy-file-to-nbd.sh \
+	copy-file-to-sparse-file.sh \
 	copy-nbd-to-file.sh \
 	copy-nbd-to-nbd.sh \
 	copy-nbd-to-small-nbd-error.sh \
+	copy-nbd-to-sparse-file.sh \
 	copy-stdin-to-nbd.sh \
 	copy-nbd-to-stdout.sh \
 	copy-sparse.sh \

copy/copy-file-to-sparse-file.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 # nbd client library in userspace
-# Copyright (C) 2013-2020 Red Hat Inc.
+# Copyright (C) 2020 Red Hat Inc.
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -15,12 +16,34 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-include $(top_srcdir)/subdir-rules.mk
+. ../tests/functions.sh
 
-EXTRA_DIST = \
-	byte-swapping.h \
-	isaligned.h \
-	ispowerof2.h \
-	iszero.h \
-	rounding.h \
-	$(NULL)
+set -e
+set -x
+
+requires cmp --version
+requires dd --version
+requires dd oflag=seek_bytes </dev/null
+requires test -r /dev/urandom
+requires test -r /dev/zero
+
+file=copy-file-to-sparse-file.file
+file2=copy-file-to-sparse-file.file2
+cleanup_fn rm -f $file $file2
+
+# Start with a random, but partially sparse file.  Copy it and make
+# sure that it doesn't get corrupted.
+
+touch $file
+
+for i in `seq 1 100`; do
+    dd if=/dev/urandom of=$file ibs=512 count=1 \
+       oflag=seek_bytes seek=$((RANDOM * 9973))
+    dd if=/dev/zero of=$file ibs=512 count=1 \
+       oflag=seek_bytes seek=$((RANDOM * 9973))
+done
+
+$VG nbdcopy -S 512 $file $file2
+
+ls -ls $file $file2
+cmp $file $file2

copy/copy-sparse-no-extents.sh
@@ -39,7 +39,7 @@ requires nbdkit eval --version
 out=copy-sparse-no-extents.out
 cleanup_fn rm -f $out
 
-$VG nbdcopy --no-extents -- \
+$VG nbdcopy --no-extents -S 0 -- \
     [ nbdkit --exit-with-parent data data='
              1
              @1073741823 1

copy/copy-sparse.sh
@@ -35,7 +35,7 @@ cleanup_fn rm -f $out
 # is logging everything.  This allows us to see exactly what nbdcopy
 # is writing, to ensure it is writing and trimming the target as
 # expected.
-$VG nbdcopy -- \
+$VG nbdcopy -S 0 -- \
     [ nbdkit --exit-with-parent data data='
              1
              @1073741823 1

copy/main.c
@@ -37,6 +37,7 @@
 
 #include <libnbd.h>
 
+#include "ispowerof2.h"
 #include "nbdcopy.h"
 
 bool allocated;                 /* --allocated flag */
@@ -46,6 +47,7 @@ bool extents = true;            /* ! --no-extents flag */
 bool flush;                     /* --flush flag */
 unsigned max_requests = 64;     /* --requests */
 bool progress;                  /* -p flag */
+unsigned sparse_size = 512;     /* --sparse */
 bool synchronous;               /* --synchronous flag */
 unsigned threads;               /* --threads */
 struct rw src, dst;             /* The source and destination. */
@@ -97,7 +99,7 @@ main (int argc, char *argv[])
     NO_EXTENTS_OPTION,
     SYNCHRONOUS_OPTION,
   };
-  const char *short_options = "C:pR:T:V";
+  const char *short_options = "C:pR:S:T:V";
   const struct option long_options[] = {
     { "help",               no_argument,       NULL, HELP_OPTION },
     { "long-options",       no_argument,       NULL, LONG_OPTIONS },
@@ -109,6 +111,7 @@ main (int argc, char *argv[])
     { "progress",           no_argument,       NULL, 'p' },
     { "requests",           required_argument, NULL, 'R' },
     { "short-options",      no_argument,       NULL, SHORT_OPTIONS },
+    { "sparse",             required_argument, NULL, 'S' },
     { "synchronous",        no_argument,       NULL, SYNCHRONOUS_OPTION },
     { "target-is-zero",     no_argument,       NULL, DESTINATION_IS_ZERO_OPTION },
     { "threads",            required_argument, NULL, 'T' },
@@ -178,6 +181,20 @@ main (int argc, char *argv[])
       if (sscanf (optarg, "%u", &max_requests) != 1 || max_requests == 0) {
         fprintf (stderr, "%s: --requests: could not parse: %s\n",
                  argv[0], optarg);
+        exit (EXIT_FAILURE);
+      }
+      break;
+
+    case 'S':
+      if (sscanf (optarg, "%u", &sparse_size) != 1) {
+        fprintf (stderr, "%s: --sparse: could not parse: %s\n",
+                 argv[0], optarg);
+        exit (EXIT_FAILURE);
+      }
+      if (sparse_size != 0 &&
+          (sparse_size < 512 || !is_power_of_2 (sparse_size))) {
+        fprintf (stderr, "%s: --sparse: must be a power of 2 and >= 512\n",
+                 argv[0]);
         exit (EXIT_FAILURE);
       }
       break;

commit 781cb44b63a87f2d5f40590ab8c446ad2e7b6702

copy/copy-nbd-to-sparse-file.sh: Skip test unless nbdkit available.
This test used nbdkit without checking it is available, which broke
the test on RHEL 8 i686.

Fixes: commit 28fe8d9d8d1ecb491070d20f22e2f34bb147f19f

copy/copy-nbd-to-sparse-file.sh
@@ -24,6 +24,7 @@ set -x
 requires cmp --version
 requires dd --version
 requires dd oflag=seek_bytes </dev/null
+requires nbdkit --version
 requires test -r /dev/urandom
 requires test -r /dev/zero
 