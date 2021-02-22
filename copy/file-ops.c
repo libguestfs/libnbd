libguestfs/libnbd

commit 747ce8dd263874fbecb6cbee21449c5c102c9a99

copy: Avoid storing struct stat in file-ops.
We only used two fields from struct stat, st_mode and st_size.
Instead of passing and storing the whole struct, only store the fields
we care about.

Updates: commit 48d1e2066c1f9ea73cbad19e86565960ef7fbebf

copy/file-ops.c
@@ -41,7 +41,7 @@ static struct rw_ops file_ops;
 struct rw_file {
   struct rw rw;
   int fd;
-  struct stat stat;
+  bool is_block;
   bool seek_hole_supported;
   int sector_size;
 };
@@ -58,18 +58,17 @@ seek_hole_supported (int fd)
 }
 
 struct rw *
-file_create (const char *name, const struct stat *stat, int fd)
+file_create (const char *name, int fd, off_t st_size, bool is_block)
 {
   struct rw_file *rwf = calloc (1, sizeof *rwf);
   if (rwf == NULL) { perror ("calloc"); exit (EXIT_FAILURE); }
 
   rwf->rw.ops = &file_ops;
   rwf->rw.name = name;
-  rwf->stat = *stat;
   rwf->fd = fd;
 
-  if (S_ISBLK (stat->st_mode)) {
-    /* Block device. */
+  if (is_block) {
+    /* Block device - ignore size passed in. */
     rwf->rw.size = lseek (fd, 0, SEEK_END);
     if (rwf->rw.size == -1) {
       perror ("lseek");
@@ -86,13 +85,11 @@ file_create (const char *name, const struct stat *stat, int fd)
       fprintf (stderr, "warning: cannot get sector size: %s: %m", name);
 #endif
   }
-  else if (S_ISREG (stat->st_mode)) {
+  else {
     /* Regular file. */
-    rwf->rw.size = stat->st_size;
+    rwf->rw.size = st_size;
     rwf->seek_hole_supported = seek_hole_supported (fd);
   }
-  else
-    abort ();
 
   return &rwf->rw;
 }
@@ -119,7 +116,7 @@ file_truncate (struct rw *rw, int64_t size)
    * truncate it to zero first so the file is completely empty and
    * sparse.
    */
-  if (! S_ISREG (rwf->stat.st_mode))
+  if (rwf->is_block)
     return;
 
   if (ftruncate (rwf->fd, 0) == -1 ||
@@ -138,9 +135,7 @@ file_flush (struct rw *rw)
 {
   struct rw_file *rwf = (struct rw_file *)rw;
 
-  if ((S_ISREG (rwf->stat.st_mode) ||
-       S_ISBLK (rwf->stat.st_mode)) &&
-      fsync (rwf->fd) == -1) {
+  if (fsync (rwf->fd) == -1) {
     perror (rw->name);
     exit (EXIT_FAILURE);
   }
@@ -249,7 +244,7 @@ file_synch_zero (struct rw *rw, uint64_t offset, uint64_t count)
 {
   struct rw_file *rwf = (struct rw_file *)rw;
 
-  if (S_ISREG (rwf->stat.st_mode)) {
+  if (! rwf->is_block) {
 #ifdef FALLOC_FL_ZERO_RANGE
     int fd = rwf->fd;
     int r;
@@ -262,8 +257,7 @@ file_synch_zero (struct rw *rw, uint64_t offset, uint64_t count)
     return true;
 #endif
   }
-  else if (S_ISBLK (rwf->stat.st_mode) &&
-           IS_ALIGNED (offset | count, rwf->sector_size)) {
+  else if (rwf->is_block && IS_ALIGNED (offset | count, rwf->sector_size)) {
 #ifdef BLKZEROOUT
     int fd = rwf->fd;
     int r;

copy/main.c
@@ -455,7 +455,7 @@ open_local (const char *prog, const char *filename, bool writing)
     exit (EXIT_FAILURE);
   }
   if (S_ISBLK (stat.st_mode) || S_ISREG (stat.st_mode))
-    return file_create (filename, &stat, fd);
+    return file_create (filename, fd, stat.st_size, S_ISBLK (stat.st_mode));
   else {
     /* Probably stdin/stdout, a pipe or a socket. */
     synchronous = true;        /* Force synchronous mode for pipes. */

copy/nbdcopy.h
@@ -22,7 +22,6 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 
 #include <libnbd.h>
 
@@ -49,8 +48,8 @@ struct rw {
 extern struct rw *src, *dst;
 
 /* Create subtypes. */
-extern struct rw *file_create (const char *name,
-                               const struct stat *stat, int fd);
+extern struct rw *file_create (const char *name, int fd,
+                               off_t st_size, bool is_block);
 extern struct rw *nbd_rw_create_uri (const char *name,
                                      const char *uri, bool writing);
 extern struct rw *nbd_rw_create_subprocess (const char **argv, size_t argc,

commit 24a5ba6e182235999065db34a33c42761114a1d0

copy/file-ops.c: Save is_block in the internal struct.
Fixes: commit 747ce8dd263874fbecb6cbee21449c5c102c9a99

copy/file-ops.c
@@ -66,6 +66,7 @@ file_create (const char *name, int fd, off_t st_size, bool is_block)
   rwf->rw.ops = &file_ops;
   rwf->rw.name = name;
   rwf->fd = fd;
+  rwf->is_block = is_block;
 
   if (is_block) {
     /* Block device - ignore size passed in. */