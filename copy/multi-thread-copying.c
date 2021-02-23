0e96b58d2856852dfc8caae871fa60834f844ad2
Nir Soffer <nirsof@gmail.com>
2021-02-23 15:41:26 +0200

    copy: Fix sparsifying if start is not aligned
    When using --sparse=, we may have a case when command start is not
    aligned to sparse_size and command length is smaller than sparse_size.
    In this case we advance i after the end of the command, and abort when
    we try to write after end.
    
    To reproduce this issue (with temporary debug logs):
    
    $ ./nbdcopy --sparse=$((64*1024)) --threads=1 --requests=1 nbd+unix:///?socket=/tmp/src.sock null:
    sparsifying range start=0 end=4096 len=4096
    write unaligned tail offset=0 end=4096 len=4096
    sparsifying range start=1048576 end=1114112 len=65536
    write last data start=1048576 end=1114112 len=65536
    sparsifying range start=2097152 end=2121728 len=24576
    write unaligned tail offset=2097152 end=2121728 len=24576
    sparsifying range start=2138112 end=2146304 len=8192
    write last data start=2138112 end=2162688 len=24576
    lt-nbdcopy: multi-thread-copying.c:351: copy_subcommand: Assertion `offset + len <= end' failed.
    Aborted (core dumped)
    
    Fixed by clipping initial value of i to end of the image.

--- copy/multi-thread-copying.c
+++ copy/multi-thread-copying.c
@@ -367,6 +367,12 @@ copy_subcommand (struct command *command, uint64_t offset, size_t len,
   return newcommand;
 }
 
+static inline uint64_t
+min(uint64_t a, uint64_t b)
+{
+    return a < b ? a : b;
+}
+
 /* Callback called when src has finished one read command.  This
  * initiates a write.
  */
@@ -396,7 +402,7 @@ finished_read (void *vp, int *error)
     /* Iterate over whole blocks in the command, starting on a block
      * boundary.
      */
-    for (i = ROUND_UP (start, sparse_size);
+    for (i = min (ROUND_UP (start, sparse_size), end);
          i + sparse_size <= end;
          i += sparse_size) {
       if (is_zero (slice_ptr (command->slice) + i-start, sparse_size)) {
