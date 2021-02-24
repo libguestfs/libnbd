libguestfs/libnbd

commit 7fb099adfbc9ee6626c3aa92254b9a91cfac12e9

copy: Implement extent metadata for efficient copying.
This implements these interconnected options and some basic tests.

  --allocated
  --destination-is-zero (alias: --target-is-zero)
  --no-extents

copy/nbd-ops.c
@@ -57,6 +57,37 @@ nbd_synch_write (struct rw *rw,
   }
 }
 
+static bool
+nbd_synch_trim (struct rw *rw, uint64_t offset, uint64_t count)
+{
+  assert (rw->t == NBD);
+
+  if (nbd_can_trim (rw->u.nbd.ptr[0]) == 0)
+    return false;
+
+  if (nbd_trim (rw->u.nbd.ptr[0], count, offset, 0) == -1) {
+    fprintf (stderr, "%s: %s\n", rw->name, nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  return true;
+}
+
+static bool
+nbd_synch_zero (struct rw *rw, uint64_t offset, uint64_t count)
+{
+  assert (rw->t == NBD);
+
+  if (nbd_can_zero (rw->u.nbd.ptr[0]) == 0)
+    return false;
+
+  if (nbd_zero (rw->u.nbd.ptr[0],
+                count, offset, LIBNBD_CMD_FLAG_NO_HOLE) == -1) {
+    fprintf (stderr, "%s: %s\n", rw->name, nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  return true;
+}
+
 static void
 nbd_asynch_read (struct rw *rw,
                  struct buffer *buffer,
@@ -87,9 +118,154 @@ nbd_asynch_write (struct rw *rw,
   }
 }
 
+static bool
+nbd_asynch_trim (struct rw *rw, struct buffer *buffer,
+                 nbd_completion_callback cb)
+{
+  assert (rw->t == NBD);
+
+  if (nbd_can_trim (rw->u.nbd.ptr[0]) == 0)
+    return false;
+
+  if (nbd_aio_trim (rw->u.nbd.ptr[buffer->index],
+                    buffer->len, buffer->offset,
+                    cb, 0) == -1) {
+    fprintf (stderr, "%s: %s\n", rw->name, nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  return true;
+}
+
+static bool
+nbd_asynch_zero (struct rw *rw, struct buffer *buffer,
+                 nbd_completion_callback cb)
+{
+  assert (rw->t == NBD);
+
+  if (nbd_can_zero (rw->u.nbd.ptr[0]) == 0)
+    return false;
+
+  if (nbd_aio_zero (rw->u.nbd.ptr[buffer->index],
+                    buffer->len, buffer->offset,
+                    cb, LIBNBD_CMD_FLAG_NO_HOLE) == -1) {
+    fprintf (stderr, "%s: %s\n", rw->name, nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  return true;
+}
+
+static int
+add_extent (void *vp, const char *metacontext,
+            uint64_t offset, uint32_t *entries, size_t nr_entries,
+            int *error)
+{
+  extent_list *ret = vp;
+  size_t i;
+
+  if (strcmp (metacontext, "base:allocation") != 0)
+    return 0;
+
+  for (i = 0; i < nr_entries; i += 2) {
+    struct extent e;
+
+    e.offset = offset;
+    e.length = entries[i];
+    /* Note we deliberately don't care about the ZERO flag. */
+    e.hole = (entries[i+1] & LIBNBD_STATE_HOLE) != 0;
+    if (extent_list_append (ret, e) == -1) {
+      perror ("realloc");
+      exit (EXIT_FAILURE);
+    }
+
+    offset += entries[i];
+  }
+
+  return 0;
+}
+
+/* This is done synchronously, but that's fine because commands from
+ * the previous work range in flight continue to run, it's difficult
+ * to (sanely) start new work until we have the full list of extents,
+ * and in almost every case the remote NBD server can answer our
+ * request for extents in a single round trip.
+ */
+static void
+nbd_get_extents (struct rw *rw, uintptr_t index,
+                  uint64_t offset, uint64_t count,
+                  extent_list *ret)
+{
+  extent_list exts = empty_vector;
+  struct nbd_handle *nbd;
+
+  assert (rw->t == NBD);
+  nbd = rw->u.nbd.ptr[index];
+
+  ret->size = 0;
+
+  while (count > 0) {
+    size_t i;
+
+    exts.size = 0;
+    if (nbd_block_status (nbd, count, offset,
+                          (nbd_extent_callback) {
+                            .user_data = &exts,
+                            .callback = add_extent
+                          }, 0) == -1) {
+      /* XXX We could call default_get_extents, but unclear if it's
+       * the right thing to do if the server is returning errors.
+       */
+      fprintf (stderr, "%s: %s\n", rw->name, nbd_get_error ());
+      exit (EXIT_FAILURE);
+    }
+
+    /* The server should always make progress. */
+    if (exts.size == 0) {
+      fprintf (stderr, "%s: NBD server is broken: it is not returning extent information.\nTry nbdcopy --no-extents as a workaround.\n",
+               rw->name);
+      exit (EXIT_FAILURE);
+    }
+
+    /* Copy the extents returned into the final list (ret).  This is
+     * complicated because the extents returned by the server may
+     * begin earlier and begin or end later than the requested size.
+     */
+    for (i = 0; i < exts.size; ++i) {
+      uint64_t d;
+
+      if (exts.ptr[i].offset + exts.ptr[i].length <= offset)
+        continue;
+      if (exts.ptr[i].offset < offset) {
+        d = offset - exts.ptr[i].offset;
+        exts.ptr[i].offset += d;
+        exts.ptr[i].length -= d;
+        assert (exts.ptr[i].offset == offset);
+      }
+      if (exts.ptr[i].offset + exts.ptr[i].length > offset + count) {
+        d = exts.ptr[i].offset + exts.ptr[i].length - offset - count;
+        exts.ptr[i].length -= d;
+        assert (exts.ptr[i].offset + exts.ptr[i].length == offset + count);
+      }
+      if (extent_list_append (ret, exts.ptr[i]) == -1) {
+        perror ("realloc");
+        exit (EXIT_FAILURE);
+      }
+
+      offset += exts.ptr[i].length;
+      count -= exts.ptr[i].length;
+    }
+  }
+
+  free (exts.ptr);
+}
+
 struct rw_ops nbd_ops = {
   .synch_read = nbd_synch_read,
   .synch_write = nbd_synch_write,
+  .synch_trim = nbd_synch_trim,
+  .synch_zero = nbd_synch_zero,
   .asynch_read = nbd_asynch_read,
   .asynch_write = nbd_asynch_write,
+  .asynch_trim = nbd_asynch_trim,
+  .asynch_zero = nbd_asynch_zero,
+  .get_extents = nbd_get_extents,
 };

TODO
@@ -30,9 +30,9 @@ Performance: Chart it over various buffer sizes and threads, as that
 Examine other fuzzers: https://gitlab.com/akihe/radamsa
 
 nbdcopy:
- - Properly handle extents/sparseness in input and output.
- - Write zeroes efficiently.
- - Detect zeroes (optionally) and turn into sparseness.
+ - --synchronous mode does not yet support extents.
+ - Detect zeroes (optionally) and turn into sparseness
+   (like qemu-img convert -S).
  - Progress bar: allow it to be written to a file descriptor
    and/or written in a machine-consumable format.
  - Minimum/preferred/maximum block size.

copy/Makefile.am
@@ -27,6 +27,9 @@ EXTRA_DIST = \
 	copy-nbd-to-small-block-error.sh \
 	copy-nbd-to-small-nbd-error.sh \
 	copy-nbd-to-stdout.sh \
+	copy-sparse.sh \
+	copy-sparse-allocated.sh \
+	copy-sparse-no-extents.sh \
 	copy-stdin-to-nbd.sh \
 	nbdcopy.pod \
 	$(NULL)
@@ -47,6 +50,7 @@ nbdcopy_SOURCES = \
 	$(NULL)
 nbdcopy_CPPFLAGS = \
 	-I$(top_srcdir)/include \
+	-I$(top_srcdir)/common/include \
 	-I$(top_srcdir)/common/utils \
 	$(NULL)
 nbdcopy_CFLAGS = \
@@ -90,6 +94,9 @@ TESTS += \
 	copy-nbd-to-small-nbd-error.sh \
 	copy-stdin-to-nbd.sh \
 	copy-nbd-to-stdout.sh \
+	copy-sparse.sh \
+	copy-sparse-allocated.sh \
+	copy-sparse-no-extents.sh \
 	$(ROOT_TESTS) \
 	$(NULL)
 

copy/copy-sparse-allocated.sh
@@ -1,0 +1,92 @@
+#!/usr/bin/env bash
+# nbd client library in userspace
+# Copyright (C) 2020 Red Hat Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+# Adapted from copy-sparse.sh.
+#
+# This test depends on the nbdkit default sparse block size (32K).
+
+. ../tests/functions.sh
+
+set -e
+set -x
+
+requires nbdkit --version
+requires nbdkit --exit-with-parent --version
+requires nbdkit data --version
+requires nbdkit eval --version
+
+out=copy-sparse-allocated.out
+cleanup_fn rm -f $out
+
+$VG nbdcopy --allocated -- \
+    [ nbdkit --exit-with-parent data data='
+             1
+             @1073741823 1
+             @4294967295 1
+             @4294967296 1
+             ' ] \
+    [ nbdkit --exit-with-parent eval \
+             get_size=' echo 7E ' \
+             pwrite=" echo \$@ >> $out " \
+             trim=" echo \$@ >> $out " \
+             zero=" echo \$@ >> $out " ]
+
+sort -o $out $out
+
+echo Output:
+cat $out
+
+if [ "$(cat $out)" != "pwrite 1 4294967296
+pwrite 32768 0
+pwrite 32768 1073709056
+pwrite 32768 4294934528
+zero 134184960 32768
+zero 134184960 4160749568
+zero 134184960 939524096
+zero 134217728 1073741824
+zero 134217728 1207959552
+zero 134217728 134217728
+zero 134217728 1342177280
+zero 134217728 1476395008
+zero 134217728 1610612736
+zero 134217728 1744830464
+zero 134217728 1879048192
+zero 134217728 2013265920
+zero 134217728 2147483648
+zero 134217728 2281701376
+zero 134217728 2415919104
+zero 134217728 2550136832
+zero 134217728 268435456
+zero 134217728 2684354560
+zero 134217728 2818572288
+zero 134217728 2952790016
+zero 134217728 3087007744
+zero 134217728 3221225472
+zero 134217728 3355443200
+zero 134217728 3489660928
+zero 134217728 3623878656
+zero 134217728 3758096384
+zero 134217728 3892314112
+zero 134217728 402653184
+zero 134217728 4026531840
+zero 134217728 536870912
+zero 134217728 671088640
+zero 134217728 805306368" ]; then
+    echo "$0: output does not match expected"
+    exit 1
+fi

copy/copy-sparse-no-extents.sh
@@ -1,0 +1,92 @@
+#!/usr/bin/env bash
+# nbd client library in userspace
+# Copyright (C) 2020 Red Hat Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+# Adapted from copy-sparse.sh
+#
+# This test depends on the nbdkit default sparse block size (32K).
+
+. ../tests/functions.sh
+
+set -e
+set -x
+
+# Skip this test under valgrind, it takes too long.
+if [ "x$LIBNBD_VALGRIND" = "x1" ]; then
+    echo "$0: test skipped under valgrind"
+    exit 77
+fi
+
+requires nbdkit --version
+requires nbdkit --exit-with-parent --version
+requires nbdkit data --version
+requires nbdkit eval --version
+
+out=copy-sparse-no-extents.out
+cleanup_fn rm -f $out
+
+$VG nbdcopy --no-extents -- \
+    [ nbdkit --exit-with-parent data data='
+             1
+             @1073741823 1
+             ' ] \
+    [ nbdkit --exit-with-parent eval \
+             get_size=' echo 7E ' \
+             pwrite=" echo \$@ >> $out " \
+             trim=" echo \$@ >> $out " \
+             zero=" echo \$@ >> $out " ]
+
+sort -n -o $out $out
+
+echo Output:
+cat $out
+
+if [ "$(cat $out)" != "pwrite 33554432 0
+pwrite 33554432 100663296
+pwrite 33554432 1006632960
+pwrite 33554432 1040187392
+pwrite 33554432 134217728
+pwrite 33554432 167772160
+pwrite 33554432 201326592
+pwrite 33554432 234881024
+pwrite 33554432 268435456
+pwrite 33554432 301989888
+pwrite 33554432 33554432
+pwrite 33554432 335544320
+pwrite 33554432 369098752
+pwrite 33554432 402653184
+pwrite 33554432 436207616
+pwrite 33554432 469762048
+pwrite 33554432 503316480
+pwrite 33554432 536870912
+pwrite 33554432 570425344
+pwrite 33554432 603979776
+pwrite 33554432 637534208
+pwrite 33554432 67108864
+pwrite 33554432 671088640
+pwrite 33554432 704643072
+pwrite 33554432 738197504
+pwrite 33554432 771751936
+pwrite 33554432 805306368
+pwrite 33554432 838860800
+pwrite 33554432 872415232
+pwrite 33554432 905969664
+pwrite 33554432 939524096
+pwrite 33554432 973078528" ]; then
+    echo "$0: output does not match expected"
+    exit 1
+fi

copy/copy-sparse.sh
@@ -1,0 +1,97 @@
+#!/usr/bin/env bash
+# nbd client library in userspace
+# Copyright (C) 2020 Red Hat Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+# This test depends on the nbdkit default sparse block size (32K).
+
+. ../tests/functions.sh
+
+set -e
+set -x
+
+requires nbdkit --version
+requires nbdkit --exit-with-parent --version
+requires nbdkit data --version
+requires nbdkit eval --version
+
+out=copy-sparse.out
+cleanup_fn rm -f $out
+
+# Copy from a sparse data disk to an nbdkit-eval-plugin instance which
+# is logging everything.  This allows us to see exactly what nbdcopy
+# is writing, to ensure it is writing and trimming the target as
+# expected.
+$VG nbdcopy -- \
+    [ nbdkit --exit-with-parent data data='
+             1
+             @1073741823 1
+             @4294967295 1
+             @4294967296 1
+             ' ] \
+    [ nbdkit --exit-with-parent eval \
+             get_size=' echo 7E ' \
+             pwrite=" echo \$@ >> $out " \
+             trim=" echo \$@ >> $out " \
+             zero=" echo \$@ >> $out " ]
+
+# Order of the output could vary because requests are sent in
+# parallel.
+sort -n -o $out $out
+
+echo Output:
+cat $out
+
+# Check the output matches expected.
+if [ "$(cat $out)" != "pwrite 1 4294967296
+pwrite 32768 0
+pwrite 32768 1073709056
+pwrite 32768 4294934528
+trim 134184960 32768
+trim 134184960 4160749568
+trim 134184960 939524096
+trim 134217728 1073741824
+trim 134217728 1207959552
+trim 134217728 134217728
+trim 134217728 1342177280
+trim 134217728 1476395008
+trim 134217728 1610612736
+trim 134217728 1744830464
+trim 134217728 1879048192
+trim 134217728 2013265920
+trim 134217728 2147483648
+trim 134217728 2281701376
+trim 134217728 2415919104
+trim 134217728 2550136832
+trim 134217728 268435456
+trim 134217728 2684354560
+trim 134217728 2818572288
+trim 134217728 2952790016
+trim 134217728 3087007744
+trim 134217728 3221225472
+trim 134217728 3355443200
+trim 134217728 3489660928
+trim 134217728 3623878656
+trim 134217728 3758096384
+trim 134217728 3892314112
+trim 134217728 402653184
+trim 134217728 4026531840
+trim 134217728 536870912
+trim 134217728 671088640
+trim 134217728 805306368" ]; then
+    echo "$0: output does not match expected"
+    exit 1
+fi

copy/file-ops.c
@@ -24,7 +24,16 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
 
+#include <pthread.h>
+
+#if defined (__linux__)
+#include <linux/fs.h>       /* For BLKZEROOUT */
+#endif
+
+#include "isaligned.h"
 #include "nbdcopy.h"
 
 static size_t
@@ -74,6 +83,64 @@ file_synch_write (struct rw *rw,
   }
 }
 
+static bool
+file_synch_trim (struct rw *rw, uint64_t offset, uint64_t count)
+{
+  assert (rw->t == LOCAL);
+
+#ifdef FALLOC_FL_PUNCH_HOLE
+  int fd = rw->u.local.fd;
+  int r;
+
+  r = fallocate (fd, FALLOC_FL_PUNCH_HOLE | FALLOC_FL_KEEP_SIZE,
+                 offset, count);
+  if (r == -1) {
+    perror ("fallocate: FALLOC_FL_PUNCH_HOLE");
+    exit (EXIT_FAILURE);
+  }
+  return true;
+#else /* !FALLOC_FL_PUNCH_HOLE */
+  return false;
+#endif
+}
+
+static bool
+file_synch_zero (struct rw *rw, uint64_t offset, uint64_t count)
+{
+  assert (rw->t == LOCAL);
+
+  if (S_ISREG (rw->u.local.stat.st_mode)) {
+#ifdef FALLOC_FL_ZERO_RANGE
+    int fd = rw->u.local.fd;
+    int r;
+
+    r = fallocate (fd, FALLOC_FL_ZERO_RANGE, offset, count);
+    if (r == -1) {
+      perror ("fallocate: FALLOC_FL_ZERO_RANGE");
+      exit (EXIT_FAILURE);
+    }
+    return true;
+#endif
+  }
+  else if (S_ISBLK (rw->u.local.stat.st_mode) &&
+           IS_ALIGNED (offset | count, rw->u.local.sector_size)) {
+#ifdef BLKZEROOUT
+    int fd = rw->u.local.fd;
+    int r;
+    uint64_t range[2] = {offset, count};
+
+    r = ioctl (fd, BLKZEROOUT, &range);
+    if (r == -1) {
+      perror ("ioctl: BLKZEROOUT");
+      exit (EXIT_FAILURE);
+    }
+    return true;
+#endif
+  }
+
+  return false;
+}
+
 static void
 file_asynch_read (struct rw *rw,
                   struct buffer *buffer,
@@ -104,9 +171,123 @@ file_asynch_write (struct rw *rw,
   }
 }
 
+static bool
+file_asynch_trim (struct rw *rw, struct buffer *buffer,
+                  nbd_completion_callback cb)
+{
+  assert (rw->t == LOCAL);
+
+  if (!file_synch_trim (rw, buffer->offset, buffer->len))
+    return false;
+  errno = 0;
+  if (cb.callback (cb.user_data, &errno) == -1) {
+    perror (rw->name);
+    exit (EXIT_FAILURE);
+  }
+  return true;
+}
+
+static bool
+file_asynch_zero (struct rw *rw, struct buffer *buffer,
+                  nbd_completion_callback cb)
+{
+  assert (rw->t == LOCAL);
+
+  if (!file_synch_zero (rw, buffer->offset, buffer->len))
+    return false;
+  errno = 0;
+  if (cb.callback (cb.user_data, &errno) == -1) {
+    perror (rw->name);
+    exit (EXIT_FAILURE);
+  }
+  return true;
+}
+
+static void
+file_get_extents (struct rw *rw, uintptr_t index,
+                  uint64_t offset, uint64_t count,
+                  extent_list *ret)
+{
+  assert (rw->t == LOCAL);
+
+  ret->size = 0;
+
+#ifdef SEEK_HOLE
+  static pthread_mutex_t lseek_lock = PTHREAD_MUTEX_INITIALIZER;
+
+  if (rw->u.local.seek_hole_supported) {
+    uint64_t end = offset + count;
+    int fd = rw->u.local.fd;
+    off_t pos;
+    struct extent e;
+
+    pthread_mutex_lock (&lseek_lock);
+
+    /* This loop is taken pretty much verbatim from nbdkit-file-plugin. */
+    do {
+      pos = lseek (fd, offset, SEEK_DATA);
+      if (pos == -1) {
+        if (errno == ENXIO)
+          pos = end;
+        else {
+          perror ("lseek: SEEK_DATA");
+          exit (EXIT_FAILURE);
+        }
+      }
+
+      /* We know there is a hole from offset to pos-1. */
+      if (pos > offset) {
+        e.offset = offset;
+        e.length = pos - offset;
+        e.hole = true;
+        if (extent_list_append (ret, e) == -1) {
+          perror ("realloc");
+          exit (EXIT_FAILURE);
+        }
+      }
+
+      offset = pos;
+      if (offset >= end)
+        break;
+
+      pos = lseek (fd, offset, SEEK_HOLE);
+      if (pos == -1) {
+        perror ("lseek: SEEK_HOLE");
+        exit (EXIT_FAILURE);
+      }
+
+      /* We know there is allocated data from offset to pos-1. */
+      if (pos > offset) {
+        e.offset = offset;
+        e.length = pos - offset;
+        e.hole = false;
+        if (extent_list_append (ret, e) == -1) {
+          perror ("realloc");
+          exit (EXIT_FAILURE);
+        }
+      }
+
+      offset = pos;
+    } while (offset < end);
+
+    pthread_mutex_unlock (&lseek_lock);
+    return;
+  }
+#endif
+
+  /* Otherwise return the default extent covering the whole range. */
+  default_get_extents (rw, index, offset, count, ret);
+}
+
+
 struct rw_ops file_ops = {
   .synch_read = file_synch_read,
   .synch_write = file_synch_write,
+  .synch_trim = file_synch_trim,
+  .synch_zero = file_synch_zero,
   .asynch_read = file_asynch_read,
   .asynch_write = file_asynch_write,
+  .asynch_trim = file_asynch_trim,
+  .asynch_zero = file_asynch_zero,
+  .get_extents = file_get_extents,
 };

commit 3735bcc05ca6d87be4462a89f83d0ef67e37dc77

copy: Simplify extents handling.
We don't have to worry about the server returning an extent either
partially or completely before the requested offset - this is
impossible because of the format of the NBD_REPLY_TYPE_BLOCK_STATUS
message.  We can therefore delete some dead code.  I replaced it with
a simple assertion in case there's something we have overlooked.

This also tightens the condition that the server is making progress -
previously a server could have returned a non-empty list of
zero-length extents causing nbdcopy to spin.

Thanks: Eric Blake
Fixes: commit 7fb099adfbc9ee6626c3aa92254b9a91cfac12e9

copy/nbd-ops.c
@@ -434,6 +434,7 @@ nbd_ops_get_extents (struct rw *rw, uintptr_t index,
   ret->size = 0;
 
   while (count > 0) {
+    const uint64_t old_offset = offset;
     size_t i;
 
     exts.size = 0;
@@ -449,28 +450,11 @@ nbd_ops_get_extents (struct rw *rw, uintptr_t index,
       exit (EXIT_FAILURE);
     }
 
-    /* The server should always make progress. */
-    if (exts.size == 0) {
-      fprintf (stderr, "%s: NBD server is broken: it is not returning extent information.\nTry nbdcopy --no-extents as a workaround.\n",
-               rw->name);
-      exit (EXIT_FAILURE);
-    }
-
-    /* Copy the extents returned into the final list (ret).  This is
-     * complicated because the extents returned by the server may
-     * begin earlier and begin or end later than the requested size.
-     */
+    /* Copy the extents returned into the final list (ret). */
     for (i = 0; i < exts.size; ++i) {
       uint64_t d;
 
-      if (exts.ptr[i].offset + exts.ptr[i].length <= offset)
-        continue;
-      if (exts.ptr[i].offset < offset) {
-        d = offset - exts.ptr[i].offset;
-        exts.ptr[i].offset += d;
-        exts.ptr[i].length -= d;
-        assert (exts.ptr[i].offset == offset);
-      }
+      assert (exts.ptr[i].offset == offset);
       if (exts.ptr[i].offset + exts.ptr[i].length > offset + count) {
         d = exts.ptr[i].offset + exts.ptr[i].length - offset - count;
         exts.ptr[i].length -= d;
@@ -485,6 +469,13 @@ nbd_ops_get_extents (struct rw *rw, uintptr_t index,
 
       offset += exts.ptr[i].length;
       count -= exts.ptr[i].length;
+    }
+
+    /* The server should always make progress. */
+    if (offset == old_offset) {
+      fprintf (stderr, "%s: NBD server is broken: it is not returning extent information.\nTry nbdcopy --no-extents as a workaround.\n",
+               rw->name);
+      exit (EXIT_FAILURE);
     }
   }
 