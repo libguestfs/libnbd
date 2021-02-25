libguestfs/libnbd

commit bae5d4be68a515436fd13e993ed9900baebd6ab8

interop: Add test of qemu-storage-daemon.

README
@@ -103,8 +103,8 @@ Optional, only needed to run the test suite:
  * nbdkit >= 1.12, the nbdkit basic plugins and the nbdkit basic
    filters are recommended as they are needed to run the test suite.
 
- * nbdkit, nbd-server and qemu-nbd if you want to do interoperability
-   testing against these servers.
+ * nbdkit, nbd-server, qemu-nbd and qemu-storage-daemon if you want to
+   do interoperability testing against these servers.
 
  * A C++ compiler is needed if you want to test that the library works
    from C++.

configure.ac
@@ -201,11 +201,15 @@ AS_IF([test "x$NBDKIT" != "x"], [
 AM_CONDITIONAL([HAVE_NBDKIT],
     [test "x$NBDKIT" != "x" && test "x$have_nbdkit_features" = "xyes"])
 
-dnl nbd-server and qemu-nbd for interop testing.
+dnl nbd-server, qemu-nbd and qemu-storage-daemon for interop testing.
 AC_CHECK_PROG([NBD_SERVER], [nbd-server], [nbd-server])
 AM_CONDITIONAL([HAVE_NBD_SERVER], [test "x$NBD_SERVER" != "x"])
 AC_CHECK_PROG([QEMU_NBD], [qemu-nbd], [qemu-nbd])
 AM_CONDITIONAL([HAVE_QEMU_NBD], [test "x$QEMU_NBD" != "x"])
+AC_CHECK_PROG([QEMU_STORAGE_DAEMON],
+               [qemu-storage-daemon], [qemu-storage-daemon])
+AM_CONDITIONAL([HAVE_QEMU_STORAGE_DAEMON],
+               [test "x$QEMU_STORAGE_DAEMON" != "x"])
 
 dnl glib2 main loop for examples that interoperate with the glib main loop.
 PKG_CHECK_MODULES([GLIB], [glib-2.0], [

interop/Makefile.am
@@ -174,6 +174,32 @@ structured_read_LDADD = $(top_builddir)/lib/libnbd.la
 
 endif HAVE_QEMU_NBD
 
+# We treat qemu-storage-daemon as effectively the same as qemu-nbd
+# since they use the same code, but there is a single test to make
+# sure we can use it.
+if HAVE_QEMU_STORAGE_DAEMON
+
+check_PROGRAMS += interop-qemu-storage-daemon
+TESTS += interop-qemu-storage-daemon
+
+interop_qemu_storage_daemon_SOURCES = interop.c
+interop_qemu_storage_daemon_CPPFLAGS = \
+	-I$(top_srcdir)/include \
+	-I$(top_srcdir)/common/utils \
+	-DUNIXSOCKET=\"`mktemp -u /tmp/libnbd-interop-qsd.XXXXXX`\" \
+	-DSERVER=\"$(QEMU_STORAGE_DAEMON)\" \
+	-DSERVER_PARAM_VARS='char blockdev[256], nbd_server[256]; snprintf (blockdev, sizeof blockdev, "raw,file.driver=file,file.filename=%s,node-name=disk0", tmpfile); snprintf (nbd_server, sizeof nbd_server, "addr.type=unix,addr.path=%s", UNIXSOCKET);' \
+	-DSERVER_PARAMS='"--blockdev", blockdev, "--nbd-server", nbd_server, "--export", "nbd,node-name=disk0,id=nbd0,name=/"' \
+	-DEXPORT_NAME='"/"' \
+	$(NULL)
+interop_qemu_storage_daemon_CFLAGS = $(WARNINGS_CFLAGS)
+interop_qemu_storage_daemon_LDADD = \
+	$(top_builddir)/common/utils/libutils.la \
+	$(top_builddir)/lib/libnbd.la \
+	$(NULL)
+
+endif HAVE_QEMU_STORAGE_DAEMON
+
 if HAVE_NBDKIT
 
 check_PROGRAMS += \

.gitignore
@@ -94,6 +94,7 @@ Makefile.in
 /interop/interop-qemu-nbd
 /interop/interop-qemu-nbd-tls-certs
 /interop/interop-qemu-nbd-tls-psk
+/interop/interop-qemu-storage-daemon
 /interop/list-exports-nbd-server
 /interop/list-exports-qemu-nbd
 /interop/socket-activation-nbdkit

interop/interop.c
@@ -1,5 +1,5 @@
 /* NBD client library in userspace
- * Copyright (C) 2013-2019 Red Hat Inc.
+ * Copyright (C) 2013-2021 Red Hat Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -47,11 +47,14 @@ main (int argc, char *argv[])
 {
   struct nbd_handle *nbd;
   char tmpfile[] = "/tmp/nbdXXXXXX";
-  char *args[] = { SERVER, SERVER_PARAMS, NULL };
   int fd;
   int64_t actual_size;
   char buf[512];
   int r = -1;
+  size_t i;
+#ifdef UNIXSOCKET
+  pid_t pid = 0;
+#endif
 
   /* Create a large sparse temporary file. */
   fd = mkstemp (tmpfile);
@@ -102,7 +105,18 @@ main (int argc, char *argv[])
   }
 #endif
 
+  /* Create the server parameters. */
+#ifdef SERVER_PARAM_VARS
+  SERVER_PARAM_VARS
+#endif
+  char *args[] = { SERVER, SERVER_PARAMS, NULL };
+  fprintf (stderr, "server: %s", args[0]);
+  for (i = 1; args[i] != NULL; ++i)
+    fprintf (stderr, " %s", args[i]);
+  fprintf (stderr, "\n");
+
   /* Start the server. */
+#ifndef UNIXSOCKET
 #if SOCKET_ACTIVATION
 #define NBD_CONNECT nbd_connect_systemd_socket_activation
 #else
@@ -112,6 +126,40 @@ main (int argc, char *argv[])
     fprintf (stderr, "%s\n", nbd_get_error ());
     goto out;
   }
+#else /* UNIXSOCKET */
+  /* This is for servers which don't support either stdin/stdout or
+   * systemd socket activation.  We have to run the command in the
+   * background and connect to the Unix domain socket.
+   */
+  pid = fork ();
+  if (pid == -1) {
+    perror ("fork");
+    goto out;
+  }
+  if (pid == 0) {
+    execvp (args[0], args);
+    perror (args[0]);
+    _exit (EXIT_FAILURE);
+  }
+  /* Unfortunately qemu-storage-daemon doesn't support pidfiles, so we
+   * have to wait for the socket to be created then sleep "a bit".
+   */
+  for (int i = 0; i < 60; ++i) {
+    if (access (UNIXSOCKET, R_OK) == 0)
+      break;
+    sleep (1);
+  }
+  sleep (1);
+  /* Connect to the socket. */
+  if (nbd_connect_unix (nbd, UNIXSOCKET) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    goto out;
+  }
+  /* We don't need the socket to exist in the filesystem any more, so
+   * we can remove it early.
+   */
+  unlink (UNIXSOCKET);
+#endif /* UNIXSOCKET */
 
 #if TLS
   if (TLS_MODE == LIBNBD_TLS_REQUIRE) {
@@ -178,6 +226,15 @@ main (int argc, char *argv[])
   r = 0;
  out:
   unlink (tmpfile);
+
+#ifdef UNIXSOCKET
+  /* Kill the background server. */
+  if (pid > 0) {
+    printf ("killing %s PID %d ...\n", args[0], (int) pid);
+    fflush (stdout);
+    kill (pid, SIGTERM);
+  }
+#endif
 
   exit (r == 0 ? EXIT_SUCCESS : EXIT_FAILURE);
 }

commit 89113f484effb0e6c322314ba75c1cbe07a04543

interop: Add test of qemu-storage-daemon (v2).
19:29 < stefanha> The syntax is --nbd-server addr.type=fd,addr.str=1234

Unfortunately the file descriptor passed in is a listening socket (not
the fully connected NBD socket), so close but no cookie.  However:

13:56 < danpb> rjones: nb even passing in a listener socket should be
               good enough for reliable parent/child synchronization
               still
13:56 < danpb> rjones: eg your parent can listen on /var/run/blah, then
               connnect to this socket but don't call accept, and pass
               the listener FD into qemu-storage-daemon
13:57 < danpb> rjones: so the storage daemon will immediately accept
               your existing client connection, or you'll get EOF on
               your client if qsd dies in startup
13:58 < danpb> this is how libvirt starts QEMU, passing in a pre-opened
               QMP listener socket

Thanks: Stefan Hajnoczi, Daniel Berrangé
Fixes: commit bae5d4be68a515436fd13e993ed9900baebd6ab8

README
@@ -103,8 +103,8 @@ Optional, only needed to run the test suite:
  * nbdkit >= 1.12, the nbdkit basic plugins and the nbdkit basic
    filters are recommended as they are needed to run the test suite.
 
- * nbdkit, nbd-server and qemu-nbd if you want to do interoperability
-   testing against these servers.
+ * nbdkit, nbd-server, qemu-nbd and qemu-storage-daemon if you want to
+   do interoperability testing against these servers.
 
  * A C++ compiler is needed if you want to test that the library works
    from C++.

configure.ac
@@ -201,11 +201,15 @@ AS_IF([test "x$NBDKIT" != "x"], [
 AM_CONDITIONAL([HAVE_NBDKIT],
     [test "x$NBDKIT" != "x" && test "x$have_nbdkit_features" = "xyes"])
 
-dnl nbd-server and qemu-nbd for interop testing.
+dnl nbd-server, qemu-nbd and qemu-storage-daemon for interop testing.
 AC_CHECK_PROG([NBD_SERVER], [nbd-server], [nbd-server])
 AM_CONDITIONAL([HAVE_NBD_SERVER], [test "x$NBD_SERVER" != "x"])
 AC_CHECK_PROG([QEMU_NBD], [qemu-nbd], [qemu-nbd])
 AM_CONDITIONAL([HAVE_QEMU_NBD], [test "x$QEMU_NBD" != "x"])
+AC_CHECK_PROG([QEMU_STORAGE_DAEMON],
+               [qemu-storage-daemon], [qemu-storage-daemon])
+AM_CONDITIONAL([HAVE_QEMU_STORAGE_DAEMON],
+               [test "x$QEMU_STORAGE_DAEMON" != "x"])
 
 dnl glib2 main loop for examples that interoperate with the glib main loop.
 PKG_CHECK_MODULES([GLIB], [glib-2.0], [

interop/Makefile.am
@@ -23,7 +23,7 @@ EXTRA_DIST = \
 	structured-read.sh \
 	$(NULL)
 
-TESTS_ENVIRONMENT = LIBNBD_DEBUG=1
+TESTS_ENVIRONMENT = LIBNBD_DEBUG=1 QEMU_STORAGE_DAEMON=$(QEMU_STORAGE_DAEMON)
 LOG_COMPILER = $(top_builddir)/run
 check_PROGRAMS =
 TESTS =
@@ -173,6 +173,15 @@ structured_read_CFLAGS = $(WARNINGS_CFLAGS)
 structured_read_LDADD = $(top_builddir)/lib/libnbd.la
 
 endif HAVE_QEMU_NBD
+
+# We treat qemu-storage-daemon as effectively the same as qemu-nbd
+# since they use the same code, but there is a single test to make
+# sure we can use it.  It was too complicated to extend interop.c to
+# support q-s-d (since it doesn't support stdin/stdio or systemd
+# socket activation) so there is a unique script to test it.
+if HAVE_QEMU_STORAGE_DAEMON
+TESTS += interop-qemu-storage-daemon.sh
+endif HAVE_QEMU_STORAGE_DAEMON
 
 if HAVE_NBDKIT
 

interop/interop-qemu-storage-daemon.sh
@@ -1,0 +1,93 @@
+#!/usr/bin/env bash
+# nbd client library in userspace
+# Copyright (C) 2019-2021 Red Hat Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+# Test interop with qemu-storage-daemon.
+
+source ../tests/functions.sh
+set -e
+set -x
+
+requires test "x$QEMU_STORAGE_DAEMON" != "x"
+requires $QEMU_STORAGE_DAEMON --version
+requires nbdsh --version
+requires qemu-img --version
+
+f="qemu-storage-daemon-disk.qcow2"
+sock=$(mktemp -u /tmp/interop-qsd.XXXXXX)
+rm -f $f $sock
+cleanup_fn rm -f $f $sock
+
+qemu-img create $f 10M -f qcow2
+
+export f sock
+$VG nbdsh -c - <<'EOF'
+import os
+import signal
+import socket
+import sys
+
+# Get the name of q-s-d defined by ./configure.
+qsd = os.environ["QEMU_STORAGE_DAEMON"]
+
+# Test disk.
+f = os.environ["f"]
+
+# Unique socket name.
+sock = os.environ["sock"]
+
+# Create two sockets for client and server.  We pass the listening
+# socket to q-s-d.
+client_sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+qsd_sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+os.set_inheritable(qsd_sock.fileno(), True)
+qsd_sock.bind(sock)
+qsd_sock.listen()
+client_sock.connect(sock)
+
+# Assemble q-s-d command line.
+argv = [
+    qsd,
+    "--blockdev",
+    "qcow2,file.driver=file,file.filename=" + f + ",node-name=disk0",
+    "--nbd-server", "addr.type=fd,addr.str=" + str(qsd_sock.fileno()),
+    "--export", "nbd,node-name=disk0,id=nbd0,name=,writable=on"
+]
+print("server: %r" % argv, file=sys.stderr)
+
+pid = os.fork()
+if pid == 0:
+    client_sock.close()
+    os.execvp(qsd, argv)
+else:
+    qsd_sock.close()
+
+# Connect to the server and test.
+h.connect_socket(client_sock.fileno())
+
+# Read and write.
+buf = b"1" * 512
+h.pwrite(buf, 512)
+buf2 = h.pread(1024, 0)
+assert bytearray(512) + buf == buf2
+
+h.shutdown()
+
+# Kill the server.
+os.kill(pid, signal.SIGTERM)
+
+EOF