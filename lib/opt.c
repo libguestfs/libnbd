libguestfs/libnbd

commit bbf1c5139248a3ef9e018271d0cfa84522c6d559

api: Give aio_opt_go a completion callback
Although NBD_OPT_GO does not need a completion callback (because you
can imply success or failure by the resulting state), it will be a lot
easier to share the code with the upcoming support for NBD_OPT_INFO
(where both success and failure end up back in Negotiating, and thus a
completion callback will be essential for preserving the server
error).  As promised in the previous patch, add in a completion
callback to aio_opt_go.  All future nbd_opt_* commands will also use
a completion callback (nbd_opt_abort remains the only special case
that doesn't need one).

Testing of this will be added later, when NBD_OPT_INFO is added.

lib/opt.c
@@ -20,8 +20,16 @@
 
 #include <stdlib.h>
 #include <stdbool.h>
+#include <assert.h>
 
 #include "internal.h"
+
+/* Internal function which frees an option with callback. */
+void
+nbd_internal_free_option (struct nbd_handle *h)
+{
+  FREE_CALLBACK (h->opt_completion);
+}
 
 int
 nbd_unlocked_set_opt_mode (struct nbd_handle *h, bool value)
@@ -48,18 +56,33 @@ wait_for_option (struct nbd_handle *h)
   return 0;
 }
 
+static int
+go_complete (void *opaque, int *err)
+{
+  int *i = opaque;
+  *i = *err;
+  return 0;
+}
+
 /* Issue NBD_OPT_GO (or NBD_OPT_EXPORT_NAME) and wait for the reply. */
 int
 nbd_unlocked_opt_go (struct nbd_handle *h)
 {
-  int r = nbd_unlocked_aio_opt_go (h);
+  int err;
+  nbd_completion_callback c = { .callback = go_complete, .user_data = &err };
+  int r = nbd_unlocked_aio_opt_go (h, c);
 
   if (r == -1)
     return r;
 
   r = wait_for_option (h);
-  if (r == 0 && nbd_internal_is_state_negotiating (get_next_state (h)))
-    return -1; /* NBD_OPT_GO failed, but can be attempted again */
+  if (r == 0 && err) {
+    assert (nbd_internal_is_state_negotiating (get_next_state (h)));
+    set_error (err, "server replied with error to opt_go request");
+    return -1;
+  }
+  if (r == 0)
+    assert (nbd_internal_is_state_ready (get_next_state (h)));
   return r;
 }
 
@@ -77,9 +100,11 @@ nbd_unlocked_opt_abort (struct nbd_handle *h)
 
 /* Issue NBD_OPT_GO (or NBD_OPT_EXPORT_NAME) without waiting. */
 int
-nbd_unlocked_aio_opt_go (struct nbd_handle *h)
+nbd_unlocked_aio_opt_go (struct nbd_handle *h,
+                         nbd_completion_callback complete)
 {
   h->opt_current = NBD_OPT_GO;
+  h->opt_completion = complete;
 
   if (nbd_internal_run (h, cmd_issue) == -1)
     debug (h, "option queued, ignoring state machine failure");

generator/API.ml
@@ -1924,7 +1924,9 @@ on the connection.";
 
   "aio_opt_go", {
     default_call with
-    args = []; ret = RErr;
+    args = [];
+    optargs = [ OClosure completion_closure ];
+    ret = RErr;
     permitted_states = [ Negotiating ];
     shortdesc = "end negotiation and move on to using an export";
     longdesc = "\
@@ -1934,7 +1936,14 @@ or L<nbd_connect_uri(3)>.  This can only be used if
 L<nbd_set_opt_mode(3)> enabled option mode.
 
 To determine when the request completes, wait for
-L<nbd_aio_is_connecting(3)> to return false.";
+L<nbd_aio_is_connecting(3)> to return false.  Or supply the optional
+C<completion_callback> which will be invoked as described in
+L<libnbd(3)/Completion callbacks>, except that it is automatically
+retired regardless of return value.  Note that directly detecting
+whether the server returns an error (as is done by the return value
+of the synchronous counterpart) is only possible with a completion
+callback; however it is also possible to indirectly detect an error
+when L<nbd_aio_is_negotiating(3)> returns true.";
     see_also = [Link "set_opt_mode"; Link "opt_go"];
   };
 

generator/states-newstyle-opt-go.c
@@ -124,6 +124,7 @@ STATE_MACHINE {
   uint64_t exportsize;
   uint16_t eflags;
   uint32_t min, pref, max;
+  int err;
 
   reply = be32toh (h->sbuf.or.option_reply.reply);
   len = be32toh (h->sbuf.or.option_reply.replylen);
@@ -241,15 +242,21 @@ STATE_MACHINE {
                  reply);
     }
     nbd_internal_reset_size_and_flags (h);
-    if (h->opt_mode)
-      SET_NEXT_STATE (%.NEGOTIATING);
-    else
-      SET_NEXT_STATE (%^PREPARE_OPT_ABORT);
+    err = nbd_get_errno () ? : ENOTSUP;
     break;
   case NBD_REP_ACK:
-    SET_NEXT_STATE (%^FINISHED);
+    err = 0;
     break;
   }
+
+  if (err == 0)
+    SET_NEXT_STATE (%^FINISHED);
+  else if (h->opt_mode)
+    SET_NEXT_STATE (%.NEGOTIATING);
+  else
+    SET_NEXT_STATE (%^PREPARE_OPT_ABORT);
+  CALL_CALLBACK (h->opt_completion, &err);
+  nbd_internal_free_option (h);
   return 0;
 
 } /* END STATE MACHINE */

generator/states.c
@@ -1,5 +1,5 @@
 /* nbd client library in userspace: state machine
- * Copyright (C) 2013-2019 Red Hat Inc.
+ * Copyright (C) 2013-2020 Red Hat Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -111,9 +111,19 @@ send_from_wbuf (struct nbd_handle *h)
   return 0;                     /* move to next state */
 }
 
+/* Forcefully fail any in-flight option */
+static void
+abort_option (struct nbd_handle *h)
+{
+  int err = nbd_get_errno () ? : ENOTCONN;
+
+  CALL_CALLBACK (h->opt_completion, &err);
+  nbd_internal_free_option(h);
+}
+
 /* Forcefully fail any remaining in-flight commands in list */
-void abort_commands (struct nbd_handle *h,
-                     struct command **list)
+static void
+abort_commands (struct nbd_handle *h, struct command **list)
 {
   struct command *next, *cmd;
 
@@ -168,6 +178,7 @@ STATE_MACHINE {
  DEAD:
   /* The caller should have used set_error() before reaching here */
   assert (nbd_get_error ());
+  abort_option (h);
   abort_commands (h, &h->cmds_to_issue);
   abort_commands (h, &h->cmds_in_flight);
   h->in_flight = 0;
@@ -178,6 +189,7 @@ STATE_MACHINE {
   return -1;
 
  CLOSED:
+  abort_option (h);
   abort_commands (h, &h->cmds_to_issue);
   abort_commands (h, &h->cmds_in_flight);
   h->in_flight = 0;

lib/handle.c
@@ -131,6 +131,7 @@ nbd_close (struct nbd_handle *h)
     free (h->exports[i].description);
   }
   free (h->exports);
+  nbd_internal_free_option (h);
   free_cmd_list (h->cmds_to_issue);
   free_cmd_list (h->cmds_in_flight);
   free_cmd_list (h->cmds_done);

lib/internal.h
@@ -102,6 +102,7 @@ struct nbd_handle {
   /* Option negotiation mode. */
   bool opt_mode;
   uint8_t opt_current; /* 0 or one of NBD_OPT_* */
+  nbd_completion_callback opt_completion;
 
   /* List exports mode. */
   bool list_exports;
@@ -420,6 +421,9 @@ extern bool nbd_internal_is_state_ready (enum state state);
 extern bool nbd_internal_is_state_processing (enum state state);
 extern bool nbd_internal_is_state_dead (enum state state);
 extern bool nbd_internal_is_state_closed (enum state state);
+
+/* opt.c */
+extern void nbd_internal_free_option (struct nbd_handle *h);
 
 /* protocol.c */
 extern int nbd_internal_errno_of_nbd_error (uint32_t error);

commit fb4440de9cc76e9c14bd3ddf3333e78621f40ad0

opt_go: Tolerate unplanned server death
While debugging some experimental nbdkit code that was triggering an
assertion failure in nbdkit, I noticed a secondary failure of nbdsh
also dying from an assertion:

libnbd: debug: nbdsh: nbd_opt_go: transition: NEWSTYLE.OPT_GO.SEND -> DEAD
libnbd: debug: nbdsh: nbd_opt_go: option queued, ignoring state machine failure
nbdsh: opt.c:86: nbd_unlocked_opt_go: Assertion `nbd_internal_is_state_negotiating (get_next_state (h))' failed.

Although my trigger was from non-production nbdkit code, libnbd should
never die from an assertion failure merely because a server
disappeared at the wrong moment during an incomplete reply to
NBD_OPT_GO or NBD_OPT_INFO.  If this is assigned a CVE, a followup
patch will add mention of it in docs/libnbd-security.pod.

Fixes: bbf1c51392 (api: Give aio_opt_go a completion callback)

lib/opt.c
@@ -1,5 +1,5 @@
 /* NBD client library in userspace
- * Copyright (C) 2020 Red Hat Inc.
+ * Copyright (C) 2020-2021 Red Hat Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -83,7 +83,8 @@ nbd_unlocked_opt_go (struct nbd_handle *h)
 
   r = wait_for_option (h);
   if (r == 0 && err) {
-    assert (nbd_internal_is_state_negotiating (get_next_state (h)));
+    assert (nbd_internal_is_state_negotiating (get_next_state (h)) ||
+            nbd_internal_is_state_dead (get_next_state (h)));
     set_error (err, "server replied with error to opt_go request");
     return -1;
   }
@@ -105,7 +106,8 @@ nbd_unlocked_opt_info (struct nbd_handle *h)
 
   r = wait_for_option (h);
   if (r == 0 && err) {
-    assert (nbd_internal_is_state_negotiating (get_next_state (h)));
+    assert (nbd_internal_is_state_negotiating (get_next_state (h)) ||
+            nbd_internal_is_state_dead (get_next_state (h)));
     set_error (err, "server replied with error to opt_info request");
     return -1;
   }