b15dde588f47894f9656ea62eb632156b7638b8b
Richard W.M. Jones <rjones@redhat.com>
2021-04-08 17:14:12 +0100

    golang: Don't run the tests if nbdkit is not available.
    Reported-By: Martin Kletzander

--- golang/run-tests.sh
+++ golang/run-tests.sh
@@ -16,7 +16,12 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
+. ../tests/functions.sh
+
 set -e
+set -x
+
+requires nbdkit --version
 
 cd src/$pkg
 
