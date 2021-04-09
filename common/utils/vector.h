libguestfs/libnbd

commit 28816a5f8829f474d07351611140f461facd0e4d

common/utils/vector.h: Suppress some warnings from clang.
These warnings prevented using clang + --enable-gcc-warnings.

common/utils/vector.h
@@ -44,6 +44,9 @@
 #include <assert.h>
 #include <string.h>
 
+#pragma clang diagnostic ignored "-Wunused-function"
+#pragma clang diagnostic ignored "-Wduplicate-decl-specifier"
+
 /* Use of this macro defines a new type called ‘name’ containing an
  * extensible vector of ‘type’ elements.  For example:
  *

commit 9e367ed38378bfee47d711d0ee55311503741db5

common/utils/vector.h: Fix pragmas on GCC.
In file included from vector.c:38:
vector.h:47: error: ignoring ‘#pragma clang diagnostic’ [-Werror=unknown-pragmas]
   47 | #pragma clang diagnostic ignored "-Wunused-function"
      |
vector.h:48: error: ignoring ‘#pragma clang diagnostic’ [-Werror=unknown-pragmas]
   48 | #pragma clang diagnostic ignored "-Wduplicate-decl-specifier"
      |
cc1: all warnings being treated as errors

Fixes: commit 28816a5f8829f474d07351611140f461facd0e4d

common/utils/vector.h
@@ -44,8 +44,10 @@
 #include <assert.h>
 #include <string.h>
 
+#ifdef __clang__
 #pragma clang diagnostic ignored "-Wunused-function"
 #pragma clang diagnostic ignored "-Wduplicate-decl-specifier"
+#endif
 
 /* Use of this macro defines a new type called ‘name’ containing an
  * extensible vector of ‘type’ elements.  For example: