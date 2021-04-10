libguestfs/libnbd

commit 6f07875b86c230adc842fe8dd1422823f9f68435

lib: New API nbd_get_uri to get an NBD URI for a connection.
This will make a best-effort attempt to construct an NBD URI for
connecting back to the current server.  In many cases this is not
really possible (eg. if we were connected with nbd_connect_socket),
and it's not guaranteed to be correct.

lib/uri.c
@@ -25,6 +25,14 @@
 #include <string.h>
 #include <errno.h>
 #include <assert.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <netdb.h>
+
+#ifdef HAVE_LINUX_VM_SOCKETS_H
+#include <linux/vm_sockets.h>
+#endif
 
 #include "internal.h"
 #include "vector.h"
@@ -335,6 +343,158 @@ cleanup:
   return ret;
 }
 
+/* This is best effort.  If we didn't save enough information when
+ * connecting then return NULL but try to set errno and the error
+ * string to something useful.
+ */
+
+static int append_query_params (char **query_params,
+                                const char *key, const char *value);
+
+char *
+nbd_unlocked_get_uri (struct nbd_handle *h)
+{
+  xmlURI uri = { 0 };
+  bool using_tls;
+  char *server = NULL;
+  char *query_params = NULL;
+  char *path = NULL;
+  char *ret = NULL;
+
+  if (h->tls == 2)              /* TLS == require */
+    using_tls = true;
+  else if (h->tls_negotiated)
+    using_tls = true;
+  else
+    using_tls = false;
+
+  /* Set scheme, server or socket. */
+  if (h->hostname && h->port) {
+    uri.scheme = using_tls ? "nbds" : "nbd";
+    if (asprintf (&server, "%s:%s", h->hostname, h->port) == -1) {
+      set_error (errno, "asprintf");
+      goto out;
+    }
+    uri.server = server;
+  }
+
+  else if (h->connaddrlen > 0) {
+    switch (h->connaddr.ss_family) {
+    case AF_INET:
+    case AF_INET6: {
+      int err;
+      char host[NI_MAXHOST];
+      char serv[NI_MAXSERV];
+
+      uri.scheme = using_tls ? "nbds" : "nbd";
+      err = getnameinfo ((struct sockaddr *) &h->connaddr, h->connaddrlen,
+                         host, sizeof host, serv, sizeof serv, NI_NUMERICHOST);
+      if (err != 0) {
+        set_error (0, "getnameinfo: %s", gai_strerror (err));
+        goto out;
+      }
+      if (asprintf (&server, "%s:%s", host, serv) == -1) {
+        set_error (errno, "asprintf");
+        goto out;
+      }
+      uri.server = server;
+      break;
+    }
+
+    case AF_UNIX: {
+      struct sockaddr_un *sun = (struct sockaddr_un *) &h->connaddr;
+
+      if (sun->sun_path[0] == '\0') {
+        /* Unix domain sockets in the abstract namespace are in theory
+         * supported in NBD URIs, but libxml2 cannot handle them so
+         * libnbd cannot use them here or in nbd_connect_uri.
+         */
+        set_error (EPROTONOSUPPORT, "Unix domain sockets in the "
+                   "abstract namespace are not yet supported");
+        goto out;
+      }
+
+      uri.scheme = using_tls ? "nbds+unix" : "nbd+unix";
+      if (append_query_params (&query_params, "socket", sun->sun_path) == -1)
+        goto out;
+      /* You have to set this otherwise xmlSaveUri generates bogus
+       * URIs "nbd+unix:/?socket=..."
+       */
+      uri.server = "";
+      break;
+    }
+
+#ifdef AF_VSOCK
+    case AF_VSOCK: {
+      struct sockaddr_vm *svm = (struct sockaddr_vm *) &h->connaddr;
+
+      uri.scheme = using_tls ? "nbds+vsock" : "nbd+vsock";
+      if (asprintf (&server, "%u:%u", svm->svm_cid, svm->svm_port) == -1) {
+        set_error (errno, "asprintf");
+        goto out;
+      }
+      uri.server = server;
+      break;
+    }
+#endif
+
+    default:
+      set_error (EAFNOSUPPORT,
+                 "address family %d not supported", h->connaddr.ss_family);
+      goto out;
+    }
+  }
+
+  else {
+    set_error (EINVAL, "cannot construct a URI for this connection type");
+    goto out;
+  }
+
+  /* Set other fields. */
+  if (h->tls_username)
+    uri.user = h->tls_username;
+  if (h->export_name) {
+    if (asprintf (&path, "/%s", h->export_name) == -1) {
+      set_error (errno, "asprintf");
+      goto out;
+    }
+    uri.path = path;
+  }
+  if (h->tls_psk_file) {
+    if (append_query_params (&query_params,
+                             "tls-psk-file", h->tls_psk_file) == -1)
+      goto out;
+  }
+
+  uri.query_raw = query_params;
+
+  /* Construct the final URI and return it. */
+  ret = (char *) xmlSaveUri (&uri);
+  if (ret == NULL)
+    set_error (errno, "xmlSaveUri failed");
+ out:
+  free (server);
+  free (query_params);
+  free (path);
+  return ret;
+}
+
+static int
+append_query_params (char **query_params, const char *key, const char *value)
+{
+  char *old_query_params = *query_params;
+
+  if (asprintf (query_params, "%s%s%s=%s",
+                old_query_params ? : "",
+                old_query_params ? "&" : "",
+                key, value) == -1) {
+    set_error (errno, "asprintf");
+    return -1;
+  }
+  free (old_query_params);
+  return 0;
+}
+
 #else /* !HAVE_LIBXML2 */
 
 #define NOT_SUPPORTED_ERROR \
@@ -352,6 +512,13 @@ nbd_unlocked_aio_connect_uri (struct nbd_handle *h, const char *raw_uri)
 {
   set_error (ENOTSUP, NOT_SUPPORTED_ERROR);
   return -1;
+}
+
+char *
+nbd_unlocked_get_uri (struct nbd_handle *h)
+{
+  set_error (ENOTSUP, NOT_SUPPORTED_ERROR);
+  return NULL;
 }
 
 #endif /* !HAVE_LIBXML2 */

generator/API.ml
@@ -1380,10 +1380,14 @@ test whether this is the case with L<nbd_supports_uri(3)>.
 
 Support for URIs that require TLS will fail if libnbd was not
 compiled with gnutls; you can test whether this is the case
-with L<nbd_supports_tls(3)>.";
+with L<nbd_supports_tls(3)>.
+
+=head2 Constructing a URI from an existing connection
+
+See L<nbd_get_uri(3)>.";
     see_also = [URLLink "https://github.com/NetworkBlockDevice/nbd/blob/master/doc/uri.md";
                 Link "set_export_name"; Link "set_tls";
-                Link "set_opt_mode"];
+                Link "set_opt_mode"; Link "get_uri"];
   };
 
   "connect_unix", {
@@ -2875,7 +2879,30 @@ to support TLS encryption, or false if not.";
     longdesc = "\
 Returns true if libnbd was compiled with libxml2 which is required
 to support NBD URIs, or false if not.";
-    see_also = [Link "connect_uri"; Link "aio_connect_uri"];
+    see_also = [Link "connect_uri"; Link "aio_connect_uri";
+                Link "get_uri"];
+  };
+
+  "get_uri", {
+    default_call with
+    args = []; ret = RString;
+    permitted_states = [ Connecting; Negotiating; Connected; Closed; Dead ];
+    shortdesc = "construct an NBD URI for a connection";
+    longdesc = "\
+This makes a best effort attempt to construct an NBD URI which
+could be used to connect back to this server (using
+L<nbd_connect_uri(3)>).
+
+In some cases there is not enough information in the handle
+to successfully create a URI (eg. if you connected with
+L<nbd_connect_socket(3)>).  In such cases the call returns
+C<NULL> and further diagnostic information is available
+via L<nbd_get_errno(3)> and L<nbd_get_error(3)> as usual.
+
+Even if a URI is returned it is not guaranteed to work, and
+it may not be optimal.";
+    see_also = [Link "connect_uri"; Link "aio_connect_uri";
+                Link "supports_uri"];
   };
 ]
 
@@ -3009,6 +3036,7 @@ let first_version = [
   (* Added in 1.7.x development cycle, will be stable and supported in 1.8. *)
   "set_private_data", (1, 8);
   "get_private_data", (1, 8);
+  "get_uri", (1, 8);
 
   (* These calls are proposed for a future version of libnbd, but
    * have not been added to any released version so far.

tests/aio-connect.c
@@ -43,6 +43,7 @@ main (int argc, char *argv[])
   pid_t pid;
   size_t i;
   struct sockaddr_in addr;
+  char *actual_uri, *expected_uri;
 
   unlink (PIDFILE);
 
@@ -95,6 +96,24 @@ main (int argc, char *argv[])
       exit (EXIT_FAILURE);
     }
   }
+
+  /* libnbd should be able to construct a URI for this connection. */
+  if (asprintf (&expected_uri, "nbd://127.0.0.1:%s/", port_str) == -1) {
+    perror ("asprintf");
+    exit (EXIT_FAILURE);
+  }
+  actual_uri = nbd_get_uri (nbd);
+  if (actual_uri == NULL) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  if (strcmp (actual_uri, expected_uri) != 0) {
+    fprintf (stderr, "%s: actual URI %s != expected URI %s\n",
+             argv[0], actual_uri, expected_uri);
+    exit (EXIT_FAILURE);
+  }
+  free (actual_uri);
+  free (expected_uri);
 
   if (nbd_shutdown (nbd, 0) == -1) {
     fprintf (stderr, "%s\n", nbd_get_error ());

tests/connect-tcp.c
@@ -22,6 +22,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <time.h>
@@ -38,6 +39,7 @@ main (int argc, char *argv[])
   char port_str[16];
   pid_t pid;
   size_t i;
+  char *actual_uri, *expected_uri;
 
   unlink (PIDFILE);
 
@@ -78,6 +80,24 @@ main (int argc, char *argv[])
     fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);
   }
+
+  /* libnbd should be able to construct a URI for this connection. */
+  if (asprintf (&expected_uri, "nbd://localhost:%s/", port_str) == -1) {
+    perror ("asprintf");
+    exit (EXIT_FAILURE);
+  }
+  actual_uri = nbd_get_uri (nbd);
+  if (actual_uri == NULL) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  if (strcmp (actual_uri, expected_uri) != 0) {
+    fprintf (stderr, "%s: actual URI %s != expected URI %s\n",
+             argv[0], actual_uri, expected_uri);
+    exit (EXIT_FAILURE);
+  }
+  free (actual_uri);
+  free (expected_uri);
 
   if (nbd_shutdown (nbd, 0) == -1) {
     fprintf (stderr, "%s\n", nbd_get_error ());

tests/connect-unix.c
@@ -22,6 +22,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
 
@@ -36,6 +37,7 @@ main (int argc, char *argv[])
   struct nbd_handle *nbd;
   pid_t pid;
   size_t i;
+  char *actual_uri, *expected_uri;
 
   if (mkstemp (socket) == -1) {
     perror (socket);
@@ -76,6 +78,24 @@ main (int argc, char *argv[])
     fprintf (stderr, "%s\n", nbd_get_error ());
     exit (EXIT_FAILURE);
   }
+
+  /* libnbd should be able to construct a URI for this connection. */
+  if (asprintf (&expected_uri, "nbd+unix:///?socket=%s", socket) == -1) {
+    perror ("asprintf");
+    exit (EXIT_FAILURE);
+  }
+  actual_uri = nbd_get_uri (nbd);
+  if (actual_uri == NULL) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  if (strcmp (actual_uri, expected_uri) != 0) {
+    fprintf (stderr, "%s: actual URI %s != expected URI %s\n",
+             argv[0], actual_uri, expected_uri);
+    exit (EXIT_FAILURE);
+  }
+  free (actual_uri);
+  free (expected_uri);
 
   if (nbd_shutdown (nbd, 0) == -1) {
     fprintf (stderr, "%s\n", nbd_get_error ());

tests/connect-uri.c
@@ -35,6 +35,7 @@ main (int argc, char *argv[])
   struct nbd_handle *nbd;
   pid_t pid;
   size_t i;
+  char *get_uri;
 
 #ifdef SOCKET
   unlink (SOCKET);
@@ -88,6 +89,22 @@ main (int argc, char *argv[])
       exit (EXIT_FAILURE);
     }
   }
+
+  /* Usually the URI returned by nbd_get_uri should be the same as the
+   * one passed to nbd_connect_uri, or at least it will be in our test
+   * cases.
+   */
+  get_uri = nbd_get_uri (nbd);
+  if (get_uri == NULL) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  if (strcmp (URI, get_uri) != 0) {
+    fprintf (stderr, "%s: connect URI %s != get URI %s\n",
+             argv[0], URI, get_uri);
+    exit (EXIT_FAILURE);
+  }
+  free (get_uri);
 
   if (nbd_shutdown (nbd, 0) == -1) {
     fprintf (stderr, "%s\n", nbd_get_error ());

commit 17df436cea517de4b295d006ff2d768ed4642535

lib: Fix parsing and construction of IPv6 URIs like nbd://[::1]/
These were improperly parsed before because libxml2 (wrongly?) sets
uri->server to the string "[::1]", which confuses getaddrinfo.
Similarly when constructing a URI we have to add the square brackets
back to the uri->server field.

This commit also adds a test.

Fixes: commit 6f07875b86c230adc842fe8dd1422823f9f68435
Fixes: commit d2dfefcefdf64864acae7a75c30d3f54e9a63ea6

.gitignore
@@ -164,6 +164,7 @@ Makefile.in
 /tests/compile-header-only
 /tests/compile-iso-c99
 /tests/connect-tcp
+/tests/connect-tcp6
 /tests/connect-tls-certs
 /tests/connect-tls-psk
 /tests/connect-unix

generator/states-connect.c
@@ -137,7 +137,7 @@ STATE_MACHINE {
   r = getaddrinfo (h->hostname, h->port, &h->hints, &h->result);
   if (r != 0) {
     SET_NEXT_STATE (%^START);
-    set_error (0, "getaddrinfo: %s:%s: %s",
+    set_error (0, "getaddrinfo: hostname \"%s\" port \"%s\": %s",
                h->hostname, h->port, gai_strerror (r));
     return -1;
   }

lib/uri.c
@@ -158,8 +158,6 @@ nbd_unlocked_aio_connect_uri (struct nbd_handle *h, const char *raw_uri)
   int i, r;
   int ret = -1;
   const char *unixsocket = NULL;
-  char port_str[32];
-  uint32_t cid, svm_port;
 
   uri = xmlParseURI (raw_uri);
   if (!uri) {
@@ -287,14 +285,35 @@ nbd_unlocked_aio_connect_uri (struct nbd_handle *h, const char *raw_uri)
     goto cleanup;
 
   switch (transport) {
-  case tcp:                     /* TCP */
+  case tcp: {                   /* TCP */
+    char port_str[32];
+    char *server;
+    size_t server_len;
+
     snprintf (port_str, sizeof port_str,
               "%d", uri->port > 0 ? uri->port : 10809);
-    if (nbd_unlocked_aio_connect_tcp (h, uri->server ? : "localhost",
-                                      port_str) == -1)
+
+    /* If the uri->server field is NULL, substitute "localhost".  This
+     * would be unusual and probably doesn't happen in reality. XXX
+     */
+    server = uri->server ? : "localhost";
+    server_len = strlen (server);
+
+    /* For a literal IPv6 address, the uri->server field will contain
+     * "[addr]" and we must remove the brackets before passing it to
+     * getaddrinfo in the state machine.
+     */
+    if (server_len >= 2 && server[0] == '[' && server[server_len-1] == ']') {
+      server_len -= 2;
+      server++;
+      server[server_len] = '\0';
+    }
+
+    if (nbd_unlocked_aio_connect_tcp (h, server, port_str) == -1)
       goto cleanup;
 
     break;
+  }
 
   case unix_sock:               /* Unix domain socket */
     if (nbd_unlocked_aio_connect_unix (h, unixsocket) == -1)
@@ -302,7 +321,9 @@ nbd_unlocked_aio_connect_uri (struct nbd_handle *h, const char *raw_uri)
 
     break;
 
-  case vsock:                   /* AF_VSOCK */
+  case vsock: {                 /* AF_VSOCK */
+    uint32_t cid, svm_port;
+
     /* Server, if present, must be the numeric CID.  Else we
      * assume the host (2).
      */
@@ -329,6 +350,7 @@ nbd_unlocked_aio_connect_uri (struct nbd_handle *h, const char *raw_uri)
       goto cleanup;
 
     break;
+  }
   }
 
   ret = 0;
@@ -370,8 +392,18 @@ nbd_unlocked_get_uri (struct nbd_handle *h)
 
   /* Set scheme, server or socket. */
   if (h->hostname && h->port) {
+    int r;
+
     uri.scheme = using_tls ? "nbds" : "nbd";
-    if (asprintf (&server, "%s:%s", h->hostname, h->port) == -1) {
+    /* We must try to guess here if the hostname is really an IPv6
+     * numeric address.  Regular hostnames or IPv4 addresses wouldn't
+     * contain ':'.
+     */
+    if (strchr (h->hostname, ':') == NULL)
+      r = asprintf (&server, "%s:%s", h->hostname, h->port);
+    else
+      r = asprintf (&server, "[%s]:%s", h->hostname, h->port);
+    if (r == -1) {
       set_error (errno, "asprintf");
       goto out;
     }
@@ -382,7 +414,7 @@ nbd_unlocked_get_uri (struct nbd_handle *h)
     switch (h->connaddr.ss_family) {
     case AF_INET:
     case AF_INET6: {
-      int err;
+      int r, err;
       char host[NI_MAXHOST];
       char serv[NI_MAXSERV];
 
@@ -393,7 +425,11 @@ nbd_unlocked_get_uri (struct nbd_handle *h)
         set_error (0, "getnameinfo: %s", gai_strerror (err));
         goto out;
       }
-      if (asprintf (&server, "%s:%s", host, serv) == -1) {
+      if (h->connaddr.ss_family == AF_INET)
+        r = asprintf (&server, "%s:%s", host, serv);
+      else /* AF_INET6 */
+        r = asprintf (&server, "[%s]:%s", host, serv);
+      if (r == -1) {
         set_error (errno, "asprintf");
         goto out;
       }

tests/Makefile.am
@@ -19,6 +19,7 @@ include $(top_srcdir)/subdir-rules.mk
 
 CLEANFILES += \
 	connect-tcp.pid \
+	connect-tcp6.pid \
 	connect-unix.pid \
 	connect-uri-nbd.pid \
 	connect-uri-nbd-unix.pid \
@@ -179,6 +180,7 @@ check_PROGRAMS += \
 	opt-list-meta \
 	connect-unix \
 	connect-tcp \
+	connect-tcp6 \
 	aio-connect \
 	aio-parallel \
 	aio-parallel-load \
@@ -220,6 +222,7 @@ TESTS += \
 	opt-list-meta \
 	connect-unix \
 	connect-tcp \
+	connect-tcp6 \
 	aio-connect \
 	aio-parallel.sh \
 	aio-parallel-load.sh \
@@ -442,6 +445,17 @@ connect_tcp_SOURCES = \
 connect_tcp_CPPFLAGS = -I$(top_srcdir)/include
 connect_tcp_CFLAGS = $(WARNINGS_CFLAGS)
 connect_tcp_LDADD = $(top_builddir)/lib/libnbd.la
+
+connect_tcp6_SOURCES = \
+	connect-tcp6.c \
+	pick-a-port.c \
+	pick-a-port.h \
+	requires.c \
+	requires.h \
+	$(NULL)
+connect_tcp6_CPPFLAGS = -I$(top_srcdir)/include
+connect_tcp6_CFLAGS = $(WARNINGS_CFLAGS)
+connect_tcp6_LDADD = $(top_builddir)/lib/libnbd.la
 
 aio_connect_SOURCES = \
 	aio-connect.c \

tests/connect-tcp6.c
@@ -1,0 +1,112 @@
+/* NBD client library in userspace
+ * Copyright (C) 2013-2019 Red Hat Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/* Test connecting to a TCP port over IPv6.
+ * This is mainly a test of URIs.
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <libnbd.h>
+
+#include "pick-a-port.h"
+#include "requires.h"
+
+#define PIDFILE "connect-tcp6.pid"
+
+int
+main (int argc, char *argv[])
+{
+  struct nbd_handle *nbd;
+  int port = pick_a_port ();
+  char port_str[16];
+  pid_t pid;
+  size_t i;
+  char uri[64], *get_uri;
+
+  /* Check the host supports IPv6. */
+  requires ("ip -o -6 addr show scope host | grep inet6");
+
+  unlink (PIDFILE);
+
+  snprintf (port_str, sizeof port_str, "%d", port);
+  snprintf (uri, sizeof uri, "nbd://[::1]:%d/", port);
+
+  pid = fork ();
+  if (pid == -1) {
+    perror ("fork");
+    exit (EXIT_FAILURE);
+  }
+  if (pid == 0) {
+    execlp ("nbdkit",
+            "nbdkit", "-f", "-p", port_str, "-P", PIDFILE,
+            "--exit-with-parent", "null", NULL);
+    perror ("nbdkit");
+    _exit (EXIT_FAILURE);
+  }
+
+  /* Wait for nbdkit to start listening. */
+  for (i = 0; i < 60; ++i) {
+    if (access (PIDFILE, F_OK) == 0)
+      break;
+    sleep (1);
+  }
+  unlink (PIDFILE);
+
+  nbd = nbd_create ();
+  if (nbd == NULL) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  if (nbd_supports_uri (nbd) != 1) {
+    fprintf (stderr, "skip: compiled without URI support\n");
+    exit (77);
+  }
+
+  if (nbd_connect_uri (nbd, uri) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  /* libnbd should be able to construct a URI for this connection. */
+  get_uri = nbd_get_uri (nbd);
+  if (get_uri == NULL) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  if (strcmp (uri, get_uri) != 0) {
+    fprintf (stderr, "%s: actual URI %s != expected URI %s\n",
+             argv[0], uri, get_uri);
+    exit (EXIT_FAILURE);
+  }
+  free (get_uri);
+
+  if (nbd_shutdown (nbd, 0) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  nbd_close (nbd);
+  exit (EXIT_SUCCESS);
+}