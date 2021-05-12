libguestfs/libnbd

commit 980b58d67451f7ece491283bc778e4b6b597b94a

info: Use portable sed in testsuite for BSD
'sed q1' is a GNUism; instead, use a pipeline to grep to control exit
status by whether the rest of the script produced output.

Reported-by: Richard W.M. Jones
Fixes: 345f2c3943 (info: List available meta-contexts)

info/info-list.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # nbd client library in userspace
-# Copyright (C) 2020 Red Hat Inc.
+# Copyright (C) 2020-2021 Red Hat Inc.
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -37,7 +37,7 @@ cat $out
 grep 'export="hello":' $out
 grep 'description: world' $out
 grep 'export-size: 1048576' $out
-sed -n '/contexts:/ { N; p; q }; $ q1' $out
+sed -n '/contexts:/ { N; p; q }' $out | grep .
 
 # ...and again with the export name included
 nbdkit -U - -e hello --filter=exportname memory 1M \
@@ -49,4 +49,4 @@ cat $out
 grep 'export="hello":' $out
 grep 'description: world' $out
 grep 'export-size: 1048576' $out
-sed -n '/contexts:/ { N; p; q }; $ q1' $out
+sed -n '/contexts:/ { N; p; q }' $out | grep .

info/info-text.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # nbd client library in userspace
-# Copyright (C) 2020 Red Hat Inc.
+# Copyright (C) 2020-2021 Red Hat Inc.
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -32,4 +32,4 @@ nbdkit -U - memory size=1M \
 cat $out
 grep "export-size: $((1024*1024))" $out
 grep "uri: nbd+unix:///?socket=" $out
-sed -n '/contexts:/ { N; p; q }; $ q1' $out
+sed -n '/contexts:/ { N; p; q }' $out | grep .

commit 03e7ebcad473a148e40b67ce5ff8ba5cf5472ad6

info: Remove another sed GNU-ism from tests
On FreeBSD each command must be followed by a semicolon when used
inside { groups }.

Fixes: commit 980b58d67451f7ece491283bc778e4b6b597b94a

info/info-list.sh
@@ -37,7 +37,7 @@ cat $out
 grep 'export="hello":' $out
 grep 'description: world' $out
 grep 'export-size: 1048576' $out
-sed -n '/contexts:/ { N; p; q }' $out | grep .
+sed -n '/contexts:/ { N; p; q; }' $out | grep .
 
 # ...and again with the export name included
 nbdkit -U - -e hello --filter=exportname memory 1M \
@@ -49,4 +49,4 @@ cat $out
 grep 'export="hello":' $out
 grep 'description: world' $out
 grep 'export-size: 1048576' $out
-sed -n '/contexts:/ { N; p; q }' $out | grep .
+sed -n '/contexts:/ { N; p; q; }' $out | grep .

info/info-text.sh
@@ -32,4 +32,4 @@ nbdkit -U - memory size=1M \
 cat $out
 grep "export-size: $((1024*1024))" $out
 grep "uri: nbd+unix:///?socket=" $out
-sed -n '/contexts:/ { N; p; q }' $out | grep .
+sed -n '/contexts:/ { N; p; q; }' $out | grep .