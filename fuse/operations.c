libguestfs/libnbd

commit 594d54c87713990e6b0da63da7e9eecdc405ed85

fuse: Fix race condition when starting operation thread
Fixes: commit 3f257eb60636f498649dc41af6a3d6996bd393bd

fuse/operations.c
@@ -131,11 +131,11 @@ operations_thread (void *arg)
     pthread_mutex_lock (&thread->start_mutex);
     while (nbd_aio_in_flight (h) == 0)
       pthread_cond_wait (&thread->start_cond, &thread->start_mutex);
-    pthread_mutex_unlock (&thread->start_mutex);
 
     /* Dispatch work while there are commands in flight. */
     while (nbd_aio_in_flight (h) > 0)
       nbd_poll (h, -1);
+    pthread_mutex_unlock (&thread->start_mutex);
   }
 
   /*NOTREACHED*/

commit f1af0e9cde9e4223dbd667c9d744da7cca821144

fuse: Further fix race condition when kicking operation thread
The previous fix still left a small window during which the start
condition could be missed, resulting a very rare but reproducible
deadlock.  Extend the lock around the whole loop to prevent this.

However even this is not completely sufficient.  It was possible that
operations threads would be slower to start than the FUSE threads, in
which case there could still be a deadlock where the FUSE threads
would signal the start condition before the operation thread had even
got to that point.  The solution is to add a barrier so we don't start
FUSE processing until the operations threads are running.

This whole thing needs to be rethought because the lock section is too
large, resulting in FUSE threads not being able to issue a command
unless there are no other commands in flight.  But at least for the
moment it should no longer deadlock.

Fixes: commit 594d54c87713990e6b0da63da7e9eecdc405ed85
Fixes: commit 3f257eb60636f498649dc41af6a3d6996bd393bd

fuse/operations.c
@@ -82,10 +82,23 @@ static struct thread {
   pthread_cond_t start_cond;
 } *threads;
 
+static pthread_barrier_t barrier;
+
 void
 start_operations_threads (void)
 {
   size_t i;
+  int err;
+
+  /* This barrier is used to ensure all operations threads have
+   * started up before we leave this function.
+   */
+  err = pthread_barrier_init (&barrier, NULL, nbd.size + 1);
+  if (err != 0) {
+    errno = err;
+    perror ("nbdfuse: pthread_barrier_init");
+    exit (EXIT_FAILURE);
+  }
 
   threads = calloc (nbd.size, sizeof (struct thread));
   if (threads == NULL) {
@@ -94,8 +107,6 @@ start_operations_threads (void)
   }
 
   for (i = 0; i < nbd.size; ++i) {
-    int err;
-
     threads[i].n = i;
     err = pthread_create (&threads[i].thread, NULL,
                           operations_thread, &threads[i]);
@@ -111,6 +122,10 @@ start_operations_threads (void)
       exit (EXIT_FAILURE);
     }
   }
+
+  /* Wait on the barrier. */
+  pthread_barrier_wait (&barrier);
+  pthread_barrier_destroy (&barrier);
 }
 
 struct completion {
@@ -126,19 +141,23 @@ operations_thread (void *arg)
   size_t n = thread->n;
   struct nbd_handle *h = nbd.ptr[n];
 
+  pthread_mutex_lock (&thread->start_mutex);
+
+  /* Signal to the main thread that we have initialized. */
+  pthread_barrier_wait (&barrier);
+
   while (1) {
     /* Sleep until a command is in flight. */
-    pthread_mutex_lock (&thread->start_mutex);
     while (nbd_aio_in_flight (h) == 0)
       pthread_cond_wait (&thread->start_cond, &thread->start_mutex);
 
     /* Dispatch work while there are commands in flight. */
     while (nbd_aio_in_flight (h) > 0)
       nbd_poll (h, -1);
-    pthread_mutex_unlock (&thread->start_mutex);
   }
 
   /*NOTREACHED*/
+  pthread_mutex_unlock (&thread->start_mutex);
   return NULL;
 }
 