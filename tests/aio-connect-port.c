30e647f801a56cb1bfa88320b460983173d64877
Richard W.M. Jones <rjones@redhat.com>
2021-06-30 20:31:05 +0100

    lib/uri.c: nbd_get_uri: Do not translate port name into service
    Because I forgot to use the NI_NUMERICSERV flag, getnameinfo would
    translate the port number into a service name (when possible) using
    /etc/services.  This is not really desirable since raw port numbers
    are more portable and descriptive.  This also caused occasional test
    failures when pick-a-port happened to pick a port number which
    coincided with a service name.
    
    This commit also adds a test.  Previous to the fix, the test failed with:
    
    $ ./run tests/aio-connect-port
    tests/.libs/aio-connect-port: actual URI nbd://127.0.0.1:sieve-filter/ != expected URI nbd://127.0.0.1:2000/
    
    This test cannot be run as part of the test suite because it requires
    a fixed port number.
    
    See also
    https://listman.redhat.com/archives/libguestfs/2021-June/msg00205.html
    
    Reported-by: Martin Kletzander
    (cherry picked from commit dc123cb099f22800a58a9497455457dc962bcf4a)

---
+++ tests/aio-connect-port.c
@@ -0,0 +1,128 @@
+/* NBD client library in userspace
+ * Copyright (C) 2013-2021 Red Hat Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/* Test connecting to a fixed IPv4 TCP port (2000) using
+ * nbd_aio_connect.  This test is compiled but not normally run
+ * because of the requirement for the fixed port number.
+ *
+ * See also:
+ * https://listman.redhat.com/archives/libguestfs/2021-June/msg00205.html
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <libnbd.h>
+
+#define PIDFILE "aio-connect-port.pid"
+
+int
+main (int argc, char *argv[])
+{
+  struct nbd_handle *nbd;
+  int port = 2000;
+  const char *port_str = "2000";
+  pid_t pid;
+  size_t i;
+  struct sockaddr_in addr;
+  char *actual_uri, *expected_uri;
+
+  unlink (PIDFILE);
+
+  pid = fork ();
+  if (pid == -1) {
+    perror ("fork");
+    exit (EXIT_FAILURE);
+  }
+  if (pid == 0) {
+    execlp ("nbdkit",
+            "nbdkit", "-f", "-p", port_str, "-P", PIDFILE,
+            "--exit-with-parent", "null", NULL);
+    perror ("nbdkit");
+    _exit (EXIT_FAILURE);
+  }
+
+  /* Wait for nbdkit to start listening. */
+  for (i = 0; i < 60; ++i) {
+    if (access (PIDFILE, F_OK) == 0)
+      break;
+    sleep (1);
+  }
+  unlink (PIDFILE);
+
+  nbd = nbd_create ();
+  if (nbd == NULL) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  if (nbd_supports_uri (nbd) != 1) {
+    fprintf (stderr, "skip: compiled without URI support\n");
+    exit (77);
+  }
+
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = htonl (INADDR_LOOPBACK);
+  addr.sin_port = htons (port);
+
+  if (nbd_aio_connect (nbd, (struct sockaddr *) &addr, sizeof addr) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  /* Wait until we have connected. */
+  while (!nbd_aio_is_ready (nbd)) {
+    if (nbd_poll (nbd, -1) == -1) {
+      fprintf (stderr, "%s\n", nbd_get_error ());
+      exit (EXIT_FAILURE);
+    }
+  }
+
+  /* libnbd should be able to construct a URI for this connection. */
+  if (asprintf (&expected_uri, "nbd://127.0.0.1:%s/", port_str) == -1) {
+    perror ("asprintf");
+    exit (EXIT_FAILURE);
+  }
+  actual_uri = nbd_get_uri (nbd);
+  if (actual_uri == NULL) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  if (strcmp (actual_uri, expected_uri) != 0) {
+    fprintf (stderr, "%s: actual URI %s != expected URI %s\n",
+             argv[0], actual_uri, expected_uri);
+    exit (EXIT_FAILURE);
+  }
+  free (actual_uri);
+  free (expected_uri);
+
+  if (nbd_shutdown (nbd, 0) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  nbd_close (nbd);
+  exit (EXIT_SUCCESS);
+}
