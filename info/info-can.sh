libguestfs/libnbd

commit 65cdf7354b8e4dd6db7dd31fa4321bea1d43650d

info: Require can_cache for info-can.sh
Signed-off-by: Martin Kletzander <mkletzan@redhat.com>

info/info-can.sh
@@ -22,7 +22,7 @@ set -e
 set -x
 
 requires nbdkit --version
-requires nbdkit sh --version
+requires bash -c "nbdkit sh --dump-config | grep has_can_cache=1"
 
 # --is read-only and --can write are tested in info-is-read-only.sh
 

commit 6e6100cb2ab0a814c5ba9db9575d69196fc8f9b7

info/info-can.sh: Fix requires test
We need to test the nbdkit sh --dump-plugin output (not --dump-config
which shows the config for the whole server).

Fixes: commit 65cdf7354b8e4dd6db7dd31fa4321bea1d43650d

info/info-can.sh
@@ -22,7 +22,7 @@ set -e
 set -x
 
 requires nbdkit --version
-requires bash -c "nbdkit sh --dump-config | grep has_can_cache=1"
+requires bash -c "nbdkit sh --dump-plugin | grep has_can_cache=1"
 
 # --is read-only and --can write are tested in info-is-read-only.sh
 