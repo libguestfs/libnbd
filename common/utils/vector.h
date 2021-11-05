libguestfs/libnbd

commit cc0567e9aed7e6b40a44bf8eac0a262ac7314fec

common/utils/vector: Rename `size` to `len`
The field `size` may be confusing with the size of the underlying array.
Rename to `len`, a common term for this concept.

Signed-off-by: Nir Soffer <nsoffer@redhat.com>

common/utils/vector.h
@@ -84,9 +84,9 @@
  */
 #define DEFINE_VECTOR_TYPE(name, type)                                  \
   struct name {                                                         \
-    type *ptr;                 /* Pointer to array of items. */         \
-    size_t size;               /* Number of valid items in the array. */ \
-    size_t cap;                /* Maximum number of items. */           \
+    type *ptr;              /* Pointer to array of items. */            \
+    size_t len;             /* Number of valid items in the array. */   \
+    size_t cap;             /* Maximum number of items. */              \
   };                                                                    \
   typedef struct name name;                                             \
                                                                         \
@@ -101,17 +101,17 @@
                                    sizeof (type));                      \
   }                                                                     \
                                                                         \
-  /* Insert at i'th element.  i=0 => beginning  i=size => append */     \
+  /* Insert at i'th element.  i=0 => beginning  i=len => append */      \
   static inline int                                                     \
   name##_insert (name *v, type elem, size_t i)                          \
   {                                                                     \
-    assert (i <= v->size);                                              \
-    if (v->size >= v->cap) {                                            \
+    assert (i <= v->len);                                               \
+    if (v->len >= v->cap) {                                             \
       if (name##_reserve (v, 1) == -1) return -1;                       \
     }                                                                   \
-    memmove (&v->ptr[i+1], &v->ptr[i], (v->size-i) * sizeof (elem));    \
+    memmove (&v->ptr[i+1], &v->ptr[i], (v->len-i) * sizeof (elem));     \
     v->ptr[i] = elem;                                                   \
-    v->size++;                                                          \
+    v->len++;                                                           \
     return 0;                                                           \
   }                                                                     \
                                                                         \
@@ -119,16 +119,16 @@
   static inline int                                                     \
   name##_append (name *v, type elem)                                    \
   {                                                                     \
-    return name##_insert (v, elem, v->size);                            \
+    return name##_insert (v, elem, v->len);                             \
   }                                                                     \
                                                                         \
-  /* Remove i'th element.  i=0 => beginning  i=size-1 => end */         \
+  /* Remove i'th element.  i=0 => beginning  i=len-1 => end */          \
   static inline void                                                    \
   name##_remove (name *v, size_t i)                                     \
   {                                                                     \
-    assert (i < v->size);                                               \
-    memmove (&v->ptr[i], &v->ptr[i+1], (v->size-i-1) * sizeof (type));  \
-    v->size--;                                                          \
+    assert (i < v->len);                                                \
+    memmove (&v->ptr[i], &v->ptr[i+1], (v->len-i-1) * sizeof (type));   \
+    v->len--;                                                           \
   }                                                                     \
                                                                         \
   /* Remove all elements and deallocate the vector. */                  \
@@ -137,7 +137,7 @@
   {                                                                     \
     free (v->ptr);                                                      \
     v->ptr = NULL;                                                      \
-    v->size = v->cap = 0;                                               \
+    v->len = v->cap = 0;                                                \
   }                                                                     \
                                                                         \
   /* Iterate over the vector, calling f() on each element. */           \
@@ -145,7 +145,7 @@
   name##_iter (name *v, void (*f) (type elem))                          \
   {                                                                     \
     size_t i;                                                           \
-    for (i = 0; i < v->size; ++i)                                       \
+    for (i = 0; i < v->len; ++i)                                        \
       f (v->ptr[i]);                                                    \
   }                                                                     \
                                                                         \
@@ -154,7 +154,7 @@
   name##_sort (name *v,                                                 \
                int (*compare) (const type *p1, const type *p2))         \
   {                                                                     \
-    qsort (v->ptr, v->size, sizeof (type), (void *) compare);           \
+    qsort (v->ptr, v->len, sizeof (type), (void *) compare);            \
   }                                                                     \
                                                                         \
   /* Search for an exactly matching element in the vector using a       \
@@ -164,15 +164,15 @@
   name##_search (const name *v, const void *key,                        \
                  int (*compare) (const void *key, const type *v))       \
   {                                                                     \
-    return bsearch (key, v->ptr, v->size, sizeof (type),                \
+    return bsearch (key, v->ptr, v->len, sizeof (type),                 \
                     (void *) compare);                                  \
   }
 
-#define empty_vector { .ptr = NULL, .size = 0, .cap = 0 }
+#define empty_vector { .ptr = NULL, .len = 0, .cap = 0 }
 
 struct generic_vector {
   void *ptr;
-  size_t size;
+  size_t len;
   size_t cap;
 };
 

common/utils/test-vector.c
@@ -73,7 +73,7 @@ test_int64_vector (void)
     assert (v.ptr[i] == i);
 
   int64_vector_remove (&v, 1);
-  assert (v.size == 9);
+  assert (v.len == 9);
   assert (v.ptr[1] == 2);
 
   tmp = 10;

copy/file-ops.c
@@ -101,7 +101,7 @@ page_size_init (void)
 
 /* Load the page cache map for a particular file into
  * rwf->cached_pages.  Only used when reading files.  This doesn't
- * fail: if a system call fails then rwf->cached_pages.size will be
+ * fail: if a system call fails then rwf->cached_pages.len will be
  * zero which is handled in page_cache_evict.
  */
 static inline void
@@ -121,7 +121,7 @@ page_cache_map (struct rw_file *rwf)
   if (mincore (ptr, rwf->rw.size, rwf->cached_pages.ptr) == -1)
     goto out;
 
-  rwf->cached_pages.size = veclen;
+  rwf->cached_pages.len = veclen;
  out:
   munmap (ptr, rwf->rw.size);
 }
@@ -133,7 +133,7 @@ static inline bool
 page_was_cached (struct rw_file *rwf, uint64_t offset)
 {
   uint64_t page = offset / page_size;
-  assert (page < rwf->cached_pages.size);
+  assert (page < rwf->cached_pages.len);
 
   return (rwf->cached_pages.ptr[page] & 1) != 0;
 }
@@ -151,7 +151,7 @@ page_cache_evict (struct rw_file *rwf, uint64_t orig_offset, size_t orig_len)
    * that pages were mapped so we will not evict them: essentially fall
    * back to doing nothing.
    */
-  if (rwf->cached_pages.size == 0) return;
+  if (rwf->cached_pages.len == 0) return;
 
   /* Only bother with whole pages. */
   offset = ROUND_UP (orig_offset, page_size);
@@ -635,7 +635,7 @@ file_get_extents (struct rw *rw, uintptr_t index,
                   uint64_t offset, uint64_t count,
                   extent_list *ret)
 {
-  ret->size = 0;
+  ret->len = 0;
 
 #ifdef SEEK_HOLE
   struct rw_file *rwf = (struct rw_file *)rw;
@@ -704,8 +704,8 @@ file_get_extents (struct rw *rw, uintptr_t index,
     /* The last extent may extend beyond the request bounds.  We must
      * truncate it.
      */
-    assert (ret->size > 0);
-    last = ret->size - 1;
+    assert (ret->len > 0);
+    last = ret->len - 1;
     assert (ret->ptr[last].offset <= end);
     if (ret->ptr[last].offset + ret->ptr[last].length > end) {
       uint64_t d = ret->ptr[last].offset + ret->ptr[last].length - end;

copy/main.c
@@ -526,7 +526,7 @@ default_get_extents (struct rw *rw, uintptr_t index,
 {
   struct extent e;
 
-  ret->size = 0;
+  ret->len = 0;
 
   e.offset = offset;
   e.length = count;

copy/multi-thread-copying.c
@@ -154,7 +154,7 @@ worker_thread (void *indexp)
     else
       default_get_extents (src, index, offset, count, &exts);
 
-    for (i = 0; i < exts.size; ++i) {
+    for (i = 0; i < exts.len; ++i) {
       struct command *command;
       struct buffer *buffer;
       char *data;

copy/nbd-ops.c
@@ -90,7 +90,7 @@ open_one_nbd_handle (struct rw_nbd *rwn)
   /* Cache these.  We assume with multi-conn that each handle will act
    * the same way.
    */
-  if (rwn->handles.size == 0) {
+  if (rwn->handles.len == 0) {
     rwn->can_zero = nbd_can_zero (nbd) > 0;
     rwn->rw.size = nbd_get_size (nbd);
     if (rwn->rw.size == -1) {
@@ -157,7 +157,7 @@ nbd_ops_close (struct rw *rw)
   struct rw_nbd *rwn = (struct rw_nbd *) rw;
   size_t i;
 
-  for (i = 0; i < rwn->handles.size; ++i) {
+  for (i = 0; i < rwn->handles.len; ++i) {
     if (nbd_shutdown (rwn->handles.ptr[i], 0) == -1) {
       fprintf (stderr, "%s: %s\n", rw->name, nbd_get_error ());
       exit (EXIT_FAILURE);
@@ -176,7 +176,7 @@ nbd_ops_flush (struct rw *rw)
   struct rw_nbd *rwn = (struct rw_nbd *) rw;
   size_t i;
 
-  for (i = 0; i < rwn->handles.size; ++i) {
+  for (i = 0; i < rwn->handles.len; ++i) {
     if (nbd_flush (rwn->handles.ptr[i], 0) == -1) {
       fprintf (stderr, "%s: %s\n", rw->name, nbd_get_error ());
       exit (EXIT_FAILURE);
@@ -189,7 +189,7 @@ nbd_ops_is_read_only (struct rw *rw)
 {
   struct rw_nbd *rwn = (struct rw_nbd *) rw;
 
-  if (rwn->handles.size > 0)
+  if (rwn->handles.len > 0)
     return nbd_is_read_only (rwn->handles.ptr[0]);
   else
     return false;
@@ -200,7 +200,7 @@ nbd_ops_can_extents (struct rw *rw)
 {
   struct rw_nbd *rwn = (struct rw_nbd *) rw;
 
-  if (rwn->handles.size > 0)
+  if (rwn->handles.len > 0)
     return nbd_can_meta_context (rwn->handles.ptr[0], "base:allocation");
   else
     return false;
@@ -211,7 +211,7 @@ nbd_ops_can_multi_conn (struct rw *rw)
 {
   struct rw_nbd *rwn = (struct rw_nbd *) rw;
 
-  if (rwn->handles.size > 0)
+  if (rwn->handles.len > 0)
     return nbd_can_multi_conn (rwn->handles.ptr[0]);
   else
     return false;
@@ -226,7 +226,7 @@ nbd_ops_start_multi_conn (struct rw *rw)
   for (i = 1; i < connections; ++i)
     open_one_nbd_handle (rwn);
 
-  assert (rwn->handles.size == connections);
+  assert (rwn->handles.len == connections);
 }
 
 static size_t
@@ -436,13 +436,13 @@ nbd_ops_get_extents (struct rw *rw, uintptr_t index,
 
   nbd = rwn->handles.ptr[index];
 
-  ret->size = 0;
+  ret->len = 0;
 
   while (count > 0) {
     const uint64_t old_offset = offset;
     size_t i;
 
-    exts.size = 0;
+    exts.len = 0;
     if (nbd_block_status (nbd, count, offset,
                           (nbd_extent_callback) {
                             .user_data = &exts,
@@ -456,7 +456,7 @@ nbd_ops_get_extents (struct rw *rw, uintptr_t index,
     }
 
     /* Copy the extents returned into the final list (ret). */
-    for (i = 0; i < exts.size; ++i) {
+    for (i = 0; i < exts.len; ++i) {
       uint64_t d;
 
       assert (exts.ptr[i].offset == offset);

copy/synch-copying.c
@@ -70,7 +70,7 @@ synch_copying (void)
       else
         default_get_extents (src, 0, offset, count, &exts);
 
-      for (i = 0; i < exts.size; ++i) {
+      for (i = 0; i < exts.len; ++i) {
         assert (exts.ptr[i].length <= count);
 
         if (exts.ptr[i].zero) {

fuse/nbdfuse.c
@@ -415,7 +415,7 @@ main (int argc, char *argv[])
       handles_append (&nbd, h); /* reserved above, so can't fail */
     }
   }
-  connections = (unsigned) nbd.size;
+  connections = (unsigned) nbd.len;
   if (verbose)
     fprintf (stderr, "nbdfuse: connections=%u\n", connections);
 
@@ -511,7 +511,7 @@ main (int argc, char *argv[])
   fuse_destroy (fuse);
 
   /* Close NBD handle(s). */
-  for (i = 0; i < nbd.size; ++i)
+  for (i = 0; i < nbd.len; ++i)
     nbd_close (nbd.ptr[i]);
 
   free (mountpoint);

commit 3d6be922e70153a220c7e5c7ae2425d1ff7197f8

common/utils/vector.h: Remove stale reference to `size`
Update stale comments to use `len` instead of `size`.

Fixes: commit cc0567e9aed7e6b40a44bf8eac0a262ac7314fec
Signed-off-by: Nir Soffer <nsoffer@redhat.com>

common/utils/vector.h
@@ -59,14 +59,14 @@
  *
  *   string_vector names = empty_vector;
  *
- * where ‘names.ptr[]’ will be an array of strings and ‘names.size’
+ * where ‘names.ptr[]’ will be an array of strings and ‘names.len’
  * will be the number of strings.  There are no get/set accessors.  To
  * iterate over the strings you can use the ‘.ptr’ field directly:
  *
- *   for (size_t i = 0; i < names.size; ++i)
+ *   for (size_t i = 0; i < names.len; ++i)
  *     printf ("%s\n", names.ptr[i]);
  *
- * Initializing with ‘empty_vector’ sets ‘.ptr = NULL’ and ‘.size = 0’.
+ * Initializing with ‘empty_vector’ sets ‘.ptr = NULL’ and ‘.len = 0’.
  *
  * DEFINE_VECTOR_TYPE also defines utility functions.  For the full
  * list see the definition below, but useful functions include:
@@ -91,8 +91,8 @@
   typedef struct name name;                                             \
                                                                         \
   /* Reserve n elements at the end of the vector.  Note space is        \
-   * allocated but the vector size is not increased and the new         \
-   * elements are not initialized.                                      \
+   * allocated and capacity is increased, but the vector length is      \
+   * not increased and the new elements are not initialized.            \
    */                                                                   \
   static inline int                                                     \
   name##_reserve (name *v, size_t n)                                    \