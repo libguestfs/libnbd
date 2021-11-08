libguestfs/libnbd

commit c7aac3639145cee2a9a2134bc151a6f570f7edf4

info: Implement nbdinfo [ CMD ... ]
Provides a simpler way to query qcow2 files, eg:

 nbdinfo -- [ qemu-nbd -r -f qcow2 file.qcow2 ]

 nbdinfo --map -- [ qemu-nbd -r -f qcow2 file.qcow2 ]

It also works with nbdkit and is in fact slightly more compact than
the usual captive nbdkit command because you don't need to specify the
-U, --run or "$uri" parts of the command.

info/info-cmd-json.sh
@@ -1,0 +1,37 @@
+#!/usr/bin/env bash
+# nbd client library in userspace
+# Copyright (C) 2020-2021 Red Hat Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+. ../tests/functions.sh
+
+set -e
+set -x
+
+requires nbdkit --version
+requires nbdkit memory --version
+requires jq --version
+
+out=info-cmd-json.out
+cleanup_fn rm -f $out
+
+$VG nbdinfo --json -- [ nbdkit -r memory size=1M ] > $out
+jq . < $out
+test $( jq -r '.protocol' < $out ) != "newstyle"
+test $( jq -r '.exports[0]."export-size"' < $out ) != "null"
+test $( jq -r '.exports[0].is_read_only' < $out ) = "true"
+test $( jq -r '.exports[0].contexts[] | select(. == "base:allocation")' \
+	   < $out ) = "base:allocation"

info/Makefile.am
@@ -22,6 +22,9 @@ info_sh_files = \
 	info-can-connect.sh \
 	info-can-read.sh \
 	info-can-zero.sh \
+	info-cmd-json.sh \
+	info-cmd-map-totals.sh \
+	info-cmd-size.sh \
 	info-is-read-only.sh \
 	info-list.sh \
 	info-list-json.sh \

info/info-cmd-map-totals.sh
@@ -1,0 +1,42 @@
+#!/usr/bin/env bash
+# nbd client library in userspace
+# Copyright (C) 2020-2021 Red Hat Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+. ../tests/functions.sh
+
+set -e
+set -x
+
+requires nbdkit --version
+requires nbdkit -U - null --run 'test "$uri" != ""'
+requires tr --version
+
+out=info-cmd-map-totals.out
+cleanup_fn rm -f $out
+rm -f $out
+
+# The sparse allocator used by nbdkit-data-plugin uses a 32K page
+# size, and extents are always aligned with this.
+$VG nbdinfo --map --totals [ nbdkit data data='1 @131072 2' size=1M ] > $out
+
+cat $out
+
+if [ "$(tr -s ' ' < $out)" != " 65536 6.2% 0 data
+ 983040 93.8% 3 hole,zero" ]; then
+    echo "$0: unexpected output from nbdinfo --map"
+    exit 1
+fi

info/info-cmd-size.sh
@@ -1,0 +1,31 @@
+#!/usr/bin/env bash
+# nbd client library in userspace
+# Copyright (C) 2020 Red Hat Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+. ../tests/functions.sh
+
+set -e
+set -x
+
+requires nbdkit --version
+requires nbdkit memory --version
+
+out=info-cmd-size.out
+cleanup_fn rm -f $out
+
+$VG nbdinfo --size [ nbdkit memory size=$((512*1024*1024)) ] > $out
+test "$(cat $out)" -eq $((512*1024*1024))

info/list.c
@@ -83,7 +83,7 @@ free_exports (void)
 }
 
 bool
-list_all_exports (const char *uri)
+list_all_exports (void)
 {
   size_t i;
   bool list_okay = true;
@@ -106,8 +106,8 @@ list_all_exports (const char *uri)
       nbd_set_opt_mode (nbd2, true);
       nbd_set_full_info (nbd2, true);
 
-      if (nbd_connect_uri (nbd2, uri) == -1 ||
-          nbd_set_export_name (nbd2, name) == -1) {
+      do_connect (nbd2);
+      if (nbd_set_export_name (nbd2, name) == -1) {
         fprintf (stderr, "%s: %s\n", progname, nbd_get_error ());
         exit (EXIT_FAILURE);
       }

info/main.c
@@ -45,6 +45,10 @@ const char *map = NULL;         /* --map option */
 bool size_only = false;         /* --size option */
 bool totals = false;            /* --totals option */
 
+/* See do_connect () */
+static enum { MODE_URI = 1, MODE_SQUARE_BRACKET } mode;
+static char **args;
+
 static void __attribute__((noreturn))
 usage (FILE *fp, int exitcode)
 {
@@ -52,12 +56,12 @@ usage (FILE *fp, int exitcode)
 "\n"
 "Display information and metadata about NBD servers and exports:\n"
 "\n"
-"    nbdinfo [--json] NBD-URI\n"
-"    nbdinfo --size [--json] NBD-URI\n"
-"    nbdinfo --is read-only|rotational NBD-URI\n"
-"    nbdinfo --can cache|connect|... NBD-URI\n"
-"    nbdinfo --map [--totals] [--json] NBD-URI\n"
-"    nbdinfo -L|--list [--json] NBD-URI\n"
+"    nbdinfo [--json] NBD-URI | [ CMD ARGS ... ]\n"
+"    nbdinfo --size [--json] NBD-URI | [ CMD ARGS ... ]\n"
+"    nbdinfo --is read-only|rotational NBD-URI | [ CMD ARGS ... ]\n"
+"    nbdinfo --can cache|connect|... NBD-URI | [ CMD ARGS ... ]\n"
+"    nbdinfo --map [--totals] [--json] NBD-URI | [ CMD ARGS ... ]\n"
+"    nbdinfo -L|--list [--json] NBD-URI | [ CMD ARGS ... ]\n"
 "\n"
 "Other options:\n"
 "\n"
@@ -74,6 +78,7 @@ usage (FILE *fp, int exitcode)
 "    nbdinfo --map nbd://example.com\n"
 "    nbdinfo --json nbd://example.com\n"
 "    nbdinfo --list nbd://example.com\n"
+"    nbdinfo --map -- [ qemu-nbd -r -f qcow2 file.qcow2 ]\n"
 "\n"
 "Please read the nbdinfo(1) manual page for full usage.\n"
 "\n"
@@ -188,9 +193,21 @@ main (int argc, char *argv[])
     }
   }
 
-  /* There must be exactly 1 parameter, the URI. */
-  if (argc - optind != 1)
+  /* Is it a URI or subprocess? */
+  if (argc - optind >= 3 &&
+      strcmp (argv[optind], "[") == 0 &&
+      strcmp (argv[argc-1], "]") == 0) {
+    mode = MODE_SQUARE_BRACKET;
+    argv[argc-1] = NULL;
+    args = &argv[optind+1];
+  }
+  else if (argc - optind == 1) {
+    mode = MODE_URI;
+    args = &argv[optind];
+  }
+  else {
     usage (stderr, EXIT_FAILURE);
+  }
 
   /* You cannot combine certain options. */
   if (!!list_all + !!can + !!map + !!size_only > 1) {
@@ -249,10 +266,7 @@ main (int argc, char *argv[])
     nbd_add_meta_context (nbd, map);
 
   /* Connect to the server. */
-  if (nbd_connect_uri (nbd, argv[optind]) == -1) {
-    fprintf (stderr, "%s: %s\n", progname, nbd_get_error ());
-    exit (EXIT_FAILURE);
-  }
+  do_connect (nbd);
 
   /* In --list mode, during negotiation we collect the list of exports. */
   if (list_all)                 /* --list */
@@ -295,7 +309,7 @@ main (int argc, char *argv[])
     if (!list_all)
       list_okay = show_one_export (nbd, NULL, true, true);
     else
-      list_okay = list_all_exports (argv[optind]);
+      list_okay = list_all_exports ();
 
     if (json_output)
       fprintf (fp, "}\n");
@@ -322,4 +336,40 @@ main (int argc, char *argv[])
 
   if (can) exit (can_exit_code);
   exit (list_okay ? EXIT_SUCCESS : EXIT_FAILURE);
+}
+
+/* Connect the handle to the server. */
+void
+do_connect (struct nbd_handle *nbd)
+{
+  int r;
+
+  switch (mode) {
+  case MODE_URI:                /* NBD-URI */
+    r = nbd_connect_uri (nbd, args[0]);
+    break;
+
+  case MODE_SQUARE_BRACKET:     /* [ CMD ARGS ... ] */
+    r = nbd_connect_systemd_socket_activation (nbd, args);
+    break;
+
+  default:
+    abort ();
+  }
+
+  if (r == -1) {
+    fprintf (stderr, "%s: %s\n", progname, nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+}
+
+/* The URI field in output is not meaningful unless there's a
+ * persistent NBD server running, that is to say that nbdinfo was
+ * invoked with a URI (not a [ subprocess ]).  If this returns false
+ * it suppresses the uri: field in output.
+ */
+bool
+uri_is_meaingful (void)
+{
+  return mode == MODE_URI;
 }

info/nbdinfo.h
@@ -35,13 +35,16 @@ extern const char *map;
 extern bool size_only;
 extern bool totals;
 
+void do_connect (struct nbd_handle *);
+bool uri_is_meaingful (void);
+
 /* can.c */
 extern int can_exit_code;
 extern void do_can (void);
 
 /* list.c */
 extern void collect_exports (void);
-extern bool list_all_exports (const char *uri);
+extern bool list_all_exports (void);
 extern void free_exports (void);
 
 /* map.c */

info/nbdinfo.pod
@@ -4,17 +4,22 @@ nbdinfo - display information and metadata about NBD servers and exports
 
 =head1 SYNOPSIS
 
- nbdinfo [--json] NBD-URI
+ nbdinfo [--json] NBD
 
- nbdinfo --size [--json] NBD-URI
+C<NBD> is an NBD URI or subprocess:
 
- nbdinfo --is read-only|rotational NBD-URI
+ NBD := nbd://... | nbd+unix:// (or other URI formats)
+      | [ CMD ARGS ... ]
 
- nbdinfo --can cache|connect|... NBD-URI
+ nbdinfo --size [--json] NBD
 
- nbdinfo --map [--totals] [--json] NBD-URI
+ nbdinfo --is read-only|rotational NBD
 
- nbdinfo -L|--list [--json] NBD-URI
+ nbdinfo --can cache|connect|... NBD
+
+ nbdinfo --map [--totals] [--json] NBD
+
+ nbdinfo -L|--list [--json] NBD
 
  nbdinfo --help
 
@@ -22,8 +27,9 @@ nbdinfo - display information and metadata about NBD servers and exports
 
 =head1 DESCRIPTION
 
-nbdinfo displays information and metadata about an NBD server.  The
-only required parameter is the NBD URI of the server (see
+nbdinfo displays information and metadata about an NBD server.
+
+The single required parameter can be the NBD URI of the server (see
 L<https://github.com/NetworkBlockDevice/nbd/blob/master/doc/uri.md>):
 
  $ nbdinfo nbd://localhost
@@ -50,6 +56,11 @@ For an NBD server on a local Unix domain socket you would use a
 command such as this (with similar output to above):
 
  $ nbdinfo "nbd+unix:///?socket=/tmp/unixsock"
+
+Or you can run the NBD server as a subprocess (see section
+L</Subprocess> below):
+
+ $ nbdinfo -- [ qemu-nbd -r -f qcow2 file.qcow2 ]
 
 =head2 JSON output
 
@@ -90,6 +101,9 @@ To display only the size in bytes of the NBD export (useful for
 scripting) use the I<--size> parameter:
 
  $ nbdinfo --size nbd://localhost
+ 1048576
+
+ $ nbdinfo --size [ nbdkit null 1M ]
  1048576
 
 =head2 Test for flags
@@ -230,6 +244,24 @@ For example:
      export-size: 3955556352 (3862848K)
      uri: nbd://localhost:10809/debian-10.4.0-amd64-DVD-1.iso
      [...]
+
+=head2 Subprocess
+
+nbdinfo can also run an NBD server as a subprocess.  This requires an
+NBD server which understands systemd socket activation, such as
+L<qemu-nbd(8)> or L<nbdkit(1)>.  All the usual nbdinfo modes can be
+used.
+
+For example, to give general information or display the map of a qcow2
+file:
+
+ nbdinfo -- [ qemu-nbd -r -f qcow2 file.qcow2 ]
+
+ nbdinfo --map -- [ qemu-nbd -r -f qcow2 file.qcow2 ]
+
+Note that S<C<[ ... ]>> are separate parameters, and must be
+surrounded by spaces.  C<--> separates nbdinfo parameters from
+subprocess parameters.
 
 =head2 Alternative tools
 

commit ae8d4d38a73095647c78b17bfeb5300d09a06e57

info/info-cmd-json.sh: Fix test when run against ancient nbdkit
nbdkit 1.10 didn't know about extents (first implemented in
nbdkit 1.11.10).  This version of nbdkit is present in Debian 10 which
is used by libnbd CI.

The test failed with:

++ jq -r '.exports[0].contexts[] | select(. == "base:allocation")'
jq: error (at <stdin>:21): Cannot iterate over null (null)

The easiest fix is just to remove this line from the test.

Fixes: commit c7aac3639145cee2a9a2134bc151a6f570f7edf4

info/info-cmd-json.sh
@@ -33,5 +33,3 @@ jq . < $out
 test $( jq -r '.protocol' < $out ) != "newstyle"
 test $( jq -r '.exports[0]."export-size"' < $out ) != "null"
 test $( jq -r '.exports[0].is_read_only' < $out ) = "true"
-test $( jq -r '.exports[0].contexts[] | select(. == "base:allocation")' \
-	   < $out ) = "base:allocation"