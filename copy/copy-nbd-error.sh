libguestfs/libnbd

commit bc896eec4dfcc621dc058f9a136c2f02fd2a8b61

copy: Implement multi-conn, multiple threads, multiple requests in flight.
This commit implements a multi-threaded copying loop, with control
over number of threads, multi-conn and requests in flight.  The
implementation is based on the AIO tests.

copy/Makefile.am
@@ -37,12 +37,23 @@ TESTS =
 nbdcopy_SOURCES = \
 	nbdcopy.c \
 	nbdcopy.h \
+	multi-thread-copying.c \
 	progress.c \
 	synch-copying.c \
 	$(NULL)
-nbdcopy_CPPFLAGS = -I$(top_srcdir)/include
-nbdcopy_CFLAGS = $(WARNINGS_CFLAGS)
-nbdcopy_LDADD = $(top_builddir)/lib/libnbd.la
+nbdcopy_CPPFLAGS = \
+	-I$(top_srcdir)/include \
+	-I$(top_srcdir)/common/utils \
+	$(NULL)
+nbdcopy_CFLAGS = \
+	$(WARNINGS_CFLAGS) \
+	$(PTHREAD_CFLAGS) \
+	$(NULL)
+nbdcopy_LDADD = \
+	$(PTHREAD_LIBS) \
+	$(top_builddir)/common/utils/libutils.la \
+	$(top_builddir)/lib/libnbd.la \
+	$(NULL)
 
 # This program depends on URI support; no point in installing it otherwise
 if HAVE_LIBXML2

copy/multi-thread-copying.c
@@ -1,0 +1,424 @@
+/* NBD client library in userspace.
+ * Copyright (C) 2020 Red Hat Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <poll.h>
+#include <errno.h>
+#include <assert.h>
+
+#include <pthread.h>
+
+#include <libnbd.h>
+
+#include "nbdcopy.h"
+
+/* Threads pick up work in units of THREAD_WORK_SIZE starting at the
+ * next_offset.  The lock protects next_offset.
+ */
+static uint64_t next_offset = 0;
+static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+static bool
+get_next_offset (uint64_t *offset, uint64_t *count)
+{
+  bool r = false;               /* returning false means no more work */
+
+  pthread_mutex_lock (&lock);
+  if (next_offset < src.size) {
+    *offset = next_offset;
+
+    /* Work out how large this range is.  The last range may be
+     * smaller than THREAD_WORK_SIZE.
+     */
+    *count = src.size - *offset;
+    if (*count > THREAD_WORK_SIZE)
+      *count = THREAD_WORK_SIZE;
+
+    next_offset += THREAD_WORK_SIZE;
+    r = true;                   /* there is more work */
+  }
+  pthread_mutex_unlock (&lock);
+  return r;
+}
+
+static void *worker_thread (void *ip);
+
+void
+multi_thread_copying (void)
+{
+  pthread_t *workers;
+  size_t i;
+  int err;
+
+  /* Some invariants that should be true if the main program called us
+   * correctly.
+   */
+  assert (threads > 0);
+  assert (threads == connections);
+  if (src.t == NBD)
+    assert (src.u.nbd.size == connections);
+  if (dst.t == NBD)
+    assert (dst.u.nbd.size == connections);
+  assert (src.size != -1);
+
+  workers = malloc (sizeof (pthread_t) * threads);
+  if (workers == NULL) {
+    perror ("malloc");
+    exit (EXIT_FAILURE);
+  }
+
+  /* Start the worker threads. */
+  for (i = 0; i < threads; ++i) {
+    err = pthread_create (&workers[i], NULL, worker_thread,
+                          (void *)(uintptr_t)i);
+    if (err != 0) {
+      errno = err;
+      perror ("pthread_create");
+      exit (EXIT_FAILURE);
+    }
+  }
+
+  /* Wait until all worker threads exit. */
+  for (i = 0; i < threads; ++i) {
+    err = pthread_join (workers[i], NULL);
+    if (err != 0) {
+      errno = err;
+      perror ("pthread_join");
+      exit (EXIT_FAILURE);
+    }
+  }
+
+  free (workers);
+}
+
+/* Each command in flight has this associated struct. */
+struct buffer {
+  uint64_t offset;
+  size_t n;
+  char *data;
+  struct nbd_handle *dst_nbd;
+};
+
+static unsigned in_flight (struct nbd_handle *src_nbd,
+                           struct nbd_handle *dst_nbd);
+static void poll_both_ends (struct nbd_handle *src_nbd,
+                            struct nbd_handle *dst_nbd);
+static int finished_read (void *vp, int *error);
+static int finished_write (void *vp, int *error);
+
+/* There are 'threads' worker threads, each copying work ranges from
+ * src to dst until there are no more work ranges.
+ */
+static void *
+worker_thread (void *indexp)
+{
+  uintptr_t index = (uintptr_t) indexp;
+  uint64_t offset, count;
+  struct nbd_handle *src_nbd, *dst_nbd;
+  bool done = false;
+
+  if (! get_next_offset (&offset, &count))
+    /* No work to do, return immediately.  Can happen for files which
+     * are smaller than THREAD_WORK_SIZE where multi-conn is enabled.
+     */
+    return NULL;
+
+  /* In the case where src or dst is NBD, use
+   * {src|dst}.u.nbd.ptr[index] so that each thread is connected to
+   * its own NBD connection.  If either src or dst is LOCAL then set
+   * src_nbd/dst_nbd to NULL so hopefully we'll crash hard if the
+   * program accidentally tries to use them.
+   */
+  if (src.t == NBD)
+    src_nbd = src.u.nbd.ptr[index];
+  else
+    src_nbd = NULL;
+  if (dst.t == NBD)
+    dst_nbd = dst.u.nbd.ptr[index];
+  else
+    dst_nbd = NULL;
+
+  while (!done) {
+    struct buffer *buffer;
+    char *data;
+    size_t n;
+
+    if (count == 0) {
+      /* Get another work range. */
+      done = ! get_next_offset (&offset, &count);
+      if (done) break;
+      assert (0 < count && count <= THREAD_WORK_SIZE);
+    }
+
+    /* If the number of requests in flight exceeds the limit, poll
+     * waiting for at least one request to finish.  This enforces the
+     * user --requests option.
+     */
+    while (in_flight (src_nbd, dst_nbd) >= max_requests)
+      poll_both_ends (src_nbd, dst_nbd);
+
+    /* Create a new buffer.  This will be freed in a callback handler. */
+    n = count;
+    if (n > MAX_REQUEST_SIZE)
+      n = MAX_REQUEST_SIZE;
+    data = malloc (n);
+    if (data == NULL) {
+      perror ("malloc");
+      exit (EXIT_FAILURE);
+    }
+    buffer = malloc (sizeof *buffer);
+    if (buffer == NULL) {
+      perror ("malloc");
+      exit (EXIT_FAILURE);
+    }
+    buffer->offset = offset;
+    buffer->n = n;
+    buffer->data = data;
+    buffer->dst_nbd = dst_nbd;
+
+    if (src.t == LOCAL) {
+      /* Reading from a local file. */
+      ssize_t r;
+      size_t c = n;
+      char *p = data;
+      uint64_t offs = offset;
+
+      while (c > 0) {
+        r = pread (src.u.local.fd, p, c, offs);
+        if (r == -1) {
+          perror (src.name);
+          exit (EXIT_FAILURE);
+        }
+        if (r == 0) {
+          /* Should never happen unless the file is truncated
+           * under us.
+           */
+          fprintf (stderr, "nbdcopy: error: %s: unexpected end of file\n",
+                   src.name);
+          exit (EXIT_FAILURE);
+        }
+        p += r;
+        c -= r;
+        offs += r;
+      }
+
+      /* Issue the write to dst which we know is NBD. */
+      assert (dst.t == NBD);
+      assert (dst_nbd != NULL);
+      if (nbd_aio_pwrite (dst_nbd, data, n, offset,
+                          (nbd_completion_callback) {
+                            .callback = finished_write,
+                            .user_data = buffer,
+                          }, 0) == -1) {
+        fprintf (stderr, "%s: %s\n", dst.name, nbd_get_error ());
+        exit (EXIT_FAILURE);
+      }
+    }
+    else /* src.t == NBD */ {
+      /* Reading from NBD connection. */
+      if (nbd_aio_pread (src_nbd, data, n, offset,
+                         (nbd_completion_callback) {
+                           .callback = finished_read,
+                           .user_data = buffer,
+                         }, 0) == -1) {
+        fprintf (stderr, "%s: %s\n", dst.name, nbd_get_error ());
+        exit (EXIT_FAILURE);
+      }
+    }
+
+    offset += n;
+    count -= n;
+  }
+
+  /* Wait for in flight NBD requests to finish. */
+  while (in_flight (src_nbd, dst_nbd) > 0)
+    poll_both_ends (src_nbd, dst_nbd);
+
+  return NULL;
+}
+
+/* Count the number of NBD commands in flight.  Since the commands are
+ * auto-retired in the callbacks we don't need to count "done"
+ * commands.
+ */
+static inline unsigned
+in_flight (struct nbd_handle *src_nbd, struct nbd_handle *dst_nbd)
+{
+  return
+    (src_nbd ? nbd_aio_in_flight (src_nbd) : 0) +
+    (dst_nbd ? nbd_aio_in_flight (dst_nbd) : 0);
+}
+
+/* Poll (optional) NBD src and NBD dst, moving the state machine(s)
+ * along.  This is a lightly modified nbd_poll.
+ */
+static void
+poll_both_ends (struct nbd_handle *src_nbd, struct nbd_handle *dst_nbd)
+{
+  struct pollfd fds[2] = { 0 };
+  int r;
+
+  /* Note: poll will ignore fd == -1 */
+
+  if (!src_nbd)
+    fds[0].fd = -1;
+  else {
+    fds[0].fd = nbd_aio_get_fd (src_nbd);
+    switch (nbd_aio_get_direction (src_nbd)) {
+    case LIBNBD_AIO_DIRECTION_READ:
+      fds[0].events = POLLIN;
+      break;
+    case LIBNBD_AIO_DIRECTION_WRITE:
+      fds[0].events = POLLOUT;
+      break;
+    case LIBNBD_AIO_DIRECTION_BOTH:
+      fds[0].events = POLLIN|POLLOUT;
+      break;
+    default:
+      fprintf (stderr, "%s: %s\n", src.name, nbd_get_error ());
+      exit (EXIT_FAILURE);
+    }
+  }
+
+  if (!dst_nbd)
+    fds[1].fd = -1;
+  else {
+    fds[1].fd = nbd_aio_get_fd (dst_nbd);
+    switch (nbd_aio_get_direction (dst_nbd)) {
+    case LIBNBD_AIO_DIRECTION_READ:
+      fds[1].events = POLLIN;
+      break;
+    case LIBNBD_AIO_DIRECTION_WRITE:
+      fds[1].events = POLLOUT;
+      break;
+    case LIBNBD_AIO_DIRECTION_BOTH:
+      fds[1].events = POLLIN|POLLOUT;
+      break;
+    default:
+      fprintf (stderr, "%s: %s\n", src.name, nbd_get_error ());
+      exit (EXIT_FAILURE);
+    }
+  }
+
+  r = poll (fds, 2, -1);
+  if (r == -1) {
+    perror ("poll");
+    exit (EXIT_FAILURE);
+  }
+  if (r == 0)
+    return;
+
+  if (src_nbd) {
+    r = 0;
+    if ((fds[0].revents & (POLLIN | POLLHUP)) != 0)
+      r = nbd_aio_notify_read (src_nbd);
+    else if ((fds[0].revents & POLLOUT) != 0)
+      r = nbd_aio_notify_write (src_nbd);
+    else if ((fds[0].revents & (POLLERR | POLLNVAL)) != 0) {
+      errno = ENOTCONN;
+      r = -1;
+    }
+    if (r == -1) {
+      perror (src.name);
+      exit (EXIT_FAILURE);
+    }
+  }
+
+  if (dst_nbd) {
+    r = 0;
+    if ((fds[1].revents & (POLLIN | POLLHUP)) != 0)
+      r = nbd_aio_notify_read (dst_nbd);
+    else if ((fds[1].revents & POLLOUT) != 0)
+      r = nbd_aio_notify_write (dst_nbd);
+    else if ((fds[1].revents & (POLLERR | POLLNVAL)) != 0) {
+      errno = ENOTCONN;
+      r = -1;
+    }
+    if (r == -1) {
+      perror (dst.name);
+      exit (EXIT_FAILURE);
+    }
+  }
+}
+
+/* Callback called when src NBD connection has finished one read
+ * command.  We either write the data directly out to a file or begin
+ * another command if the dst is NBD.
+ */
+static int
+finished_read (void *vp, int *error)
+{
+  struct buffer *buffer = vp;
+
+  if (dst.t == LOCAL) {
+    /* Write to the local file. */
+    ssize_t r;
+    size_t c = buffer->n;
+    const char *p = buffer->data;
+    uint64_t offs = buffer->offset;
+
+    while (c > 0) {
+      r = pwrite (dst.u.local.fd, p, c, offs);
+      if (r == -1) {
+        perror (dst.name);
+        exit (EXIT_FAILURE);
+      }
+      p += r;
+      c -= r;
+      offs += r;
+    }
+
+    /* We can now free the buffer. */
+    free (buffer->data);
+    free (buffer);
+  }
+  else /* dst.t == NBD */ {
+    /* Begin a write command to dst NBD. */
+    if (nbd_aio_pwrite (buffer->dst_nbd,
+                        buffer->data, buffer->n, buffer->offset,
+                        (nbd_completion_callback) {
+                          .callback = finished_write,
+                          .user_data = buffer,
+                        }, 0) == -1) {
+      fprintf (stderr, "%s: %s\n", dst.name, nbd_get_error ());
+      exit (EXIT_FAILURE);
+    }
+  }
+
+  return 1; /* auto-retires the command */
+}
+
+/* Callback called when dst NBD connection has finished one write
+ * command.  We can now free the buffer.
+ */
+static int
+finished_write (void *vp, int *error)
+{
+  struct buffer *buffer = vp;
+
+  free (buffer->data);
+  free (buffer);
+  return 1; /* auto-retires the command */
+}

copy/nbdcopy.h
@@ -22,7 +22,14 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include "vector.h"
+
 #define MAX_REQUEST_SIZE (32 * 1024 * 1024)
+
+/* This must be a multiple of MAX_REQUEST_SIZE. */
+#define THREAD_WORK_SIZE (128 * 1024 * 1024)
+
+DEFINE_VECTOR_TYPE (handles, struct nbd_handle *)
 
 struct rw {
   enum { LOCAL, NBD } t;
@@ -33,15 +40,19 @@ struct rw {
       int fd;
       struct stat stat;
     } local;
-    struct nbd_handle *nbd;     /* For NBD, the libnbd handle. */
+    handles nbd;                /* For NBD, one handle per connection. */
   } u;
 };
 
+extern unsigned connections;
+extern unsigned max_requests;
 extern bool progress;
 extern bool synchronous;
+extern unsigned threads;
 extern struct rw src, dst;
 
 extern void progress_bar (off_t pos, int64_t size);
 extern void synch_copying (void);
+extern void multi_thread_copying (void);
 
 #endif /* NBDCOPY_H */

commit 8d444b41d09a700c7ee6f9182a649f3f2d325abb

copy: CVE-2022-0485: Fail nbdcopy if NBD read or write fails
nbdcopy has a nasty bug when performing multi-threaded copies using
asynchronous nbd calls - it was blindly treating the completion of an
asynchronous command as successful, rather than checking the *error
parameter.  This can result in the silent creation of a corrupted
image in two different ways: when a read fails, we blindly wrote
garbage to the destination; when a write fails, we did not flag that
the destination was not written.

Since nbdcopy already calls exit() on a synchronous read or write
failure to a file, doing the same for an asynchronous op to an NBD
server is the simplest solution.  A nicer solution, but more invasive
to code and thus not done here, might be to allow up to N retries of
the transaction (in case the read or write failure was transient), or
even having a mode where as much data is copied as possible (portions
of the copy that failed would be logged on stderr, and nbdcopy would
still fail with a non-zero exit status, but this would copy more than
just stopping at the first error, as can be done with rsync or
ddrescue).

Note that since we rely on auto-retiring and do NOT call
nbd_aio_command_completed, our completion callbacks must always return
1 (if they do not exit() first), even when acting on *error, so as not
leave the command allocated until nbd_close.  As such, there is no
sane way to return an error to a manual caller of the callback, and
therefore we can drop dead code that calls perror() and exit() if the
callback "failed".  It is also worth documenting the contract on when
we must manually call the callback during the asynch_zero callback, so
that we do not leak or double-free the command; thankfully, all the
existing code paths were correct.

The added testsuite script demonstrates several scenarios, some of
which fail without the rest of this patch in place, and others which
showcase ways in which sparse images can bypass errors.

Once backports are complete, a followup patch on the main branch will
edit docs/libnbd-security.pod with the mailing list announcement of
the stable branch commit ids and release versions that incorporate
this fix.

Reported-by: Nir Soffer <nsoffer@redhat.com>
Fixes: bc896eec4d ("copy: Implement multi-conn, multiple threads, multiple requests in flight.", v1.5.6)

TODO
@@ -33,6 +33,7 @@ nbdcopy:
  - Better page cache usage, see nbdkit-file-plugin options
    fadvise=sequential cache=none.
  - Consider io_uring if there are performance bottlenecks.
+ - Configurable retries in response to read or write failures.
 
 nbdfuse:
  - If you write beyond the end of the virtual file, it returns EIO.

copy/Makefile.am
@@ -1,5 +1,5 @@
 # nbd client library in userspace
-# Copyright (C) 2020 Red Hat Inc.
+# Copyright (C) 2020-2022 Red Hat Inc.
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -33,6 +33,7 @@ EXTRA_DIST = \
 	copy-nbd-to-small-nbd-error.sh \
 	copy-nbd-to-sparse-file.sh \
 	copy-nbd-to-stdout.sh \
+	copy-nbd-error.sh \
 	copy-progress-bar.sh \
 	copy-sparse.sh \
 	copy-sparse-allocated.sh \
@@ -124,6 +125,7 @@ TESTS += \
 	copy-stdin-to-nbd.sh \
 	copy-stdin-to-null.sh \
 	copy-nbd-to-stdout.sh \
+	copy-nbd-error.sh \
 	copy-progress-bar.sh \
 	copy-sparse.sh \
 	copy-sparse-allocated.sh \

copy/copy-nbd-error.sh
@@ -1,0 +1,81 @@
+#!/usr/bin/env bash
+# nbd client library in userspace
+# Copyright (C) 2022 Red Hat Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+# Tests several scenarios of handling NBD server errors
+# Serves as a regression test for the CVE-2022-0485 fix.
+
+. ../tests/functions.sh
+
+set -e
+set -x
+
+requires nbdkit --exit-with-parent --version
+requires nbdkit --filter=noextents null --version
+requires nbdkit --filter=error pattern --version
+requires nbdkit --filter=nozero memory --version
+
+fail=0
+
+# Failure to get block status should not be fatal, but merely downgrade to
+# reading the entire image as if data
+echo "Testing extents failures on source"
+$VG nbdcopy -- [ nbdkit --exit-with-parent -v --filter=error pattern 5M \
+    error-extents-rate=1 ] null: || fail=1
+
+# Failure to read should be fatal
+echo "Testing read failures on non-sparse source"
+$VG nbdcopy -- [ nbdkit --exit-with-parent -v --filter=error pattern 5M \
+    error-pread-rate=0.5 ] null: && fail=1
+
+# However, reliable block status on a sparse image can avoid the need to read
+echo "Testing read failures on sparse source"
+$VG nbdcopy -- [ nbdkit --exit-with-parent -v --filter=error null 5M \
+    error-pread-rate=1 ] null: || fail=1
+
+# Failure to write data should be fatal
+echo "Testing write data failures on arbitrary destination"
+$VG nbdcopy -- [ nbdkit --exit-with-parent -v pattern 5M ] \
+    [ nbdkit --exit-with-parent -v --filter=error --filter=noextents \
+        memory 5M error-pwrite-rate=0.5 ] && fail=1
+
+# However, writing zeroes can bypass the need for normal writes
+echo "Testing write data failures from sparse source"
+$VG nbdcopy -- [ nbdkit --exit-with-parent -v null 5M ] \
+    [ nbdkit --exit-with-parent -v --filter=error --filter=noextents \
+        memory 5M error-pwrite-rate=1 ] || fail=1
+
+# Failure to write zeroes should be fatal
+echo "Testing write zero failures on arbitrary destination"
+$VG nbdcopy -- [ nbdkit --exit-with-parent -v null 5M ] \
+    [ nbdkit --exit-with-parent -v --filter=error memory 5M \
+        error-zero-rate=1 ] && fail=1
+
+# However, assuming/learning destination is zero can skip need to write
+echo "Testing write failures on pre-zeroed destination"
+$VG nbdcopy --destination-is-zero -- \
+    [ nbdkit --exit-with-parent -v null 5M ] \
+    [ nbdkit --exit-with-parent -v --filter=error memory 5M \
+        error-pwrite-rate=1 error-zero-rate=1 ] || fail=1
+
+# Likewise, when write zero is not advertised, fallback to normal write works
+echo "Testing write zeroes to destination without zero support"
+$VG nbdcopy -- [ nbdkit --exit-with-parent -v null 5M ] \
+    [ nbdkit --exit-with-parent -v --filter=nozero --filter=error memory 5M \
+        error-zero-rate=1 ] || fail=1
+
+exit $fail

copy/file-ops.c
@@ -591,10 +591,8 @@ file_asynch_read (struct rw *rw,
 
   file_synch_read (rw, slice_ptr (command->slice),
                    command->slice.len, command->offset);
-  if (cb.callback (cb.user_data, &dummy) == -1) {
-    perror (rw->name);
-    exit (EXIT_FAILURE);
-  }
+  /* file_synch_read called exit() on error */
+  cb.callback (cb.user_data, &dummy);
 }
 
 static void
@@ -606,10 +604,8 @@ file_asynch_write (struct rw *rw,
 
   file_synch_write (rw, slice_ptr (command->slice),
                     command->slice.len, command->offset);
-  if (cb.callback (cb.user_data, &dummy) == -1) {
-    perror (rw->name);
-    exit (EXIT_FAILURE);
-  }
+  /* file_synch_write called exit() on error */
+  cb.callback (cb.user_data, &dummy);
 }
 
 static bool
@@ -620,10 +616,7 @@ file_asynch_zero (struct rw *rw, struct command *command,
 
   if (!file_synch_zero (rw, command->offset, command->slice.len, allocate))
     return false;
-  if (cb.callback (cb.user_data, &dummy) == -1) {
-    perror (rw->name);
-    exit (EXIT_FAILURE);
-  }
+  cb.callback (cb.user_data, &dummy);
   return true;
 }
 

copy/multi-thread-copying.c
@@ -28,6 +28,7 @@
 #include <errno.h>
 #include <assert.h>
 #include <sys/stat.h>
+#include <inttypes.h>
 
 #include <pthread.h>
 
@@ -376,6 +377,12 @@ finished_read (void *vp, int *error)
 {
   struct command *command = vp;
 
+  if (*error) {
+    fprintf (stderr, "read at offset %" PRId64 " failed: %s\n",
+             command->offset, strerror (*error));
+    exit (EXIT_FAILURE);
+  }
+
   if (allocated || sparse_size == 0) {
     /* If sparseness detection (see below) is turned off then we write
      * the whole command.
@@ -547,6 +554,12 @@ free_command (void *vp, int *error)
 {
   struct command *command = vp;
   struct buffer *buffer = command->slice.buffer;
+
+  if (*error) {
+    fprintf (stderr, "write at offset %" PRId64 " failed: %s\n",
+             command->offset, strerror (*error));
+    exit (EXIT_FAILURE);
+  }
 
   if (buffer != NULL) {
     if (--buffer->refs == 0) {

copy/nbdcopy.h
@@ -1,5 +1,5 @@
 /* NBD client library in userspace.
- * Copyright (C) 2020-2021 Red Hat Inc.
+ * Copyright (C) 2020-2022 Red Hat Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -157,7 +157,8 @@ struct rw_ops {
                       bool allocate);
 
   /* Asynchronous I/O operations.  These start the operation and call
-   * 'cb' on completion.
+   * 'cb' on completion.  'cb' will return 1, for auto-retiring with
+   * asynchronous libnbd calls.
    *
    * The file_ops versions are actually implemented synchronously, but
    * still call 'cb'.
@@ -173,7 +174,7 @@ struct rw_ops {
                         nbd_completion_callback cb);
 
   /* Asynchronously zero.  command->slice.buffer is not used.  If not possible,
-   * returns false.
+   * returns false.  'cb' must be called only if returning true.
    */
   bool (*asynch_zero) (struct rw *rw, struct command *command,
                        nbd_completion_callback cb, bool allocate);

copy/null-ops.c
@@ -119,10 +119,7 @@ null_asynch_write (struct rw *rw,
 {
   int dummy = 0;
 
-  if (cb.callback (cb.user_data, &dummy) == -1) {
-    perror (rw->name);
-    exit (EXIT_FAILURE);
-  }
+  cb.callback (cb.user_data, &dummy);
 }
 
 static bool
@@ -131,10 +128,7 @@ null_asynch_zero (struct rw *rw, struct command *command,
 {
   int dummy = 0;
 
-  if (cb.callback (cb.user_data, &dummy) == -1) {
-    perror (rw->name);
-    exit (EXIT_FAILURE);
-  }
+  cb.callback (cb.user_data, &dummy);
   return true;
 }
 