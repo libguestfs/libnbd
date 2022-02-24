libguestfs/libnbd

commit baae33e2768fbe5c19fc09637b212b6e0b9edda5

interop: Skip block-size tests on old qemu-nbd
The qemu-nbd on the CI's x86_64-ubuntu-1804 setup is so old it still
uses oldstyle negotiation, which lacks block size advertisements.
Skip in that case.

On x86_64-opensuse-tumbleweed, the CI timed out.  Obviously, the test
wedged (probably a mismatch where both nbdsh as client and old
qemu-nbd as server server are each waiting on the other to send more
data), but on timeout I don't have a nice .log file to see how far it
got before wedging.  Force qemu to quit after a reasonble time so that
I have more to debug by.

Fixes: 0a9adf6b ("api: Add set_request_block_size", v1.12.0)

interop/interop-qemu-block-size.sh
@@ -26,6 +26,7 @@ requires qemu-nbd --version
 requires nbdsh --version
 requires qemu-img --version
 requires truncate --version
+requires timeout --version
 
 f="qemu-block-size.raw"
 sock=$(mktemp -u /tmp/interop-qemu.XXXXXX)
@@ -39,9 +40,9 @@ fail=0
 # run_test FMT REQUEST EXP_INFO EXP_GO
 run_test() {
     # No -t or -e, so qemu-nbd should exit once nbdsh disconnects
-    qemu-nbd -k $sock $1 $f &
+    timeout 60s qemu-nbd -k $sock $1 $f &
     pid=$!
-    $VG nbdsh -c - <<EOF || fail=1
+    $VG nbdsh -c - <<EOF
 import os
 
 sock = os.environ["sock"]
@@ -51,6 +52,10 @@ assert h.get_request_block_size()
 h.set_request_block_size($2)
 assert h.get_request_block_size() is $2
 h.connect_unix(sock)
+
+if not h.aio_is_negotiating():
+    nbd.shutdown()
+    exit(77)    # Oldstyle negotiation lacks block size advertisement
 
 try:
     h.opt_info()
@@ -63,6 +68,13 @@ assert h.get_block_size(nbd.SIZE_MINIMUM) == $4
 
 h.shutdown()
 EOF
+    st=$?
+    if [ $st = 77 ]; then
+        echo "$0: skipping: qemu-nbd too old for this test"
+        exit 77
+    elif [ $st != 0 ]; then
+        fail=1
+    fi
     wait $pid || fail=1
 }
 

commit 5c0b2a576165ee363f5dcf70011fde0d3729cfc8

interop: More test improvements
The x86_64-ubuntu-1804 pipeline still failed, because I used the wrong
python object:
AttributeError: module 'nbd' has no attribute 'shutdown'

And now I have at least one root cause for the
x86_64-opensuse-tumbleweed failure: I captured a log showing that
nbdsh was trying to connect before qemu-nbd had created the socket.
There may still be other things to fix, but such is the joy of
iterative CI debugging...

Fixes: baae33e2

interop/interop-qemu-block-size.sh
@@ -39,9 +39,17 @@ fail=0
 
 # run_test FMT REQUEST EXP_INFO EXP_GO
 run_test() {
+    rm -f $sock
     # No -t or -e, so qemu-nbd should exit once nbdsh disconnects
     timeout 60s qemu-nbd -k $sock $1 $f &
     pid=$!
+    # Wait for the socket to appear
+    for i in {1..30}; do
+        if test -e $sock; then
+            break
+        fi
+        sleep 1
+    done
     $VG nbdsh -c - <<EOF
 import os
 
@@ -54,7 +62,7 @@ assert h.get_request_block_size() is $2
 h.connect_unix(sock)
 
 if not h.aio_is_negotiating():
-    nbd.shutdown()
+    h.shutdown()
     exit(77)    # Oldstyle negotiation lacks block size advertisement
 
 try: