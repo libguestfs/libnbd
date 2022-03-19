d29c4894a7aca016d0bc391e67d199339341690d
Richard W.M. Jones <rjones@redhat.com>
2022-03-19 10:21:00 +0000

    fuzzing: Allow capture of test cases
    Enhance libnbd-fuzz-wrapper to allow test cases to be captured.

--- fuzzing/libnbd-fuzz-wrapper.c
+++ fuzzing/libnbd-fuzz-wrapper.c
@@ -27,6 +27,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <inttypes.h>
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -39,11 +42,29 @@
 #include <libnbd.h>
 
 #ifndef SOCK_CLOEXEC
-#define SOCK_CLOEXEC 0 /* This file doesn't use exec */
+/* We do use exec, but only when using --write, which is a maintainer
+ * operation that always runs on Linux.
+ */
+#define SOCK_CLOEXEC 0
 #endif
+
+/* If defined, instead of creating the fuzzer wrapper, write a working
+ * testcase to the named file.  This runs nbdkit and captures the
+ * input to libnbd (output from nbdkit) in the file.  Use it like
+ * this:
+ *
+ * Generate test case:
+ * $ fuzzing/libnbd-fuzz-wrapper --write testcase
+ * Test it:
+ * $ LIBNBD_DEBUG=1 fuzzing/libnbd-fuzz-wrapper testcase
+ * If it's good, copy it to the test case directory:
+ * $ mv testcase fuzzing/testcase_dir/
+ */
+static const char *write_testcase;
 
 static void client (int s);
 static void server (int fd, int s);
+static void nbdkit (int fd, int s);
 
 int
 main (int argc, char *argv[])
@@ -52,15 +73,25 @@ main (int argc, char *argv[])
   pid_t pid;
   int sv[2], r, status;
 
-  if (argc != 2) {
-    fprintf (stderr, "libnbd-fuzz-wrapper testcase\n");
-    exit (EXIT_FAILURE);
-  }
+  if (argc == 3 && strcmp (argv[1], "--write") == 0) {
+    write_testcase = argv[2];
 
-  /* Open the test case before we fork so we know the file exists. */
-  fd = open (argv[1], O_RDONLY);
-  if (fd == -1) {
-    perror (argv[1]);
+    fd = open (argv[2], O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    if (fd == -1) {
+      perror (argv[1]);
+      exit (EXIT_FAILURE);
+    }
+  }
+  else if (argc == 2) {
+    /* Open the test case before we fork so we know the file exists. */
+    fd = open (argv[1], O_RDONLY);
+    if (fd == -1) {
+      perror (argv[1]);
+      exit (EXIT_FAILURE);
+    }
+  }
+  else {
+    fprintf (stderr, "libnbd-fuzz-wrapper testcase\n");
     exit (EXIT_FAILURE);
   }
 
@@ -71,7 +102,7 @@ main (int argc, char *argv[])
   }
 
   /* Fork: The parent will be the libnbd process (client).  The child
-   * will be the phony NBD server listening on the socket.
+   * will be the (usually phony) NBD server listening on the socket.
    */
   pid = fork ();
   if (pid == -1) {
@@ -99,10 +130,13 @@ main (int argc, char *argv[])
       exit (EXIT_SUCCESS);
   }
 
-  /* Child: phony NBD server. */
+  /* Child: NBD server. */
   close (sv[0]);
 
-  server (fd, sv[1]);
+  if (!write_testcase)
+    server (fd, sv[1]);
+  else
+    nbdkit (fd, sv[1]);
 
   close (sv[1]);
 
@@ -115,6 +149,8 @@ chunk_callback (void *user_data, const void *subbuf,
                 size_t count, uint64_t offset,
                 unsigned status, int *error)
 {
+  //fprintf (stderr, "chunk called: %" PRIu64 " %zu %u\n",
+  //         offset, count, status);
   return 0;
 }
 
@@ -125,6 +161,7 @@ extent_callback (void *user_data,
                  uint64_t offset, uint32_t *entries,
                  size_t nr_entries, int *error)
 {
+  //fprintf (stderr, "extent called, nr_entries = %zu\n", nr_entries);
   return 0;
 }
 
@@ -246,4 +283,144 @@ server (int fd, int sock)
       }
     }
   } /* for (;;) */
+}
+
+static void xwrite (int fd, const char *buf, size_t n);
+
+/* This is used for --write mode where we capture nbdkit output into a
+ * testcase file.
+ */
+static void
+nbdkit (int fd, int sock)
+{
+  pid_t pid;
+  int rfd[2], wfd[2];
+  struct pollfd pfds[2];
+  char buf[512];
+  ssize_t r;
+  bool parent_dead = false;
+
+  if (pipe (rfd) == -1) {       /* Will be our input, nbdkit's stdout */
+    perror ("pipe");
+    _exit (EXIT_FAILURE);
+  }
+
+  if (pipe (wfd) == -1) {       /* Will be our output, nbdkit's stdin */
+    perror ("pipe");
+    _exit (EXIT_FAILURE);
+  }
+
+  /* Run nbdkit as another subprocess. */
+  pid = fork ();
+  if (pid == -1) {
+    perror ("fork");
+    _exit (EXIT_FAILURE);
+  }
+
+  if (pid == 0) {               /* Child - nbdkit */
+    close (fd);
+    close (sock);
+
+    close (rfd[0]);
+    dup2 (rfd[1], STDOUT_FILENO);
+
+    close (wfd[1]);
+    dup2 (wfd[0], STDIN_FILENO);
+
+    execlp ("nbdkit",
+            "nbdkit", "--exit-with-parent", "-s",
+            "sparse-random",
+            "--filter=cow",
+            "size=1M",
+            "runlength=8192", "percent=50", "random-content=true",
+            NULL);
+    perror ("execlp");
+    _exit (EXIT_FAILURE);
+  }
+
+  /* Here we shuffle the data:
+   *
+   * Our parent         This process            Our child
+   * (libnbd)                                   (nbdkit)
+   *      ------------->   ------>  -----wfd[1]---->
+   *          sock
+   *      <-------------  <------  /<----rfd[0]-----
+   *                              |
+   *                              v
+   *             writes from nbdkit tee'd to fd (testcase)
+   *
+   * We do everything blocking because that is easier and performance
+   * doesn't matter since we're only capturing a test case.
+   */
+  close (rfd[1]);
+  close (wfd[0]);
+
+  while (!parent_dead && rfd[0] >= 0) {
+    pfds[0].fd = parent_dead ? -1 : sock;
+    pfds[0].events = POLLIN;
+    pfds[0].revents = 0;
+
+    pfds[1].fd = rfd[0];
+    pfds[1].events = POLLIN;
+    pfds[1].revents = 0;
+
+    if (poll (pfds, 2, -1) == -1) {
+      if (errno == EINTR)
+        continue;
+      perror ("poll");
+      _exit (EXIT_FAILURE);
+    }
+
+    if (!parent_dead && (pfds[0].revents & POLLIN) != 0) {
+      r = read (sock, buf, sizeof buf);
+      if (r == -1 && errno != EINTR) {
+        perror ("read (libnbd)");
+        _exit (EXIT_FAILURE);
+      }
+      else if (r == 0) {
+        parent_dead = true;
+        continue;
+      }
+      else if (r > 0)
+        xwrite (wfd[1], buf, r);
+    }
+
+    if (rfd[0] != -1 && (pfds[1].revents & POLLIN) != 0) {
+      r = read (rfd[0], buf, sizeof buf);
+      if (r == -1 && errno == EINTR) {
+        perror ("read (nbdkit)");
+        _exit (EXIT_FAILURE);
+      }
+      else if (r == 0) {
+        close (rfd[0]);
+        rfd[0] = -1;
+        continue;
+      }
+      else if (r > 0) {
+        xwrite (fd, buf, r);
+        xwrite (sock, buf, r);
+      }
+    }
+  }
+
+  if (close (fd) == -1) {
+    perror ("close");
+    _exit (EXIT_FAILURE);
+  }
+}
+
+static void
+xwrite (int fd, const char *buf, size_t n)
+{
+  ssize_t r;
+
+  while (n > 0) {
+    r = write (fd, buf, n);
+    if (r == -1) {
+      perror ("write");
+      _exit (EXIT_FAILURE);
+    }
+    buf += r;
+    n -= r;
+  }
 }
