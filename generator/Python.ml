libguestfs/libnbd

commit f05b8ec5088cec1d1a1c804585c432d7eec06c24

python: Change aio_buffer into nbd.Buffer class.
Create a class for AIO buffers.  This is a mostly neutral code
refactoring, but we add a convenient function for getting the size of
the buffer (same as previous commit for OCaml).

python/handle.c
@@ -72,7 +72,7 @@ nbd_internal_py_close (PyObject *self, PyObject *args)
   return Py_None;
 }
 
-static char aio_buffer_name[] = "struct py_aio_buffer";
+static const char aio_buffer_name[] = "nbd.Buffer";
 
 struct py_aio_buffer *
 nbd_internal_py_get_aio_buffer (PyObject *capsule)
@@ -89,9 +89,7 @@ free_aio_buffer (PyObject *capsule)
   free (buf);
 }
 
-/* Allocate a persistent buffer used for nbd_aio_pread and
- * nbd_aio_pwrite.
- */
+/* Allocate a persistent buffer used for nbd_aio_pread. */
 PyObject *
 nbd_internal_py_alloc_aio_buffer (PyObject *self, PyObject *args)
 {
@@ -115,7 +113,7 @@ nbd_internal_py_alloc_aio_buffer (PyObject *self, PyObject *args)
     free (buf);
     return NULL;
   }
-  buf->data = calloc (1, buf->len);
+  buf->data = malloc (buf->len);
   if (buf->data == NULL) {
     PyErr_NoMemory ();
     free (buf);
@@ -192,4 +190,22 @@ nbd_internal_py_aio_buffer_to_bytearray (PyObject *self, PyObject *args)
     return NULL;
 
   return PyByteArray_FromStringAndSize (buf->data, buf->len);
+}
+
+PyObject *
+nbd_internal_py_aio_buffer_size (PyObject *self, PyObject *args)
+{
+  PyObject *obj;
+  struct py_aio_buffer *buf;
+
+  if (!PyArg_ParseTuple (args,
+                         (char *) "O:nbd_internal_py_aio_buffer_size",
+                         &obj))
+    return NULL;
+
+  buf = nbd_internal_py_get_aio_buffer (obj);
+  if (buf == NULL)
+    return NULL;
+
+  return PyLong_FromSsize_t (buf->len);
 }

python/t/510-aio-pwrite.py
@@ -33,17 +33,17 @@ h = nbd.NBD ()
 h.connect_command (["nbdkit", "-s", "--exit-with-parent", "-v",
                     "file", datafile])
 
-buf1 = nbd.aio_buffer_from_bytearray (buf)
+buf1 = nbd.Buffer.from_bytearray (buf)
 cookie = h.aio_pwrite (buf1, 0, nbd.CMD_FLAG_FUA)
 while not (h.aio_command_completed (cookie)):
     h.poll (-1)
 
-buf2 = nbd.aio_buffer (512)
+buf2 = nbd.Buffer (512)
 cookie = h.aio_pread (buf2, 0)
 while not (h.aio_command_completed (cookie)):
     h.poll (-1)
 
-assert buf == nbd.aio_buffer_to_bytearray (buf2)
+assert buf == buf2.to_bytearray ()
 
 with open (datafile, "rb") as f:
     content = f.read ()

generator/generator
@@ -3981,8 +3981,10 @@ raise_exception ()
       pr "extern PyObject *nbd_internal_py_%s (PyObject *self, PyObject *args);\n"
          name;
   ) ([ "create"; "close";
-       "alloc_aio_buffer"; "aio_buffer_from_bytearray";
-       "aio_buffer_to_bytearray" ] @ List.map fst handle_calls);
+       "alloc_aio_buffer";
+       "aio_buffer_from_bytearray";
+       "aio_buffer_to_bytearray";
+       "aio_buffer_size" ] @ List.map fst handle_calls);
 
   pr "\n";
   pr "#endif /* LIBNBD_METHODS_H */\n"
@@ -4009,8 +4011,10 @@ let generate_python_libnbdmod_c () =
       pr "  { (char *) \"%s\", nbd_internal_py_%s, METH_VARARGS, NULL },\n"
          name name;
   ) ([ "create"; "close";
-       "alloc_aio_buffer"; "aio_buffer_from_bytearray";
-       "aio_buffer_to_bytearray" ] @ List.map fst handle_calls);
+       "alloc_aio_buffer";
+       "aio_buffer_from_bytearray";
+       "aio_buffer_to_bytearray";
+       "aio_buffer_size" ] @ List.map fst handle_calls);
   pr "  { NULL, NULL, 0, NULL }\n";
   pr "};\n";
   pr "\n";
@@ -4502,17 +4506,28 @@ Error.__str__ = _str
 
   pr "\
 
-# AIO buffer functions.
-def aio_buffer (len):
-    '''allocate an AIO buffer used for nbd.aio_pread and nbd.aio_pwrite'''
-    return libnbdmod.alloc_aio_buffer (len)
+class Buffer (object):
+    '''Asynchronous I/O persistent buffer'''
 
-def aio_buffer_from_bytearray (ba):
-    '''create an AIO buffer from a bytearray'''
-    return libnbdmod.aio_buffer_from_bytearray (ba)
-def aio_buffer_to_bytearray (buf):
-    '''copy an AIO buffer into a bytearray'''
-    return libnbdmod.aio_buffer_to_bytearray (buf)
+    def __init__ (self, len):
+        '''allocate an uninitialized AIO buffer used for nbd.aio_pread'''
+        self._o = libnbdmod.alloc_aio_buffer (len)
+
+    @classmethod
+    def from_bytearray (cls, ba):
+        '''create an AIO buffer from a bytearray'''
+        o = libnbdmod.aio_buffer_from_bytearray (ba)
+        self = cls (0)
+        self._o = o
+        return self
+
+    def to_bytearray (self):
+        '''copy an AIO buffer into a bytearray'''
+        return libnbdmod.aio_buffer_to_bytearray (self._o)
+
+    def size (self):
+        '''return the size of an AIO buffer'''
+        return libnbdmod.aio_buffer_size (self._o)
 
 class NBD (object):
     '''NBD handle'''
@@ -4532,42 +4547,46 @@ class NBD (object):
       let args =
         List.map (
           function
-          | Bool n -> n
-          | BytesIn (n, _) | BytesPersistIn (n, _) -> n
-          | BytesPersistOut (n, _) -> n
-          | BytesOut (_, count) -> count
-          | Closure { cbname } -> cbname
-          | Int n -> n
-          | Int64 n -> n
-          | Path n -> n
-          | SockAddrAndLen (n, _) -> n
-          | String n -> n
-          | StringList n -> n
-          | UInt n -> n
-          | UInt32 n -> n
-          | UInt64 n -> n
+          | Bool n -> n, None, None
+          | BytesIn (n, _) -> n, None, None
+          | BytesOut (_, count) -> count, None, None
+          | BytesPersistIn (n, _) -> n, None, Some (sprintf "%s._o" n)
+          | BytesPersistOut (n, _) -> n, None, Some (sprintf "%s._o" n)
+          | Closure { cbname } -> cbname, None, None
+          | Int n -> n, None, None
+          | Int64 n -> n, None, None
+          | Path n -> n, None, None
+          | SockAddrAndLen (n, _) -> n, None, None
+          | String n -> n, None, None
+          | StringList n -> n, None, None
+          | UInt n -> n, None, None
+          | UInt32 n -> n, None, None
+          | UInt64 n -> n, None, None
         ) args in
       let optargs =
         List.map (
           function
-          | OFlags n -> n, "0"
+          | OFlags n -> n, Some "0", None
         ) optargs in
-      let () =
-        let params = args @ List.map (fun (n, def) -> n ^ "=" ^ def) optargs in
-        let params = List.map ((^) ", ") params in
-        let params = String.concat "" params in
-        pr "    def %s (self%s):\n" name params in
-      let () =
-        let longdesc = Str.global_replace py_fn_rex "C<nbd." longdesc in
-        let longdesc = Str.global_replace py_const_rex "C<" longdesc in
-        let longdesc = pod2text longdesc in
-        pr "        '''▶ %s\n\n%s'''\n"
-           shortdesc (String.concat "\n" longdesc) in
-      let () =
-        let vars = args @ List.map fst optargs in
-        let vars = List.map ((^) ", ") vars in
-        let vars = String.concat "" vars in
-        pr "        return libnbdmod.%s (self._o%s)\n" name vars in
+      let args = args @ optargs in
+      pr "    def %s (self" name;
+      List.iter (
+        function
+        | n, None, _ -> pr ", %s" n
+        | n, Some default, _ -> pr ", %s=%s" n default
+      ) args;
+      pr "):\n";
+      let longdesc = Str.global_replace py_fn_rex "C<nbd." longdesc in
+      let longdesc = Str.global_replace py_const_rex "C<" longdesc in
+      let longdesc = pod2text longdesc in
+      pr "        '''▶ %s\n\n%s'''\n" shortdesc (String.concat "\n" longdesc);
+      pr "        return libnbdmod.%s (self._o" name;
+      List.iter (
+        function
+        | _, _, Some getter -> pr ", %s" getter
+        | n, _, None -> pr ", %s" n
+      ) args;
+      pr ")\n";
       pr "\n"
   ) handle_calls;
 

python/t/500-aio-pread.py
@@ -20,12 +20,12 @@ import nbd
 h = nbd.NBD ()
 h.connect_command (["nbdkit", "-s", "--exit-with-parent", "-v",
                     "pattern", "size=512"])
-buf = nbd.aio_buffer (512)
+buf = nbd.Buffer (512)
 cookie = h.aio_pread (buf, 0)
 while not (h.aio_command_completed (cookie)):
     h.poll (-1)
 
-buf = nbd.aio_buffer_to_bytearray (buf)
+buf = buf.to_bytearray ()
 
 print ("%r" % buf)
 

python/t/505-aio-pread-callback.py
@@ -43,21 +43,21 @@ def callback (user_data, err):
     assert user_data == 42
 
 # First try: succeed in both callbacks
-buf = nbd.aio_buffer (512)
+buf = nbd.Buffer (512)
 cookie = h.aio_pread_structured_callback (buf, 0,
                                           lambda *args: chunk (42, *args),
                                           lambda *args: callback (42, *args))
 while not (h.aio_command_completed (cookie)):
     h.poll (-1)
 
-buf = nbd.aio_buffer_to_bytearray (buf)
+buf = buf.to_bytearray ()
 
 print ("%r" % buf)
 
 assert buf == expected
 
 # Second try: fail only during callback
-buf = nbd.aio_buffer (512)
+buf = nbd.Buffer (512)
 cookie = h.aio_pread_structured_callback (buf, 0,
                                           lambda *args: chunk (42, *args),
                                           lambda *args: callback (43, *args))
@@ -69,7 +69,7 @@ except nbd.Error as ex:
     assert ex.errnum == errno.ENOMEM
 
 # Third try: fail during both
-buf = nbd.aio_buffer (512)
+buf = nbd.Buffer (512)
 cookie = h.aio_pread_structured_callback (buf, 0,
                                           lambda *args: chunk (43, *args),
                                           lambda *args: callback (44, *args))
@@ -81,7 +81,7 @@ except nbd.Error as ex:
     assert ex.errnum == errno.ENOMEM
 
 # Fourth try: fail only during chunk
-buf = nbd.aio_buffer (512)
+buf = nbd.Buffer (512)
 cookie = h.aio_pread_structured_callback (buf, 0,
                                           lambda *args: chunk (43, *args),
                                           lambda *args: callback (42, *args))

commit 9344154cf650cab3d14f1a29f1d86102ab0463ef

python: Plug uninit leak in nbd.Buffer.to_bytearray
When we created nbd.Buffer to handle both aio_pread and aio_pwrite, we
provided two different approaches to construct an instance.  The
constructor nbd.Buffer(int) allocates uninitialized data, to be filled
in by aio_pread (rather than wasting time pre-zeroing the data just to
be changed again - although that speedup is only possible when opting
in to h.set_pread_initialized(False)).  Meanwhile, the classmethod
nbd.Buffer.from_bytearray(...) allocates initialized data, to be
consumed by aio_pwrite.  However, this scheme makes it trivially easy
to use the wrong instance creation approach for a given handle
operation, and thereby leak uninitialized heap contents into the
destination with something as simple-looking as
h.aio_pwrite(nbd.Buffer(512), 0).  It doesn't help that while
bytearray(10) is all zeroes (a conscientious choice of the Python
language), and therefore nbd.Buffer.from_bytearray(10).is_zero() is
deterministically True, nbd.Buffer(10).is_zero() is non-deterministic;
nor that nbd.Buffer(10).to_bytearray() can allow Python code to play
with uninitialized data.

It is always a dangerous practice to leak heap contents; however, as
this leak is triggered only by poor client Python code, and not under
control of anything the server sends, we don't think it rises to the
level of the heap leak that was assigned CVE-2022-0485 as plugged back
in commit 8d444b41.

The solution employed here is to mark when a buffer has been
initialized, in nbd.Buffer.from_bytearray() and
h.aio_pread[_structured], as well as force-initialize an uninitialized
buffer before b.to_bytearray() or h.aio_pwrite.  Furthermore, we can
make b.is_zero() pretend an uninitialized buffer is all zeroes (since
the user can no longer easily get at any other contents).

The approach is not bulletproof - if h.aio_pread triggers a subsequent
read error in the C code (such as client-side bounds check or server
error response) AND the user has disabled h.set_pread_initialized(),
we've already marked the buffer initialized at that point even though
the buffer may not be fully written.  But that's okay - we've already
documented that a user opting in to that setting is responsible for
safe buffer practices, and should not dereference a read buffer when
an error is reported while retiring the command.

Generated code changes as follows:

| --- python/methods.h.bak	2022-06-06 07:43:33.167181594 -0500
| +++ python/methods.h	2022-06-06 07:43:40.494190038 -0500
| @@ -31,6 +31,7 @@
|  struct py_aio_buffer {
|    Py_ssize_t len;
|    void *data;
| +  bool initialized;
|  };
|
|  extern char **nbd_internal_py_get_string_list (PyObject *);
| --- python/methods.c.bak	2022-06-06 07:43:33.165181592 -0500
| +++ python/methods.c	2022-06-06 07:56:25.109094074 -0500
| @@ -3201,6 +3201,7 @@ nbd_internal_py_aio_pread (PyObject *sel
|    completion_user_data->buf = buf;
|    offset_u64 = offset;
|
| +  buf_buf->initialized = true;
|    ret = nbd_aio_pread (h, buf_buf->data, buf_buf->len, offset_u64, completion, flags_u32);
|    completion_user_data = NULL;
|    if (ret == -1) {
| @@ -3274,6 +3275,7 @@ nbd_internal_py_aio_pread_structured (Py
|    Py_INCREF (py_chunk_fn);
|    chunk_user_data->fn = py_chunk_fn;
|
| +  buf_buf->initialized = true;
|    ret = nbd_aio_pread_structured (h, buf_buf->data, buf_buf->len, offset_u64, chunk, completion, flags_u32);
|    chunk_user_data = NULL;
|    completion_user_data = NULL;
| @@ -3335,6 +3337,10 @@ nbd_internal_py_aio_pwrite (PyObject *se
|    completion_user_data->buf = buf;
|    offset_u64 = offset;
|
| +  if (!buf_buf->initialized) {
| +    memset (buf_buf->data, 0, buf_buf->len);
| +    buf_buf->initialized = true;
| +  }
|    ret = nbd_aio_pwrite (h, buf_buf->data, buf_buf->len, offset_u64, completion, flags_u32);
|    completion_user_data = NULL;
|    if (ret == -1) {
| --- python/nbd.py.bak	2022-06-06 07:43:33.169181596 -0500
| +++ python/nbd.py	2022-06-06 07:43:40.516190063 -0500
| @@ -150,7 +150,9 @@ class Buffer(object):
|      def is_zero(self, offset=0, size=-1):
|          '''Returns true if and only if all bytes in the buffer are zeroes.
|
| -        Note that a freshly allocated buffer is uninitialized, not zero.
| +        Note that although a freshly allocated buffer is uninitialized,
| +        this will report it as all zeroes, as it will be force-initialized
| +        to zero before any code that can access the buffer's contents.
|
|          By default this tests the whole buffer, but you can restrict
|          the test to a sub-range of the buffer using the optional

Fixes: f05b8ec5 ("python: Change aio_buffer into nbd.Buffer class.", v0.9.8)

generator/Python.ml
@@ -37,6 +37,7 @@ let generate_python_methods_h () =
 struct py_aio_buffer {
   Py_ssize_t len;
   void *data;
+  bool initialized;
 };
 
 extern char **nbd_internal_py_get_string_list (PyObject *);
@@ -408,6 +409,7 @@ let print_python_binding name { args; optargs; ret; may_set_error } =
   pr "))\n";
   pr "    goto out;\n";
 
+  (* Two passes over parameters. Any 'goto err' must be in first pass. *)
   pr "  h = get_handle (py_h);\n";
   pr "  if (!h) goto out;\n";
   List.iter (
@@ -475,7 +477,18 @@ let print_python_binding name { args; optargs; ret; may_set_error } =
   ) args;
   pr "\n";
 
-  (* Call the underlying C function. *)
+  (* Second pass, and call the underlying C function. *)
+  List.iter (
+    function
+    | BytesPersistIn (n, _) ->
+       pr "  if (!%s_buf->initialized) {\n" n;
+       pr "    memset (%s_buf->data, 0, %s_buf->len);\n" n n;
+       pr "    %s_buf->initialized = true;\n" n;
+       pr "  }\n"
+    | BytesPersistOut (n, _) ->
+       pr "  %s_buf->initialized = true;\n" n
+    | _ -> ()
+  ) args;
   pr "  ret = nbd_%s (h" name;
   List.iter (
     function
@@ -790,7 +803,9 @@ class Buffer(object):
     def is_zero(self, offset=0, size=-1):
         '''Returns true if and only if all bytes in the buffer are zeroes.
 
-        Note that a freshly allocated buffer is uninitialized, not zero.
+        Note that although a freshly allocated buffer is uninitialized,
+        this will report it as all zeroes, as it will be force-initialized
+        to zero before any code that can access the buffer's contents.
 
         By default this tests the whole buffer, but you can restrict
         the test to a sub-range of the buffer using the optional

python/handle.c
@@ -127,6 +127,7 @@ nbd_internal_py_alloc_aio_buffer (PyObject *self, PyObject *args)
     return NULL;
   }
 
+  buf->initialized = false;
   if (!PyArg_ParseTuple (args, "n:nbd_internal_py_alloc_aio_buffer",
                          &buf->len)) {
     free (buf);
@@ -202,6 +203,7 @@ nbd_internal_py_aio_buffer_from_bytearray (PyObject *self, PyObject *args)
   }
   memcpy (buf->data, data, len);
   Py_XDECREF (arr);
+  buf->initialized = true;
 
   ret = PyCapsule_New (buf, aio_buffer_name, free_aio_buffer);
   if (ret == NULL) {
@@ -227,6 +229,11 @@ nbd_internal_py_aio_buffer_to_bytearray (PyObject *self, PyObject *args)
   buf = nbd_internal_py_get_aio_buffer (obj);
   if (buf == NULL)
     return NULL;
+
+  if (!buf->initialized) {
+    memset (buf->data, 0, buf->len);
+    buf->initialized = true;
+  }
 
   return PyByteArray_FromStringAndSize (buf->data, buf->len);
 }
@@ -288,7 +295,7 @@ nbd_internal_py_aio_buffer_is_zero (PyObject *self, PyObject *args)
     return NULL;
   }
 
-  if (is_zero (buf->data + offset, size))
+  if (!buf->initialized || is_zero (buf->data + offset, size))
     Py_RETURN_TRUE;
   else
     Py_RETURN_FALSE;

python/t/510-aio-pwrite.py
@@ -1,5 +1,5 @@
 # libnbd Python bindings
-# Copyright (C) 2010-2019 Red Hat Inc.
+# Copyright (C) 2010-2022 Red Hat Inc.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -49,5 +49,16 @@ with open(datafile, "rb") as f:
     content = f.read()
 
 assert buf == content
+
+# Also check that an uninitialized buffer doesn't leak heap contents
+buf3 = nbd.Buffer(512)
+cookie = h.aio_pwrite(buf3, 0, flags=nbd.CMD_FLAG_FUA)
+while not h.aio_command_completed(cookie):
+    h.poll(-1)
+
+with open(datafile, "rb") as f:
+    content = f.read()
+
+assert nbd.Buffer.from_bytearray(content).is_zero()
 
 os.unlink(datafile)

python/t/580-aio-is-zero.py
@@ -69,3 +69,10 @@ assert not buf.is_zero(2**20-1, 1)
 assert buf.is_zero(2**20, 1)
 assert not buf.is_zero(0, 1)
 assert buf.is_zero(2**21-1, 1)
+
+# A buffer created with only a size is generally uninitialized until
+# used with aio_pread; but it will be zeroed if accessed prematurely
+buf = nbd.Buffer(1024)
+assert buf.size() == 1024
+assert buf.is_zero()
+assert nbd.Buffer.from_bytearray(buf.to_bytearray()).is_zero()