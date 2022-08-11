700e9b4070e3f64dab3c6107fff635cdaccf0293
Richard W.M. Jones <rjones@redhat.com>
2022-08-11 12:52:44 +0100

    copy/copy-file-to-qcow2-compressed.sh: Alternate test for compressed clusters
    This test is not very stable in CI.  This alternate way to test for
    compressed clusters was suggested by Kevin Wolf and ought to be more
    reliable.
    
    Thanks: Kevin Wolf, Vera Wu
    Update: commit 4058fe1ff03fb41156b67302ba1006b9d06b0218

--- copy/copy-file-to-qcow2-compressed.sh
+++ copy/copy-file-to-qcow2-compressed.sh
@@ -25,7 +25,7 @@ requires $QEMU_NBD --version
 requires nbdkit --exit-with-parent --version
 requires nbdkit sparse-random --dump-plugin
 requires qemu-img --version
-requires stat --version
+#requires stat --version
 
 # Check the compress driver is supported by this qemu-nbd.
 # Note that qemu-nbd opens the socket before checking --image-opts!
@@ -37,8 +37,10 @@ requires_not bash -c 'qemu-nbd -k $sock --image-opts driver=compress |&
 
 file1=copy-file-to-qcow2-compressed.file1
 file2=copy-file-to-qcow2-compressed.file2
-rm -f $file1 $file2
-cleanup_fn rm -f $file1 $file2
+out1=copy-file-to-qcow2-compressed.out1
+out2=copy-file-to-qcow2-compressed.out2
+rm -f $file1 $file2 $out1 $out2
+cleanup_fn rm -f $file1 $file2 $out1 $out2
 
 size=1G
 seed=$RANDOM
@@ -67,13 +69,31 @@ nbdcopy -- [ nbdkit --exit-with-parent sparse-random $size seed=$seed ] \
 
 ls -l $file2
 
+# We used to do this but the test is not very reliable on some
+# platforms.  Maybe due to type of filesystem or unpredictable content
+# of sparse-random?
+#
 # file1 < file2 (shows the compression is having some effect).
-size1="$( stat -c %s $file1 )"
-size2="$( stat -c %s $file2 )"
-if [ $size1 -ge $size2 ]; then
-    echo "$0: qcow2 compression did not make the file smaller"
-    exit 1
-fi
+#size1="$( stat -c %s $file1 )"
+#size2="$( stat -c %s $file2 )"
+#if [ $size1 -ge $size2 ]; then
+#    echo "$0: qcow2 compression did not make the file smaller"
+#    exit 1
+#fi
+
+# Check that there are some compressed clusters in the first file and
+# no compressed clusters in the second file.  See:
+# https://bugzilla.redhat.com/show_bug.cgi?id=2047660#c16
+
+# Since we are comparing qemu-img strings.
+export LC_ALL=C
+
+qemu-img check $file1 > $out1
+cat $out1
+qemu-img check $file2 > $out2
+cat $out2
+! grep " 0.00% compressed clusters" $out1
+grep " 0.00% compressed clusters" $out2
 
 # Logical content of the files should be identical.
 qemu-img compare -f qcow2 $file1 -F qcow2 $file2
