libguestfs/libnbd

commit 2c93dc5a0ff2f8f0c08913f3a35334f1ebb4dc62

states: Wire in a read callback
When a server supports structured reads, knowing where the holes are
in the responses can aid the client's behavior.  Furthermore, since
the server can send data out of order or interleaved with other
replies, a client may be able to start operating on the partial
results as soon as they are available rather than waiting for the
entire buffer to be reconstructed.  As such, it makes sense to have an
optional callback invoked each time we finish reading a chunk (for
simple replies, we behave as if everything was a single data chunk).

This also requires special handling of NBD_REPLY_TYPE_ERROR_OFFSET, as
that is the one situation where we want to call the callback to inform
them that the server pointed out a specific offset for the error - a
client can utilize that knowledge to form a wrapper that permits short
reads (rather than all-or-nothing failures). But as that is the only
error with an offset, all other errors continue to affect only the
final pread return value without any use of the callback.

If any callback fails, and if no prior error was set, then the
callback's failure becomes the failure reason for the overall
read. (Note that this is different from the block status callback,
which for now quits using the callback on subsequent chunks if an
earlier chunk failed - we may decide to change one or the other for
consistency before the API is stable.)

Nothing actually passes a callback function yet, so for now this is no
functional change; but this will make it possible for the next patch
to add an 'nbd_aio_pread_structred' API.

Language bindings may not be able to set a sane value for errno when
the callback fails; we may need to introduce another means for the
callback to set its preferred failure status for the overall
command. But that can be a followup patch.

generator/states-reply-structured.c
@@ -228,6 +228,7 @@
     type = be16toh (h->sbuf.sr.structured_reply.type);
 
     assert (cmd); /* guaranteed by CHECK */
+    error = nbd_internal_errno_of_nbd_error (error);
 
     /* The spec requires the server to send a non-zero error */
     if (error == NBD_SUCCESS) {
@@ -236,7 +237,9 @@
     }
     error = nbd_internal_errno_of_nbd_error (error);
 
-    /* Sanity check that any error offset is in range */
+    /* Sanity check that any error offset is in range, then invoke
+     * user callback if present.
+     */
     if (type == NBD_REPLY_TYPE_ERROR_OFFSET) {
       offset = be64toh (h->sbuf.sr.payload.error.offset);
       if (offset < cmd->offset || offset >= cmd->offset + cmd->count) {
@@ -247,6 +250,17 @@
                    "this is likely to be a bug in the server",
                    offset, cmd->offset, cmd->count);
         return -1;
+      }
+      if (cmd->cb.fn.read) {
+        /* Different from successful reads: inform the callback about the
+         * current error rather than any earlier one. If the callback fails
+         * without setting errno, then use the server's error below.
+         */
+        errno = 0;
+        if (cmd->cb.fn.read (cmd->cb.opaque, cmd->data + (offset - cmd->offset),
+                             0, offset, error, LIBNBD_READ_ERROR) == -1)
+          if (cmd->error == 0)
+            cmd->error = errno;
       }
     }
 
@@ -315,9 +329,27 @@
   return 0;
 
  REPLY.STRUCTURED_REPLY.RECV_OFFSET_DATA_DATA:
+  struct command_in_flight *cmd = h->reply_cmd;
+  uint64_t offset;
+  uint32_t length;
+
   switch (recv_into_rbuf (h)) {
   case -1: SET_NEXT_STATE (%.DEAD); return -1;
-  case 0:  SET_NEXT_STATE (%FINISH);
+  case 0:
+    length = be32toh (h->sbuf.sr.structured_reply.length);
+    offset = be64toh (h->sbuf.sr.payload.offset_data.offset);
+
+    assert (cmd); /* guaranteed by CHECK */
+    if (cmd->cb.fn.read) {
+      errno = 0;
+      if (cmd->cb.fn.read (cmd->cb.opaque, cmd->data + (offset - cmd->offset),
+                           length - sizeof offset, offset, cmd->error,
+                           LIBNBD_READ_DATA) == -1)
+        if (cmd->error == 0)
+          cmd->error = errno ? errno : EPROTO;
+    }
+
+    SET_NEXT_STATE (%FINISH);
   }
   return 0;
 
@@ -372,6 +404,14 @@
      * them as an extension, and this works even when length == 0.
      */
     memset (cmd->data + offset, 0, length);
+    if (cmd->cb.fn.read) {
+      errno = 0;
+      if (cmd->cb.fn.read (cmd->cb.opaque, cmd->data + offset, length,
+                           cmd->offset + offset, cmd->error,
+                           LIBNBD_READ_HOLE) == -1)
+        if (cmd->error == 0)
+          cmd->error = errno ? errno : EPROTO;
+    }
 
     SET_NEXT_STATE(%FINISH);
   }

generator/generator
@@ -1934,6 +1934,10 @@ let constants = [
   "CMD_FLAG_FUA",        1 lsl 0;
   "CMD_FLAG_NO_HOLE",    1 lsl 1;
   "CMD_FLAG_REQ_ONE",    1 lsl 3;
+
+  "READ_DATA",           1;
+  "READ_HOLE",           2;
+  "READ_ERROR",          3;
 ]
 
 (*----------------------------------------------------------------------*)

generator/states-reply-simple.c
@@ -49,9 +49,22 @@
   return 0;
 
  REPLY.SIMPLE_REPLY.RECV_READ_PAYLOAD:
+  struct command_in_flight *cmd = h->reply_cmd;
+
   switch (recv_into_rbuf (h)) {
   case -1: SET_NEXT_STATE (%.DEAD); return -1;
-  case 0:  SET_NEXT_STATE (%^FINISH_COMMAND);
+  case 0:
+    /* guaranteed by START */
+    assert (cmd);
+    if (cmd->cb.fn.read) {
+      assert (cmd->error == 0);
+      errno = 0;
+      if (cmd->cb.fn.read (cmd->cb.opaque, cmd->data, cmd->count,
+                           cmd->offset, 0, LIBNBD_READ_DATA) == -1)
+        cmd->error = errno ? errno : EPROTO;
+    }
+
+    SET_NEXT_STATE (%^FINISH_COMMAND);
   }
   return 0;
 

lib/internal.h
@@ -233,12 +233,14 @@ struct socket {
 
 typedef int (*extent_fn) (void *data, const char *metacontext, uint64_t offset,
                           uint32_t *entries, size_t nr_entries);
+typedef int (*read_fn) (void *data, const void *buf, size_t count,
+                        uint64_t offset, int error, int status);
 
 struct command_cb {
   void *opaque;
   union {
     extent_fn extent;
-    /* More to come */
+    read_fn read;
   } fn;
 };
 

commit 185195da1108d00c37eb620c9e592093cd42ca0c

states: Fix double-translation of structured reply errors
Due to poor merge resolution of several patch series that were in
flight at the same time, commit 2c93dc5a reintroduces the very same
category of bug that commit 71d1cd17 tried to fix in relation to
commit 97fab5e9.  On Linux, where protocol NBD_E* and errno values E*
are identical, the bug cannot be observed (the second call is
idempotent).  But on other platforms, a second call to to translate
what is a system errno value instead of a wire value may end up
slamming the errno value to EINVAL instead of what was transmitted
over the wire.

Fixes: 2c93dc5a ("states: Wire in a read callback", v0.1.5)

generator/states-reply-structured.c
@@ -293,7 +293,6 @@ STATE_MACHINE {
     type = be16toh (h->sbuf.sr.structured_reply.type);
 
     assert (cmd); /* guaranteed by CHECK */
-    error = nbd_internal_errno_of_nbd_error (error);
 
     /* The spec requires the server to send a non-zero error */
     if (error == NBD_SUCCESS) {