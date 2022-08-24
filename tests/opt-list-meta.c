libguestfs/libnbd

commit 9c6fecaa7a323ade6167335d8a2310f0258adf57

api: Add nbd_opt_abort and nbd_aio_opt_abort
It is finally time to introduce our first negotiating option command.
With this change, we can now enter NEWSTYLE.START more than once; as
such, it needs to know whether it is the first entry (proceed with
gflags/cflags, TLS, and structured reply) or a later entry (all
nbd_opt_* will cause an IssueCommand event to kick the state machine
out of NEGOTIATING, at which point we want to jump to the sub-state
appropriate for that option).  This is done by setting h->opt_current
prior to entering NEWSTYLE.

We also need a couple of new states to implement NBD_OPT_ABORT; this
was not deemed worth a separate sub-group and new
states-newstyle-opt-abort.c file, so I just inlined it in NEWSTYLE
instead.  One nice benefit of these new states: when NBD_OPT_GO fails
and we are not in opt mode, or when we require TLS but the server does
not support it, we can now hang up gracefully instead of abruptly
disconnecting from a newstyle-fixed server.  This is observable when
using qemu-nbd, in that we now silence the former noise from qemu:

$ truncate --size=512 /tmp/f
$ ./run nbdsh -c '
import errno
try:
  h.connect_systemd_socket_activation(["qemu-nbd", "-f", "raw", "-x", "b",
    "/tmp/f"])
except nbd.Error as e:
  assert e.errnum == errno.ENOENT
'
qemu-nbd: option negotiation failed: Failed to read opts magic: Unexpected end-of-file before all bytes were read

The aio_opt_abort counterpart does not need a completion closure, for
the same reason that aio_disconnect does not have one - the whole
point of this command is a clean shutdown, so you aren't going to rely
on a server reply, and there are no parameters that need cleanup.

This patch adds some testsuite coverage of the new APIs across
multiple languages, although I will admit that my ocaml and golang is
weak enough that the tests there may not be idiomatic, even if they
pass.

Yes, it's a bit awkward that with just this patch, enabling opt_mode

generator/states-newstyle-opt-structured-reply.c
@@ -1,5 +1,5 @@
 /* nbd client library in userspace: state machine
- * Copyright (C) 2013-2019 Red Hat Inc.
+ * Copyright (C) 2013-2020 Red Hat Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -84,7 +84,10 @@ STATE_MACHINE {
   }
 
   /* Next option. */
-  SET_NEXT_STATE (%^OPT_SET_META_CONTEXT.START);
+  if (h->opt_mode)
+    SET_NEXT_STATE (%.NEGOTIATING);
+  else
+    SET_NEXT_STATE (%^OPT_SET_META_CONTEXT.START);
   return 0;
 
 } /* END STATE MACHINE */

generator/states-newstyle.c
@@ -112,6 +112,26 @@ handle_reply_error (struct nbd_handle *h)
 
 STATE_MACHINE {
  NEWSTYLE.START:
+  if (h->opt_mode) {
+    /* NEWSTYLE can be entered multiple times, from MAGIC.CHECK_MAGIC and
+     * during various nbd_opt_* calls during NEGOTIATION.  Each previous
+     * state has informed us what we still need to do.
+     */
+    switch (h->opt_current) {
+    case NBD_OPT_ABORT:
+      if ((h->gflags & LIBNBD_HANDSHAKE_FLAG_FIXED_NEWSTYLE) == 0) {
+        SET_NEXT_STATE (%.DEAD);
+        set_error (ENOTSUP, "handshake: server is not using fixed newstyle");
+        return 0;
+      }
+      SET_NEXT_STATE (%PREPARE_OPT_ABORT);
+      return 0;
+    case 0:
+      break;
+    default:
+      abort ();
+    }
+  }
   h->rbuf = &h->sbuf;
   h->rlen = sizeof h->sbuf.gflags;
   SET_NEXT_STATE (%RECV_GFLAGS);
@@ -153,10 +173,45 @@ STATE_MACHINE {
   case -1: SET_NEXT_STATE (%.DEAD); return 0;
   case 0:
     /* Start sending options. */
-    if ((h->gflags & LIBNBD_HANDSHAKE_FLAG_FIXED_NEWSTYLE) == 0)
-      SET_NEXT_STATE (%OPT_EXPORT_NAME.START);
+    if ((h->gflags & LIBNBD_HANDSHAKE_FLAG_FIXED_NEWSTYLE) == 0) {
+      if (h->opt_mode)
+        SET_NEXT_STATE (%.NEGOTIATING);
+      else
+        SET_NEXT_STATE (%OPT_EXPORT_NAME.START);
+    }
     else
       SET_NEXT_STATE (%OPT_STARTTLS.START);
+  }
+  return 0;
+
+ NEWSTYLE.PREPARE_OPT_ABORT:
+  assert ((h->gflags & LIBNBD_HANDSHAKE_FLAG_FIXED_NEWSTYLE) != 0);
+  h->sbuf.option.version = htobe64 (NBD_NEW_VERSION);
+  h->sbuf.option.option = htobe32 (NBD_OPT_ABORT);
+  h->sbuf.option.optlen = htobe32 (0);
+  h->wbuf = &h->sbuf;
+  h->wlen = sizeof h->sbuf.option;
+  SET_NEXT_STATE (%SEND_OPT_ABORT);
+  return 0;
+
+ NEWSTYLE.SEND_OPT_ABORT:
+  switch (send_from_wbuf (h)) {
+  case -1: SET_NEXT_STATE (%.DEAD); return 0;
+  case 0:
+    SET_NEXT_STATE (%SEND_OPTION_SHUTDOWN);
+  }
+  return 0;
+
+ NEWSTYLE.SEND_OPTION_SHUTDOWN:
+  /* We don't care if the server replies to NBD_OPT_ABORT.  However,
+   * unless we are in opt mode, we want to preserve the error message
+   * from a failed OPT_GO by moving to DEAD instead.
+   */
+  if (h->sock->ops->shut_writes (h, h->sock)) {
+    if (h->opt_mode)
+      SET_NEXT_STATE (%.CLOSED);
+    else
+      SET_NEXT_STATE (%.DEAD);
   }
   return 0;
 

.gitignore
@@ -172,6 +172,7 @@ Makefile.in
 /tests/meta-base-allocation
 /tests/newstyle-limited
 /tests/oldstyle
+/tests/opt-abort
 /tests/pki/
 /tests/read-only-flag
 /tests/read-write-flag

generator/API.ml
@@ -699,10 +699,11 @@ negotiating everything on your behalf using settings made before
 starting the connection.  To leave the mode and proceed on to the
 ready state, you must use nbd_opt_go successfully; a failed
 C<nbd_opt_go> returns to the negotiating state to allow a change of
-export name before trying again.  You may also use nbd_opt_abort
+export name before trying again.  You may also use L<nbd_opt_abort(3)>
 to end the connection without finishing negotiation.";
     example = Some "examples/list-exports.c";
-    see_also = [Link "get_opt_mode"; Link "aio_is_negotiating"];
+    see_also = [Link "get_opt_mode"; Link "aio_is_negotiating";
+                Link "opt_abort"];
   };
 
   "get_opt_mode", {
@@ -713,6 +714,19 @@ to end the connection without finishing negotiation.";
     longdesc = "\
 Return true if option negotiation mode was enabled on this handle.";
     see_also = [Link "set_opt_mode"];
+  };
+
+  "opt_abort", {
+    default_call with
+    args = []; ret = RErr;
+    permitted_states = [ Negotiating ];
+    shortdesc = "end negotiation and close the connection";
+    longdesc = "\
+Request that the server finish negotiation, gracefully if possible, then
+close the connection.  This can only be used if L<nbd_set_opt_mode(3)>
+enabled option mode.";
+    example = Some "examples/list-exports.c";
+    see_also = [Link "set_opt_mode"; Link "aio_opt_abort"];
   };
 
   "set_list_exports", {
@@ -1883,6 +1897,21 @@ and completed the NBD handshake by calling L<nbd_aio_is_ready(3)>,
 on the connection.";
   };
 
+  "aio_opt_abort", {
+    default_call with
+    args = []; ret = RErr;
+    permitted_states = [ Negotiating ];
+    shortdesc = "end negotiation and close the connection";
+    longdesc = "\
+Request that the server finish negotiation, gracefully if possible, then
+close the connection.  This can only be used if L<nbd_set_opt_mode(3)>
+enabled option mode.
+
+To determine when the request completes, wait for
+L<nbd_aio_is_connecting(3)> to return false.";
+    see_also = [Link "set_opt_mode"; Link "opt_abort"];
+  };
+
   "aio_pread", {
     default_call with
     args = [ BytesPersistOut ("buf", "count"); UInt64 "offset" ];
@@ -2504,6 +2533,8 @@ let first_version = [
   "set_opt_mode", (1, 4);
   "get_opt_mode", (1, 4);
   "aio_is_negotiating", (1, 4);
+  "opt_abort", (1, 4);
+  "aio_opt_abort", (1, 4);
 
   (* These calls are proposed for a future version of libnbd, but
    * have not been added to any released version so far.

generator/state_machine.ml
@@ -278,6 +278,8 @@ and newstyle_state_machine = [
   (* Options.  These state groups are always entered unconditionally,
    * in this order.  The START state in each group will check if the
    * state needs to run and skip to the next state in the list if not.
+   * When opt_mode is set, control is returned to the user in state
+   * NEGOTIATING after OPT_STRUCTURED_REPLY or any failed OPT_GO.
    *)
   Group ("OPT_STARTTLS", newstyle_opt_starttls_state_machine);
   Group ("OPT_LIST", newstyle_opt_list_state_machine);
@@ -285,6 +287,30 @@ and newstyle_state_machine = [
   Group ("OPT_SET_META_CONTEXT", newstyle_opt_set_meta_context_state_machine);
   Group ("OPT_GO", newstyle_opt_go_state_machine);
   Group ("OPT_EXPORT_NAME", newstyle_opt_export_name_state_machine);
+
+  (* When NBD_OPT_GO fails, or when opt_mode is enabled, option parsing
+   * can be cleanly ended without moving through the %READY state.
+   *)
+  State {
+    default_state with
+    name = "PREPARE_OPT_ABORT";
+    comment = "Prepare to send NBD_OPT_ABORT";
+    external_events = [];
+  };
+
+  State {
+    default_state with
+    name = "SEND_OPT_ABORT";
+    comment = "Send NBD_OPT_ABORT to end negotiation";
+    external_events = [ NotifyWrite, "" ];
+  };
+
+  State {
+    default_state with
+    name = "SEND_OPTION_SHUTDOWN";
+    comment = "Sending write shutdown notification to the remote server";
+    external_events = [ NotifyWrite, "" ];
+  };
 
   (* When option parsing has successfully finished negotiation
    * it will jump to this state for final steps before moving to

generator/states-magic.c
@@ -1,5 +1,5 @@
 /* nbd client library in userspace: state machine
- * Copyright (C) 2013-2019 Red Hat Inc.
+ * Copyright (C) 2013-2020 Red Hat Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -42,8 +42,10 @@ STATE_MACHINE {
   }
 
   version = be64toh (h->sbuf.new_handshake.version);
-  if (version == NBD_NEW_VERSION)
+  if (version == NBD_NEW_VERSION) {
+    assert (h->opt_current == 0);
     SET_NEXT_STATE (%.NEWSTYLE.START);
+  }
   else if (version == NBD_OLD_VERSION)
     SET_NEXT_STATE (%.OLDSTYLE.START);
   else {

generator/states-newstyle-opt-go.c
@@ -240,7 +240,7 @@ STATE_MACHINE {
       set_error (0, "handshake: unknown reply from NBD_OPT_GO: 0x%" PRIx32,
                  reply);
     }
-    SET_NEXT_STATE (%.DEAD);
+    SET_NEXT_STATE (%^PREPARE_OPT_ABORT);
     break;
   case NBD_REP_ACK:
     SET_NEXT_STATE (%^FINISHED);

generator/states-newstyle-opt-starttls.c
@@ -1,5 +1,5 @@
 /* nbd client library in userspace: state machine
- * Copyright (C) 2013-2019 Red Hat Inc.
+ * Copyright (C) 2013-2020 Red Hat Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -93,7 +93,7 @@ STATE_MACHINE {
      * then we can continue unencrypted.
      */
     if (h->tls == LIBNBD_TLS_REQUIRE) {
-      SET_NEXT_STATE (%.DEAD);
+      SET_NEXT_STATE (%^PREPARE_OPT_ABORT);
       set_error (ENOTSUP, "handshake: server refused TLS, "
                  "but handle TLS setting is 'require' (2)");
       return 0;

commit 1ea26554ae9ef31eb9775bf662b7635237ad2032

api: Fix opt_mode assertion failure with SR disabled
When nbd_set_opt_mode() is enabled, we want nbd_connect_* to drop into
the NEGOTIATING state as soon as we are done with the
OPT_STRUCTURED_REPLY state machine.  But we missed a path: if
nbd_set_request_structured_replies() skips the SR probe, we ended up
moving into OPT_META_CONTEXT.START and then triggering a subsequent
assertion failure:

$ nbdsh --opt-mode -c 'h.set_request_structured_replies(False)' \
  -c 'h.connect_command(["nbdkit", "-s", "memory", "1"])'
nbdsh: generator/states-newstyle-opt-go.c:34: enter_STATE_NEWSTYLE_OPT_GO_START: Assertion `!h->opt_mode' failed.

Improve comments that I found confusing while figuring out where the
fix should go.

Fixes: 9c6fecaa ("api: Add nbd_opt_abort and nbd_aio_opt_abort", v1.3.12)

generator/states-newstyle-opt-structured-reply.c
@@ -1,5 +1,5 @@
 /* nbd client library in userspace: state machine
- * Copyright (C) 2013-2020 Red Hat Inc.
+ * Copyright (C) 2013-2022 Red Hat Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -22,7 +22,10 @@ STATE_MACHINE {
  NEWSTYLE.OPT_STRUCTURED_REPLY.START:
   assert (h->gflags & LIBNBD_HANDSHAKE_FLAG_FIXED_NEWSTYLE);
   if (!h->request_sr) {
-    SET_NEXT_STATE (%^OPT_META_CONTEXT.START);
+    if (h->opt_mode)
+      SET_NEXT_STATE (%.NEGOTIATING);
+    else
+      SET_NEXT_STATE (%^OPT_META_CONTEXT.START);
     return 0;
   }
 

generator/states-newstyle.c
@@ -1,5 +1,5 @@
 /* nbd client library in userspace: state machine
- * Copyright (C) 2013-2020 Red Hat Inc.
+ * Copyright (C) 2013-2022 Red Hat Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -113,9 +113,12 @@ handle_reply_error (struct nbd_handle *h)
 STATE_MACHINE {
  NEWSTYLE.START:
   if (h->opt_mode) {
-    /* NEWSTYLE can be entered multiple times, from MAGIC.CHECK_MAGIC and
-     * during various nbd_opt_* calls during NEGOTIATION.  Each previous
-     * state has informed us what we still need to do.
+    /* NEWSTYLE can be entered multiple times, from MAGIC.CHECK_MAGIC
+     * (h->opt_current is 0, run through OPT_STRUCTURED_REPLY for
+     * opt_mode, or OPT_GO otherwise) and during various nbd_opt_*
+     * calls during NEGOTIATING (h->opt_current is set, run just the
+     * states needed).  Each previous state has informed us what we
+     * still need to do.
      */
     switch (h->opt_current) {
     case NBD_OPT_GO:
@@ -145,6 +148,8 @@ STATE_MACHINE {
       abort ();
     }
   }
+
+  assert (h->opt_current == 0);
   h->rbuf = &h->sbuf;
   h->rlen = sizeof h->sbuf.gflags;
   SET_NEXT_STATE (%RECV_GFLAGS);

tests/opt-list-meta.c
@@ -1,5 +1,5 @@
 /* NBD client library in userspace
- * Copyright (C) 2013-2020 Red Hat Inc.
+ * Copyright (C) 2013-2022 Red Hat Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -163,5 +163,26 @@ main (int argc, char *argv[])
 
   nbd_opt_abort (nbd);
   nbd_close (nbd);
+
+  /* Repeat but this time without structured replies. */
+  nbd = nbd_create ();
+  if (nbd == NULL ||
+      nbd_set_opt_mode (nbd, true) == -1 ||
+      nbd_set_request_structured_replies (nbd, false) == -1 ||
+      nbd_connect_command (nbd, args) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  /* FIXME: For now, we reject this client-side, but it is overly strict. */
+  p = (struct progress) { .count = 0 };
+  r = nbd_opt_list_meta_context (nbd,
+                                 (nbd_context_callback) { .callback = check,
+                                                          .user_data = &p});
+  if (r != -1) {
+    fprintf (stderr, "not expecting command to succeed\n");
+    exit (EXIT_FAILURE);
+  }
+
   exit (EXIT_SUCCESS);
 }