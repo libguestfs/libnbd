libguestfs/libnbd

commit 9ac33310730a373acedb2f767b0a12084b827f85

api: Add nbd_poll2
This is the same as nbd_poll but allows an extra file descriptor to be
passed in.

generator/API.ml
@@ -2318,6 +2318,19 @@ This function is mainly useful as an example of how you might
 integrate libnbd with your own main loop, rather than being
 intended as something you would use.";
     example = Some "examples/aio-connect-read.c";
+    see_also = [ Link "poll2" ];
+  };
+
+  "poll2", {
+    default_call with
+    args = [Int "fd"; Int "timeout" ]; ret = RInt;
+    shortdesc = "poll the handle once, with fd";
+    longdesc = "\
+This is the same as L<nbd_poll(3)>, but an additional
+file descriptor parameter is passed.  The additional
+fd is also polled (using C<POLLIN>).  One use for this is to
+wait for an L<eventfd(2)>.";
+    see_also = [ Link "poll" ];
   };
 
   "aio_connect", {
@@ -3256,6 +3269,7 @@ let first_version = [
   "get_request_block_size", (1, 12);
 
   (* Added in 1.15.x development cycle, will be stable and supported in 1.16. *)
+  "poll2", (1, 16);
   "supports_vsock", (1, 16);
 
   (* These calls are proposed for a future version of libnbd, but

lib/poll.c
@@ -27,14 +27,18 @@
 #include "internal.h"
 
 /* A simple main loop implementation using poll(2). */
-int
-nbd_unlocked_poll (struct nbd_handle *h, int timeout)
+static int
+do_poll (struct nbd_handle *h, int extra_fd, int timeout)
 {
-  struct pollfd fds[1];
+  struct pollfd fds[2];
   int r;
 
   /* fd might be negative, and poll will ignore it. */
   fds[0].fd = nbd_unlocked_aio_get_fd (h);
+  fds[1].fd = extra_fd;
+  fds[1].events = POLLIN;
+  fds[1].revents = 0;
+
   switch (nbd_internal_aio_get_direction (get_next_state (h))) {
   case LIBNBD_AIO_DIRECTION_READ:
     fds[0].events = POLLIN;
@@ -58,7 +62,7 @@ nbd_unlocked_poll (struct nbd_handle *h, int timeout)
    * passed to poll.
    */
   do {
-    r = poll (fds, 1, timeout);
+    r = poll (fds, 2, timeout);
     debug (h, "poll end: r=%d revents=%x", r, fds[0].revents);
   } while (r == -1 && errno == EINTR);
 
@@ -90,4 +94,16 @@ nbd_unlocked_poll (struct nbd_handle *h, int timeout)
     return -1;
 
   return 1;
+}
+
+int
+nbd_unlocked_poll (struct nbd_handle *h, int timeout)
+{
+  return do_poll (h, -1, timeout);
+}
+
+int
+nbd_unlocked_poll2 (struct nbd_handle *h, int fd, int timeout)
+{
+  return do_poll (h, fd, timeout);
 }

commit cc43c5559f677b273b7db68dfb232c32e11a0c44

api: Fix poll2 file descriptor parameter type
The file descriptor parameter is an int in C, but in other languages
it's a language-specific file descriptor.  The generator can already
express this, but we forgot to use the correct type when adding the
API.  The API signature in C does not change.

Fixes: commit 9ac33310730a373acedb2f767b0a12084b827f85

generator/API.ml
@@ -2325,7 +2325,7 @@ intended as something you would use.";
 
   "poll2", {
     default_call with
-    args = [Int "fd"; Int "timeout" ]; ret = RInt;
+    args = [Fd "fd"; Int "timeout" ]; ret = RInt;
     shortdesc = "poll the handle once, with fd";
     longdesc = "\
 This is the same as L<nbd_poll(3)>, but an additional