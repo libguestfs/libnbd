libguestfs/libnbd

commit 437a472d4e4de031b3e4b03efe2b53cfd35fcca6

api: Add nbd_opt_go and nbd_aio_opt_go
Time to make option negotiation actually useful, by now allowing the
user to request an export name before (re-)trying NBD_OPT_GO.  This
also means that we have to reset state before each attempt at
NBD_OPT_GO (any meta contexts from the previous try must not be
inherited into the next try).

To make things easier to review, this version of the patch determines
success or failure solely based on the resulting state.  However, that
approach will not work for NBD_OPT_INFO (since it returns to
Negotiating on both success and failure), so the next patch will add
in a completion callback, similar to aio data commands.

Testsuite coverage will be added when NBD_OPT_INFO is added.

lib/flags.c
@@ -27,6 +27,31 @@
 
 #include "internal.h"
 
+/* Reset connection data.  Called after swapping export name, after
+ * failed OPT_GO/OPT_INFO, or when starting a fresh OPT_SET_META_CONTEXT.
+ */
+void
+nbd_internal_reset_size_and_flags (struct nbd_handle *h)
+{
+  struct meta_context *m, *m_next;
+
+  h->exportsize = 0;
+  h->eflags = 0;
+  for (m = h->meta_contexts; m != NULL; m = m_next) {
+    m_next = m->next;
+    free (m->name);
+    free (m);
+  }
+  h->meta_contexts = NULL;
+  h->block_minimum = 0;
+  h->block_preferred = 0;
+  h->block_maximum = 0;
+  free (h->canonical_name);
+  h->canonical_name = NULL;
+  free (h->description);
+  h->description = NULL;
+}
+
 /* Set the export size and eflags on the handle, validating them.
  * This is called from the state machine when either the newstyle or
  * oldstyle negotiation reaches the point that these are available.
@@ -194,7 +219,8 @@ nbd_unlocked_get_size (struct nbd_handle *h)
   return h->exportsize;
 }
 
-int64_t nbd_unlocked_get_block_size (struct nbd_handle *h, int type)
+int64_t
+nbd_unlocked_get_block_size (struct nbd_handle *h, int type)
 {
   switch (type) {
   case LIBNBD_SIZE_MINIMUM:

lib/handle.c
@@ -112,7 +112,6 @@ nbd_create (void)
 void
 nbd_close (struct nbd_handle *h)
 {
-  struct meta_context *m, *m_next;
   size_t i;
 
   nbd_internal_set_error_context ("nbd_close");
@@ -126,18 +125,12 @@ nbd_close (struct nbd_handle *h)
   nbd_unlocked_clear_debug_callback (h);
 
   free (h->bs_entries);
-  for (m = h->meta_contexts; m != NULL; m = m_next) {
-    m_next = m->next;
-    free (m->name);
-    free (m);
-  }
+  nbd_internal_reset_size_and_flags (h);
   for (i = 0; i < h->nr_exports; ++i) {
     free (h->exports[i].name);
     free (h->exports[i].description);
   }
   free (h->exports);
-  free (h->canonical_name);
-  free (h->description);
   free_cmd_list (h->cmds_to_issue);
   free_cmd_list (h->cmds_in_flight);
   free_cmd_list (h->cmds_done);

generator/API.ml
@@ -285,9 +285,15 @@ export names.  The NBD protocol limits export names to
 4096 bytes, but servers may not support the full length.
 The encoding of export names is always UTF-8.
 
+When option mode is not in use, the export name must be set
+before beginning a connection.  However, when L<nbd_set_opt_mode(3)>
+has enabled option mode, it is possible to change the export
+name prior to L<nbd_opt_go(3)>.
+
 This call may be skipped if using L<nbd_connect_uri(3)> to connect
 to a URI that includes an export name.";
-    see_also = [Link "get_export_name"; Link "connect_uri"];
+    see_also = [Link "get_export_name"; Link "connect_uri";
+                Link "set_opt_mode"; Link "opt_go"];
   };
 
   "get_export_name", {
@@ -697,13 +703,13 @@ When option mode is enabled, you have fine-grained control over which
 options are negotiated, compared to the default of the server
 negotiating everything on your behalf using settings made before
 starting the connection.  To leave the mode and proceed on to the
-ready state, you must use nbd_opt_go successfully; a failed
-C<nbd_opt_go> returns to the negotiating state to allow a change of
+ready state, you must use L<nbd_opt_go(3)> successfully; a failed
+L<nbd_opt_go(3)> returns to the negotiating state to allow a change of
 export name before trying again.  You may also use L<nbd_opt_abort(3)>
 to end the connection without finishing negotiation.";
     example = Some "examples/list-exports.c";
     see_also = [Link "get_opt_mode"; Link "aio_is_negotiating";
-                Link "opt_abort"];
+                Link "opt_abort"; Link "opt_go"];
   };
 
   "get_opt_mode", {
@@ -716,6 +722,25 @@ Return true if option negotiation mode was enabled on this handle.";
     see_also = [Link "set_opt_mode"];
   };
 
+  "opt_go", {
+    default_call with
+    args = []; ret = RErr;
+    permitted_states = [ Negotiating ];
+    shortdesc = "end negotiation and move on to using an export";
+    longdesc = "\
+Request that the server finish negotiation and move on to serving the
+export previously specified by the most recent L<nbd_set_export_name(3)>
+or L<nbd_connect_uri(3)>.  This can only be used if
+L<nbd_set_opt_mode(3)> enabled option mode.
+
+If this fails, the server may still be in negotiation, where it is
+possible to attempt another option such as a different export name;
+although older servers will instead have killed the connection.";
+    example = Some "examples/list-exports.c";
+    see_also = [Link "set_opt_mode"; Link "aio_opt_go"; Link "opt_abort";
+                Link "set_export_name"; Link "connect_uri"];
+  };
+
   "opt_abort", {
     default_call with
     args = []; ret = RErr;
@@ -726,7 +751,7 @@ Request that the server finish negotiation, gracefully if possible, then
 close the connection.  This can only be used if L<nbd_set_opt_mode(3)>
 enabled option mode.";
     example = Some "examples/list-exports.c";
-    see_also = [Link "set_opt_mode"; Link "aio_opt_abort"];
+    see_also = [Link "set_opt_mode"; Link "aio_opt_abort"; Link "opt_go"];
   };
 
   "set_list_exports", {
@@ -938,8 +963,8 @@ L<nbd_set_export_name(3)> and L<nbd_set_tls(3)> and other
 calls as needed, followed by L<nbd_connect_tcp(3)> or
 L<nbd_connect_unix(3)>.  However, it is possible to override the
 export name portion of a URI by using L<nbd_set_opt_mode(3)> to
-enable option mode, then using L<nbd_set_export_name(3)> as part
-of later negotiation.
+enable option mode, then using L<nbd_set_export_name(3)> and
+L<nbd_opt_go(3)> as part of subsequent negotiation.
 
 This call returns when the connection has been made.
 
@@ -1897,6 +1922,22 @@ and completed the NBD handshake by calling L<nbd_aio_is_ready(3)>,
 on the connection.";
   };
 
+  "aio_opt_go", {
+    default_call with
+    args = []; ret = RErr;
+    permitted_states = [ Negotiating ];
+    shortdesc = "end negotiation and move on to using an export";
+    longdesc = "\
+Request that the server finish negotiation and move on to serving the
+export previously specified by the most recent L<nbd_set_export_name(3)>
+or L<nbd_connect_uri(3)>.  This can only be used if
+L<nbd_set_opt_mode(3)> enabled option mode.
+
+To determine when the request completes, wait for
+L<nbd_aio_is_connecting(3)> to return false.";
+    see_also = [Link "set_opt_mode"; Link "opt_go"];
+  };
+
   "aio_opt_abort", {
     default_call with
     args = []; ret = RErr;
@@ -2533,7 +2574,9 @@ let first_version = [
   "set_opt_mode", (1, 4);
   "get_opt_mode", (1, 4);
   "aio_is_negotiating", (1, 4);
+  "opt_go", (1, 4);
   "opt_abort", (1, 4);
+  "aio_opt_go", (1, 4);
   "aio_opt_abort", (1, 4);
 
   (* These calls are proposed for a future version of libnbd, but

generator/states-newstyle-opt-go.c
@@ -240,7 +240,11 @@ STATE_MACHINE {
       set_error (0, "handshake: unknown reply from NBD_OPT_GO: 0x%" PRIx32,
                  reply);
     }
-    SET_NEXT_STATE (%^PREPARE_OPT_ABORT);
+    nbd_internal_reset_size_and_flags (h);
+    if (h->opt_mode)
+      SET_NEXT_STATE (%.NEGOTIATING);
+    else
+      SET_NEXT_STATE (%^PREPARE_OPT_ABORT);
     break;
   case NBD_REP_ACK:
     SET_NEXT_STATE (%^FINISHED);

generator/states-newstyle-opt-set-meta-context.c
@@ -28,6 +28,7 @@ STATE_MACHINE {
    * contexts.
    */
   assert (h->gflags & LIBNBD_HANDSHAKE_FLAG_FIXED_NEWSTYLE);
+  nbd_internal_reset_size_and_flags (h);
   if (!h->structured_replies ||
       h->request_meta_contexts == NULL ||
       nbd_internal_string_list_length (h->request_meta_contexts) == 0) {

generator/states-newstyle.c
@@ -118,6 +118,12 @@ STATE_MACHINE {
      * state has informed us what we still need to do.
      */
     switch (h->opt_current) {
+    case NBD_OPT_GO:
+      if ((h->gflags & LIBNBD_HANDSHAKE_FLAG_FIXED_NEWSTYLE) == 0)
+        SET_NEXT_STATE (%OPT_EXPORT_NAME.START);
+      else
+        SET_NEXT_STATE (%OPT_SET_META_CONTEXT.START);
+      return 0;
     case NBD_OPT_ABORT:
       if ((h->gflags & LIBNBD_HANDSHAKE_FLAG_FIXED_NEWSTYLE) == 0) {
         SET_NEXT_STATE (%.DEAD);

lib/internal.h
@@ -405,6 +405,7 @@ extern void nbd_internal_set_last_error (int errnum, char *error);
   } while (0)
 
 /* flags.c */
+extern void nbd_internal_reset_size_and_flags (struct nbd_handle *h);
 extern int nbd_internal_set_size_and_flags (struct nbd_handle *h,
                                             uint64_t exportsize,
                                             uint16_t eflags);

lib/opt.c
@@ -48,6 +48,21 @@ wait_for_option (struct nbd_handle *h)
   return 0;
 }
 
+/* Issue NBD_OPT_GO (or NBD_OPT_EXPORT_NAME) and wait for the reply. */
+int
+nbd_unlocked_opt_go (struct nbd_handle *h)
+{
+  int r = nbd_unlocked_aio_opt_go (h);
+
+  if (r == -1)
+    return r;
+
+  r = wait_for_option (h);
+  if (r == 0 && nbd_internal_is_state_negotiating (get_next_state (h)))
+    return -1; /* NBD_OPT_GO failed, but can be attempted again */
+  return r;
+}
+
 /* Issue NBD_OPT_ABORT and wait for the state change. */
 int
 nbd_unlocked_opt_abort (struct nbd_handle *h)
@@ -58,6 +73,17 @@ nbd_unlocked_opt_abort (struct nbd_handle *h)
     return r;
 
   return wait_for_option (h);
+}
+
+/* Issue NBD_OPT_GO (or NBD_OPT_EXPORT_NAME) without waiting. */
+int
+nbd_unlocked_aio_opt_go (struct nbd_handle *h)
+{
+  h->opt_current = NBD_OPT_GO;
+
+  if (nbd_internal_run (h, cmd_issue) == -1)
+    debug (h, "option queued, ignoring state machine failure");
+  return 0;
 }
 
 /* Issue NBD_OPT_ABORT without waiting. */

commit 75eb4bf6e455d560d470c6174d75a0f33d57e783

api: Reset state on changed nbd_set_export_name()
The documentation for nbd_internal_reset_size_and_flags() claims we
should wipe all knowledge of a previously-negotiated export when
swapping export names.  However, we weren't actually doing that, which
could lead to a user calling nbd_opt_info() for one export, then
switching to another export name, then having nbd_get_size() still
report the size of the old export.  The next patch will add testsuite
coverage (done separately, to make it easier to prove the test catches
our flaw without this fix by applying patches out of order).

We should also wipe state after successful NBD_OPT_STARTTLS; although
this is academic for now (because we don't yet allow any other
NBD_OPT_ to go over the wire before NBD_OPT_STARTTLS).

While at it, there is no need to wipe state if we change to the same
name.  Note, however, that we do not bother to store the last name
we've exposed to the server; as a result, there are instances where we
clear state but the server does not.  But in general, this is not a
problem; it's always okay to be more conservative and not utilize
something the server supports, than to be be overly risky and use the
server on the basis of stale state.  Here's an example (unlikely to
happen in real code) where we are over-eager in wiping state, even
though the server never knows we considered a different name:

nbd_set_export_name(nbd, "a");
nbd_opt_info(nbd, callback);    => NBD_OPT_SET_META_CONTEXT("a")
                                => NBD_OPT_INFO("a")
nbd_set_export_name(nbd, "b");
nbd_set_request_meta_context(nbd, false);
nbd_set_export_name(nbd, "a");
nbd_opt_go(nbd);                => NBD_OPT_GO("a")

At any rate, this patch, plus several earlier ones, give us the
following state transitions:

export information (nbd_get_size, nbd_is_read_only, nbd_can_trim, ...)
 - gated by h->eflags being non-zero, but tracked in multiple
   variables such as h->eflags, h->exportsize, h->block_minimum, ...
 - made valid by successful NBD_OPT_INFO or NBD_OPT_GO
 - wiped by:
   - failed NBD_OPT_INFO or NBD_OPT_GO
   - any NBD_OPT_STARTTLS (a later patch may add nbd_opt_starttls;
     for now, it is not possible to tickle this)
   - nbd_set_export_name() changing names (not directly caused by
     server state, but because of how many of our interfaces
     implicitly reuse h->export_name)
 - persists over:
   - NBD_OPT_LIST_META_CONTEXT
   - NBD_OPT_SET_META_CONTEXT
context mappings (nbd_can_meta_context)
 - gated by h->meta_valid, tracked by h->meta_contexts
 - made valid (returns 0/1 for all names) by:
   - successful NBD_OPT_SET_META_CONTEXT
   - if nbd_set_request_meta_context() is true, a successful
     nbd_opt_info() or nbd_opt_go() that was unable to set contexts
     (server was oldstyle, or structured replies are lacking, or
     client didn't use nbd_add_meta_context)
 - wiped (returns -1 for all names) by:
   - failed NBD_OPT_SET_META_CONTEXT
   - any NBD_OPT_STARTTLS (not tickled until nbd_opt_starttls is added)
   - nbd_set_export_name() changing names
 - persists over:
  - NBD_OPT_GO, NBD_OPT_LIST
  - NBD_OPT_LIST_META_CONTEXT

Fixes: 437a472d ("api: Add nbd_opt_go and nbd_aio_opt_go", v1.3.12)

generator/states-newstyle-opt-starttls.c
@@ -72,6 +72,7 @@ STATE_MACHINE {
   reply = be32toh (h->sbuf.or.option_reply.reply);
   switch (reply) {
   case NBD_REP_ACK:
+    nbd_internal_reset_size_and_flags (h);
     new_sock = nbd_internal_crypto_create_session (h, h->sock);
     if (new_sock == NULL) {
       SET_NEXT_STATE (%.DEAD);

lib/flags.c
@@ -28,7 +28,7 @@
 #include "internal.h"
 
 /* Reset connection data.  Called after swapping export name, after
- * failed OPT_GO/OPT_INFO, or when starting a fresh OPT_SET_META_CONTEXT.
+ * failed OPT_GO/OPT_INFO, or after successful OPT_STARTTLS.
  */
 void
 nbd_internal_reset_size_and_flags (struct nbd_handle *h)

lib/handle.c
@@ -232,6 +232,9 @@ nbd_unlocked_set_export_name (struct nbd_handle *h, const char *export_name)
     return -1;
   }
 
+  if (strcmp (export_name, h->export_name) == 0)
+    return 0;
+
   new_name = strdup (export_name);
   if (!new_name) {
     set_error (errno, "strdup");
@@ -240,6 +243,7 @@ nbd_unlocked_set_export_name (struct nbd_handle *h, const char *export_name)
 
   free (h->export_name);
   h->export_name = new_name;
+  nbd_internal_reset_size_and_flags (h);
   h->meta_valid = false;
   return 0;
 }