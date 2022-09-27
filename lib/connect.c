libguestfs/libnbd

commit 0b7bdf62c87f39dc4551e01057a023bc50e7deb4

generator: Improve trace messages.
Improve function trace output so that:
 - Long strings are truncated.
 - Strings with non-printable characters are escaped.
 - Buffers are partially printed.
 - Lists of strings are printed.

lib/internal.h
@@ -409,5 +409,8 @@ extern char **nbd_internal_copy_string_list (char **argv);
 extern void nbd_internal_free_string_list (char **argv);
 extern const char *nbd_internal_fork_safe_itoa (long v, char *buf, size_t len);
 extern void nbd_internal_fork_safe_perror (const char *s);
+extern char *nbd_internal_printable_buffer (const void *buf, size_t count);
+extern char *nbd_internal_printable_string (const char *str);
+extern char *nbd_internal_printable_string_list (char **list);
 
 #endif /* LIBNBD_INTERNAL_H */

lib/utils.c
@@ -167,3 +167,109 @@ nbd_internal_fork_safe_perror (const char *s)
    */
   errno = err;
 }
+
+/* nbd_internal_printable_* functions are used by the API code to
+ * print debug messages when we trace calls in and out of libnbd.  The
+ * calls should attempt to convert the parameter into something
+ * printable.
+ *
+ * They cannot fail, but it's OK if they return NULL.
+ *
+ * Caller frees the result.
+ */
+
+char *
+nbd_internal_printable_buffer (const void *buf, size_t count)
+{
+  char *s = NULL;
+  size_t len = 0, truncated;
+  FILE *fp;
+
+  fp = open_memstream (&s, &len);
+  if (fp == NULL)
+    return NULL;
+
+  /* If the buffer is very long, truncate it to 1 sector. */
+  if (count > 512) {
+    truncated = count - 512;
+    count = 512;
+  }
+  else
+    truncated = 0;
+
+  fprintf (fp, "\n");
+  nbd_internal_hexdump (buf, count, fp);
+
+  if (truncated)
+    fprintf (fp, "[... %zu more bytes truncated ...]\n", truncated);
+  fclose (fp);
+
+  return s;
+}
+
+static void
+printable_string (const char *str, FILE *fp)
+{
+  size_t i, n, truncated;
+
+  n = strlen (str);
+  if (n > 512) {
+    truncated = n - 512;
+    n = 512;
+  }
+  else
+    truncated = 0;
+
+  fprintf (fp, "\"");
+  for (i = 0; i < n; ++i) {
+    if (isprint (str[i]))
+      fputc (str[i], fp);
+    else
+      fprintf (fp, "\\x%02x", str[i]);
+  }
+
+  if (truncated)
+    fprintf (fp, "[... %zu more bytes truncated ...]", truncated);
+  fprintf (fp, "\"");
+}
+
+char *
+nbd_internal_printable_string (const char *str)
+{
+  char *s = NULL;
+  size_t len = 0;
+  FILE *fp;
+
+  fp = open_memstream (&s, &len);
+  if (fp == NULL)
+    return NULL;
+
+  printable_string (str, fp);
+  fclose (fp);
+
+  return s;
+}
+
+char *
+nbd_internal_printable_string_list (char **list)
+{
+  char *s = NULL;
+  size_t len = 0, i;
+  FILE *fp;
+
+  fp = open_memstream (&s, &len);
+  if (fp == NULL)
+    return NULL;
+
+  fprintf (fp, "[");
+  for (i = 0; list[i] != NULL; ++i) {
+    if (i > 0)
+      fprintf (fp, ", ");
+    printable_string (list[i], fp);
+  }
+  fprintf (fp, "]");
+  fclose (fp);
+
+  return s;
+
+}

TODO
@@ -22,12 +22,6 @@ Performance: Chart it over various buffer sizes and threads, as that
 
 Examine other fuzzers: https://gitlab.com/akihe/radamsa
 
-Improve function trace output so that:
- - Long strings are truncated.
- - Strings with non-printable characters are escaped.
- - Buffers are partially printed.
- - Lists of strings are printed.
-
 Should we ship a "nbdcp" copying tool?
  - Could upload, download or copy between servers.
  - Duplicates functionality already available in qemu-img convert.

generator/generator
@@ -4608,14 +4608,33 @@ let generate_lib_api_c () =
 
   (* Print the trace when we enter a call with debugging enabled. *)
   and print_trace_enter args optargs =
-    pr "  debug (h, \"enter:";
+    pr "  if_debug (h) {\n";
+    List.iter (
+      function
+      | BytesIn (n, count)
+      | BytesPersistIn (n, count) ->
+         pr "    char *%s_printable =\n" n;
+         pr "        nbd_internal_printable_buffer (%s, %s);\n" n count
+      | Path n
+      | String n ->
+         pr "    char *%s_printable =\n" n;
+         pr "        nbd_internal_printable_string (%s);\n" n
+      | StringList n ->
+         pr "    char *%s_printable =\n" n;
+         pr "        nbd_internal_printable_string_list (%s);\n" n
+      | BytesOut _ | BytesPersistOut _
+      | Bool _ | Closure _ | Enum _ | Flags _ | Fd _ | Int _
+      | Int64 _ | SockAddrAndLen _ | UInt _ | UInt32 _ | UInt64 _ -> ()
+    ) args;
+    pr "    debug (h, \"enter:";
     List.iter (
       function
       | Bool n -> pr " %s=%%s" n
-      | BytesIn (n, count)
-      | BytesPersistIn (n, count)
       | BytesOut (n, count)
       | BytesPersistOut (n, count) -> pr " %s=<buf> %s=%%zu" n count
+      | BytesIn (n, count)
+      | BytesPersistIn (n, count) ->
+         pr " %s=\\\"%%s\\\" %s=%%zu" n count
       | Closure { cbname } -> pr " %s=<fun>" cbname
       | Enum (n, _) -> pr " %s=%%d" n
       | Flags (n, _) -> pr " %s=0x%%x" n
@@ -4623,8 +4642,8 @@ let generate_lib_api_c () =
       | Int64 n -> pr " %s=%%\" PRIi64 \"" n
       | SockAddrAndLen (n, len) -> pr " %s=<sockaddr> %s=%%d" n len
       | Path n
-      | String n -> pr " %s=\\\"%%s\\\"" n
-      | StringList n -> pr " %s=<list>" n
+      | String n -> pr " %s=%%s" n
+      | StringList n -> pr " %s=%%s" n
       | UInt n -> pr " %s=%%u" n
       | UInt32 n -> pr " %s=%%\" PRIu32 \"" n
       | UInt64 n -> pr " %s=%%\" PRIu64 \"" n
@@ -4638,18 +4657,21 @@ let generate_lib_api_c () =
     List.iter (
       function
       | Bool n -> pr ", %s ? \"true\" : \"false\"" n
-      | BytesIn (_, count)
-      | BytesPersistIn (_, count)
-      | BytesOut (_, count)
-      | BytesPersistOut (_, count) -> pr ", %s" count
+      | BytesOut (n, count)
+      | BytesPersistOut (n, count) -> pr ", %s" count
+      | BytesIn (n, count)
+      | BytesPersistIn (n, count) ->
+         pr ", %s_printable ? %s_printable : \"\", %s" n n count
       | Closure { cbname } -> ()
       | Enum (n, _) -> pr ", %s" n
       | Flags (n, _) -> pr ", %s" n
       | Fd n | Int n -> pr ", %s" n
       | Int64 n -> pr ", %s" n
       | SockAddrAndLen (_, len) -> pr ", (int) %s" len
-      | Path n | String n -> pr ", %s ? %s : \"NULL\"" n n
-      | StringList n -> ()
+      | Path n | String n ->
+         pr ", %s ? %s_printable ? %s_printable : \"\" : \"NULL\"" n n n
+      | StringList n ->
+         pr ", %s_printable ? %s_printable : \"\"" n n
       | UInt n | UInt32 n | UInt64 n -> pr ", %s" n
     ) args;
     List.iter (
@@ -4658,7 +4680,20 @@ let generate_lib_api_c () =
          pr ", CALLBACK_IS_NULL (%s_callback) ? \"<fun>\" : \"NULL\"" cbname
       | OFlags (n, _) -> pr ", %s" n
     ) optargs;
-    pr ");\n"
+    pr ");\n";
+    List.iter (
+      function
+      | BytesIn (n, _)
+      | BytesPersistIn (n, _)
+      | Path n
+      | String n
+      | StringList n ->
+         pr "    free (%s_printable);\n" n
+      | BytesOut _ | BytesPersistOut _
+      | Bool _ | Closure _ | Enum _ | Flags _ | Fd _ | Int _
+      | Int64 _ | SockAddrAndLen _ | UInt _ | UInt32 _ | UInt64 _ -> ()
+    ) args;
+    pr "  }\n"
   (* Print the trace when we leave a call with debugging enabled. *)
   and print_trace_leave ret =
     pr "  if_debug (h) {\n";
@@ -4670,15 +4705,31 @@ let generate_lib_api_c () =
         pr "    if (0)\n";
     );
     pr "      debug (h, \"leave: error=\\\"%%s\\\"\", nbd_get_error ());\n";
-    pr "    else\n";
-    pr "      debug (h, \"leave: ret=";
+    pr "    else {\n";
     (match ret with
-     | RBool | RErr | RFd | RInt -> pr "%%d"
-     | RInt64 | RCookie -> pr "%%\" PRIi64 \""
-     | RStaticString | RString -> pr "\\\"%%s\\\""
-     | RUInt -> pr "%%u"
+     | RStaticString | RString ->
+        pr "      char *ret_printable =\n";
+        pr "          nbd_internal_printable_string (ret);\n"
+     | RBool | RErr | RFd | RInt
+     | RInt64 | RCookie
+     | RUInt -> ()
     );
-    pr "\", ret);\n";
+    pr "      debug (h, \"leave: ret=\" ";
+    (match ret with
+     | RBool | RErr | RFd | RInt -> pr "\"%%d\", ret"
+     | RInt64 | RCookie -> pr "\"%%\" PRIi64 \"\", ret"
+     | RStaticString | RString ->
+        pr "\"\\\"%%s\\\"\", ret_printable ? ret_printable : \"\""
+     | RUInt -> pr "\"%%u\", ret"
+    );
+    pr ");\n";
+    (match ret with
+     | RStaticString | RString -> pr "      free (ret_printable);\n"
+     | RBool | RErr | RFd | RInt
+     | RInt64 | RCookie
+     | RUInt -> ()
+    );
+    pr "    }\n";
     pr "  }\n"
   in
 

commit d0fbb769286a97728b0d1358e7accc2eb708d795

api: Fix assertion on nbd_connect_command with bad argv
nbd_connect_command (h, (char **) { NULL }) triggers SIGABRT when
preparing to exec a NULL command name (during
enter_STATE_CONNECT_COMMAND_START, since v1.0).

nbd_connect_command (h, NULL) in newer releases triggers SIGSEGV by
trying to dereference NULL (with LIBNBD_DEBUG=1, during
nbd_internal_printable_string_list in v1.4; otherwise, during
nbd_internal_set_argv in v1.6).  That behavior is a regression, since
in older releases, it behaves the same as (char **) { NULL } and
triggers SIGABRT.

Both crashes are corner cases that have existed for a long time, and
unlikely to hit in real code.  Still, we shouldn't crash in a library,
at least not when our documentation is not explicit about behavior
being undefined.

There is still ongoing conversations on the list about how to handle a
NULL StringList argument, with arguments for treating it the same as
non-NULL list with argv[0]==NULL (what this patch does to fix the
1.4/1.6 regression), or for merely documenting that it is bad and then
letting SEGV or assert flag it for the user that didn't read the docs,
or for trying to use gcc's attribute((nonnull)) markers in the .h
files to let capable compilers flag it, or treating things the same as
NULL String where we error with EFAULT.  So there may be later patches
on that front.  But we all agreed that gracefully handling
argv[0]==NULL without asserting is worthwhile, whether or not we later
change argv==NULL to behave differently than argv[0]==NULL.

To do this, refactor nbd_internal_set_argv() to split out the vector
population into a new helper function that can only fail with ENOMEM
(this function will also be useful in later patches that want to copy
a StringList, but can tolerate an empty list with 0 elements), as well
as to set errors itself (all 2 callers updated) since detecting NULL
for argv[0] is unique to argv.

Tests are added in the next patch, to make it easier to review by
temporarily swapping patch order.

Changes from:

$ nbdsh -c 'h.connect_command([])'
nbdsh: generator/states-connect.c:247: enter_STATE_CONNECT_COMMAND_START: Assertion `h->argv.ptr[0]' failed.
Aborted (core dumped)

to:

nbdsh: command line script failed: nbd_connect_command: missing command name in argv list: Invalid argument

Right now, our Python bindings treat h.connect_command(None) as a
TypeError, instead of a way to test passing NULL to the C code; that
may also be something we revisit in patches that make the generator
more consistent on how NULL is handled for various pointer parameters.

Fixes: 0b7bdf62 ("generator: Improve trace messages.", v1.3.2)

lib/connect.c
@@ -1,5 +1,5 @@
 /* NBD client library in userspace
- * Copyright (C) 2013-2020 Red Hat Inc.
+ * Copyright (C) 2013-2022 Red Hat Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -251,10 +251,8 @@ nbd_unlocked_aio_connect_socket (struct nbd_handle *h, int sock)
 int
 nbd_unlocked_aio_connect_command (struct nbd_handle *h, char **argv)
 {
-  if (nbd_internal_set_argv (&h->argv, argv) == -1) {
-    set_error (errno, "realloc");
+  if (nbd_internal_set_argv (h, argv) == -1)
     return -1;
-  }
 
   return nbd_internal_run (h, cmd_connect_command);
 }
@@ -263,10 +261,8 @@ int
 nbd_unlocked_aio_connect_systemd_socket_activation (struct nbd_handle *h,
                                                     char **argv)
 {
-  if (nbd_internal_set_argv (&h->argv, argv) == -1) {
-    set_error (errno, "realloc");
+  if (nbd_internal_set_argv (h, argv) == -1)
     return -1;
-  }
 
   return nbd_internal_run (h, cmd_connect_sa);
 }

lib/internal.h
@@ -476,7 +476,8 @@ extern int nbd_internal_aio_get_direction (enum state state);
 
 /* utils.c */
 extern void nbd_internal_hexdump (const void *data, size_t len, FILE *fp);
-extern int nbd_internal_set_argv (string_vector *v, char **argv);
+extern int nbd_internal_copy_string_list (string_vector *v, char **in);
+extern int nbd_internal_set_argv (struct nbd_handle *h, char **argv);
 extern const char *nbd_internal_fork_safe_itoa (long v, char *buf, size_t len);
 extern void nbd_internal_fork_safe_perror (const char *s);
 extern char *nbd_internal_printable_buffer (const void *buf, size_t count);

lib/utils.c
@@ -1,5 +1,5 @@
 /* NBD client library in userspace
- * Copyright (C) 2013-2020 Red Hat Inc.
+ * Copyright (C) 2013-2022 Red Hat Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -53,16 +53,17 @@ nbd_internal_hexdump (const void *data, size_t len, FILE *fp)
   }
 }
 
-/* Replace the string_vector with a deep copy of argv (including final NULL). */
+/* Replace a string_vector with a deep copy of in (including final NULL). */
 int
-nbd_internal_set_argv (string_vector *v, char **argv)
+nbd_internal_copy_string_list (string_vector *v, char **in)
 {
   size_t i;
 
+  assert (in);
   string_vector_reset (v);
 
-  for (i = 0; argv[i] != NULL; ++i) {
-    char *copy = strdup (argv[i]);
+  for (i = 0; in[i] != NULL; ++i) {
+    char *copy = strdup (in[i]);
     if (copy == NULL)
       return -1;
     if (string_vector_append (v, copy) == -1) {
@@ -72,6 +73,23 @@ nbd_internal_set_argv (string_vector *v, char **argv)
   }
 
   return string_vector_append (v, NULL);
+}
+
+/* Store argv into h, or diagnose an error on failure. */
+int
+nbd_internal_set_argv (struct nbd_handle *h, char **argv)
+{
+  /* FIXME: Do we want to assert(argv)? */
+  if (argv == NULL || argv[0] == NULL) {
+    set_error (EINVAL, "missing command name in argv list");
+    return -1;
+  }
+  if (nbd_internal_copy_string_list (&h->argv, argv) == -1) {
+    set_error (errno, "realloc");
+    return -1;
+  }
+
+  return 0;
 }
 
 /* Like sprintf ("%ld", v), but safe to use between fork and exec.  Do
@@ -247,13 +265,17 @@ nbd_internal_printable_string_list (char **list)
   if (fp == NULL)
     return NULL;
 
-  fprintf (fp, "[");
-  for (i = 0; list[i] != NULL; ++i) {
-    if (i > 0)
-      fprintf (fp, ", ");
-    printable_string (list[i], fp);
+  if (list == NULL)
+    fprintf (fp, "NULL");
+  else {
+    fprintf (fp, "[");
+    for (i = 0; list[i] != NULL; ++i) {
+      if (i > 0)
+        fprintf (fp, ", ");
+      printable_string (list[i], fp);
+    }
+    fprintf (fp, "]");
   }
-  fprintf (fp, "]");
   fclose (fp);
 
   return s;