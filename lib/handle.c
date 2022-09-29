libguestfs/libnbd

commit e33762a86cd5f064e5ef841520baa5fe7361d2c2

api: Localize list used during NBD_OPT_*_META_CONTEXT
Right now, our generator ensures that we are in the Negotiating state
before a client can call nbd_add_meta_context() while there is an
active server connection.  That is, even if the user piles up enough
meta contexts that a slow-response server then causes nbd_aio_opt_go()
and friends to return early on blocked writes, with the state machine
now tracking pointers into the middle of the global list, those
pointers are not at risk of becoming a use-after-free because the user
cannot successfully call nbd_add_meta_context() to alter the global
list until they first resume the state machine (using nbd_aio_poll or
similar) to finish flushing the client's write to the server and
eventually get back to the Negotiating state.

However, it took me a while to perform that audit to ensure that the
user can't mess with the global list while the state machine is
peering into the middle of it (that is, state machine interlocks act
at a distance).  Easier is to have the state machine act on a local
copy, which cannot be directly altered by user action regardless of
the current state; and therefore even if we later change our mind
about which states can call nbd_add_meta_context() (the state
restrictions in API.ml), that change won't accidentally cause our
state machine to start seeing corrupted list contents on the uncommon
case of blocked writes (in states-newstyle-opt-meta-context.c).

In this patch, I added a helper function to copy from either a
StringList (no clients of that parameter in this patch, but coming
soon) or from the implicit list.  Also, at the time of this patch, the
copying is done once in the state machine, but later patches will
hoist it earlier into lib/opt.c as part of adding more APIs for
fine-grained control over nbd_set_opt_mode management of context
lists, starting with nbd_opt_list_meta_context_queries() which will
take an explicit list of meta contexts to query.

lib/handle.c
@@ -133,6 +133,7 @@ nbd_close (struct nbd_handle *h)
   /* Free user callbacks first. */
   nbd_unlocked_clear_debug_callback (h);
 
+  string_vector_reset (&h->querylist);
   free (h->bs_entries);
   nbd_internal_reset_size_and_flags (h);
   for (i = 0; i < h->meta_contexts.len; ++i)

lib/utils.c
@@ -92,6 +92,42 @@ nbd_internal_set_argv (struct nbd_handle *h, char **argv)
   return 0;
 }
 
+/* Copy queries (defaulting to h->request_meta_contexts) into h->querylist.
+ * Set an error on failure.
+ */
+int
+nbd_internal_set_querylist (struct nbd_handle *h, char **queries)
+{
+  size_t i;
+
+  if (queries) {
+    if (nbd_internal_copy_string_list (&h->querylist, queries) == -1) {
+      set_error (errno, "realloc");
+      return -1;
+    }
+    /* Drop trailing NULL */
+    assert (h->querylist.len > 0);
+    string_vector_remove (&h->querylist, h->querylist.len - 1);
+  }
+  else {
+    string_vector_reset (&h->querylist);
+    for (i = 0; i < h->request_meta_contexts.len; ++i) {
+      char *copy = strdup (h->request_meta_contexts.ptr[i]);
+      if (copy == NULL) {
+        set_error (errno, "strdup");
+        return -1;
+      }
+      if (string_vector_append (&h->querylist, copy) == -1) {
+        set_error (errno, "realloc");
+        free (copy);
+        return -1;
+      }
+    }
+  }
+
+  return 0;
+}
+
 /* Like sprintf ("%ld", v), but safe to use between fork and exec.  Do
  * not use this function in any other context.
  *

generator/states-newstyle-opt-meta-context.c
@@ -65,10 +65,14 @@ STATE_MACHINE {
 
   assert (!h->meta_valid);
 
+  if (nbd_internal_set_querylist (h, NULL) == -1) {
+    SET_NEXT_STATE (%.DEAD);
+    return 0;
+  }
   /* Calculate the length of the option request data. */
   len = 4 /* exportname len */ + strlen (h->export_name) + 4 /* nr queries */;
-  for (i = 0; i < h->request_meta_contexts.len; ++i)
-    len += 4 /* length of query */ + strlen (h->request_meta_contexts.ptr[i]);
+  for (i = 0; i < h->querylist.len; ++i)
+    len += 4 /* length of query */ + strlen (h->querylist.ptr[i]);
 
   h->sbuf.option.version = htobe64 (NBD_NEW_VERSION);
   h->sbuf.option.option = htobe32 (opt);
@@ -107,7 +111,7 @@ STATE_MACHINE {
   switch (send_from_wbuf (h)) {
   case -1: SET_NEXT_STATE (%.DEAD); return 0;
   case 0:
-    h->sbuf.nrqueries = htobe32 (h->request_meta_contexts.len);
+    h->sbuf.nrqueries = htobe32 (h->querylist.len);
     h->wbuf = &h->sbuf;
     h->wlen = sizeof h->sbuf.nrqueries;
     h->wflags = MSG_MORE;
@@ -125,12 +129,12 @@ STATE_MACHINE {
   return 0;
 
  NEWSTYLE.OPT_META_CONTEXT.PREPARE_NEXT_QUERY:
-  if (h->querynum >= h->request_meta_contexts.len) {
+  if (h->querynum >= h->querylist.len) {
     /* end of list of requested meta contexts */
     SET_NEXT_STATE (%PREPARE_FOR_REPLY);
     return 0;
   }
-  const char *query = h->request_meta_contexts.ptr[h->querynum];
+  const char *query = h->querylist.ptr[h->querynum];
 
   h->sbuf.len = htobe32 (strlen (query));
   h->wbuf = &h->sbuf.len;
@@ -140,7 +144,7 @@ STATE_MACHINE {
   return 0;
 
  NEWSTYLE.OPT_META_CONTEXT.SEND_QUERYLEN:
-  const char *query = h->request_meta_contexts.ptr[h->querynum];
+  const char *query = h->querylist.ptr[h->querynum];
 
   switch (send_from_wbuf (h)) {
   case -1: SET_NEXT_STATE (%.DEAD); return 0;

lib/internal.h
@@ -285,6 +285,7 @@ struct nbd_handle {
   int connect_errno;
 
   /* When sending metadata contexts, this is used. */
+  string_vector querylist;
   size_t querynum;
 
   /* When receiving block status, this is used. */
@@ -478,6 +479,7 @@ extern int nbd_internal_aio_get_direction (enum state state);
 extern void nbd_internal_hexdump (const void *data, size_t len, FILE *fp);
 extern int nbd_internal_copy_string_list (string_vector *v, char **in);
 extern int nbd_internal_set_argv (struct nbd_handle *h, char **argv);
+extern int nbd_internal_set_querylist (struct nbd_handle *h, char **queries);
 extern const char *nbd_internal_fork_safe_itoa (long v, char *buf, size_t len);
 extern void nbd_internal_fork_safe_perror (const char *s);
 extern char *nbd_internal_printable_buffer (const void *buf, size_t count);

commit 21b279a78eefda92ce8dab589df31a6b3b7155cf

lib: Free h->querylist correctly
*_reset only frees the vector (ie. container), not the elements since
the vector considers the elements to be opaque blobs.  To free the
strings in this vector we must use:

  string_vector_iter (&h->querylist, (void *) free);

Also we have to free the vector along both paths of
nbd_internal_set_querylist.

Found by valgrind.

Fixes: commit e33762a86cd5f064e5ef841520baa5fe7361d2c2

lib/handle.c
@@ -134,7 +134,8 @@ nbd_close (struct nbd_handle *h)
   /* Free user callbacks first. */
   nbd_unlocked_clear_debug_callback (h);
 
-  string_vector_reset (&h->querylist);
+  string_vector_iter (&h->querylist, (void *) free);
+  free (h->querylist.ptr);
   free (h->bs_entries);
   nbd_internal_reset_size_and_flags (h);
   for (i = 0; i < h->meta_contexts.len; ++i)

lib/utils.c
@@ -100,6 +100,9 @@ nbd_internal_set_querylist (struct nbd_handle *h, char **queries)
 {
   size_t i;
 
+  string_vector_iter (&h->querylist, (void *) free);
+  string_vector_reset (&h->querylist);
+
   if (queries) {
     if (nbd_internal_copy_string_list (&h->querylist, queries) == -1) {
       set_error (errno, "realloc");
@@ -110,7 +113,6 @@ nbd_internal_set_querylist (struct nbd_handle *h, char **queries)
     string_vector_remove (&h->querylist, h->querylist.len - 1);
   }
   else {
-    string_vector_reset (&h->querylist);
     for (i = 0; i < h->request_meta_contexts.len; ++i) {
       char *copy = strdup (h->request_meta_contexts.ptr[i]);
       if (copy == NULL) {