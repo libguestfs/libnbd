libguestfs/libnbd

commit 53617c92a41a9e1ead0e08ae5c04afca0c08cef0

api: Add nbd_[aio_]opt_starttls
Very similar to the recent addition of nbd_opt_structured_reply, with
the new nbd_opt_starttls() finally giving us fine-grained control over
NBD_OPT_STARTTLS. and allowing productive handshaking with a server in
SELECTIVETLS mode.

With this patch, it is now easy to reproduce the scenario of nbdkit's
CVE-2021-3716, where a plaintext meddler-in-the-middle attacker could
cause client denial of service when a --tls=on server failed to reset
state after NBD_OPT_STARTTLS.  This also exposed the fact that nbdkit
was not gracefully handling NBD_OPT_INFO from a plaintext client
connecting to a --tls=require server; the testsuite is skipped unless
using a fixed nbdkit (v1.33.2 or later).

generator/states-newstyle-opt-starttls.c
@@ -21,10 +21,15 @@
 STATE_MACHINE {
  NEWSTYLE.OPT_STARTTLS.START:
   assert (h->gflags & LIBNBD_HANDSHAKE_FLAG_FIXED_NEWSTYLE);
-  /* If TLS was not requested we skip this option and go to the next one. */
-  if (h->tls == LIBNBD_TLS_DISABLE) {
-    SET_NEXT_STATE (%^OPT_STRUCTURED_REPLY.START);
-    return 0;
+  if (h->opt_current == NBD_OPT_STARTTLS)
+    assert (h->opt_mode);
+  else {
+    /* If TLS was not requested we skip this option and go to the next one. */
+    if (h->tls == LIBNBD_TLS_DISABLE) {
+      SET_NEXT_STATE (%^OPT_STRUCTURED_REPLY.START);
+      return 0;
+    }
+    assert (CALLBACK_IS_NULL (h->opt_cb.completion));
   }
 
   h->sbuf.option.version = htobe64 (NBD_NEW_VERSION);
@@ -68,12 +73,14 @@ STATE_MACHINE {
  NEWSTYLE.OPT_STARTTLS.CHECK_REPLY:
   uint32_t reply;
   struct socket *new_sock;
+  int err = ENOTSUP;
 
   reply = be32toh (h->sbuf.or.option_reply.reply);
   switch (reply) {
   case NBD_REP_ACK:
     nbd_internal_reset_size_and_flags (h);
     h->structured_replies = false;
+    h->meta_valid = false;
     new_sock = nbd_internal_crypto_create_session (h, h->sock);
     if (new_sock == NULL) {
       SET_NEXT_STATE (%.DEAD);
@@ -86,6 +93,9 @@ STATE_MACHINE {
       SET_NEXT_STATE (%TLS_HANDSHAKE_WRITE);
     return 0;
 
+  case NBD_REP_ERR_INVALID:
+    err = EINVAL;
+    /* fallthrough */
   default:
     if (handle_reply_error (h) == -1) {
       SET_NEXT_STATE (%.DEAD);
@@ -102,10 +112,17 @@ STATE_MACHINE {
       return 0;
     }
 
-    debug (h,
-           "server refused TLS (%s), continuing with unencrypted connection",
-           reply == NBD_REP_ERR_POLICY ? "policy" : "not supported");
-    SET_NEXT_STATE (%^OPT_STRUCTURED_REPLY.START);
+    debug (h, "server refused TLS (%s)",
+           reply == NBD_REP_ERR_POLICY ? "policy" :
+           reply == NBD_REP_ERR_INVALID ? "invalid request" : "not supported");
+    CALL_CALLBACK (h->opt_cb.completion, &err);
+    nbd_internal_free_option (h);
+    if (h->opt_current == NBD_OPT_STARTTLS)
+      SET_NEXT_STATE (%.NEGOTIATING);
+    else {
+      debug (h, "continuing with unencrypted connection");
+      SET_NEXT_STATE (%^OPT_STRUCTURED_REPLY.START);
+    }
     return 0;
   }
   return 0;
@@ -149,12 +166,19 @@ STATE_MACHINE {
   return 0;
 
  NEWSTYLE.OPT_STARTTLS.TLS_HANDSHAKE_DONE:
+  int err = 0;
+
   /* Finished handshake. */
   h->tls_negotiated = true;
   nbd_internal_crypto_debug_tls_enabled (h);
+  CALL_CALLBACK (h->opt_cb.completion, &err);
+  nbd_internal_free_option (h);
 
   /* Continue with option negotiation. */
-  SET_NEXT_STATE (%^OPT_STRUCTURED_REPLY.START);
+  if (h->opt_current == NBD_OPT_STARTTLS)
+    SET_NEXT_STATE (%.NEGOTIATING);
+  else
+    SET_NEXT_STATE (%^OPT_STRUCTURED_REPLY.START);
   return 0;
 
 } /* END STATE MACHINE */

.gitignore
@@ -236,6 +236,7 @@ Makefile.in
 /tests/opt-list-meta-queries
 /tests/opt-set-meta
 /tests/opt-set-meta-queries
+/tests/opt-starttls
 /tests/opt-structured-twice
 /tests/pki/
 /tests/pread-initialize

generator/API.ml
@@ -599,7 +599,15 @@ NBD server.  The possible settings are:
 =item C<LIBNBD_TLS_DISABLE>
 
 Disable TLS.  (The default setting, unless using L<nbd_connect_uri(3)> with
-a URI that requires TLS)
+a URI that requires TLS).
+
+This setting is also necessary if you use L<nbd_set_opt_mode(3)>
+and want to interact in plaintext with a server that implements
+the NBD protocol's C<SELECTIVETLS> mode, prior to enabling TLS
+with L<nbd_opt_starttls(3)>.  Most NBD servers with TLS support
+prefer the NBD protocol's C<FORCEDTLS> mode, so this sort of
+manual interaction tends to be useful mainly during integration
+testing.
 
 =item C<LIBNBD_TLS_ALLOW>
 
@@ -632,7 +640,8 @@ zero will fail if libnbd was not compiled against gnutls; you can
 test whether this is the case with L<nbd_supports_tls(3)>.";
     example = Some "examples/encryption.c";
     see_also = [SectionLink "ENCRYPTION AND AUTHENTICATION";
-                Link "get_tls"; Link "get_tls_negotiated"];
+                Link "get_tls"; Link "get_tls_negotiated";
+                Link "opt_starttls"];
   };
 
   "get_tls", {
@@ -657,16 +666,19 @@ on a particular connection use L<nbd_get_tls_negotiated(3)> instead.";
 After connecting you may call this to find out if the
 connection is using TLS.
 
-This is only really useful if you set the TLS request mode
+This is normally useful only if you set the TLS request mode
 to C<LIBNBD_TLS_ALLOW> (see L<nbd_set_tls(3)>), because in this
 mode we try to use TLS but fall back to unencrypted if it was
 not available.  This function will tell you if TLS was
 negotiated or not.
 
 In C<LIBNBD_TLS_REQUIRE> mode (the most secure) the connection
-would have failed if TLS could not be negotiated, and in
-C<LIBNBD_TLS_DISABLE> mode TLS is not tried.";
-    see_also = [Link "set_tls"; Link "get_tls"];
+would have failed if TLS could not be negotiated.  With
+C<LIBNBD_TLS_DISABLE> mode, TLS is not tried automatically;
+but if the NBD server uses the less-common C<SELECTIVETLS>
+mode, this function reports whether a manual L<nbd_opt_starttls(3)>
+enabled TLS or if the connection is still plaintext.";
+    see_also = [Link "set_tls"; Link "get_tls"; Link "opt_starttls"];
   };
 
   "set_tls_certificates", {
@@ -1092,11 +1104,12 @@ proceeding all the way to the ready state, when communicating with a
 newstyle server.  This setting has no effect when connecting to an
 oldstyle server.
 
-Note that libnbd defaults to attempting
+Note that libnbd defaults to attempting C<NBD_OPT_STARTTLS> and
 C<NBD_OPT_STRUCTURED_REPLY> before letting you control remaining
-negotiation steps; if you need control over this step as well,
-first set L<nbd_set_request_structured_replies(3)> to false before
-starting the connection attempt.
+negotiation steps; if you need control over these steps as well,
+first set L<nbd_set_tls(3)> to C<LIBNBD_TLS_DISABLE> and
+L<nbd_set_request_structured_replies(3)> to false before starting
+the connection attempt.
 
 When option mode is enabled, you have fine-grained control over which
 options are negotiated, compared to the default of the server
@@ -1110,9 +1123,9 @@ to end the connection without finishing negotiation.";
     see_also = [Link "get_opt_mode"; Link "aio_is_negotiating";
                 Link "opt_abort"; Link "opt_go"; Link "opt_list";
                 Link "opt_info"; Link "opt_list_meta_context";
-                Link "opt_set_meta_context";
+                Link "opt_set_meta_context"; Link "opt_starttls";
                 Link "opt_structured_reply";
-                Link "set_request_structured_replies";
+                Link "set_tls"; Link "set_request_structured_replies";
                 Link "aio_connect"];
   };
 
@@ -1164,6 +1177,44 @@ close the connection.  This can only be used if L<nbd_set_opt_mode(3)>
 enabled option mode.";
     example = Some "examples/list-exports.c";
     see_also = [Link "set_opt_mode"; Link "aio_opt_abort"; Link "opt_go"];
+  };
+
+  "opt_starttls", {
+    default_call with
+    args = []; ret = RBool;
+    permitted_states = [ Negotiating ];
+    shortdesc = "request the server to initiate TLS";
+    longdesc = "\
+Request that the server initiate a secure TLS connection, by
+sending C<NBD_OPT_STARTTLS>.  This can only be used if
+L<nbd_set_opt_mode(3)> enabled option mode; furthermore, if you
+use L<nbd_set_tls(3)> to request anything other than the default
+of C<LIBNBD_TLS_DISABLE>, then libnbd will have already attempted
+a TLS connection prior to allowing you control over option
+negotiation.  This command is disabled if L<nbd_supports_tls(3)>
+reports false.
+
+This function is mainly useful for integration testing of corner
+cases in server handling; in particular, misuse of this function
+when coupled with a server that is not careful about resetting
+stateful commands such as L<nbd_opt_structured_reply(3)> could
+result in a security hole (see CVE-2021-3716 against nbdkit, for
+example).  Thus, when security is a concern, you should instead
+prefer to use L<nbd_set_tls(3)> with C<LIBNBD_TLS_REQUIRE> and
+let libnbd negotiate TLS automatically.
+
+This function returns true if the server replies with success,
+false if the server replies with an error, and fails only if
+the server does not reply (such as for a loss of connection,
+which can include when the server rejects credentials supplied
+during the TLS handshake).  Note that the NBD protocol documents
+that requesting TLS after it is already enabled is a client
+error; most servers will gracefully fail a second request, but
+that does not downgrade a TLS session that has already been
+established, as reported by L<nbd_get_tls_negotiated(3)>.";
+    see_also = [Link "set_opt_mode"; Link "aio_opt_starttls";
+                Link "set_tls"; Link "get_tls_negotiated";
+                Link "supports_tls"]
   };
 
   "opt_structured_reply", {
@@ -2815,6 +2866,30 @@ L<nbd_aio_is_connecting(3)> to return false.";
     see_also = [Link "set_opt_mode"; Link "opt_abort"];
   };
 
+  "aio_opt_starttls", {
+    default_call with
+    args = [];
+    optargs = [ OClosure completion_closure ];
+    ret = RErr;
+    permitted_states = [ Negotiating ];
+    shortdesc = "request the server to initiate TLS";
+    longdesc = "\
+Request that the server initiate a secure TLS connection, by
+sending C<NBD_OPT_STARTTLS>.  This behaves like the synchronous
+counterpart L<nbd_opt_starttls(3)>, except that it does
+not wait for the server's response.
+
+To determine when the request completes, wait for
+L<nbd_aio_is_connecting(3)> to return false.  Or supply the optional
+C<completion_callback> which will be invoked as described in
+L<libnbd(3)/Completion callbacks>, except that it is automatically
+retired regardless of return value.  Note that detecting whether the
+server returns an error (as is done by the return value of the
+synchronous counterpart) is only possible with a completion
+callback.";
+    see_also = [Link "set_opt_mode"; Link "opt_starttls"];
+  };
+
   "aio_opt_structured_reply", {
     default_call with
     args = [];
@@ -3744,6 +3819,8 @@ let first_version = [
   "aio_opt_set_meta_context_queries", (1, 16);
   "opt_structured_reply", (1, 16);
   "aio_opt_structured_reply", (1, 16);
+  "opt_starttls", (1, 16);
+  "aio_opt_starttls", (1, 16);
 
   (* These calls are proposed for a future version of libnbd, but
    * have not been added to any released version so far.

generator/states-newstyle.c
@@ -146,6 +146,9 @@ STATE_MACHINE {
     case NBD_OPT_STRUCTURED_REPLY:
       SET_NEXT_STATE (%OPT_STRUCTURED_REPLY.START);
       return 0;
+    case NBD_OPT_STARTTLS:
+      SET_NEXT_STATE (%OPT_STARTTLS.START);
+      return 0;
     case 0:
       break;
     default:

lib/opt.c
@@ -139,6 +139,31 @@ nbd_unlocked_opt_abort (struct nbd_handle *h)
   return wait_for_option (h);
 }
 
+/* Issue NBD_OPT_STARTTLS and wait for the reply. */
+int
+nbd_unlocked_opt_starttls (struct nbd_handle *h)
+{
+  int err;
+  nbd_completion_callback c = { .callback = go_complete, .user_data = &err };
+  int r = nbd_unlocked_aio_opt_starttls (h, &c);
+
+  if (r == -1)
+    return r;
+
+  r = wait_for_option (h);
+  if (r == 0) {
+    if (nbd_internal_is_state_negotiating (get_next_state (h)))
+      r = err == 0;
+    else {
+      assert (nbd_internal_is_state_dead (get_next_state (h)));
+      set_error (err,
+                 "failed to get response to opt_starttls request");
+      r = -1;
+    }
+  }
+  return r;
+}
+
 /* Issue NBD_OPT_STRUCTURED_REPLY and wait for the reply. */
 int
 nbd_unlocked_opt_structured_reply (struct nbd_handle *h)
@@ -334,6 +359,31 @@ nbd_unlocked_aio_opt_abort (struct nbd_handle *h)
   if (nbd_internal_run (h, cmd_issue) == -1)
     debug (h, "option queued, ignoring state machine failure");
   return 0;
+}
+
+/* Issue NBD_OPT_STARTTLS without waiting. */
+int
+nbd_unlocked_aio_opt_starttls (struct nbd_handle *h,
+                               nbd_completion_callback *complete)
+{
+  if ((h->gflags & LIBNBD_HANDSHAKE_FLAG_FIXED_NEWSTYLE) == 0) {
+    set_error (ENOTSUP, "server is not using fixed newstyle protocol");
+    return -1;
+  }
+
+#ifndef HAVE_GNUTLS
+  set_error (ENOTSUP, "libnbd was compiled without TLS support");
+  return -1;
+
+#else
+  h->opt_current = NBD_OPT_STARTTLS;
+  h->opt_cb.completion = *complete;
+  SET_CALLBACK_TO_NULL (*complete);
+
+  if (nbd_internal_run (h, cmd_issue) == -1)
+    debug (h, "option queued, ignoring state machine failure");
+  return 0;
+#endif
 }
 
 /* Issue NBD_OPT_STRUCTURED_REPLY without waiting. */

tests/Makefile.am
@@ -658,12 +658,14 @@ if HAVE_PSKTOOL
 
 check_PROGRAMS += \
 	connect-tls-psk \
+	opt-starttls \
 	aio-parallel-tls \
 	aio-parallel-load-tls \
 	synch-parallel-tls \
 	$(NULL)
 TESTS += \
 	connect-tls-psk \
+	opt-starttls \
 	aio-parallel-tls.sh \
 	aio-parallel-load-tls.sh \
 	synch-parallel-tls.sh \
@@ -676,6 +678,9 @@ connect_tls_psk_CPPFLAGS = \
 	-DPSK=1 \
 	$(NULL)
 connect_tls_psk_LDADD = $(top_builddir)/lib/libnbd.la
+
+opt_starttls_SOURCES = opt-starttls.c requires.c requires.h
+opt_starttls_LDADD = $(top_builddir)/lib/libnbd.la
 
 aio_parallel_tls_SOURCES = aio-parallel.c
 aio_parallel_tls_CPPFLAGS = \

tests/opt-starttls.c
@@ -1,0 +1,206 @@
+/* NBD client library in userspace
+ * Copyright (C) 2013-2022 Red Hat Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/* Test nbd_opt_starttls to nbdkit in various tls modes. */
+
+#include <config.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <time.h>
+
+#include <libnbd.h>
+
+#include "requires.h"
+
+struct expected {
+  int first_opt_sr;
+  int64_t first_size;
+  int first_opt_meta;
+  int first_can_meta;
+  int first_opt_tls;
+  int first_get_sr;
+  int second_opt_sr;
+  int second_can_meta;
+  int second_opt_meta;
+  int second_opt_tls;
+  int get_tls;
+  int second_get_sr;
+  int third_can_meta;
+  int64_t second_size;
+};
+
+static int
+meta (void *user_data, const char *name)
+{
+  return 0;
+}
+
+#define check(got, exp) do_check (#got, got, exp)
+
+static void
+do_check (const char *act, int64_t got, int64_t exp)
+{
+  fprintf (stderr, "trying %s\n", act);
+  if (got == -1)
+    fprintf (stderr, "%s\n", nbd_get_error ());
+  else
+    fprintf (stderr, "succeeded, result %" PRId64 "\n", got);
+  if (got != exp) {
+    fprintf (stderr, "got %" PRId64 ", but expected %" PRId64 "\n", got, exp);
+    exit (EXIT_FAILURE);
+  }
+}
+
+static void
+do_test (const char *server_tls, struct expected exp)
+{
+  struct nbd_handle *nbd = nbd_create ();
+
+  if (nbd == NULL) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  if (nbd_supports_tls (nbd) != 1) {
+    fprintf (stderr, "SKIP: missing TLS support in libnbd\n");
+    exit (77);
+  }
+
+  if (nbd_set_opt_mode (nbd, true) == -1 ||
+      nbd_set_request_structured_replies (nbd, false) == -1 ||
+      nbd_set_request_meta_context (nbd, false) == -1 ||
+      nbd_add_meta_context (nbd, LIBNBD_CONTEXT_BASE_ALLOCATION) == -1 ||
+      nbd_set_tls_username (nbd, "alice") == -1 ||
+      nbd_set_tls_psk_file (nbd, "keys.psk") == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  /* Run nbdkit as a subprocess. */
+  const char *args[] = { "nbdkit", "-sv", "--exit-with-parent", server_tls,
+                         "--tls-verify-peer", "--tls-psk=keys.psk",
+                         "--filter=tls-fallback", "pattern",
+                         "size=1M", "tlsreadme=fallback", NULL };
+
+  if (nbd_connect_command (nbd, (char **) args) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+
+  check (nbd_opt_structured_reply (nbd), exp.first_opt_sr);
+  check (nbd_opt_info (nbd), exp.first_size > 0 ? 0 : -1);
+  check (nbd_get_size (nbd), exp.first_size);
+  check (nbd_opt_set_meta_context (nbd,
+                                   (nbd_context_callback) {.callback = meta}),
+         exp.first_opt_meta);
+  check (nbd_can_meta_context (nbd, LIBNBD_CONTEXT_BASE_ALLOCATION),
+         exp.first_can_meta);
+  check (nbd_opt_starttls (nbd), exp.first_opt_tls);
+  check (nbd_get_structured_replies_negotiated (nbd), exp.first_get_sr);
+  check (nbd_opt_structured_reply (nbd), exp.second_opt_sr);
+  check (nbd_can_meta_context (nbd, LIBNBD_CONTEXT_BASE_ALLOCATION),
+         exp.second_can_meta);
+  check (nbd_opt_set_meta_context (nbd,
+                                   (nbd_context_callback) {.callback = meta}),
+         exp.second_opt_meta);
+  check (nbd_opt_starttls (nbd), exp.second_opt_tls);
+  check (nbd_get_tls_negotiated (nbd), exp.get_tls);
+  check (nbd_get_structured_replies_negotiated (nbd), exp.second_get_sr);
+  check (nbd_can_meta_context (nbd, LIBNBD_CONTEXT_BASE_ALLOCATION),
+         exp.third_can_meta);
+  check (nbd_opt_info (nbd), 0);
+  check (nbd_get_size (nbd), exp.second_size);
+  check (nbd_opt_abort (nbd), 0);
+
+  nbd_close (nbd);
+}
+
+int
+main (int argc, char *argv[])
+{
+  /* Check --tls-verify-peer option is supported. */
+  requires ("nbdkit --tls-verify-peer -U - null --run 'exit 0'");
+  /* Check for nbdkit tls-fallback filter. */
+  requires ("nbdkit --filter=tls-fallback null --dump-plugin");
+
+  /* Reject older nbdkit that chokes on NBD_OPT_INFO to --tls=require */
+  requires ("test 0 = \"$(nbdkit --tls=require --tls-psk=keys.psk -U - null "
+            "--run 'nbdinfo \"nbd+unix://?socket=$unixsocket\"' 2>&1 |"
+            "grep -c 'nbdkit.*unknown option version')\"");
+
+  /* Behavior of a server with no TLS support */
+  do_test ("--tls=no", (struct expected) {
+      .first_opt_sr = 1,        /* Structured reply succeeds */
+      .first_size = 512,        /* Sees the tls-fallback safe size */
+      .first_opt_meta = 1,      /* Requesting meta context works */
+      .first_can_meta = 1,      /* Context is negotiated */
+      .first_opt_tls = 0,       /* Server lacks TLS, but connection stays up */
+      .first_get_sr = 1,        /* Structured reply still good */
+      .second_opt_sr = 0,       /* Server rejects second SR as redundant */
+      .second_can_meta = 1,     /* Context still negotiated */
+      .second_opt_meta = 1,     /* Second meta request works */
+      .second_opt_tls = 0,      /* Server still lacks TLS */
+      .get_tls = 0,             /* Final state of TLS - not secure */
+      .second_get_sr = 1,       /* Structured reply still good */
+      .third_can_meta = 1,      /* Meta context still works */
+      .second_size = 512,       /* Still the tls-fallback safe size */
+    });
+
+  /* Behavior of a server with opportunistic TLS support */
+  do_test ("--tls=on", (struct expected) {
+      .first_opt_sr = 1,        /* Structured reply succeeds */
+      .first_opt_meta = 1,      /* Requesting meta context works */
+      .first_can_meta = 1,      /* Context is negotiated */
+      .first_size = 512,        /* Sees the tls-fallback safe size */
+      .first_opt_tls = 1,       /* Server takes TLS */
+      .first_get_sr = 0,        /* Structured reply wiped by TLS */
+      .second_opt_sr = 1,       /* Server accepts second SR */
+      .second_can_meta = -1,    /* Contexts not requested since TLS */
+      .second_opt_meta = 1,     /* Requesting meta context works */
+      .second_opt_tls = 0,      /* Server rejects second TLS as redundant */
+      .get_tls = 1,             /* Final state of TLS - secure */
+      .second_get_sr = 1,       /* Structured reply still good */
+      .third_can_meta = 1,      /* Meta context still works */
+      .second_size = 1024*1024, /* Sees the actual size */
+    });
+
+  /* Behavior of a server that requires TLS support */
+  do_test ("--tls=require", (struct expected) {
+      .first_opt_sr = 0,        /* Structured reply fails without TLS first */
+      .first_opt_meta = -1,     /* Meta context fails without TLS first */
+      .first_can_meta = -1,     /* Context requires successful request */
+      .first_size = -1,         /* Cannot request info */
+      .first_opt_tls = 1,       /* Server takes TLS */
+      .first_get_sr = 0,        /* Structured reply hasn't been requested */
+      .second_opt_sr = 1,       /* Server accepts second SR */
+      .second_can_meta = -1,    /* Contexts not requested since TLS */
+      .second_opt_meta = 1,     /* Requesting meta context works */
+      .second_opt_tls = 0,      /* Server rejects second TLS as redundant */
+      .get_tls = 1,             /* Final state of TLS - secure */
+      .second_get_sr = 1,       /* Structured reply still good */
+      .third_can_meta = 1,      /* Meta context still works */
+      .second_size = 1024*1024, /* Sees the actual size */
+    });
+
+  exit (EXIT_SUCCESS);
+}

commit 6099a54edd722d1438f1ffb840002ffa7425c0fa

api: Don't nest TLS sessions
According to the NBD protocol, a client should not send a second
NBD_OPT_STARTTLS request after one succeeds; but we allow this for
corner-case testing of server response.  If the server doesn't
actually reject the nested request, move the connection to DEAD
because we don't support it.

Fixes: 53617c92 ("api: Add nbd_aio_]opt_starttls")

generator/states-newstyle-opt-starttls.c
@@ -78,6 +78,12 @@ STATE_MACHINE {
   reply = be32toh (h->sbuf.or.option_reply.reply);
   switch (reply) {
   case NBD_REP_ACK:
+    if (h->tls_negotiated) {
+      set_error (EPROTO,
+                 "handshake: unable to support server accepting TLS twice");
+      SET_NEXT_STATE (%.DEAD);
+      return 0;
+    }
     nbd_internal_reset_size_and_flags (h);
     h->structured_replies = false;
     h->meta_valid = false;