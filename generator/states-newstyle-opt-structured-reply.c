libguestfs/libnbd

commit 575ed78f3a9622d522de4fafe0fef561ae0dc20e

api: Add nbd_[aio_]opt_structured_reply()
Recent commits have lamented that we do not have a way to provoke
atypical ordering of option negotiation during server integration
testing.  This commit adds nbd_opt_structured_reply() to make it
possible to fine-tune when structured replies are requested, and fills
in a couple of FIXME comments left in previous commits.  The new API
can be called more than once; the testsuite is also enhanced with a
test that shows that nbdkit rejects the second attempt but libnbd
still remembers the first one succeeded (this requires moving the wipe
of h->structured_reply out of NBD_OPT_STRUCTURED_REPLY error handling,
and instead putting it into NBD_OPT_STARTTLS success, which becomes
important when a later patch adds nbd_opt_starttls()).

The C test changes depend on a new API, so they can't reasonably be
applied out of order.  However, porting the FIXME changes of the
testsuite to other language bindings will be done in the next patch,
to ease review of this one.  The test opt-structured-twice borders on
being an integration test (in fact, nbdkit commit 9418ec58 [upcoming
v1.34] changes nbdkit from accepting a second structured reply request
to instead diagnosing it as redundant; both behaviors are compatible
with the NBD spec), so that one will remain C only.

Acked-by: Richard W.M. Jones <rjones@redhat.com>

generator/states-newstyle-opt-structured-reply.c
@@ -21,12 +21,17 @@
 STATE_MACHINE {
  NEWSTYLE.OPT_STRUCTURED_REPLY.START:
   assert (h->gflags & LIBNBD_HANDSHAKE_FLAG_FIXED_NEWSTYLE);
-  if (!h->request_sr) {
-    if (h->opt_mode)
-      SET_NEXT_STATE (%.NEGOTIATING);
-    else
-      SET_NEXT_STATE (%^OPT_META_CONTEXT.START);
-    return 0;
+  if (h->opt_current == NBD_OPT_STRUCTURED_REPLY)
+    assert (h->opt_mode);
+  else {
+    assert (CALLBACK_IS_NULL(h->opt_cb.completion));
+    if (!h->request_sr) {
+      if (h->opt_mode)
+        SET_NEXT_STATE (%.NEGOTIATING);
+      else
+        SET_NEXT_STATE (%^OPT_META_CONTEXT.START);
+      return 0;
+    }
   }
 
   h->sbuf.option.version = htobe64 (NBD_NEW_VERSION);
@@ -69,12 +74,14 @@ STATE_MACHINE {
 
  NEWSTYLE.OPT_STRUCTURED_REPLY.CHECK_REPLY:
   uint32_t reply;
+  int err = ENOTSUP;
 
   reply = be32toh (h->sbuf.or.option_reply.reply);
   switch (reply) {
   case NBD_REP_ACK:
     debug (h, "negotiated structured replies on this connection");
     h->structured_replies = true;
+    err = 0;
     break;
   default:
     if (handle_reply_error (h) == -1) {
@@ -83,7 +90,6 @@ STATE_MACHINE {
     }
 
     debug (h, "structured replies are not supported by this server");
-    h->structured_replies = false;
     break;
   }
 
@@ -92,6 +98,8 @@ STATE_MACHINE {
     SET_NEXT_STATE (%.NEGOTIATING);
   else
     SET_NEXT_STATE (%^OPT_META_CONTEXT.START);
+  CALL_CALLBACK (h->opt_cb.completion, &err);
+  nbd_internal_free_option (h);
   return 0;
 
 } /* END STATE MACHINE */

.gitignore
@@ -236,6 +236,7 @@ Makefile.in
 /tests/opt-list-meta-queries
 /tests/opt-set-meta
 /tests/opt-set-meta-queries
+/tests/opt-structured-twice
 /tests/pki/
 /tests/pread-initialize
 /tests/private-data

generator/API.ml
@@ -794,9 +794,12 @@ server, as this protocol extension must be in use before
 L<nbd_can_meta_context(3)> or L<nbd_can_df(3)> can return true.  However,
 for integration testing, it can be useful to clear this flag
 rather than find a way to alter the server to fail the negotiation
-request.";
+request.  It is also useful to set this to false prior to using
+L<nbd_set_opt_mode(3)> if it is desired to control when to send
+L<nbd_opt_structured_reply(3)> during negotiation.";
     see_also = [Link "get_request_structured_replies";
                 Link "set_handshake_flags"; Link "set_strict_mode";
+                Link "opt_structured_reply";
                 Link "get_structured_replies_negotiated";
                 Link "can_meta_context"; Link "can_df"];
   };
@@ -824,9 +827,12 @@ L<nbd_get_structured_replies_negotiated(3)> instead.";
     shortdesc = "see if structured replies are in use";
     longdesc = "\
 After connecting you may call this to find out if the connection is
-using structured replies.";
+using structured replies.  Note that this setting is sticky; this
+can return true even after a second L<nbd_opt_structured_reply(3)>
+returns false because the server detected a duplicate request.";
     see_also = [Link "set_request_structured_replies";
                 Link "get_request_structured_replies";
+                Link "opt_structured_reply";
                 Link "get_protocol"];
   };
 
@@ -1086,6 +1092,12 @@ proceeding all the way to the ready state, when communicating with a
 newstyle server.  This setting has no effect when connecting to an
 oldstyle server.
 
+Note that libnbd defaults to attempting
+C<NBD_OPT_STRUCTURED_REPLY> before letting you control remaining
+negotiation steps; if you need control over this step as well,
+first set L<nbd_set_request_structured_replies(3)> to false before
+starting the connection attempt.
+
 When option mode is enabled, you have fine-grained control over which
 options are negotiated, compared to the default of the server
 negotiating everything on your behalf using settings made before
@@ -1099,6 +1111,8 @@ to end the connection without finishing negotiation.";
                 Link "opt_abort"; Link "opt_go"; Link "opt_list";
                 Link "opt_info"; Link "opt_list_meta_context";
                 Link "opt_set_meta_context";
+                Link "opt_structured_reply";
+                Link "set_request_structured_replies";
                 Link "aio_connect"];
   };
 
@@ -1150,6 +1164,32 @@ close the connection.  This can only be used if L<nbd_set_opt_mode(3)>
 enabled option mode.";
     example = Some "examples/list-exports.c";
     see_also = [Link "set_opt_mode"; Link "aio_opt_abort"; Link "opt_go"];
+  };
+
+  "opt_structured_reply", {
+    default_call with
+    args = []; ret = RBool;
+    permitted_states = [ Negotiating ];
+    shortdesc = "request the server to enable structured replies";
+    longdesc = "\
+Request that the server use structured replies, by sending
+C<NBD_OPT_STRUCTURED_REPLY>.  This can only be used if
+L<nbd_set_opt_mode(3)> enabled option mode; furthermore, libnbd
+defaults to automatically requesting this unless you use
+L<nbd_set_request_structured_replies(3)> prior to connecting.
+This function is mainly useful for integration testing of corner
+cases in server handling.
+
+This function returns true if the server replies with success,
+false if the server replies with an error, and fails only if
+the server does not reply (such as for a loss of connection).
+Note that some servers fail a second request as redundant;
+libnbd assumes that once one request has succeeded, then
+structured replies are supported (as visible by
+L<nbd_get_structured_replies_negotiated(3)>) regardless if
+later calls to this function return false.";
+    see_also = [Link "set_opt_mode"; Link "aio_opt_structured_reply";
+                Link "opt_go"; Link "set_request_structured_replies"]
   };
 
   "opt_list", {
@@ -2775,6 +2815,30 @@ L<nbd_aio_is_connecting(3)> to return false.";
     see_also = [Link "set_opt_mode"; Link "opt_abort"];
   };
 
+  "aio_opt_structured_reply", {
+    default_call with
+    args = [];
+    optargs = [ OClosure completion_closure ];
+    ret = RErr;
+    permitted_states = [ Negotiating ];
+    shortdesc = "request the server to enable structured replies";
+    longdesc = "\
+Request that the server use structured replies, by sending
+C<NBD_OPT_STRUCTURED_REPLY>.  This behaves like the synchronous
+counterpart L<nbd_opt_structured_reply(3)>, except that it does
+not wait for the server's response.
+
+To determine when the request completes, wait for
+L<nbd_aio_is_connecting(3)> to return false.  Or supply the optional
+C<completion_callback> which will be invoked as described in
+L<libnbd(3)/Completion callbacks>, except that it is automatically
+retired regardless of return value.  Note that detecting whether the
+server returns an error (as is done by the return value of the
+synchronous counterpart) is only possible with a completion
+callback.";
+    see_also = [Link "set_opt_mode"; Link "opt_structured_reply"];
+  };
+
   "aio_opt_list", {
     default_call with
     args = [ Closure list_closure ];
@@ -3678,6 +3742,8 @@ let first_version = [
   "opt_set_meta_context_queries", (1, 16);
   "aio_opt_set_meta_context", (1, 16);
   "aio_opt_set_meta_context_queries", (1, 16);
+  "opt_structured_reply", (1, 16);
+  "aio_opt_structured_reply", (1, 16);
 
   (* These calls are proposed for a future version of libnbd, but
    * have not been added to any released version so far.

generator/states-newstyle-opt-starttls.c
@@ -73,6 +73,7 @@ STATE_MACHINE {
   switch (reply) {
   case NBD_REP_ACK:
     nbd_internal_reset_size_and_flags (h);
+    h->structured_replies = false;
     new_sock = nbd_internal_crypto_create_session (h, h->sock);
     if (new_sock == NULL) {
       SET_NEXT_STATE (%.DEAD);

generator/states-newstyle.c
@@ -143,6 +143,9 @@ STATE_MACHINE {
     case NBD_OPT_SET_META_CONTEXT:
       SET_NEXT_STATE (%OPT_META_CONTEXT.START);
       return 0;
+    case NBD_OPT_STRUCTURED_REPLY:
+      SET_NEXT_STATE (%OPT_STRUCTURED_REPLY.START);
+      return 0;
     case 0:
       break;
     default:

lib/opt.c
@@ -127,6 +127,31 @@ nbd_unlocked_opt_abort (struct nbd_handle *h)
   return wait_for_option (h);
 }
 
+/* Issue NBD_OPT_STRUCTURED_REPLY and wait for the reply. */
+int
+nbd_unlocked_opt_structured_reply (struct nbd_handle *h)
+{
+  int err;
+  nbd_completion_callback c = { .callback = go_complete, .user_data = &err };
+  int r = nbd_unlocked_aio_opt_structured_reply (h, &c);
+
+  if (r == -1)
+    return r;
+
+  r = wait_for_option (h);
+  if (r == 0) {
+    if (nbd_internal_is_state_negotiating (get_next_state (h)))
+      r = err == 0;
+    else {
+      assert (nbd_internal_is_state_dead (get_next_state (h)));
+      set_error (err,
+                 "failed to get response to opt_structured_reply request");
+      r = -1;
+    }
+  }
+  return r;
+}
+
 struct list_helper {
   int count;
   nbd_list_callback list;
@@ -294,6 +319,25 @@ int
 nbd_unlocked_aio_opt_abort (struct nbd_handle *h)
 {
   h->opt_current = NBD_OPT_ABORT;
+
+  if (nbd_internal_run (h, cmd_issue) == -1)
+    debug (h, "option queued, ignoring state machine failure");
+  return 0;
+}
+
+/* Issue NBD_OPT_STRUCTURED_REPLY without waiting. */
+int
+nbd_unlocked_aio_opt_structured_reply (struct nbd_handle *h,
+                                       nbd_completion_callback *complete)
+{
+  if ((h->gflags & LIBNBD_HANDSHAKE_FLAG_FIXED_NEWSTYLE) == 0) {
+    set_error (ENOTSUP, "server is not using fixed newstyle protocol");
+    return -1;
+  }
+
+  h->opt_current = NBD_OPT_STRUCTURED_REPLY;
+  h->opt_cb.completion = *complete;
+  SET_CALLBACK_TO_NULL (*complete);
 
   if (nbd_internal_run (h, cmd_issue) == -1)
     debug (h, "option queued, ignoring state machine failure");

tests/Makefile.am
@@ -217,6 +217,7 @@ check_PROGRAMS += \
 	opt-list-meta-queries \
 	opt-set-meta \
 	opt-set-meta-queries \
+	opt-structured-twice \
 	connect-systemd-socket-activation \
 	connect-unix \
 	connect-tcp \
@@ -286,6 +287,7 @@ TESTS += \
 	opt-list-meta-queries \
 	opt-set-meta \
 	opt-set-meta-queries \
+	opt-structured-twice \
 	connect-systemd-socket-activation \
 	connect-unix \
 	connect-tcp \
@@ -556,6 +558,9 @@ opt_set_meta_LDADD = $(top_builddir)/lib/libnbd.la
 
 opt_set_meta_queries_SOURCES = opt-set-meta-queries.c
 opt_set_meta_queries_LDADD = $(top_builddir)/lib/libnbd.la
+
+opt_structured_twice_SOURCES = opt-structured-twice.c
+opt_structured_twice_LDADD = $(top_builddir)/lib/libnbd.la
 
 connect_systemd_socket_activation_SOURCES = \
 	connect-systemd-socket-activation.c \

tests/opt-list-meta.c
@@ -269,7 +269,34 @@ main (int argc, char *argv[])
     }
   }
 
-  /* FIXME: Once nbd_opt_structured_reply() exists, use it here and retry. */
+  /* Now enable structured replies, and a retry should pass. */
+  r = nbd_opt_structured_reply (nbd);
+  if (r == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  if (r != 1) {
+    fprintf (stderr, "expecting structured replies to be supported\n");
+    exit (EXIT_FAILURE);
+  }
+
+  p = (struct progress) { .count = 0 };
+  r = nbd_opt_list_meta_context (nbd,
+                                 (nbd_context_callback) { .callback = check,
+                                                          .user_data = &p});
+  if (r == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  if (r != p.count) {
+    fprintf (stderr, "inconsistent return value %d, expected %d\n",
+             r, p.count);
+    exit (EXIT_FAILURE);
+  }
+  if (r < 1 || !p.seen) {
+    fprintf (stderr, "server did not reply with base:allocation\n");
+    exit (EXIT_FAILURE);
+  }
 
   nbd_opt_abort (nbd);
   nbd_close (nbd);

commit 6094bf47139d315c264946914bbdeca06c3986ab

api: Nicer debug on redundant nbd_opt_structured_reply
Improve the debug message issued when we have already seen a previous
successful structured reply request.

Fixes: 575ed78f ("api: Add nbd_[aio_]opt_structured_reply()")

generator/states-newstyle-opt-structured-reply.c
@@ -83,13 +83,19 @@ STATE_MACHINE {
     h->structured_replies = true;
     err = 0;
     break;
+  case NBD_REP_ERR_INVALID:
+    err = EINVAL;
+    /* fallthrough */
   default:
     if (handle_reply_error (h) == -1) {
       SET_NEXT_STATE (%.DEAD);
       return 0;
     }
 
-    debug (h, "structured replies are not supported by this server");
+    if (h->structured_replies)
+      debug (h, "structured replies already negotiated");
+    else
+      debug (h, "structured replies are not supported by this server");
     break;
   }
 