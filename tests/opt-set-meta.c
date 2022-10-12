libguestfs/libnbd

commit db3e8adaf8bce29eec46f0fcb95a41d58b24f665

info: Add --is tls, --can structured-reply
While these are per-connection rather than per-export settings, it is
still useful to have a quick-and-silent command-line query rather than
having to parse full info output.

While touching info-can.sh, strengthen it to insist that an
unsupported feature is reported with status 2, rather than any
non-zero status.

Message-Id: <20221011215213.214246-3-eblake@redhat.com>
Acked-by: Richard W.M. Jones <rjones@redhat.com>

info/info-can-connect.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # nbd client library in userspace
-# Copyright (C) 2020-2021 Red Hat Inc.
+# Copyright (C) 2020-2022 Red Hat Inc.
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -27,3 +27,10 @@ requires nbdkit null --version
 
 nbdkit -v -U - null \
        --run '$VG nbdinfo --can connect "nbd+unix:///?socket=$unixsocket"'
+
+# --is tls is false for unencrypted connections.
+
+st=0
+nbdkit -v -U - null \
+       --run '$VG nbdinfo --is tls "nbd+unix:///?socket=$unixsocket"' || st=$?
+test $st = 2

info/can.c
@@ -1,5 +1,5 @@
 /* NBD client library in userspace
- * Copyright (C) 2020-2021 Red Hat Inc.
+ * Copyright (C) 2020-2022 Red Hat Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -36,6 +36,19 @@ do_can (void)
   if (strcasecmp (can, "connect") == 0 ||
       strcasecmp (can, "read") == 0)
     feature = 1;
+
+  else if (strcasecmp (can, "tls") == 0)
+    feature = nbd_get_tls_negotiated (nbd);
+
+  else if (strcasecmp (can, "sr") == 0 ||
+           strcasecmp (can, "structured") == 0 ||
+           strcasecmp (can, "structured reply") == 0 ||
+           strcasecmp (can, "structured-reply") == 0 ||
+           strcasecmp (can, "structured_reply") == 0 ||
+           strcasecmp (can, "structured replies") == 0 ||
+           strcasecmp (can, "structured-replies") == 0 ||
+           strcasecmp (can, "structured_replies") == 0)
+    feature = nbd_get_structured_replies_negotiated (nbd);
 
   else if (strcasecmp (can, "readonly") == 0 ||
            strcasecmp (can, "read-only") == 0 ||

info/info-can.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # nbd client library in userspace
-# Copyright (C) 2020-2021 Red Hat Inc.
+# Copyright (C) 2020-2022 Red Hat Inc.
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -26,6 +26,8 @@ requires bash -c "nbdkit sh --dump-plugin | grep has_can_cache=1"
 
 # --is read-only and --can write are tested in info-is-read-only.sh
 
+# --is tls is tested in info-uri-nbds.sh and info-can-connect.sh
+
 # --can connect is tested in info-can-connect.sh
 
 # --can read is tested in info-can-read.sh
@@ -35,6 +37,29 @@ requires bash -c "nbdkit sh --dump-plugin | grep has_can_cache=1"
 # --can df is hard to test.  nbdkit newstyle probably always sets this
 # and oldstyle never, but that feels like depending a bit too much on
 # the implementation.
+
+# --can structured-reply is not a per-export setting, but rather
+# something set on the server as a whole.
+
+nbdkit -v -U - sh - \
+       --run '$VG nbdinfo --can structured-reply "nbd+unix:///?socket=$unixsocket"' <<'EOF'
+case "$1" in
+  get_size) echo 1024 ;;
+  pread) ;;
+  *) exit 2 ;;
+esac
+EOF
+
+st=0
+nbdkit -v -U - --no-sr sh - \
+       --run '$VG nbdinfo --can structured-reply "nbd+unix:///?socket=$unixsocket"' <<'EOF' || st=$?
+case "$1" in
+  get_size) echo 1024 ;;
+  pread) ;;
+  *) exit 2 ;;
+esac
+EOF
+test $st = 2
 
 # --can cache and --can fua require special handling because in
 # nbdkit-sh-plugin we must print "native" or "none".  Also the can_fua
@@ -53,8 +78,9 @@ case "$1" in
 esac
 EOF
 
+    st=0
     nbdkit -v -U - sh - \
-           --run '! $VG nbdinfo --can $flag "nbd+unix:///?socket=$unixsocket"' <<'EOF'
+           --run '$VG nbdinfo --can $flag "nbd+unix:///?socket=$unixsocket"' <<'EOF' || st=$?
 case "$1" in
   get_size) echo 1024 ;;
   pread) ;;
@@ -63,6 +89,7 @@ case "$1" in
   *) exit 2 ;;
 esac
 EOF
+    test $st = 2
 done
 
 # These ones are normal booleans.
@@ -80,8 +107,9 @@ case "$1" in
 esac
 EOF
 
+    st=0
     nbdkit -v -U - sh - \
-           --run '! $VG nbdinfo --can $flag "nbd+unix:///?socket=$unixsocket"' <<'EOF'
+           --run '$VG nbdinfo --can $flag "nbd+unix:///?socket=$unixsocket"' <<'EOF' || st=$?
 case "$1" in
   get_size) echo 1024 ;;
   pread) ;;
@@ -90,4 +118,5 @@ case "$1" in
   *) exit 2 ;;
 esac
 EOF
+    test $st = 2
 done

info/info-uri-nbds.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # nbd client library in userspace
-# Copyright (C) 2020-2021 Red Hat Inc.
+# Copyright (C) 2020-2022 Red Hat Inc.
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -47,7 +47,8 @@ cleanup_fn rm -rf $d
 export pki
 nbdkit -U - --tls=require --tls-verify-peer --tls-certificates=$pki \
        null size=1M \
-       --run '$VG nbdinfo --json "nbds+unix:///?socket=$unixsocket&tls-certificates=$pki"' > $out
+       --run '$VG nbdinfo --json "nbds+unix:///?socket=$unixsocket&tls-certificates=$pki" &&
+              $VG nbdinfo --is tls "nbds+unix:///?socket=$unixsocket&tls-certificates=$pki"' > $out
 cat $out
 jq . < $out
 

info/nbdinfo.pod
@@ -156,6 +156,15 @@ All NBD servers must support read, so this always exits with success
 
 Test if we can connect to the NBD URI.
 
+=item nbdinfo --is tls URI
+
+Test if the NBD URI connection is using TLS.
+
+=item nbdinfo --can structured-reply URI
+
+Test if server can respond with structured replies (a prerequisite
+for supporting block status commands).
+
 =item nbdinfo --is rotational URI
 
 Test if the server export is backed by something which behaves like a
@@ -313,23 +322,26 @@ Display brief command line help and exit.
 
 =item B<--can read>
 
+=item B<--can structured-reply>
+
 =item B<--can trim>
 
 =item B<--can write>
 
 =item B<--can zero>
 
-Test properties of the NBD server export.  The command does not print
-anything.  Instead it exits with success (S<exit code 0>) if true, or
-failure (S<exit code 2>) if false.  (Other exit codes indicate an
-error querying the flag).
+Test properties of the NBD server export or the connection itself.
+The command does not print anything.  Instead it exits with success
+(S<exit code 0>) if true, or failure (S<exit code 2>) if false.
+(Other exit codes indicate an error querying the flag).
 
 For further information see the L<NBD
 protocol|https://github.com/NetworkBlockDevice/nbd/blob/master/doc/proto.md>
 and the following libnbd functions: L<nbd_can_cache(3)>,
 L<nbd_can_df(3)>, L<nbd_can_fast_zero(3)>, L<nbd_can_flush(3)>,
 L<nbd_can_fua(3)>, L<nbd_can_multi_conn(3)>, L<nbd_can_trim(3)>,
-L<nbd_can_zero(3)>, L<nbd_is_read_only(3)>.
+L<nbd_can_zero(3)>, L<nbd_is_read_only(3)>,
+L<nbd_get_structured_replies_negotiated(3)>.
 
 =item B<--color>
 
@@ -362,15 +374,18 @@ use I<--list --content>.
 
 =item B<--is rotational>
 
-Test if the NBD server export is read-only and rotational.  The
-command does not print anything.  Instead it exits with success
-(S<exit code 0>) if true, or failure (S<exit code 2>) if false.
-(Other exit codes indicate an error querying the flag).
+=item B<--is tls>
+
+Test if the NBD server export is read-only and rotational, or whether
+the connection itself is using TLS.  The command does not print
+anything.  Instead it exits with success (S<exit code 0>) if true, or
+failure (S<exit code 2>) if false.  (Other exit codes indicate an
+error querying the flag).
 
 For further information see the L<NBD
 protocol|https://github.com/NetworkBlockDevice/nbd/blob/master/doc/proto.md>
 and the following libnbd functions: L<nbd_is_read_only(3)>,
-L<nbd_is_rotational(3)>.
+L<nbd_is_rotational(3)>, L<nbd_get_tls_negotiated(3)>.
 
 =item B<--json>
 

commit 018d55a83be047824017e23eec6c0ed25b16a730

tests: Skip rather than fail on missing nbdkit --no-sr
CI was reporting various test failures on a debian-10 host, due to an
nbdkit older than 1.14 that introduced the --no-sr kill switch.  Skip
(portions of) tests in these cases.  For the language ports of test
250 (opt-set-meta), the second process isn't adding enough coverage to
be worth the hassle of porting requires() to non-C code, so drop it
instead.

Fixes: db3e8ada ("info: Add --is tls, --can structured-reply")
Fixes: cc60a2fa ("info: Expose whether structured replies were negotiated")
Fixes: 760c37c2 ("tests: Language port of nbd_opt_set_meta_context() tests")
Fixes: 97e6ab81 ("api: Add nbd_[aio_]opt_set_meta_context[_queries]")

info/info-can-connect.sh
@@ -23,7 +23,7 @@ set -x
 
 # --can connect always returns true.
 
-requires nbdkit null --version
+requires --no-sr nbdkit null --version
 
 nbdkit -v -U - null \
        --run '$VG nbdinfo --can connect "nbd+unix:///?socket=$unixsocket"'

info/info-packets.sh
@@ -22,7 +22,7 @@ set -e
 set -x
 
 requires nbdkit --version
-requires nbdkit memory --version
+requires nbdkit --no-sr memory --version
 
 out=info-packets.out
 cleanup_fn rm -f $out

golang/libnbd_250_opt_set_meta_test.go
@@ -35,7 +35,7 @@ func setmetaf(user_data int, name string) int {
 }
 
 func Test250OptSetMeta(t *testing.T) {
-	/* First process, delay structured replies. Get into negotiating state. */
+	/* Get into negotiating state without structured replies. */
 	h, err := Create()
 	if err != nil {
 		t.Fatalf("could not create handle: %s", err)
@@ -229,74 +229,6 @@ func Test250OptSetMeta(t *testing.T) {
 		t.Fatalf("could not check can meta context: %s", err)
 	}
 	if !meta {
-		t.Fatalf("unexpected can meta context state")
-	}
-
-	err = h.Shutdown(nil)
-	if err != nil {
-		t.Fatalf("could not request shutdown: %s", err)
-	}
-
-	/* Second process, this time without structured replies server-side. */
-	h, err = Create()
-	if err != nil {
-		t.Fatalf("could not create handle: %s", err)
-	}
-	defer h.Close()
-
-	err = h.SetOptMode(true)
-	if err != nil {
-		t.Fatalf("could not set opt mode: %s", err)
-	}
-
-	err = h.AddMetaContext(context_base_allocation)
-	if err != nil {
-		t.Fatalf("could not request add_meta_context: %s", err)
-	}
-
-	err = h.ConnectCommand([]string{
-		"nbdkit", "-s", "--exit-with-parent", "-v",
-		"memory", "size=1M", "--no-sr",
-	})
-	if err != nil {
-		t.Fatalf("could not connect: %s", err)
-	}
-
-	sr, err = h.GetStructuredRepliesNegotiated()
-	if err != nil {
-		t.Fatalf("could not check structured replies negotiated: %s", err)
-	}
-	if sr {
-		t.Fatalf("unexpected structured replies state")
-	}
-
-	/* Expect server-side failure here */
-	set_count = 0
-	set_seen = false
-	_, err = h.OptSetMetaContext(func(name string) int {
-		return setmetaf(42, name)
-	})
-	if err == nil {
-		t.Fatalf("expected error")
-	}
-	if set_count != 0 || set_seen {
-		t.Fatalf("unexpected set_count after opt_set_meta_context")
-	}
-	_, err = h.CanMetaContext(context_base_allocation)
-	if err == nil {
-		t.Fatalf("expected error")
-	}
-
-	/* Even though CanMeta fails after failed SET, it returns 0 after go */
-	err = h.OptGo()
-	if err != nil {
-		t.Fatalf("could not request opt_go: %s", err)
-	}
-	meta, err = h.CanMetaContext(context_base_allocation)
-	if err != nil {
-		t.Fatalf("could not check can meta context: %s", err)
-	}
-	if meta {
 		t.Fatalf("unexpected can meta context state")
 	}
 

ocaml/tests/test_250_opt_set_meta.ml
@@ -27,7 +27,7 @@ let f user_data name =
   0
 
 let () =
-  (* First process, delay structured replies. Get into negotiating state. *)
+  (* Get into negotiating state without structured replies. *)
   let nbd = NBD.create () in
   NBD.set_opt_mode nbd true;
   NBD.set_request_structured_replies nbd false;
@@ -128,42 +128,6 @@ let () =
   assert (s = 1048576_L);
   let m = NBD.can_meta_context nbd NBD.context_base_allocation in
   assert m;
-
-  NBD.shutdown nbd;
-
-  (* Second process, this time without structured replies server-side. *)
-  let nbd = NBD.create () in
-  NBD.set_opt_mode nbd true;
-  NBD.add_meta_context nbd NBD.context_base_allocation;
-  NBD.connect_command nbd
-                      ["nbdkit"; "-s"; "--exit-with-parent"; "-v";
-                       "memory"; "size=1M"; "--no-sr"];
-  let sr = NBD.get_structured_replies_negotiated nbd in
-  assert (not sr);
-
-  (* Expect server-side failure here *)
-  count := 0;
-  seen := false;
-  NBD.add_meta_context nbd "base:";
-  (try
-     let _ = NBD.opt_set_meta_context nbd (f 42) in
-     assert false
-   with
-     NBD.Error (errstr, errno) -> ()
-  );
-  assert (0 = !count);
-  assert (not !seen);
-  (try
-     let _ = NBD.can_meta_context nbd NBD.context_base_allocation in
-     assert false
-   with
-     NBD.Error (errstr, errno) -> ()
-  );
-
-  (* Even though can_meta fails after failed SET, it returns 0 after go *)
-  NBD.opt_go nbd;
-  let m = NBD.can_meta_context nbd NBD.context_base_allocation in
-  assert (not m);
 
   NBD.shutdown nbd
 

python/t/250-opt-set-meta.py
@@ -39,7 +39,7 @@ def must_fail(f, *args, **kwds):
         pass
 
 
-# First process, delay structured replies. Get into negotiating state.
+# Get into negotiating state without structured replies.
 h = nbd.NBD()
 h.set_opt_mode(True)
 h.set_request_structured_replies(False)
@@ -108,27 +108,5 @@ must_fail(h.opt_set_meta_context, lambda *args: f(42, *args))
 assert count == 0
 assert seen is False
 assert h.can_meta_context(nbd.CONTEXT_BASE_ALLOCATION) is True
-
-h.shutdown()
-
-# Second process, this time without structured replies server-side.
-h = nbd.NBD()
-h.set_opt_mode(True)
-h.add_meta_context(nbd.CONTEXT_BASE_ALLOCATION)
-h.connect_command(["nbdkit", "-s", "--exit-with-parent", "-v",
-                   "memory", "size=1M", "--no-sr"])
-assert h.get_structured_replies_negotiated() is False
-
-# Expect server-side failure here
-count = 0
-seen = False
-must_fail(h.opt_set_meta_context, lambda *args: f(42, *args))
-assert count == 0
-assert seen is False
-must_fail(h.can_meta_context, nbd.CONTEXT_BASE_ALLOCATION)
-
-# Even though can_meta fails after failed SET, it returns 0 after go
-h.opt_go()
-assert h.can_meta_context(nbd.CONTEXT_BASE_ALLOCATION) is False
 
 h.shutdown()

tests/Makefile.am
@@ -549,7 +549,7 @@ opt_list_meta_LDADD = $(top_builddir)/lib/libnbd.la
 opt_list_meta_queries_SOURCES = opt-list-meta-queries.c
 opt_list_meta_queries_LDADD = $(top_builddir)/lib/libnbd.la
 
-opt_set_meta_SOURCES = opt-set-meta.c
+opt_set_meta_SOURCES = opt-set-meta.c requires.c requires.h
 opt_set_meta_CPPFLAGS = \
 	$(AM_CPPFLAGS) \
 	-I$(top_srcdir)/common/include \

tests/opt-set-meta.c
@@ -32,6 +32,7 @@
 #include <libnbd.h>
 
 #include "array-size.h"
+#include "requires.h"
 
 struct progress {
   int count;
@@ -213,7 +214,11 @@ main (int argc, char *argv[])
   nbd_shutdown (nbd, 0);
   nbd_close (nbd);
 
-  /* Second process, this time without structured replies server-side. */
+  /* Second process, this time without structured replies server-side.
+   * This part of the test is C-only, because it depends on nbdkit 1.14
+   * or newer with its --no-sr kill switch.
+   */
+  requires ("nbdkit --no-sr --help");
   args[ARRAY_SIZE (args) - 2] = (char *) "--no-sr";
   nbd = nbd_create ();
   if (nbd == NULL ||