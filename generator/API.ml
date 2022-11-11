libguestfs/libnbd

commit 76bc0f0b041d5e3bd1bf6358d31b6d8959c86175

api: Add STRICT_BOUNDS/ZERO_SIZE to nbd_set_strict_mode
The NBD protocol states that a 0-length request is undefined; we were
inconsistent in that we let it through for read, write, and cache, but
blocked it for trim, zero, and block_status.  The NBD protocol also
has documented rules on handling access beyond EOF, but we are
currently wasting traffic to the server when we can give the same
answer ourselves.  Exposing these as two more strictness knobs gives
the user finer-grained control over what behavior they want.

generator/API.ml
@@ -194,6 +194,8 @@ let strict_flags = {
   flags = [
     "COMMANDS",       1 lsl 0;
     "FLAGS",          1 lsl 1;
+    "BOUNDS",         1 lsl 2;
+    "ZERO_SIZE",      1 lsl 3;
   ]
 }
 let allow_transport_flags = {
@@ -779,6 +781,18 @@ instead.
 Note that the NBD protocol only supports 16 bits of command flags,
 even though the libnbd API uses C<uint32_t>; bits outside of the
 range permitted by the protocol are always a client-side error.
+
+=item C<LIBNBD_STRICT_BOUNDS> = 3
+
+If set, this flag rejects client requests that would exceed the export
+bounds without sending any traffic to the server.  If clear, this flag
+relies on the server to detect out-of-bounds requests.
+
+=item C<LIBNBD_STRICT_ZERO_SIZE> = 4
+
+If set, this flag rejects client requests with length 0.  If clear,
+this permits zero-length requests to the server, which may produce
+undefined results.
 
 =back
 

lib/disconnect.c
@@ -64,7 +64,8 @@ nbd_unlocked_aio_disconnect (struct nbd_handle *h, uint32_t flags)
 {
   int64_t id;
 
-  id = nbd_internal_command_common (h, flags, NBD_CMD_DISC, 0, 0, NULL, NULL);
+  id = nbd_internal_command_common (h, flags, NBD_CMD_DISC, 0, 0, 0,
+                                    NULL, NULL);
   if (id == -1)
     return -1;
   h->disconnect_request = true;

lib/internal.h
@@ -432,7 +432,8 @@ extern const char *nbd_internal_name_of_nbd_cmd (uint16_t type);
 extern int64_t nbd_internal_command_common (struct nbd_handle *h,
                                             uint16_t flags, uint16_t type,
                                             uint64_t offset, uint64_t count,
-                                            void *data, struct command_cb *cb);
+                                            int count_err, void *data,
+                                            struct command_cb *cb);
 
 /* socket.c */
 struct socket *nbd_internal_socket_create (int fd);

lib/rw.c
@@ -168,10 +168,11 @@ nbd_unlocked_block_status (struct nbd_handle *h,
   return wait_for_command (h, cookie);
 }
 
+/* count_err represents the errno to return if bounds check fail */
 int64_t
 nbd_internal_command_common (struct nbd_handle *h,
                              uint16_t flags, uint16_t type,
-                             uint64_t offset, uint64_t count,
+                             uint64_t offset, uint64_t count, int count_err,
                              void *data, struct command_cb *cb)
 {
   struct command *cmd;
@@ -183,6 +184,19 @@ nbd_internal_command_common (struct nbd_handle *h,
   if (h->in_flight == INT_MAX) {
       set_error (ENOMEM, "too many commands already in flight");
       goto err;
+  }
+
+  if (count_err) {
+    if ((h->strict & LIBNBD_STRICT_ZERO_SIZE) && count == 0) {
+      set_error (EINVAL, "count cannot be 0");
+      goto err;
+    }
+
+    if ((h->strict & LIBNBD_STRICT_BOUNDS) &&
+        (offset > h->exportsize || count > h->exportsize - offset)) {
+      set_error (count_err, "request out of bounds");
+      return -1;
+    }
   }
 
   switch (type) {
@@ -282,7 +296,7 @@ nbd_unlocked_aio_pread (struct nbd_handle *h, void *buf,
 
   SET_CALLBACK_TO_NULL (*completion);
   return nbd_internal_command_common (h, flags, NBD_CMD_READ, offset, count,
-                                      buf, &cb);
+                                      EINVAL, buf, &cb);
 }
 
 int64_t
@@ -306,7 +320,7 @@ nbd_unlocked_aio_pread_structured (struct nbd_handle *h, void *buf,
   SET_CALLBACK_TO_NULL (*chunk);
   SET_CALLBACK_TO_NULL (*completion);
   return nbd_internal_command_common (h, flags, NBD_CMD_READ, offset, count,
-                                      buf, &cb);
+                                      EINVAL, buf, &cb);
 }
 
 int64_t
@@ -332,7 +346,7 @@ nbd_unlocked_aio_pwrite (struct nbd_handle *h, const void *buf,
 
   SET_CALLBACK_TO_NULL (*completion);
   return nbd_internal_command_common (h, flags, NBD_CMD_WRITE, offset, count,
-                                      (void *) buf, &cb);
+                                      ENOSPC, (void *) buf, &cb);
 }
 
 int64_t
@@ -351,7 +365,7 @@ nbd_unlocked_aio_flush (struct nbd_handle *h,
 
   SET_CALLBACK_TO_NULL (*completion);
   return nbd_internal_command_common (h, flags, NBD_CMD_FLUSH, 0, 0,
-                                      NULL, &cb);
+                                      0, NULL, &cb);
 }
 
 int64_t
@@ -379,14 +393,9 @@ nbd_unlocked_aio_trim (struct nbd_handle *h,
     }
   }
 
-  if (count == 0) {             /* NBD protocol forbids this. */
-    set_error (EINVAL, "count cannot be 0");
-    return -1;
-  }
-
   SET_CALLBACK_TO_NULL (*completion);
   return nbd_internal_command_common (h, flags, NBD_CMD_TRIM, offset, count,
-                                      NULL, &cb);
+                                      ENOSPC, NULL, &cb);
 }
 
 int64_t
@@ -410,7 +419,7 @@ nbd_unlocked_aio_cache (struct nbd_handle *h,
 
   SET_CALLBACK_TO_NULL (*completion);
   return nbd_internal_command_common (h, flags, NBD_CMD_CACHE, offset, count,
-                                      NULL, &cb);
+                                      EINVAL, NULL, &cb);
 }
 
 int64_t
@@ -444,14 +453,9 @@ nbd_unlocked_aio_zero (struct nbd_handle *h,
     }
   }
 
-  if (count == 0) {             /* NBD protocol forbids this. */
-    set_error (EINVAL, "count cannot be 0");
-    return -1;
-  }
-
   SET_CALLBACK_TO_NULL (*completion);
   return nbd_internal_command_common (h, flags, NBD_CMD_WRITE_ZEROES, offset,
-                                      count, NULL, &cb);
+                                      count, ENOSPC, NULL, &cb);
 }
 
 int64_t
@@ -478,13 +482,8 @@ nbd_unlocked_aio_block_status (struct nbd_handle *h,
     }
   }
 
-  if (count == 0) {             /* NBD protocol forbids this. */
-    set_error (EINVAL, "count cannot be 0");
-    return -1;
-  }
-
   SET_CALLBACK_TO_NULL (*extent);
   SET_CALLBACK_TO_NULL (*completion);
   return nbd_internal_command_common (h, flags, NBD_CMD_BLOCK_STATUS, offset,
-                                      count, NULL, &cb);
+                                      count, EINVAL, NULL, &cb);
 }

tests/errors.c
@@ -263,14 +263,28 @@ main (int argc, char *argv[])
   }
   check (EINVAL, "nbd_aio_command_completed: ");
 
-  /* Read from an invalid offset */
-  if (nbd_pread (nbd, buf, 1, -1, 0) != -1) {
+  /* Read from an invalid offset, client-side */
+  strict = nbd_get_strict_mode (nbd) | LIBNBD_STRICT_BOUNDS;
+  if (nbd_set_strict_mode (nbd, strict) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  if (nbd_aio_pread (nbd, buf, 1, -1, NBD_NULL_COMPLETION, 0) != -1) {
     fprintf (stderr, "%s: test failed: "
-             "nbd_pread did not fail with bogus offset\n",
+             "nbd_aio_pread did not fail with bogus offset\n",
              argv[0]);
     exit (EXIT_FAILURE);
   }
-  check (EINVAL, "nbd_pread: ");
+  check (EINVAL, "nbd_aio_pread: ");
+  /* Read from an invalid offset, server-side */
+  strict &= ~LIBNBD_STRICT_BOUNDS;
+  if (nbd_set_strict_mode (nbd, strict) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  check_server_fail (nbd,
+                     nbd_aio_pread (nbd, buf, 1, -1, NBD_NULL_COMPLETION, 0),
+                     "nbd_aio_pread with bogus offset", EINVAL);
 
   /* Use in-range unknown command flags, client-side */
   strict = nbd_get_strict_mode (nbd) | LIBNBD_STRICT_FLAGS;
@@ -351,6 +365,29 @@ main (int argc, char *argv[])
     exit (EXIT_FAILURE);
   }
   check (ENOMEM, "nbd_pread: ");
+
+  /* Send a zero-sized read, client-side */
+  strict = nbd_get_strict_mode (nbd) | LIBNBD_STRICT_ZERO_SIZE;
+  if (nbd_set_strict_mode (nbd, strict) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  if (nbd_aio_pread (nbd, buf, 0, 0, NBD_NULL_COMPLETION, 0) != -1) {
+    fprintf (stderr, "%s: test failed: "
+             "zero-size nbd_aio_pread did not fail\n",
+             argv[0]);
+    exit (EXIT_FAILURE);
+  }
+  check (EINVAL, "nbd_aio_pread: ");
+  /* Send a zero-sized read, server-side */
+  strict &= ~LIBNBD_STRICT_ZERO_SIZE;
+  if (nbd_set_strict_mode (nbd, strict) == -1) {
+    fprintf (stderr, "%s\n", nbd_get_error ());
+    exit (EXIT_FAILURE);
+  }
+  check_server_fail (nbd,
+                     nbd_aio_pread (nbd, buf, 0, 0, NBD_NULL_COMPLETION, 0),
+                     "zero-size nbd_aio_pread", EINVAL);
 
   /* Queue up two write commands so large that we block on POLLIN (the
    * first might not block when under load, such as valgrind, but the

commit 8084f2870a86b1ce2896f3b202c9028fc5659c3f

docs: Fix values of documented constants
In nbd_set_strict_mode(), the flags are powers of 2, not sequential
integers, because they can be bitwise-OR'd together.  In
nbd_set_uri_allow_transports(), it does not hurt to document our flag
values.

Fixes: 76bc0f0b ("api: Add STRICT_BOUNDS/ZERO_SIZE to nbd_set_strict_mode", v1.5.3)
Fixes: ba86bfd1 ("api: Add STRICT_ALIGN to nbd_set_strict_mode", v1.5.3)
Fixes: c96d6b1c ("api: Allow NBD URIs to be restricted.", v1.1.7)

generator/API.ml
@@ -1018,7 +1018,7 @@ following strictness flags:
 
 =over 4
 
-=item C<LIBNBD_STRICT_COMMANDS> = 1
+=item C<LIBNBD_STRICT_COMMANDS> = 0x1
 
 If set, this flag rejects client requests that do not comply with the
 set of advertised server flags (for example, attempting a write on
@@ -1026,7 +1026,7 @@ a read-only server, or attempting to use C<LIBNBD_CMD_FLAG_FUA> when
 L<nbd_can_fua(3)> returned false).  If clear, this flag relies on the
 server to reject unexpected commands.
 
-=item C<LIBNBD_STRICT_FLAGS> = 2
+=item C<LIBNBD_STRICT_FLAGS> = 0x2
 
 If set, this flag rejects client requests that attempt to set a
 command flag not recognized by libnbd (those outside of
@@ -1045,19 +1045,19 @@ Note that the NBD protocol only supports 16 bits of command flags,
 even though the libnbd API uses C<uint32_t>; bits outside of the
 range permitted by the protocol are always a client-side error.
 
-=item C<LIBNBD_STRICT_BOUNDS> = 3
+=item C<LIBNBD_STRICT_BOUNDS> = 0x4
 
 If set, this flag rejects client requests that would exceed the export
 bounds without sending any traffic to the server.  If clear, this flag
 relies on the server to detect out-of-bounds requests.
 
-=item C<LIBNBD_STRICT_ZERO_SIZE> = 4
+=item C<LIBNBD_STRICT_ZERO_SIZE> = 0x8
 
 If set, this flag rejects client requests with length 0.  If clear,
 this permits zero-length requests to the server, which may produce
 undefined results.
 
-=item C<LIBNBD_STRICT_ALIGN> = 5
+=item C<LIBNBD_STRICT_ALIGN> = 0x10
 
 If set, and the server provided minimum block sizes (see
 L<nbd_get_block_size(3)>, this flag rejects client requests that
@@ -1649,11 +1649,11 @@ ORed together:
 
 =over 4
 
-=item C<LIBNBD_ALLOW_TRANSPORT_TCP>
+=item C<LIBNBD_ALLOW_TRANSPORT_TCP> = 0x1
 
-=item C<LIBNBD_ALLOW_TRANSPORT_UNIX>
+=item C<LIBNBD_ALLOW_TRANSPORT_UNIX> = 0x2
 
-=item C<LIBNBD_ALLOW_TRANSPORT_VSOCK>
+=item C<LIBNBD_ALLOW_TRANSPORT_VSOCK> = 0x4
 
 =back
 