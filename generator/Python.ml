libguestfs/libnbd

commit b7f85610ac19085a1631439fbcb7dd241f1e9ebf

python: Add explicit h.close() method
Normally we allow the handle to go out of scope which closes the
handle through __del__.  But there was no way to explicitly close the
handle which made testing various corner cases difficult.

This commit adds an explicit close method, and incidentally modifies
the __del__ method so it doesn't double-free the underlying module
handle.

After calling the explicit close you must not call any other method on
the handle.  The handle will raise the new nbd.ClosedHandle exception
if you do this.

This code is largely copied from the libguestfs python bindings,
released under a compatible license.

See: https://listman.redhat.com/archives/libguestfs/2023-February/030873.html
Thanks: Eric Blake

generator/Python.ml
@@ -688,6 +688,13 @@ def _str(self):
 
 Error.__str__ = _str
 
+
+class ClosedHandle(ValueError):
+    '''This exception is thrown when any method is called on an
+    nbd handle after you have called h.close() on the same handle.'''
+    pass
+
+
 ";
 
   List.iter (
@@ -817,8 +824,27 @@ class NBD(object):
 
     def __del__(self):
         '''Close the NBD handle and underlying connection.'''
-        if hasattr(self, '_o'):
+        if self._o:
             libnbdmod.close(self._o)
+
+    def _check_not_closed(self):
+        if not self._o:
+            raise ClosedHandle(\"GuestFS: method called on closed handle\")
+
+    def close(self):
+        '''Explicitly close the NBD handle and underlying connection.
+
+        The handle is closed implicitly when its reference count goes
+        to zero (eg. when it goes out of scope or the program ends).
+
+        This call is only needed if you want to force the handle to
+        close now.  After calling this, the program must not call
+        any method on the handle (except the implicit call to
+        __del__ which happens when the final reference is cleaned up).
+        '''
+        self._check_not_closed()
+        libnbdmod.close(self._o)
+        self._o = None
 
 ";
 
@@ -867,6 +893,7 @@ class NBD(object):
       let longdesc = Str.global_replace py_const_rex "C<\\1>" longdesc in
       let longdesc = pod2text longdesc in
       pr "        u'''▶ %s\n\n%s'''\n" shortdesc (String.concat "\n" longdesc);
+      pr "        self._check_not_closed()\n";
       pr "        return libnbdmod.%s(" name;
       pr_wrap ',' (fun () ->
           pr "self._o";

commit 254ac22dde42f4445846d66b77278c208e488bfe

python: Tweak closed behavior
Mention the correct library name in the exception, and make sure
__del__ can be invoked more than once.

Fixes: b7f85610 ("python: Add explicit h.close() method")

generator/Python.ml
@@ -1,6 +1,6 @@
 (* hey emacs, this is OCaml code: -*- tuareg -*- *)
 (* nbd client library in userspace: Python bindings
- * Copyright (C) 2013-2022 Red Hat Inc.
+ * Copyright (C) 2013-2023 Red Hat Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -841,10 +841,11 @@ class NBD(object):
         '''Close the NBD handle and underlying connection.'''
         if self._o:
             libnbdmod.close(self._o)
+            self._o = None
 
     def _check_not_closed(self):
         if not self._o:
-            raise ClosedHandle(\"GuestFS: method called on closed handle\")
+            raise ClosedHandle(\"libnbd: method called on closed handle\")
 
     def close(self):
         '''Explicitly close the NBD handle and underlying connection.